3d2215e023dea5814253b97c92fb8515985f3996
Gagandeep Singh <gdp.1807@gmail.com>
2022-03-09 22:43:07 +0530

    fixed adjustl test

--- integration_tests/intrinsics_36.f90
+++ integration_tests/intrinsics_36.f90
@@ -1,6 +1,7 @@
 program test_adjustl
     character(len=20) :: str = '   gfortran'
+    integer :: one = 1
     str = adjustl(str)
     print *, str
-    if (len(str) /= 8) error stop
+    if (str(one:one) /= 'g') error stop
 end program test_adjustl
