dd403960d66f787c557de461c5c4c0001e274e1f
Gagandeep Singh <gdp.1807@gmail.com>
2022-03-12 20:03:26 +0530

    Handeled corner cases in keyword arguments

--- integration_tests/intrinsics_39.f90
+++ integration_tests/intrinsics_39.f90
@@ -1,13 +1,13 @@
 program test_time_and_date
-    character(8)  :: date
-    character(10) :: time
-    character(5)  :: zone
+    character(len=8)  :: date
+    character(len=10) :: time
+    character(len=5)  :: zone
     integer,dimension(8) :: values
     ! using keyword arguments
     call date_and_time(date,time,zone,values)
-    call date_and_time(DATE=date,ZONE=zone)
-    call date_and_time(TIME=time)
-    call date_and_time(VALUES=values)
+    call date_and_time(date=date,zone=zone)
+    call date_and_time(time=time)
+    call date_and_time(values=values)
     print '(a,2x,a,2x,a)', date, time, zone
     print '(8i5)', values
 end program test_time_and_date
