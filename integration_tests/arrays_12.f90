f4fd2c521c37e31b2055cdde5405e2205f5c9977
Devesh Sawant <devsaw115@gmail.com>
2022-03-13 16:44:54 +0530

    Integration test for arrays - added vector subscript, reshape, spread intrinsics

--- integration_tests/arrays_12.f90
+++ integration_tests/arrays_12.f90
@@ -1,12 +1,27 @@
-program constant_arrays
+program arrays_12
     implicit none
+
     integer :: i
-    real, parameter :: realArray(5) = [1.1, 3.0, 10.0, 2.1, 5.5]
-    integer, parameter:: matrix(3,2,2) = reshape([1,2,3,4,5,6,7,8,9,10,11,12], [3,2,2])
+    real, dimension(2,2) :: B
+    integer, parameter:: C(3,2,3) = reshape([(i, i = 1, 18)], [3,2,3])
 
-    do i = 1,5
-        print *, realArray(i)
-    end do
+    ! vector subscripts
+    integer, dimension(3) :: U = [1,3,2]
+    integer, dimension(4) :: V = [3, 1, 1, 2]
 
-    print *, matrix(2,1,2)
-end program constant_arrays
+    ! test back to back reshape - C is 3x2x3 function
+    B = reshape(reshape(C, [4]), [2,2])
+
+    print *, B
+
+    print *, C(1,:, :)
+    print *, C(2,:, :)
+    print *, C(3, :, :)
+
+    ! test vector as subscripts for rank 3 array
+    print *, C(U, 2, V)
+    print *, C(3, U(3:), V)
+
+    ! test spread intrinsic
+    print *, spread(C, 2,2)
+end program 
