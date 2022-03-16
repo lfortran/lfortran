f18ab430c5e7d741495501c8c5deb3714e0e75c7
Gagandeep Singh <gdp.1807@gmail.com>
2022-03-16 05:36:05 +0000

    Code formatting

--- integration_tests/arrays_intrin_02.f90
+++ integration_tests/arrays_intrin_02.f90
@@ -4,12 +4,12 @@ program arrays_intrin_02
   l1 = [.true., .true., .true.]
   l2 = [.true., .true., .false.]
   l3 = [.false., .false., .false.]
-  if(.not. all(l1)) error stop
-  if(all(l2)) error stop
-  if(all(l3)) error stop
-  if(.not. any(l1)) error stop
-  if(.not. any(l2)) error stop
-  if(any(l3)) error stop
+  if (.not. all(l1)) error stop
+  if (all(l2)) error stop
+  if (all(l3)) error stop
+  if (.not. any(l1)) error stop
+  if (.not. any(l2)) error stop
+  if (any(l3)) error stop
   m1 = 1 
   m2 = 1
   m2(1,2) = 2
@@ -18,11 +18,11 @@ program arrays_intrin_02
   contains
     subroutine matrixCheck(m1, m2)
       integer, intent(in) :: m1(2,3), m2(2,3)
-      if(all(m1==m2)) error stop
-      if(.not. all(all(m1==m2, 1) .eqv. [.true., .false., .true.])) error stop
-      if(.not. all(all(m1==m2, 2) .eqv. [.false., .false.])) error stop
-      if(.not. any(m1==m2)) error stop
-      if(.not. all(any(m1==m2, 1) .eqv. [.true., .false., .true.])) error stop
-      if(.not. all(any(m1==m2, 2) .eqv. [.true., .true.])) error stop
+      if (all(m1 == m2)) error stop
+      if (.not. all(all(m1 == m2, 1) .eqv. [.true., .false., .true.])) error stop
+      if (.not. all(all(m1 == m2, 2) .eqv. [.false., .false.])) error stop
+      if (.not. any(m1 == m2)) error stop
+      if (.not. all(any(m1 == m2, 1) .eqv. [.true., .false., .true.])) error stop
+      if (.not. all(any(m1 == m2, 2) .eqv. [.true., .true.])) error stop
     end subroutine matrixCheck
 end program arrays_intrin_02
