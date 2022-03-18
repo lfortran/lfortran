3627771de5113dd71ae6ce44b0a85614aa44ddad
Devesh Sawant <devsaw115@gmail.com>
2022-03-18 18:47:53 +0530

    Format file

--- integration_tests/arrays_12.f90
+++ integration_tests/arrays_12.f90
@@ -2,20 +2,20 @@ program arrays_12
     implicit none
 
     integer :: i
-    real, dimension(2,2) :: B
-    integer, parameter:: C(3,2,3) = reshape([(i, i = 1, 18)], [3,2,3])
+    real, dimension(2, 2) :: B
+    integer, parameter:: C(3, 2, 3) = reshape([(i, i = 1, 18)], [3, 2, 3])
 
     ! vector subscripts
-    integer, dimension(3) :: U = [1,3,2]
+    integer, dimension(3) :: U = [1, 3, 2]
     integer, dimension(4) :: V = [3, 1, 1, 2]
 
-    ! test back to back reshape - C is 3x2x3 function
-    B = reshape(reshape(C, [4]), [2,2])
+    ! test back to back reshape - C is 3 x 2 x 3 function
+    B = reshape(reshape(C, [4]), [2, 2])
 
     print *, B
 
-    print *, C(1,:, :)
-    print *, C(2,:, :)
+    print *, C(1, :, :)
+    print *, C(2, :, :)
     print *, C(3, :, :)
 
     ! test vector as subscripts for rank 3 array
@@ -23,5 +23,6 @@ program arrays_12
     print *, C(3, U(3:), V)
 
     ! test spread intrinsic
-    print *, spread(C, 2,2)
+    print *, spread(C, 2, 2)
 end program 
+
