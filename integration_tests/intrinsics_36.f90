e291d117f1637f0a2772ce2886d8aa8ef525f3d5
Ubaid <shaikhubaid769@gmail.com>
2022-04-22 06:57:23 +0530

    Improve adjustl test case. Also update reference test

--- integration_tests/intrinsics_36.f90
+++ integration_tests/intrinsics_36.f90
@@ -1,7 +1,27 @@
-program test_adjustl
-    character(len=20) :: str = '   gfortran'
-    integer :: one = 1
-    str = adjustl(str)
-    print *, str
-    if (str(one:one) /= 'g') error stop
-end program test_adjustl
+program intrinsics_36
+    implicit none
+    character(len=25) :: empty = adjustl('')
+    character(len=25) :: all_spaces = adjustl('    ')
+    character(len=25) :: simple = adjustl('gfortran')
+    character(len=25) :: space_at_start = adjustl('   gfortran')
+    character(len=25) :: space_at_end = adjustl('gfortran   ')
+    character(len=25) :: space_in_between = adjustl('   g for tran   ')
+    character(len=25) :: spaces_with_symbols = adjustl('  # gfor* t $ ran &    ')
+
+    if (empty /= '') error stop
+    if (all_spaces /= '    ') error stop
+    if (simple /= 'gfortran') error stop
+    if (space_at_start /= 'gfortran   ') error stop
+    if (space_at_end /= 'gfortran   ') error stop
+    if (space_in_between /= 'g for tran      ') error stop
+    if (spaces_with_symbols /= '# gfor* t $ ran &      ') error stop
+    
+    print *, empty
+    print *, all_spaces
+    print *, simple
+    print *, space_at_start
+    print *, space_at_end
+    print *, space_in_between
+    print *, spaces_with_symbols
+
+end program
