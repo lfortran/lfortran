114e1e031adeb89955949442324d613eef1c271b
Gagandeep Singh <gdp.1807@gmail.com>
2022-04-28 19:37:46 +0530

    Updated block_01 test

--- integration_tests/block_01.f90
+++ integration_tests/block_01.f90
@@ -3,12 +3,15 @@ implicit none
 integer :: a, b, d
 a = 1
 b = 2
+associate (n => a)
 block
     integer :: b, c
     b = 3
     c = 4
     d = 5
+    d = cos(5.0)
 end block
+end associate
 if (b /= 2) error stop
 if (d /= 5) error stop
 end
