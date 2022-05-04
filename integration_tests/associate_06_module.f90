8b64bf6960f410cc0fcdb39c1716147fdf28568d
Gagandeep Singh <gdp.1807@gmail.com>
2022-05-04 22:14:21 +0530

    Add AssociateBlock and Block in serialization.cpp

---
+++ integration_tests/associate_06_module.f90
@@ -0,0 +1,17 @@
+module stdlib_string_type
+
+contains
+
+    elemental subroutine unused_dummy_argument(dummy)
+        class(*), intent(in) :: dummy
+        associate(dummy => dummy); end associate
+    end subroutine unused_dummy_argument
+
+    subroutine read_formatted(v_list)
+        integer, intent(in) :: v_list(:)
+
+        call unused_dummy_argument(v_list)
+
+    end subroutine
+
+end module
