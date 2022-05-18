f9b7f99129fae20483fb0412a72a5ad62943f8b6
Gagandeep Singh <gdp.1807@gmail.com>
2022-05-18 23:24:40 +0530

    Added support for transfer intrinsic

--- src/libasr/asr_utils.h
+++ src/libasr/asr_utils.h
@@ -134,6 +134,7 @@ static inline ASR::ttype_t* expr_type(const ASR::expr_t *f)
         case ASR::exprType::ArrayTranspose: { return ((ASR::ArrayTranspose_t*)f)->m_type; }
         case ASR::exprType::ArrayMatMul: { return ((ASR::ArrayMatMul_t*)f)->m_type; }
         case ASR::exprType::ArrayPack: { return ((ASR::ArrayPack_t*)f)->m_type; }
+        case ASR::exprType::ArrayTransfer: { return ((ASR::ArrayTransfer_t*)f)->m_type; }
         default : throw LFortranException("Not implemented");
     }
 }
