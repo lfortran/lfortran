82dbdccfecd1af8fce79ba6dc5bd07910ef23311
Ondřej Čertík <ondrej@certik.us>
2022-06-03 16:34:06 -0600

    Move the wasm_instructions_visitor.py to libasr

--- build0.sh
+++ build0.sh
@@ -11,7 +11,7 @@ python grammar/asdl_cpp.py
 # Generate a Fortran ASR from ASR.asdl (C++)
 python grammar/asdl_cpp.py src/libasr/ASR.asdl src/libasr/asr.h
 # Generate a wasm_visitor.h from src/libasr/wasm_instructions.txt (C++)
-python grammar/wasm_instructions_visitor.py
+python src/libasr/wasm_instructions_visitor.py
 
 # Generate the tokenizer and parser
 (cd src/lfortran/parser && re2c -W -b tokenizer.re -o tokenizer.cpp)
