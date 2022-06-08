bfca97690509e10ab73e107849bae43bf375a47b
Ondřej Čertík <ondrej@certik.us>
2022-06-07 22:58:11 -0500

    Only do the load if it is a pointer

--- src/libasr/codegen/asr_to_llvm.cpp
+++ src/libasr/codegen/asr_to_llvm.cpp
@@ -4282,12 +4282,16 @@ public:
                                             } else if (is_a<ASR::CPtr_t>(*arg_type)) {
                                                 // pass
                                             } else {
-                                                // Dereference the pointer argument (unless it is a CPtr)
-                                                // to pass by value
-                                                // E.g.:
-                                                // i32* -> i32
-                                                // {double,double}* -> {double,double}
-                                                tmp = CreateLoad(tmp);
+                                                // TODO: it would be cleaner to always convert a value to a pointer first at the beginning
+                                                // of the function/subroutine
+                                                if (tmp->getType()->isPointerTy()) {
+                                                    // Dereference the pointer argument (unless it is a CPtr)
+                                                    // to pass by value
+                                                    // E.g.:
+                                                    // i32* -> i32
+                                                    // {double,double}* -> {double,double}
+                                                    tmp = CreateLoad(tmp);
+                                                }
                                             }
                                         }
                                 }
