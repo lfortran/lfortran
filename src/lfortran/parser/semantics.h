334dfa17a15b04c5786269be97a53d893bb61657
Ondřej Čertík <ondrej@certik.us>
2022-06-27 16:05:43 +0300

    Pass in decl

--- src/lfortran/parser/semantics.h
+++ src/lfortran/parser/semantics.h
@@ -1948,9 +1948,11 @@ ast_t* COARRAY(Allocator &al, const ast_t *id,
         DECLS(decl), decl.size(), \
         VEC_CAST(contains, procedure_decl), contains.size())
 
-#define TEMPLATE(name, namelist, decl, contains, contains_block, l) \
+#define TEMPLATE(name, namelist, decl, contains, l) \
         make_Template_t(p.m_a, l, name2char(name), \
-        REDUCE_ARGS(p.m_a, namelist), namelist.size())
+        REDUCE_ARGS(p.m_a, namelist), namelist.size(), \
+        /*unit_decl2_t** a_decl*/ DECLS(decl), /*size_t n_decl*/ decl.size() \
+        )
 
 #define DERIVED_TYPE_PROC(attr, syms, trivia, l) make_DerivedTypeProc_t(p.m_a, l, \
         nullptr, VEC_CAST(attr, decl_attribute), attr.size(), \
