d4e00cf3c31d57a1624ab4f935f4bf326dda2a69
Ondřej Čertík <ondrej@certik.us>
2022-06-27 15:56:58 +0300

    Parser: Pass in contains_block

--- src/lfortran/parser/semantics.h
+++ src/lfortran/parser/semantics.h
@@ -1948,7 +1948,7 @@ ast_t* COARRAY(Allocator &al, const ast_t *id,
         DECLS(decl), decl.size(), \
         VEC_CAST(contains, procedure_decl), contains.size())
 
-#define TEMPLATE(name, namelist, decl, contains, l) \
+#define TEMPLATE(name, namelist, decl, contains, contains_block, l) \
         make_Template_t(p.m_a, l, name2char(name), \
         REDUCE_ARGS(p.m_a, namelist), namelist.size())
 
