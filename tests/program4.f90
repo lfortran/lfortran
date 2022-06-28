17d93ce4192085faf5bf5cf81f551577533f7a58
Andrew Best <andrew@blamsoft.com>
2022-06-28 07:55:08 -0700

    Improved test case program4

--- tests/program4.f90
+++ tests/program4.f90
@@ -13,7 +13,7 @@ z = func2(x, y)
 print *, "6.0 == ", z
 
 z = func2(x, y)
-print *, "7.0 == ", z
+print *, "8.0 == ", z
 contains
 
 function func1(a, b) result(c)
@@ -28,7 +28,9 @@ function func2(a, b) result(c)
     real, intent(in) :: a, b
     real :: c
     real :: saved = 2.0
+    real, save :: d
     saved = saved + 1.0
-    c = c + a + b + saved
+    c = d + c + a + b + saved
+    d = d + 1.0
 end function func2
 end program program4
