6dbacf2189b652ff8639fdf54337a1b8ad2e34ac
Ubaid <shaikhubaid769@gmail.com>
2022-06-30 18:18:57 +0530

    WASM: Add support for If-Else

--- src/libasr/codegen/asr_to_wasm.cpp
+++ src/libasr/codegen/asr_to_wasm.cpp
@@ -658,6 +658,22 @@ class ASRToWASMVisitor : public ASR::BaseVisitor<ASRToWASMVisitor> {
         wasm::emit_i32_const(m_code_section, m_al, 1); // non-zero exit code
         exit();
     }
+
+    void visit_If(const ASR::If_t &x) {
+        this->visit_expr(*x.m_test);
+        wasm::emit_b8(m_code_section, m_al, 0x04);
+        wasm::emit_b8(m_code_section, m_al, 0x40); // empty block type
+        for (size_t i=0; i<x.n_body; i++) {
+            this->visit_stmt(*x.m_body[i]);
+        }
+        if(x.n_orelse){
+            wasm::emit_b8(m_code_section, m_al, 0x05); // starting of else
+            for (size_t i=0; i<x.n_orelse; i++) {
+                this->visit_stmt(*x.m_orelse[i]);
+            }
+        }
+        wasm::emit_expr_end(m_code_section, m_al);
+    }
 };
 
 Result<Vec<uint8_t>> asr_to_wasm_bytes_stream(ASR::TranslationUnit_t &asr, Allocator &al, diag::Diagnostics &diagnostics) {
