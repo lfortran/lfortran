4abe2fad02dd5ea5fe5970fff198ebf62ebc77d3
Naman Gera <namangera15@gmail.com>
2022-07-04 11:39:01 +0530

    LLVM: Handle LogicalToReal cast kind

--- src/libasr/codegen/asr_to_llvm.cpp
+++ src/libasr/codegen/asr_to_llvm.cpp
@@ -3879,6 +3879,11 @@ public:
                 }
                 break;
             }
+            case (ASR::cast_kindType::LogicalToReal) : {
+                int a_kind = ASRUtils::extract_kind_from_ttype_t(x.m_type);
+                tmp = builder->CreateUIToFP(tmp, getFPType(a_kind, false));
+                break;
+            }
             case (ASR::cast_kindType::RealToInteger) : {
                 llvm::Type *target_type;
                 int a_kind = ASRUtils::extract_kind_from_ttype_t(x.m_type);
