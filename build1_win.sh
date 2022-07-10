9cd71666ea63b84a82be59d423711646d34b4ca0
Ondrej Certik <ondrej@certik.us>
2022-07-09 22:45:47 -0600

    Win: generate wasm file

--- build1_win.sh
+++ build1_win.sh
@@ -6,3 +6,4 @@ python grammar/asdl_cpp.py src/libasr/ASR.asdl src/libasr/asr.h
 pushd src/lfortran/parser && re2c -W -b tokenizer.re -o tokenizer.cpp && popd
 pushd src/lfortran/parser && re2c -W -b preprocessor.re -o preprocessor.cpp && popd
 pushd src/lfortran/parser && bison -Wall -d -r all parser.yy && popd
+python src/libasr/wasm_instructions_visitor.py
