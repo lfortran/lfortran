30dd750534dda78750cf0c09ed606f0568c9b790
Ondřej Čertík <ondrej@certik.us>
2022-07-12 16:52:14 -0600

    More difficult cases

--- tests/fixed_form3.f
+++ tests/fixed_form3.f
@@ -1,8 +1,16 @@
       integerfunctionfunctionf(n)
       integerfunctionfunctionf(n)
+      integern
       endfunctionfunctionf
 
       integerfunctionf(endfunctionf)
       integerendfunctionf
       endfunctionf=4
       endfunctionf
+
+      integerfunctiong(integerx)
+      integerintegerx
+      integerx
+      integerx=5
+      x=5
+      endfunctiong
