0677b40296c95185f0b74263379c0a11e2877704
Naman Gera <namangera15@gmail.com>
2022-07-22 23:16:44 +0530

    LLVM: Add StringChr and StringOrd visitors

--- src/libasr/codegen/asr_to_llvm.cpp
+++ src/libasr/codegen/asr_to_llvm.cpp
@@ -3327,6 +3327,26 @@ public:
         tmp = lfortran_str_len(parg);
     }
 
+    void visit_StringOrd(const ASR::StringOrd_t &x) {
+        if (x.m_value) {
+            this->visit_expr_wrapper(x.m_value, true);
+            return;
+        }
+        this->visit_expr_wrapper(x.m_arg, true);
+        llvm::AllocaInst *parg = builder->CreateAlloca(character_type, nullptr);
+        builder->CreateStore(tmp, parg);
+        tmp = lfortran_str_ord(parg);
+    }
+
+    void visit_StringChr(const ASR::StringChr_t &x) {
+        if (x.m_value) {
+            this->visit_expr_wrapper(x.m_value, true);
+            return;
+        }
+        this->visit_expr_wrapper(x.m_arg, true);
+        tmp = lfortran_str_chr(tmp);
+    }
+
     void visit_StringItem(const ASR::StringItem_t& x) {
         if (x.m_value) {
             this->visit_expr_wrapper(x.m_value, true);
