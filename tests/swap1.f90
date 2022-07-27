37ce2ae810d97b182e99cbae8125c7c7b84e4777
oshanath <oshanath.19@cse.mrt.ac.lk>
2022-07-27 17:35:15 +0200

    Add AST and ASR tests for generics

---
+++ tests/swap1.f90
@@ -0,0 +1,28 @@
+module swap_m
+    implicit none
+    private
+    public :: swap_t
+
+    template swap_t(T)
+        private
+        public :: swap
+
+        type :: t_pair
+            integer :: i
+            real :: x
+        end type
+
+        type :: T
+        end type
+
+    contains
+        subroutine swap_(x, y)
+            type(T), intent(inout) :: x, y
+            type(T) :: tmp
+
+            tmp = x
+            x = y
+            y = tmp
+        end subroutine
+    end template
+end module
