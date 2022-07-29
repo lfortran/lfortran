9b165e3245c80d5920d675c9f2a39a8d30ec0324
Ondřej Čertík <ondrej@certik.us>
2022-07-29 17:28:49 +0200

    Implement short error format

--- src/libasr/diagnostics.h
+++ src/libasr/diagnostics.h
@@ -184,10 +184,13 @@ struct Diagnostics {
 };
 
 std::string render_diagnostic_human(const Diagnostic &d, bool use_colors);
+std::string render_diagnostic_short(const Diagnostic &d);
 
 // Fills Diagnostic with span details and renders it
 std::string render_diagnostic_human(Diagnostic &d, const std::string &input,
         const LocationManager &lm, bool use_colors, bool show_stacktrace); 
+std::string render_diagnostic_short(Diagnostic &d, const std::string &input,
+        const LocationManager &lm); 
 
 } // namespace diag
 } // namespace LFortran
