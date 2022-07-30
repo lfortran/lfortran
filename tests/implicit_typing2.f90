67a746cf0f07689ec7857001b4c8441c80e2d3da
Ondřej Čertík <ondrej@certik.us>
2022-07-30 16:56:20 +0200

    Add test for an array of implicit dummy length
    Both local array and a dummy argument array.

--- tests/implicit_typing2.f90
+++ tests/implicit_typing2.f90
@@ -1,9 +1,13 @@
-subroutine f(ifault, r)
+subroutine f(ifault, r, n, b)
+integer :: a(n)
+integer :: b(n)
 ifault = 4
 r = 3
 end subroutine
 
-integer function g(ifault, r)
+integer function g(ifault, r, n, b)
+integer :: a(n)
+integer :: b(n)
 ifault = 4
 r = 3
 end function
