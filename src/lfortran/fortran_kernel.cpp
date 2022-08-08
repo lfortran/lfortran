8f8e72f686706632d09374b6feb26ea085697299
Wolf Vollprecht <w.vollprecht@gmail.com>
2022-08-08 08:50:06 +0200

    move kernel to header file

--- src/lfortran/fortran_kernel.cpp
+++ src/lfortran/fortran_kernel.cpp
@@ -71,41 +71,6 @@ namespace LFortran
         int stdout_fileno;
     };
 
-    class custom_interpreter : public xeus::xinterpreter
-    {
-    private:
-        FortranEvaluator e;
-
-    public:
-        custom_interpreter() : e{CompilerOptions()} {}
-        virtual ~custom_interpreter() = default;
-
-    private:
-
-        void configure_impl() override;
-
-        nl::json execute_request_impl(int execution_counter,
-                                      const std::string& code,
-                                      bool silent,
-                                      bool store_history,
-                                      nl::json user_expressions,
-                                      bool allow_stdin) override;
-
-        nl::json complete_request_impl(const std::string& code,
-                                       int cursor_pos) override;
-
-        nl::json inspect_request_impl(const std::string& code,
-                                      int cursor_pos,
-                                      int detail_level) override;
-
-        nl::json is_complete_request_impl(const std::string& code) override;
-
-        nl::json kernel_info_request_impl() override;
-
-        void shutdown_request_impl() override;
-    };
-
-
     nl::json custom_interpreter::execute_request_impl(int execution_counter, // Typically the cell number
                                                       const std::string& code, // Code to execute
                                                       bool /*silent*/,
@@ -473,5 +438,4 @@ namespace LFortran
 
         return 0;
     }
-
 }
--- src/lfortran/fortran_kernel.h
+++ src/lfortran/fortran_kernel.h
@@ -2,8 +2,46 @@
 #define LFORTRAN_FORTRAN_KERNEL_H
 
 #include <libasr/config.h>
+#include <lfortran/fortran_evaluator.h>
+
+#include <xeus/xinterpreter.hpp>
+
 
 namespace LFortran {
+
+    class custom_interpreter : public xeus::xinterpreter
+    {
+    private:
+        FortranEvaluator e;
+
+    public:
+        custom_interpreter() : e{CompilerOptions()} {}
+        virtual ~custom_interpreter() = default;
+
+    private:
+
+        void configure_impl() override;
+
+        nl::json execute_request_impl(int execution_counter,
+                                      const std::string& code,
+                                      bool silent,
+                                      bool store_history,
+                                      nl::json user_expressions,
+                                      bool allow_stdin) override;
+
+        nl::json complete_request_impl(const std::string& code,
+                                       int cursor_pos) override;
+
+        nl::json inspect_request_impl(const std::string& code,
+                                      int cursor_pos,
+                                      int detail_level) override;
+
+        nl::json is_complete_request_impl(const std::string& code) override;
+
+        nl::json kernel_info_request_impl() override;
+
+        void shutdown_request_impl() override;
+    };
 
 #ifdef HAVE_LFORTRAN_XEUS
     int run_kernel(const std::string &connection_filename);
