db98ae9551de116b40cbdf6b8cc8a0bbc09c7de6
Ondřej Čertík <ondrej@certik.us>
2022-08-17 20:04:55 +0200

    Implement the rest of the *_kinds
    Add a test.

--- integration_tests/intrinsics_45.f90
+++ integration_tests/intrinsics_45.f90
@@ -1,6 +1,6 @@
 program intrinsics_45
-use iso_fortran_env, only : real_kinds
-print *, size(real_kinds)
+use iso_fortran_env, only : real_kinds, integer_kinds, character_kinds, logical_kinds
+print *, size(real_kinds), size(integer_kinds), size(character_kinds), size(logical_kinds)
 ! Does not work yet
 !print *, real_kinds
 end program
