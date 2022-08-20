c36f24912bb345a05d4abb218bca575f910b41a6
meow464 <meow464@protonmail.com>
2022-08-20 17:22:23 +0000

    Add cmake support of rlfortran

--- cmake/CMakeFortranInformation.cmake
+++ cmake/CMakeFortranInformation.cmake
@@ -1,5 +1,6 @@
 # This file sets the basic flags for the Fortran compiler
 if(NOT CMAKE_Fortran_COMPILE_OBJECT)
-    set(CMAKE_Fortran_COMPILE_OBJECT "<CMAKE_Fortran_COMPILER> <INCLUDES> <FLAGS> <SOURCE> -o <OBJECT>")
+    set(CMAKE_Fortran_COMPILE_OBJECT "<CMAKE_Fortran_COMPILER> <DEFINES> <INCLUDES> <FLAGS> <SOURCE> -o <OBJECT>")
+    set(CMAKE_Fortran_PREPROCESS_SOURCE "<CMAKE_Fortran_COMPILER> <DEFINES> <INCLUDES> <FLAGS> <SOURCE> -o <OBJECT>")
 endif()
 set(CMAKE_Fortran_INFORMATION_LOADED 1)
