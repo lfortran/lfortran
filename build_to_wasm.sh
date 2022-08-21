371009a7710652b50a08bd06e1eb88a2636d72fa
Ubaid <shaikhubaid769@gmail.com>
2022-08-21 16:14:33 +0530

    WASM: Replace buid1.sh with its contents and set WITH_STACKTRACE=no

--- build_to_wasm.sh
+++ build_to_wasm.sh
@@ -2,7 +2,15 @@
 
 set -ex
 
-./build1.sh
+cmake \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DWITH_LLVM=yes \
+    -DLFORTRAN_BUILD_ALL=yes \
+    -DWITH_STACKTRACE=no \
+    -DCMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH_LFORTRAN;$CONDA_PREFIX" \
+    -DCMAKE_INSTALL_PREFIX=`pwd`/inst \
+    .
+cmake --build . -j16 --target install
 
 mkdir -p src/bin/asset_dir
 cp src/runtime/*.mod src/bin/asset_dir
