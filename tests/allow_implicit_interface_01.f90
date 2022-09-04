83c41ef5040033f5caff780abe531ec8af7742ed
Pranavchiku <goswami.4@iitj.ac.in>
2022-09-04 13:07:34 +0530

    Add update tests/references

---
+++ tests/allow_implicit_interface_01.f90
@@ -0,0 +1,7 @@
+subroutine implicit_function()
+integer n,j,krank,k
+complex y(n),r(krank+2,n)
+
+r(j,k) = conjg(y(k))
+
+end
--- tests/variable01.f90
+++ tests/variable01.f90
@@ -1,5 +1,5 @@
 subroutine variable01()
 integer m,n
-complex*16 x(m), y(n), p1, p2, p3, p4
+complex x(m), y(n), p1, p2, p3, p4
 call matveca(x,m,n,y,p1,p2,p3,p4)
 end
