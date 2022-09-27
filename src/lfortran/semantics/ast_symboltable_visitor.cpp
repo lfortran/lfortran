15a8a362b3f0f4ec162834b31fc9a4a878c007d1
Ondřej Čertík <ondrej@certik.us>
2022-09-27 12:21:12 -0600

    Add a TODO list

--- src/lfortran/semantics/ast_symboltable_visitor.cpp
+++ src/lfortran/semantics/ast_symboltable_visitor.cpp
@@ -413,6 +413,8 @@ public:
     }
 
     void visit_Function(const AST::Function_t &x) {
+        // TODO: The most common (modern) path is explicit typing, so in this
+        // path things should be as fast as possible. Currently they are slow.
         std::map<std::string, ASR::ttype_t*> implicit_dictionary;
         //populate the implicit_dictionary
         populate_implicit_dictionary(x, implicit_dictionary);
