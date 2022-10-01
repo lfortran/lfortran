dd089110f626dda7c07d793015092e74acff47bf
Pranavchiku <goswami.4@iitj.ac.in>
2022-10-01 11:30:58 +0530

    created a shared implicit_mapping between symbol table and body visitor

--- src/lfortran/semantics/ast_to_asr.cpp
+++ src/lfortran/semantics/ast_to_asr.cpp
@@ -23,7 +23,8 @@ Result<ASR::asr_t*> symbol_table_visitor(Allocator &al, AST::TranslationUnit_t &
         diag::Diagnostics &diagnostics,
         SymbolTable *symbol_table,
         CompilerOptions &compiler_options,
-        std::map<std::string, std::vector<ASR::asr_t*>>& template_type_parameters);
+        std::map<std::string, std::vector<ASR::asr_t*>>& template_type_parameters,
+        std::map<uint64_t, std::map<std::string, ASR::ttype_t*>>& implicit_mapping);
 
 Result<ASR::TranslationUnit_t*> body_visitor(Allocator &al,
         AST::TranslationUnit_t &ast,
@@ -38,14 +39,16 @@ Result<ASR::TranslationUnit_t*> ast_to_asr(Allocator &al,
     CompilerOptions &compiler_options)
 {
     std::map<std::string, std::vector<ASR::asr_t*>> template_type_parameters;
+    std::map<uint64_t, std::map<std::string, ASR::ttype_t*>> implicit_mapping;
     ASR::asr_t *unit;
     auto res = symbol_table_visitor(al, ast, diagnostics, symbol_table,
-        compiler_options, template_type_parameters);
+        compiler_options, template_type_parameters, implicit_mapping);
     if (res.ok) {
         unit = res.result;
     } else {
         return res.error;
     }
+
     ASR::TranslationUnit_t *tu = ASR::down_cast2<ASR::TranslationUnit_t>(unit);
     LFORTRAN_ASSERT(asr_verify(*tu));
 
