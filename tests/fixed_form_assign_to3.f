2fd0e1918e30132a4c6783732ef2004e4d57f8d5
Konrad Handrick <konradha@yahoo.com>
2022-10-04 15:53:57 +0200

    add missing test

---
+++ tests/fixed_form_assign_to3.f
@@ -0,0 +1,12 @@
+      program main
+   10 assign 30 to next
+   20 GO TO next,(30, 50, 70, 110)
+      assign 50 to next
+      assign 70 to next
+      assign 110 to next
+   30 print *, 30
+   50 print *, 50
+   70 print *, 70
+  110 print *, 110
+      end program
+
