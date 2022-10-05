5ff09f34492c4eb8df4f819bbde07e2586a8dde8
Pranavchiku <goswami.4@iitj.ac.in>
2022-10-05 10:41:45 +0530

    performed suggested changes in common_visitor

--- src/lfortran/semantics/ast_symboltable_visitor.cpp
+++ src/lfortran/semantics/ast_symboltable_visitor.cpp
@@ -89,8 +89,8 @@ public:
     };
 
     SymbolTableVisitor(Allocator &al, SymbolTable *symbol_table,
-        diag::Diagnostics &diagnostics, CompilerOptions &compiler_options)
-      : CommonVisitor(al, symbol_table, diagnostics, compiler_options) {}
+        diag::Diagnostics &diagnostics, CompilerOptions &compiler_options, std::map<uint64_t, std::map<std::string, ASR::ttype_t*>> &implicit_mapping)
+      : CommonVisitor(al, symbol_table, diagnostics, compiler_options, implicit_mapping) {}
 
     void visit_TranslationUnit(const AST::TranslationUnit_t &x) {
         if (!current_scope) {
@@ -1459,7 +1459,7 @@ Result<ASR::asr_t*> symbol_table_visitor(Allocator &al, AST::TranslationUnit_t &
         std::map<std::string, std::vector<ASR::asr_t*>>& template_type_parameters,
         std::map<uint64_t, std::map<std::string, ASR::ttype_t*>>& implicit_mapping)
 {
-    SymbolTableVisitor v(al, symbol_table, diagnostics, compiler_options);
+    SymbolTableVisitor v(al, symbol_table, diagnostics, compiler_options, implicit_mapping);
     try {
         v.visit_TranslationUnit(ast);
     } catch (const SemanticError &e) {
