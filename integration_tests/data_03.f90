bb2e971fc44d299b59b1651263cad88be7fc1cf0
Ondřej Čertík <ondrej@certik.us>
2022-10-04 20:38:18 -0600

    Improve the test, move data inside an if stmt

--- integration_tests/data_03.f90
+++ integration_tests/data_03.f90
@@ -6,11 +6,15 @@ if (x /= 1) error stop
 if (abs(y-2.0) > 1e-5) error stop
 if (abs(z-3.0) > 1e-5) error stop
 if (abs(c1(1)-0.0) > 1e-5) error stop
-if (abs(c1(2)-0.22) > 1e-5) error stop
-data x, td, b /1, 4, 3/
+if (abs(c1(2)-0.22) > 1e-5) then
+    error stop
+    data x, td, b /1, 4, 3/
+end if
 if (abs(c1(4)+0.21) > 1e-5) error stop
-if (abs(bf1-0.8) > 1e-5) error stop
-data y, z /2.0, 3.0/
+if (abs(bf1-0.8) > 1e-5) then
+    data y, z /2.0, 3.0/
+    error stop
+end if
 if (abs(xx90-0.55) > 1e-5) error stop
 if (abs(xx95-0.62) > 1e-5) error stop
 data c1 /0.0, 0.22, -0.14, -0.21/
