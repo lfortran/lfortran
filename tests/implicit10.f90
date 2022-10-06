1ccde6162e3414917171442dcf65742162c0fce8
Pranavchiku <goswami.4@iitj.ac.in>
2022-10-07 01:16:01 +0530

    add test and update test references

---
+++ tests/implicit10.f90
@@ -0,0 +1,24 @@
+integer function a()
+implicit none
+integer :: b, c
+real :: d, e, f
+real*8 :: g, h, i
+complex :: j, k
+complex*8 :: l, m
+complex *16 :: n
+double precision :: o, p
+integer :: Y(e)
+real :: X(d, e, e)
+Y(1) = 3
+X(1, 1, 1) = 3
+a = 1
+b = 2
+d = 3
+f = 4
+h = 5
+i = 6
+l = 7
+m = 8
+o = 9
+p = 10
+end function
