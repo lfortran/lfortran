0391f8c852a53fe9497f071ad80f34b74ef60c21
Konrad Handrick <konradha@yahoo.com>
2022-10-06 13:26:51 +0200

    add integration tests

---
+++ integration_tests/assign_to1.f90
@@ -0,0 +1,4 @@
+program main
+1000 if (0<1) assign 100 to k
+100 goto k
+end program
---
+++ integration_tests/assign_to2.f90
@@ -0,0 +1,10 @@
+program main
+10 assign 30 to next
+sum = ZERO
+i = 1
+20    GO TO next,(30, 50, 70)
+30 IF( 0<1) GO TO 70
+50 print *, 50
+assign 50 to next
+70 print *, 70
+end program
