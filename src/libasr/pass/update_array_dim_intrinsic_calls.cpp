9fa2475c91aff4bfc2a5778a63cf6bc5fdd1feb6
meow464 <meow464@protonmail.com>
2022-10-10 23:14:59 -0300

    add compiler_options to pass_update_array_dim_intrinsic_calls

--- src/libasr/pass/update_array_dim_intrinsic_calls.cpp
+++ src/libasr/pass/update_array_dim_intrinsic_calls.cpp
@@ -144,7 +144,8 @@ class ArrayDimIntrinsicCallsVisitor : public ASR::CallReplacerOnExpressionsVisit
 };
 
 void pass_update_array_dim_intrinsic_calls(Allocator &al, ASR::TranslationUnit_t &unit,
-                                           const LCompilers::PassOptions& /*pass_options*/) {
+                                           const LCompilers::PassOptions& /*pass_options*/,
+					   const LFortran::CompilerOptions& /* compiler_options */) {
     ArrayDimIntrinsicCallsVisitor v(al);
     v.visit_TranslationUnit(unit);
     LFORTRAN_ASSERT(asr_verify(unit));
