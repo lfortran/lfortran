a4130b0f8e90867e503f5f81ee3512c628aaabf8
meow464 <meow464@protonmail.com>
2022-10-12 21:19:31 -0300

    indent

--- src/lfortran/fortran_evaluator.cpp
+++ src/lfortran/fortran_evaluator.cpp
@@ -315,7 +315,7 @@ Result<std::unique_ptr<LLVMModule>> FortranEvaluator::get_llvm3(
     Result<std::unique_ptr<LFortran::LLVMModule>> res
         = asr_to_llvm(asr, diagnostics,
             e->get_context(), al, pass_manager,
-	    compiler_options.platform, run_fn, compiler_options);
+            compiler_options.platform, run_fn, compiler_options);
     if (res.ok) {
         m = std::move(res.result);
     } else {
