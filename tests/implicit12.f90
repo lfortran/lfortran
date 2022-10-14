57c32ea8623ea3cdc547af6e35f444c02a67c843
Pranavchiku <goswami.4@iitj.ac.in>
2022-10-14 16:51:05 +0530

    Add and update test, references

--- tests/implicit12.f90
+++ tests/implicit12.f90
@@ -1,7 +1,29 @@
 subroutine a()
-implicit real (a,b,c,d)
-implicit integer (e,f,g)
-implicit real (h-l, r-y)
-implicit complex (x-z)
-implicit double precision (m-q)
+implicit integer (b-c)
+implicit integer*4 (d-e)
+implicit integer*8 (f-g)
+implicit real (h)
+implicit real*4 (i-k)
+implicit real*8 (l)
+implicit complex (m, n)
+implicit complex*8 (o)
+implicit complex*16 (p)
+implicit double precision (q)
+implicit double precision (r)
+integer :: Y(e)
+real :: X(d, e, e)
+real :: b
+Y(1) = 3
+X(1, 1, 1) = 3
+b = 2
+d = 3
+f = 4
+h = 5
+i = 6
+l = 7
+m = 8
+o = 9
+p = 10
+q = 11
+r = 12
 end subroutine
