76008d793b4282739556ff86c4c90c85cdae5de6
Pranavchiku <goswami.4@iitj.ac.in>
2022-10-22 00:19:39 +0530

    update statement1.f90 and test references

--- integration_tests/statement1.f90
+++ integration_tests/statement1.f90
@@ -1,9 +1,16 @@
 program statement1
-integer i,j
+integer :: i,j
 integer, parameter :: dp = kind(0.d0)
-real dfloat, ti, sum, test
-dfloat(i,j) = i*j
-test = 1.90
-ti = dfloat(1,8)
-sum = dfloat(12,9)+12.42
+real(dp) :: dfloat
+real :: sfloat, ti, sum
+integer :: ifloat
+sfloat(i,j) = i*j
+dfloat(i,j) = i+j
+ifloat(i,j) = i+j
+ti = sfloat(1,8)
+if (abs(ti-8) > 1e-5) error stop
+sum = sfloat(12,9)+12.42
+if (abs(sum-120.42) > 1e-5) error stop
+if (abs(dfloat(5,4)-9) > 1e-10) error stop
+if (ifloat(5,4) /= 9) error stop
 end program
