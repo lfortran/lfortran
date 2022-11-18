d8350c463f4b2a5a0e27f52c2652ae4d0895ee6f
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2022-11-18 15:30:22 +0530

    Add more test

--- integration_tests/arrays_14.f90
+++ integration_tests/arrays_14.f90
@@ -6,12 +6,17 @@ b = [14, 12]
 
 call foo()
 
+if( b(1) /= 243 ) error stop
+if( b(2) /= 930 ) error stop
+
 contains
 
 subroutine foo()
 if( a /= 10 ) error stop
 if( b(1) /= 14 ) error stop
 if( b(2) /= 12 ) error stop
+b(1) = 243
+b(2) = 930
 end subroutine
 
 end program
