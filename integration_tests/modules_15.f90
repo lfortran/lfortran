7090f531b7049e2903e003f46a9dd2beceb8a4fe
Ondřej Čertík <ondrej@certik.us>
2022-12-03 12:56:02 +0200

    Add a test for A(*) style arrays in bind(c)
    It turns out this feature is already implemented, but it has not been
    tested. This adds tests for integer, single and double precision real
    arrays.

--- integration_tests/modules_15.f90
+++ integration_tests/modules_15.f90
@@ -6,6 +6,7 @@ use modules_15b, only: &
     f_int_float_value, f_int_double_value, &
     sub_int_float_value, sub_int_double_value, &
     f_int_intarray, f_int_floatarray, f_int_doublearray, &
+    f_int_intarray_star, f_int_floatarray_star, f_int_doublearray_star, &
     sub_int_intarray, sub_int_floatarray, sub_int_doublearray, &
     f_int_float_complex, f_int_double_complex, &
     sub_int_float_complex, sub_int_double_complex, &
@@ -184,6 +185,30 @@ X64(1) = 1.1_dp
 X64(2) = 2.2_dp
 X64(3) = 3.3_dp
 r64 = f_int_doublearray(n, X64)
+print *, r64
+if (abs(r64 - 6.6_dp) > 1e-10_dp) error stop
+
+n = 3
+I32(1) = 1
+I32(2) = 2
+I32(3) = 3
+i = f_int_intarray_star(n, I32)
+print *, i
+if (i /= 6) error stop
+
+n = 3
+X32(1) = 1.1_dp
+X32(2) = 2.2_dp
+X32(3) = 3.3_dp
+r32 = f_int_floatarray_star(n, X32)
+print *, r32
+if (abs(r32 - 6.6_sp) > 1e-5_dp) error stop
+
+n = 3
+X64(1) = 1.1_dp
+X64(2) = 2.2_dp
+X64(3) = 3.3_dp
+r64 = f_int_doublearray_star(n, X64)
 print *, r64
 if (abs(r64 - 6.6_dp) > 1e-10_dp) error stop
 
