1f3d3df38d278553ed27c0c28f847596896600f0
Ubaid <shaikhubaid769@gmail.com>
2022-12-07 06:14:30 +0530

    Print module details in ASR Json
    Also update it for location info

--- src/lfortran/pickle.cpp
+++ src/lfortran/pickle.cpp
@@ -313,11 +313,48 @@ public:
     void visit_Module(const ASR::Module_t &x) {
         s.append("{");
         inc_indent(); s.append("\n" + indtd);
-        s.append("\"node\": \"Module\",");
-        s.append("\n" + indtd);
+        s.append("\"node\": \"Module\"");
+        s.append(",\n" + indtd);
         s.append("\"fields\": {");
         inc_indent(); s.append("\n" + indtd);
-        s.append("\"name\": \"" + std::string(x.m_name) + "\"");
+        s.append("\"name\": ");
+        s.append("\"" + std::string(x.m_name) + "\"");
+        s.append(",\n" + indtd);
+        s.append("\"dependencies\": ");
+        s.append("[");
+        if (x.n_dependencies > 0) {
+            inc_indent(); s.append("\n" + indtd);
+            for (size_t i=0; i<x.n_dependencies; i++) {
+                s.append("\"" + std::string(x.m_dependencies[i]) + "\"");
+                if (i < x.n_dependencies-1) {
+                    s.append(",\n" + indtd);
+                };
+            }
+            dec_indent(); s.append("\n" + indtd);
+        }
+        s.append("]");
+        s.append(",\n" + indtd);
+        s.append("\"loaded_from_mod\": ");
+        if (x.m_loaded_from_mod) {
+            s.append("true");
+        } else {
+            s.append("false");
+        }
+        s.append(",\n" + indtd);
+        s.append("\"intrinsic\": ");
+        if (x.m_intrinsic) {
+            s.append("true");
+        } else {
+            s.append("false");
+        }
+        dec_indent(); s.append("\n" + indtd);
+        s.append("}");
+        s.append(",\n" + indtd);
+        s.append("\"loc\": {");
+        inc_indent(); s.append("\n" + indtd);
+        s.append("\"first\": " + std::to_string(x.base.base.loc.first));
+        s.append(",\n" + indtd);
+        s.append("\"last\": " + std::to_string(x.base.base.loc.last));
         dec_indent(); s.append("\n" + indtd);
         s.append("}");
         dec_indent(); s.append("\n" + indtd);
