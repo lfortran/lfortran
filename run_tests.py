628dc3c11018b29e9564019134a2f874d7c19d0b
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2022-12-08 11:15:17 +0530

    Add option for llvm codegen test with implicit-typing and interface

--- run_tests.py
+++ run_tests.py
@@ -31,6 +31,7 @@ def single_test(test: Dict, verbose: bool, no_llvm: bool, update_reference: bool
     asr_implicit_typing = is_included("asr_implicit_typing")
     asr_implicit_interface = is_included("asr_implicit_interface")
     asr_implicit_interface_and_typing = is_included("asr_implicit_interface_and_typing")
+    asr_implicit_interface_and_typing_with_llvm = is_included("asr_implicit_interface_and_typing_with_llvm")
     asr_preprocess = is_included("asr_preprocess")
     asr_indent = is_included("asr_indent")
     mod_to_asr = is_included("mod_to_asr")
@@ -152,6 +153,15 @@ def single_test(test: Dict, verbose: bool, no_llvm: bool, update_reference: bool
             filename,
             "asr",
             "lfortran --indent --show-asr --implicit-typing --allow-implicit-interface --no-color {infile} -o {outfile}",
+            filename,
+            update_reference,
+            extra_args)
+
+    if asr_implicit_interface_and_typing_with_llvm:
+        run_test(
+            filename,
+            "llvm",
+            "lfortran --show-llvm --implicit-typing --allow-implicit-interface {infile} -o {outfile}",
             filename,
             update_reference,
             extra_args)
