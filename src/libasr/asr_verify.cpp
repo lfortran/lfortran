b5596f781c8f9c3019a8b0fd8745e4902e82394a
Gagandeep Singh <gdp.1807@gmail.com>
2022-12-31 16:30:51 +0530

    Implement GenericAssignment for DerivedTypes in ASR level

--- src/libasr/asr_verify.cpp
+++ src/libasr/asr_verify.cpp
@@ -340,7 +340,8 @@ public:
         for (auto &a : x.m_symtab->get_scope()) {
             this->visit_symbol(*a.second);
             if( ASR::is_a<ASR::ClassProcedure_t>(*a.second) ||
-                ASR::is_a<ASR::GenericProcedure_t>(*a.second) ) {
+                ASR::is_a<ASR::GenericProcedure_t>(*a.second) ||
+                ASR::is_a<ASR::CustomOperator_t>(*a.second) ) {
                 continue ;
             }
             ASR::ttype_t* var_type = ASRUtils::type_get_past_pointer(ASRUtils::symbol_type(a.second));
