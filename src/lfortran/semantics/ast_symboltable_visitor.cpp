326678a0cdaa3497e2c4d04734bf9ff44e481eb2
Thirumalai-Shaktivel <thirumalaishaktivel@gmail.com>
2023-01-02 20:30:46 +0530

    Append the generic_procedures that have the same interface name

--- src/lfortran/semantics/ast_symboltable_visitor.cpp
+++ src/lfortran/semantics/ast_symboltable_visitor.cpp
@@ -1033,7 +1033,12 @@ public:
             interface_name = generic_name;
             std::vector<std::string> proc_names;
             fill_interface_proc_names(x, proc_names);
-            generic_procedures[std::string(generic_name)] = proc_names;
+            if( generic_procedures.find(generic_name) != generic_procedures.end() ) {
+                generic_procedures[generic_name].insert(generic_procedures[generic_name].end(),
+                    proc_names.begin(), proc_names.end());
+            } else {
+                generic_procedures[generic_name] = proc_names;
+            }
             interface_name.clear();
         } else if (AST::is_a<AST::InterfaceHeader_t>(*x.m_header) ||
                    AST::is_a<AST::AbstractInterfaceHeader_t>(*x.m_header)) {
