8fea1bf012a4a3baa7cf1149e9c3a729c8ab154d
Pranav <85227306+Pranavchiku@users.noreply.github.com>
2023-01-05 22:25:17 +0530

    Add provision to avoid `character*(*)` in creation of `implicit_interface` (#1124)
    * Add check for type character in implicit_interface
    * Add test and register it in tests.toml

--- src/lfortran/semantics/ast_common_visitor.h
+++ src/lfortran/semantics/ast_common_visitor.h
@@ -3255,7 +3255,8 @@ public:
         }
         if (compiler_options.implicit_interface
                 && ASR::is_a<ASR::Variable_t>(*v)
-                && (!ASRUtils::is_array(ASRUtils::symbol_type(v)))) {
+                && (!ASRUtils::is_array(ASRUtils::symbol_type(v)))
+                && (!ASRUtils::is_character(*ASR::down_cast<ASR::Variable_t>(v)->m_type))) {
             // If implicit interface is allowed, we have to handle the
             // following case here:
             // real :: x
