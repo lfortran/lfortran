a38e30391548392eae2ab1206c4991f412951511
Pranavchiku <goswami.4@iitj.ac.in>
2023-01-11 00:00:10 +0530

    add and register test

---
+++ tests/errors/external_02.f90
@@ -0,0 +1,10 @@
+program main
+    real, external :: enorm
+    print *, enorm(1.0d0)
+end program
+
+double precision function enorm(n) result(y)
+    double precision, intent(in) :: n
+    y = n
+    return
+end function
