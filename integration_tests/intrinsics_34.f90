c6375bd6880725f4fbdc1d9ef985f8c323567790
Pranavchiku <goswami.4@iitj.ac.in>
2023-01-13 12:40:33 +0530

    update test

--- integration_tests/intrinsics_34.f90
+++ integration_tests/intrinsics_34.f90
@@ -1,6 +1,13 @@
 program intrinsics_34
+    integer, parameter :: dp=kind(0d0)
     real :: x = 3.143
-    real(8) :: y = 2.33
+    real(dp) :: y = 2.33
     print *, epsilon(x)
     print *, epsilon(y)
+    print *, epsilon(1._dp) ** 0.5 !Part of Minpack
+
+    ! Below numbers are corresponding output of gfortran and not magic numbers.
+    if (abs(epsilon(x) - 1.19209290E-07) > 1e-7) error stop
+    if (abs(epsilon(y) - 2.2204460492503131E-016) > 1e-15) error stop
+    if (abs((epsilon(1._dp) ** 0.5) - 1.4901161193847656E-008) > 1e-15) error stop
 end program
