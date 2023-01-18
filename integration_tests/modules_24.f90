c0377701cd9709c03a911b60a3ec61187993a68d
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2023-01-18 10:32:45 +0530

    Update tests

--- integration_tests/modules_24.f90
+++ integration_tests/modules_24.f90
@@ -11,5 +11,16 @@ module tomlf_de_tokenizer
        type(toml_table), pointer :: current => null()
     end type toml_tokenizer
 
+    contains
+
+    subroutine parse_select(de)
+
+       class(toml_tokenizer), intent(inout), target :: de
+       type(toml_table), pointer :: table
+
+       nullify(table)
+       de%current => table
+
+    end subroutine parse_select
 
 end module tomlf_de_tokenizer
