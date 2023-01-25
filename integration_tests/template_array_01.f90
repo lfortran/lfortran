9427bd2f8da961b3e7e97191d0c74eea1402d176
Lubis <luthfanlubis@gmail.com>
2023-01-26 00:41:55 +0900

    Updated tests

--- integration_tests/template_array_01.f90
+++ integration_tests/template_array_01.f90
@@ -15,22 +15,22 @@ module template_array_m
         function insert_t(lst, i) result(r)
             type(t), intent(in) :: lst(:), i
             type(t) :: r
-            !lst(1) = i
-            !r = lst(1)
+            lst(1) = i
+            r = lst(1)
         end function
     end template
 
 contains
 
-    !subroutine test_template()
-    !    instantiate array_tmpl(integer), only: insert_int => insert_t
-    !    integer :: a(1), i
-    !    a(1) = 0
-    !    i = 1
-    !    print *, a(1)
-    !    call insert_int(a, i)
-    !    print *, a(1)
-    !end subroutine
+    subroutine test_template()
+        instantiate array_tmpl(integer), only: insert_int => insert_t
+        integer :: a(1), i, r
+        a(1) = 0
+        i = 1
+        print *, a(1)
+        r = insert_int(a, i)
+        print *, a(1)
+    end subroutine
 
 end module
 
@@ -39,6 +39,6 @@ program template_array
     use template_array_m
     implicit none
 
-    !call test_template()
+    call test_template()
 
 end
