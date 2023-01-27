b22e01848b61cb3d7c8e3bbabfe304e42993947b
Pranavchiku <goswami.4@iitj.ac.in>
2023-01-27 16:22:00 +0530

    Refactor test and update test references

--- tests/save1.f90
+++ tests/save1.f90
@@ -1,4 +1,35 @@
+subroutine save_sub()
+    real :: x = 5, z
+    real, save :: y = 5
+end subroutine
+
+real function save_fun()
+    implicit none
+    real :: x = 5, z
+    real, save :: y = 5
+end function
+
+module save_module
+    implicit none
+    public
+    real :: x = 5, z
+    real, save :: y = 5
+    contains
+
+    subroutine sub_save()
+        real :: x = 5, z
+        real, save :: y = 5
+    end subroutine
+    
+    real function fun_save()
+        implicit none
+        real :: x = 5, z
+        real, save :: y = 5
+    end function
+
+end module
+
 program main
-    real :: x = 5
+    real :: x = 5, z
     real, save :: y = 5
 end program
