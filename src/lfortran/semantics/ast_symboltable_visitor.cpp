be165048bdee5cc8c12949db5302dca685ea769f
Thirumalai-Shaktivel <thirumalaishaktivel@gmail.com>
2023-02-10 11:51:32 +0530

    Allow importing DefinedOperators using use statement

--- src/lfortran/semantics/ast_symboltable_visitor.cpp
+++ src/lfortran/semantics/ast_symboltable_visitor.cpp
@@ -1691,6 +1691,11 @@ public:
                         remote_sym = intrinsic2str[op_type];
                         break;
                     }
+                    case AST::use_symbolType::DefinedOperator: {
+                        remote_sym = AST::down_cast<AST::DefinedOperator_t>(
+                            x.m_symbols[i])->m_opName;
+                        break;
+                    }
                     default:
                         throw SemanticError("Symbol with use not supported yet", x.base.base.loc);
                 }
