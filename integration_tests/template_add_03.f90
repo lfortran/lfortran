9477c3dc2f0e1e9ec9d3cfb28ca1bf2659b78635
Lubis <luthfanlubis@gmail.com>
2023-02-18 03:18:59 +0900

    Added and updated test

---
+++ integration_tests/template_add_03.f90
@@ -0,0 +1,45 @@
+module template_add_m
+    implicit none
+    private
+    public :: add_t
+
+    requirement r(t, f)
+        type :: t; end type
+        function f(x, y) result(z)
+            type(t), intent(in) :: x, y, a
+            type(t) :: z
+        end function
+    end requirement
+
+    template add_t(t, f)
+        requires r(t, f)
+        private
+        public :: add_generic
+    contains
+        function add_generic(x, y) result(z)
+            type(t), intent(in) :: x, y
+            type(t) :: z
+            z = f(x, y)
+        end function
+    end template
+
+contains
+
+    subroutine test_template()
+        instantiate add_t(real, operator(+)), only: add_real => add_generic
+        real :: x, y
+        x = 5.1
+        y = 7.2
+        print*, "The result is ", add_real(x, y)
+        if (abs(add_real(x, y) - 12.3) > 1e-5) error stop
+    end subroutine
+
+end module
+
+program template_add
+use template_add_m
+implicit none
+
+call test_template()
+
+end program template_add
