c5d3d1d134dc1905293abb23a354f3b7035f6ce7
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2023-02-22 15:47:56 +0530

    Enable and update tests

--- integration_tests/allocate_04.f90
+++ integration_tests/allocate_04.f90
@@ -5,5 +5,7 @@ implicit none
     character(len=:), allocatable :: string
     allocate(character(len=8)::string)
     allocate(c(3, 3, 3))
+    string = "lfortran"
+    if (string /= "lfortran") error stop
 
 end program
