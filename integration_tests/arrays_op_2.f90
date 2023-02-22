c8d2f52b9c4ecff46351f371a0632e781aabc7eb
Gagandeep Singh <gdp.1807@gmail.com>
2023-02-22 13:20:38 +0530

    Made integration_tests stronger

--- integration_tests/arrays_op_2.f90
+++ integration_tests/arrays_op_2.f90
@@ -21,14 +21,30 @@ print *, b(1, 1, 1), b(1, 2, 1), b(2, 1, 1), b(2, 2, 1)
 
 c = a + b
 print *, c(1, 1, 1), c(1, 2, 1), c(2, 1, 1), c(2, 2, 1)
+if( c(1, 1, 1) /= 2 ) error stop
+if( c(1, 2, 1) /= 5 ) error stop
+if( c(2, 1, 1) /= 5 ) error stop
+if( c(2, 2, 1) /= 8 ) error stop
 
 c = a - b
 print *, c(1, 1, 1), c(1, 2, 1), c(2, 1, 1), c(2, 2, 1)
+if( c(1, 1, 1) /= 0 ) error stop
+if( c(1, 2, 1) /= -3 ) error stop
+if( c(2, 1, 1) /= 3 ) error stop
+if( c(2, 2, 1) /= 0 ) error stop
 
 c = a*b
 print *, c(1, 1, 1), c(1, 2, 1), c(2, 1, 1), c(2, 2, 1)
+if( c(1, 1, 1) /= 1 ) error stop
+if( c(1, 2, 1) /= 4 ) error stop
+if( c(2, 1, 1) /= 4 ) error stop
+if( c(2, 2, 1) /= 16 ) error stop
 
 c = b/a
 print *, c(1, 1, 1), c(1, 2, 1), c(2, 1, 1), c(2, 2, 1)
+if( c(1, 1, 1) /= 1 ) error stop
+if( c(1, 2, 1) /= 4 ) error stop
+if( c(2, 1, 1) /= 0 ) error stop
+if( c(2, 2, 1) /= 1 ) error stop
 
 end program
