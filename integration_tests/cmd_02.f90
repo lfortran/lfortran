29972685a43d16008f15cf1796c9501d36e99b5e
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2023-02-22 14:15:57 +0530

    Enable and update tests

--- integration_tests/cmd_02.f90
+++ integration_tests/cmd_02.f90
@@ -1,8 +1,7 @@
 program test_exec
-    integer :: i
 
     character(len=255) :: homedir
-    call get_environment_variable("home", homedir)
+    call get_environment_variable("HOME", homedir)
     print *, trim(homedir)
 
     call execute_command_line("printenv")
