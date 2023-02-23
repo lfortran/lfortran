8f1e0adcfffaaea16bc76d58872e5c1d97849afa
Pranavchiku <goswami.4@iitj.ac.in>
2023-02-23 10:22:40 +0530

    update test

--- integration_tests/array_section_02.f90
+++ integration_tests/array_section_02.f90
@@ -8,9 +8,18 @@ subroutine vecfcn(n)
     real :: fvec(n)
 
     fvec(:n) = 0.0
+    if(fvec(1) /= 0.0) error stop
+    if(fvec(2) /= 0.0) error stop
+    if(fvec(10) /= 0.0) error stop
     fvec(2:) = 1.0
+    if (fvec(1) /= 0.0) error stop
+    if (fvec(2) /= 1.0) error stop
+    if (fvec(10) /= 1.0) error stop
     fvec(:) = 2.0
     print *, fvec
+    if (fvec(1) /= 2.0) error stop
+    if (fvec(2) /= 2.0) error stop
+    if (fvec(10) /= 2.0) error stop
 
 end subroutine vecfcn
 
