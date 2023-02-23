96d6eb5a1bd3f449c4017ea762baaa9df2251b36
Ondřej Čertík <ondrej@certik.us>
2023-02-22 22:21:04 -0700

    instantiate_LogGamma

--- src/libasr/pass/intrinsic_function.cpp
+++ src/libasr/pass/intrinsic_function.cpp
@@ -33,6 +33,20 @@ class ReplaceIntrinsicFunction: public ASR::BaseExprReplacer<ReplaceIntrinsicFun
     ReplaceIntrinsicFunction(Allocator& al_) : al(al_)
     {}
 
+    ASR::symbol_t* instantiate_LogGamma(ASR::ttype_t *arg_type) {
+        ASR::asr_t* new_subrout = nullptr; /*ASRUtils::make_Function_t_util(al, x->base.base.loc,
+                                new_symtab, s2c(al, new_name), x->m_dependencies, x->n_dependencies,
+                                new_args.p, new_args.size(),  new_body.p, new_body.size(),
+                                return_var, x_func_type->m_abi, x->m_access, x_func_type->m_deftype,
+                                s2c(al, new_bindc_name), x_func_type->m_elemental,
+                                x_func_type->m_pure, x_func_type->m_module, x_func_type->m_inline,
+                                x_func_type->m_static, nullptr, 0, nullptr, 0, false, false, false); */
+        ASR::symbol_t *new_symbol = ASR::down_cast<ASR::symbol_t>(new_subrout);
+        //current_scope->add_symbol(new_name, new_symbol);
+        return new_symbol;
+    };
+
+
     void replace_IntrinsicFunction(ASR::IntrinsicFunction_t* x) {
         switch (x->m_intrinsic_id) {
             case (static_cast<int64_t>(ASRUtils::IntrinsicFunctions::LogGamma)) : {
@@ -43,20 +57,18 @@ class ReplaceIntrinsicFunction: public ASR::BaseExprReplacer<ReplaceIntrinsicFun
                 replace_expr(x->m_args[0]);
                 ASR::expr_t *arg = *current_expr; // Use the converted arg
                 current_expr = current_expr_copy_;
-                // TODO: here we must get access to the pure ASR implementation
-                // of LogGamma, as provided by the frontend, let's say
-                // it is assigned to this symbol:
-                ASR::symbol_t* new_func_sym = nullptr;
+                ASR::symbol_t* new_func_sym = instantiate_LogGamma(ASRUtils::expr_type(x->m_args[0]));
 
                 Vec<ASR::call_arg_t> new_args;
                 new_args.reserve(al, x->n_args);
+                ASR::call_arg_t arg0;
+                arg0.m_value = arg;
+                new_args.push_back(al, arg0);
                 ASR::expr_t* new_call = ASRUtils::EXPR(ASR::make_FunctionCall_t(al,
                                             x->base.base.loc, new_func_sym, new_func_sym,
-                                            new_args.p, new_args.size(), x->m_type, nullptr,
+                                            new_args.p, new_args.size(), ASRUtils::symbol_type(new_func_sym), nullptr,
                                             nullptr));
 
-                // For now we will just return the argument directly:
-                new_call = arg;
                 *current_expr = new_call;
                 break;
             }
