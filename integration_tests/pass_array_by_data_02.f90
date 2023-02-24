efcb51a50d2f2d0cc21212b42d5bdf95a9e6aac2
Pranavchiku <goswami.4@iitj.ac.in>
2023-02-24 21:27:57 +0530

    add test

---
+++ integration_tests/pass_array_by_data_02.f90
@@ -0,0 +1,13 @@
+program test_hybrd
+
+    implicit none
+
+    contains
+
+    subroutine compare_solutions(x)
+
+    implicit none
+    real, dimension(:), intent(in) :: x 
+    real, dimension(:), allocatable :: temp
+    end subroutine compare_solutions
+end program
