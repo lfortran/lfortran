cc83fb147a5dfbb183dfb24dc1830bafa2069484
Pranavchiku <goswami.4@iitj.ac.in>
2023-03-15 21:19:02 +0530

    update test to check real and imaginary values

--- integration_tests/complex_11.f90
+++ integration_tests/complex_11.f90
@@ -1,5 +1,9 @@
 SUBROUTINE CPDSA()
-    print *, DCMPLX(1.0D0, 6.0D0)
+    COMPLEX :: C1
+    C1 = DCMPLX(1.0D0, 6.0D0)
+    print *, C1
+    IF (REAL(C1) /= 1.0D0) ERROR STOP
+    IF (AIMAG(C1) /= 6.0D0) ERROR STOP
 ENDSUBROUTINE
 
 program main
