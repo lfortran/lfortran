fbd82c3f3946334fd630d56fce125f1f59d575bc
Thirumalai-Shaktivel <thirumalaishaktivel@gmail.com>
2023-04-10 19:46:24 +0530

    [CPP] Register intrinsics_02 (sin) in tests.toml

--- integration_tests/intrinsics_02.f90
+++ integration_tests/intrinsics_02.f90
@@ -5,8 +5,8 @@ real(8) :: y
 y = sin(1.5)
 print *, x, y
 
-if (x /= 0.997494996) error stop
-if (sin(x) /= 0.840114892) error stop
+if (abs(x - 0.997494996) > 1.e-6) error stop
+if (abs(sin(x) - 0.840114892) > 1.e-6) error stop
 if (abs(y - 0.997494995) > 1e-7) error stop
 if (abs(sin(sin(1.5) + sin(0.5+sin(0.5))) - 0.967188418) > 1e-7) error stop
 if (abs(sin(sin(y) + sin(x+sin(x))) - 0.97276188) > 1e-7) error stop
