4a9e8244a623186d88c921d31042516a6b44e18e
Gagandeep Singh <gdp.1807@gmail.com>
2023-04-26 13:12:43 +0530

    Do not add external symbol twice

---
+++ integration_tests/modules_44_module.f90
@@ -0,0 +1,10 @@
+module modules_44_module_fpm_filesystem
+
+contains
+
+    subroutine warnwrite(fname, data)
+    character(len=*),intent(in) :: fname
+    character(len=*),intent(in) :: data(:)
+    end subroutine warnwrite
+
+end module modules_44_module_fpm_filesystem
