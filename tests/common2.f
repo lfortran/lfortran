9af5a5787e26d183c31505f2f582d404c8304db1
Pranavchiku <goswami.4@iitj.ac.in>
2023-05-17 11:59:42 +0530

    add and register test

---
+++ tests/common2.f
@@ -0,0 +1,5 @@
+        subroutine f()
+            integer :: a, b, c, d, e, g, h, i, j, k
+            common / block_1 / a, b, h / block_2 / c, d, / block_3 / e, g
+            common / block_4 / i, / block_5 / j, k
+        end subroutine
