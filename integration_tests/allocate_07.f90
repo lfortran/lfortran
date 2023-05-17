94d67962c484768b078216b8c6734be4c9a9398a
Gagandeep Singh <gdp.1807@gmail.com>
2023-05-17 11:59:22 +0530

    Updated reference tests and registered integration tests

--- integration_tests/allocate_07.f90
+++ integration_tests/allocate_07.f90
@@ -7,13 +7,16 @@ program main
 
     subroutine compare_solutions()
         real, dimension(:), allocatable :: temp
+        integer :: i
         temp = solution()
         print *, temp
+        do i = lbound(temp, 1), ubound(temp, 1)
+            if (abs(temp(i) * i - 1.0) > 1e-8) error stop
+        end do
     end subroutine compare_solutions
 
     pure function solution() result(x)
-        real,dimension(:),allocatable :: x
-        x = [0.10,0.10,1., .1, .23]
-        print *, x
+        real, dimension(:), allocatable :: x
+        x = [1.0, 0.5, 1.0/3.0, 0.25, 0.2]
     end function solution
 end program main
