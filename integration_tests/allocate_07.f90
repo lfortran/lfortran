14fda53f887c1b035b7e29fe2c430d04c5852319
Gagandeep Singh <gdp.1807@gmail.com>
2023-05-17 11:50:32 +0530

    Use allocatable as storageType for temp_var if result_var is allocatable
    Co-authored-by: Thirumalai Shaktivel <74826228+Thirumalai-Shaktivel@users.noreply.github.com>

---
+++ integration_tests/allocate_07.f90
@@ -0,0 +1,19 @@
+program main
+    implicit none
+
+    call compare_solutions()
+
+    contains
+
+    subroutine compare_solutions()
+        real, dimension(:), allocatable :: temp
+        temp = solution()
+        print *, temp
+    end subroutine compare_solutions
+
+    pure function solution() result(x)
+        real,dimension(:),allocatable :: x
+        x = [0.10,0.10,1., .1, .23]
+        print *, x
+    end function solution
+end program main
