d282ce82790d822fc0022a93c0e7aee94a3b4d05
Gagandeep Singh <gdp.1807@gmail.com>
2023-05-18 16:18:31 +0530

    Updated tests

--- integration_tests/modules_42.f90
+++ integration_tests/modules_42.f90
@@ -1,4 +1,4 @@
-module fpm_targets
+module modules_42_fpm_targets
 implicit none
 
 type int_t
@@ -10,14 +10,26 @@ contains
 subroutine prune_build_targets()
     type(int_t) :: modules_used(1)
 
+    modules_used(1)%i = 0
+    call collect_used_modules()
+    print *, modules_used(1)%i
+
     contains
 
     recursive subroutine collect_used_modules()
 
-        modules_used = [modules_used]
+        modules_used = [modules_used(1)]
 
     end subroutine collect_used_modules
 
 end subroutine prune_build_targets
 
-end module fpm_targets
+end module modules_42_fpm_targets
+
+program modules_42
+use modules_42_fpm_targets
+implicit none
+
+call prune_build_targets()
+
+end program modules_42
