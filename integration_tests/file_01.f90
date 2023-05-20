9b06321edbdbc10ed6d5c58997c27e8e5ea7f756
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2023-05-20 15:40:49 +0530

    Add tests

--- integration_tests/file_01.f90
+++ integration_tests/file_01.f90
@@ -1,9 +1,11 @@
 program fileops
     implicit none
     integer :: j, i
+    character(len=1) :: c
     j=11
     open(j, file="../file_01_data.txt")
     read(j, *) i
+    read(j, *) c
     close(j)
 
 end program fileops
