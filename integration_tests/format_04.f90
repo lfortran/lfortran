bf51c43040faad5d70fadafc3c6e43f00b3d321a
gptsarthak <sarthakgpt95@gmail.com>
2023-06-17 14:17:36 +0530

    Add test for integers and repeat count

--- integration_tests/format_04.f90
+++ integration_tests/format_04.f90
@@ -2,4 +2,6 @@ program format_04
 print *, "ok", "b"
 print '(a,a)', "ok", "b"
 print '("Success!",/,10X,A6,"World!")',"Hello 123"
+print '(4a4)',"dancing","in","the","moonlight"
+print '(i3,i10.5,/i6.6,2x,i3)' , 123,456,12345,6789
 end program
