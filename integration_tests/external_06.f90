43b5895fe3101edf012739f28574f3e91f10c9f5
Ondřej Čertík <ondrej@certik.us>
2023-06-20 15:27:45 -0600

    Use both original and new test

--- integration_tests/external_06.f90
+++ integration_tests/external_06.f90
@@ -1,10 +1,10 @@
-program main
-    double precision, external :: enorm
-    print *, enorm(1.0d0)
-end program
-
 double precision function enorm(n) result(y)
     double precision, intent(in) :: n
     y = n
     return
 end function
+
+program main
+    double precision, external :: enorm
+    print *, enorm(1.0d0)
+end program
