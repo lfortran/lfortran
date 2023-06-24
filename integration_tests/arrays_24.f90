f6bfcda541dc4bb3268d39d3686d0e14c84457b6
HarshitaKalani <kalani.1@iitj.ac.in>
2023-06-24 14:23:42 +0530

    applied suggestion from code review

--- integration_tests/arrays_24.f90
+++ integration_tests/arrays_24.f90
@@ -1,18 +1,23 @@
 program arrays_24
+    implicit none
 
-real :: x(1), y(1), z(1)
+    real :: x(1), y(1), z(1)
+    x = [5.15]
+    y = [-3.20]
 
-z = f(x, y)
+    z = f(x, y)
+    print *, z
 
-contains
+    if (abs(z(1) - 1.95) > 1e-5) error stop
 
-function f(a, b) result(r)
-real, intent(in) :: a(:), b(:)
-real :: r(size(a))
-integer :: i
-do i = 1, size(a)
-    r(i) = a(i) + b(i)
-end do
-end function
+    contains
 
+    function f(a, b) result(r)
+        real, intent(in) :: a(:), b(:)
+        real :: r(size(a))
+        integer :: i
+        do i = 1, size(a)
+            r(i) = a(i) + b(i)
+        end do
+    end function
 end program
