1c562e3b1a3c27e492953d70e2c2cfc42ebacb38
HarshitaKalani <kalani.1@iitj.ac.in>
2023-07-01 13:58:06 +0530

    corrected the indentation

--- tests/errors/subroutine3.f90
+++ tests/errors/subroutine3.f90
@@ -1,11 +1,7 @@
 program sizef
-
-    call f()
-    
-    contains
-    
-        subroutine f()
-        print *, size(f)
-        end subroutine
-    
-    end program
+call f()
+contains
+    subroutine f()
+    print *, size(f)
+    end subroutine
+end program
