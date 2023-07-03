b3298bbf4adfc26381c756bf71b0019e836bf42f
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2023-07-03 09:17:54 +0530

    Add a test

--- integration_tests/file_01.f90
+++ integration_tests/file_01.f90
@@ -2,10 +2,12 @@ program fileops
     implicit none
     integer :: j, i
     character(len=1) :: c
+    real :: z
     j=11
     open(j, file="../file_01_data.txt")
     read(j, *) i
     read(j, *) c
+    read(j, *) z
     rewind(j)
     close(j)
 
