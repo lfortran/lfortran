3317bda10b699257fca74228385fa5986d20750c
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2023-07-04 11:13:39 +0530

    Enable tests

--- integration_tests/arrays_25.f90
+++ integration_tests/arrays_25.f90
@@ -1,22 +1,25 @@
 program arrays_25
-implicit none
-type :: model_t
-    integer, allocatable :: decoder_idx(:)
-end type
-type(model_t) :: m
-allocate(m%decoder_idx(10))
-m%decoder_idx(:) = 0
-m%decoder_idx(5) = 5
-print *, m%decoder_idx
-! Does not compile yet:
-!call decode(m%decoder_idx)
+    implicit none
+    type :: model_t
+        integer, allocatable :: decoder_idx(:)
+    end type
 
-contains
+    type(model_t) :: m
 
-    subroutine decode(idx)
-    integer, intent(in) :: idx(:)
-    if (idx(4) /= 0) error stop
-    if (idx(5) /= 5) error stop
-    end subroutine
+    allocate(m%decoder_idx(10))
+
+    m%decoder_idx(:) = 0
+    m%decoder_idx(5) = 5
+
+    print *, m%decoder_idx
+    call decode(m%decoder_idx)
+
+    contains
+
+        subroutine decode(idx)
+        integer, intent(in) :: idx(:)
+        if (idx(4) /= 0) error stop
+        if (idx(5) /= 5) error stop
+        end subroutine
 
 end program
