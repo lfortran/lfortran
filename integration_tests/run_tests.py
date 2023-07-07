56f58df0827af1468847403f2deb798cdf03ab1c
Pranav Goswami <goswami.4@iitj.ac.in>
2023-07-07 11:34:40 +0530

    set llvmImplicitArgument flag in integration_tests

--- integration_tests/run_tests.py
+++ integration_tests/run_tests.py
@@ -7,7 +7,7 @@ import os
 # Initialization
 NO_OF_THREADS = 8 # default no of threads is 8
 SUPPORTED_BACKENDS = ['llvm', 'llvm2', 'llvm_rtlib', 'c',
-                      'cpp', 'x86', 'wasm', 'gfortran', 'llvmImplicit']
+                      'cpp', 'x86', 'wasm', 'gfortran', 'llvmImplicit', 'llvmImplicitArgument']
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 LFORTRAN_PATH = f"{BASE_DIR}/../src/bin:$PATH"
 
