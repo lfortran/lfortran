58a2e29387a8bc255259407d9efb2bc20d2a5d71
Shaikh Ubaid <shaikhubaid769@gmail.com>
2023-07-22 04:07:46 +0530

    TEST: Fix test and add print, assert
    This test works when running from integration_tests directory.

--- integration_tests/file_01.f90
+++ integration_tests/file_01.f90
@@ -4,11 +4,16 @@ program fileops
     character(len=1) :: c
     real :: z
     j=11
-    open(j, file="../file_01_data.txt")
+    open(j, file="file_01_data.txt")
     read(j, *) i
     read(j, *) c
     read(j, *) z
     rewind(j)
     close(j)
+
+    print *, i, c, z
+    if (i /= 10130) error stop
+    if (c /= "c") error stop
+    if (abs(z - 4.20) > 1e-6) error stop
 
 end program fileops
