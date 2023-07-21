d12bee9bfec15bb5d5c4384844fb8618163743b9
Ondřej Čertík <ondrej@certik.us>
2023-07-21 15:55:39 -0600

    Initial support for f8.3 format
    The formatting of f8.3 is not correct, but at least it prints the right
    number now.

--- integration_tests/format_05.f90
+++ integration_tests/format_05.f90
@@ -7,7 +7,7 @@ b = 123.45678
 c = 12.34
 d = 123.45
 
-print '(-1pe10.2,-2pe15.6,1pe010.2,2x,1pe9.2)', -a, b, -c, d
+print '(-1pe10.2,-2pe15.6,1pe010.2,2x,1pe9.2,f10.3)', -a, b, -c, d, d
 write (s, '(-1pe10.2,-2pe15.6,1pe010.2,2x,1pe9.2)') -a, b, -c, d
 if (trim(s) /= " -0.01E+04   0.001235E+05 -1.23E+01   1.23E+02") error stop
 end program
