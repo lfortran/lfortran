403cbdd9bc2f4f2a2b47db5bc2a5e0e8f7a36c02
Gagandeep Singh <gdp.1807@gmail.com>
2023-07-25 11:56:52 +0530

    Added test for associating array slices

---
+++ integration_tests/associate_11.f90
@@ -0,0 +1,12 @@
+program associate_11
+    implicit none
+    integer :: x(2)
+    integer :: i, j
+    x = 123
+    associate(a => x(:))
+        a(1) = 124
+    end associate
+    print *, x
+    if( x(1) /= 124 ) error stop
+    if( x(2) /= 123 ) error stop
+end program test
