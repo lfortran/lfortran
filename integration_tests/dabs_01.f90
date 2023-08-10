0ee08dc9a7baa1307e37ef9fb10b7a40b461b95e
Ondřej Čertík <ondrej@certik.us>
2023-08-10 13:43:53 -0600

    Fix the test

--- integration_tests/dabs_01.f90
+++ integration_tests/dabs_01.f90
@@ -1,8 +1,8 @@
 subroutine e1z(z)
    implicit complex*16 (c,z)
    implicit double precision (a,d-h,o-y)
-   if (abs(dabs(dimag(z)) - 0.0000000000000000) > 1e-10) error stop
-   if (abs(dabs(0.0d0)) - 0.0000000000000000 > 1e-10) error stop
+   if (abs(dabs(dimag(z))) > 1d-10) error stop
+   if (abs(dabs(0.0d0)) > 1d-10) error stop
    print *, "dimag(z) =", dimag(z)
    print *, "dabs(dimag(z)) =", dabs(dimag(z))
    print *, "dabs(0.0) = ", dabs(0.0d0)
