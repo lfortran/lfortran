4920e5b31f0c127cd646052c821074a85e3f30fe
HarshitaKalani <kalani.1@iitj.ac.in>
2023-08-13 12:29:09 +0530

    test for min for 3 args

---
+++ integration_tests/min_three_args.f90
@@ -0,0 +1,17 @@
+module mintest
+    implicit none
+contains 
+    subroutine take_min(arr)   
+        real(8), intent(out) :: arr(:,:)  
+        real(8) :: tmp1, tmp2  
+        tmp1 = huge(1.d0)
+        tmp2 = huge(1.d0)  
+        arr(:,1) = min( 0.d0,tmp1,tmp2 )   
+
+        if (arr(1, 1) /= 0.d0) error stop
+        if (size(arr) /= 2) error stop
+        
+    end subroutine take_min
+end module mintest
+program name
+end program name
