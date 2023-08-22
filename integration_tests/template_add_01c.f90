7e9d32dbf8f31c32d88e26d43a45f8f7b66ceec9
Lubis <luthfanlubis@gmail.com>
2023-08-22 18:56:50 +0900

    Add an example with an instantiate statement without specifying names

---
+++ integration_tests/template_add_01c.f90
@@ -0,0 +1,47 @@
+module template_add_01b_m
+    implicit none
+    private
+    public :: add_t
+
+    requirement R(T, F)
+        type :: T; end type
+        function F(x, y) result(z)
+            type(T), intent(in) :: x, y
+            type(T) :: z
+        end function
+    end requirement
+
+    template add_t(T, F)
+        requires R(T, F)
+        private
+        public :: add_generic
+    contains
+        function add_generic(x, y) result(z)
+            type(T), intent(in) :: x, y
+            type(T) :: z
+            z = F(x, y)
+        end function
+    end template
+
+contains
+
+  integer function func_arg_int(x, y) result(z)
+      integer, intent(in) :: x, y
+      z = x + y
+  end function
+
+  subroutine test_template()
+      integer :: n
+      instantiate add_t(integer, func_arg_int)
+      n = add_generic(5, 9)
+      print*, "The result is", n
+  end subroutine
+end module
+
+program template_add_01b
+use template_add_01b_m
+implicit none
+
+call test_template()
+
+end program
