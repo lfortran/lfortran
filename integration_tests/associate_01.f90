7cc1897f897efd31113ab1891283321475186e84
Thirumalai-Shaktivel <thirumalaishaktivel@gmail.com>
2023-08-22 15:37:37 +0530

    Add a test and update refs.

--- integration_tests/associate_01.f90
+++ integration_tests/associate_01.f90
@@ -2,19 +2,23 @@ program associate_01
 implicit none
 real, allocatable :: a(:), b(:,:), c(:,:,:)
 real, pointer :: x(:), y(:,:,:)
-real :: a_1, c_234
+real :: a_1, c_234, c_121
 integer :: n
 a_1 = 0
 n = 10
 allocate(a(5))
 allocate(b(n,n), c(n, 5, n))
-1 loop: associate (x => a, y => c)
+1 loop: associate (x => a, y => c, z => c(:, 2, :))
     x(1) = a_1 + 5
     y(2,3,4) = 3
+    z(1, 1) = 17
+    if (size(z) /= 100) error stop
 end associate loop
 a_1 = a(1)
 if(a(1).EQ.5) GO TO 1
 c_234 = c(2,3,4)
-print *, a_1
-print *, c_234
-end
+c_121 = c(1,2,1)
+if (a_1   /= 10.) error stop
+if (c_234 /= 3.) error stop
+if (c_121 /= 17.) error stop
+end program associate_01
