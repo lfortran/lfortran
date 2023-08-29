a32b1811fc721545ab551266afb6e839a9453e01
Gagandeep Singh <gdp.1807@gmail.com>
2023-08-29 20:37:00 +0530

    wip

---
+++ integration_tests/arrays_op_20.f90
@@ -0,0 +1,20 @@
+program arrays_op_20
+implicit none
+
+integer :: m
+real, allocatable :: array(:, :), arrayoutput(:, :)
+m = 3
+allocate(array(3, 3))
+arrayoutput = f(5, array)
+print *, size(arrayoutput)
+if( size(arrayoutput) /= 24 ) error stop
+
+contains
+
+function f(m, input) result(output)
+integer :: m
+real :: input(m)
+real :: output(2:m, m:2*m)
+end function
+
+end program
