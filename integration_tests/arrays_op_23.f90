4351dc9f95e15087c702083a4306a38ab6c0ff34
Gagandeep Singh <gdp.1807@gmail.com>
2023-09-02 23:39:40 +0530

    Add test for BinOp as argument to function call

---
+++ integration_tests/arrays_op_23.f90
@@ -0,0 +1,34 @@
+program arrays_op_23
+implicit none
+
+real :: array(10, 10), output(10, 10)
+array = 3.0
+
+output = attention(10, 10, 10, array)
+print *, output
+if( any(output - 8.52982235 > 1e-6) ) error stop
+
+contains
+
+function attention(l, n, m, mask) result(y)
+integer, intent(in) :: l, n, m
+real, intent(in) :: mask(n, m)
+real :: y(l, m)
+real :: tmp(n, m)
+tmp = 4.0
+call copy_array(twice(tmp / sqrt(real(l, 4)) + mask), y)
+end function
+
+function twice(x) result(y)
+real, intent(in) :: x(:, :)
+real :: y(size(x, 1), size(x, 2))
+y = 2*x
+end function
+
+subroutine copy_array(src, dest)
+real, intent(in) :: src(:, :)
+real, intent(out) :: dest(:, :)
+dest = src
+end subroutine
+
+end program
