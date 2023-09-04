7cf92732464808ef8d6728e484a8b1678bd8ce08
Thirumalai-Shaktivel <thirumalaishaktivel@gmail.com>
2023-09-04 16:47:58 +0530

    Update tests

--- integration_tests/optional_03.f90
+++ integration_tests/optional_03.f90
@@ -1,25 +1,35 @@
-module optional_03_module
+module optional_03_module_01
     implicit none
     type :: string
         character(:), allocatable :: s
     end type
-end module optional_03_module
+contains
+    subroutine sub_01(x)
+        type(string), optional, intent(in) :: x(:)
+        if ( present(x) ) then
+            if ( x(1)%s /= "12345" ) error stop
+        end if
+    end subroutine
+end module optional_03_module_01
+
+module optional_03_module_02
+    use optional_03_module_01, only: sub_01
+    implicit none
+contains
+    subroutine sub_02()
+        call sub_01()
+    end subroutine sub_02
+
+end module optional_03_module_02
 
 program optional_03
-    use optional_03_module
+    use optional_03_module_01
+    use optional_03_module_02, only: sub_02
     implicit none
     type(string) :: x(1)
     allocate(character(5) :: x(1)%s)
     x(1)%s = "12345"
 
-    call f(x)
-    call f()
-
-contains
-    subroutine f(x)
-    type(string), optional, intent(in) :: x(:)
-    if ( present(x) ) then
-        if ( x(1)%s /= "12345" ) error stop
-    end if
-    end subroutine
+    call sub_01(x)
+    call sub_02()
 end program
