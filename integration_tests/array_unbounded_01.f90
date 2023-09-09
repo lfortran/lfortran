9805ff9c659d2a6f117c7c81eae1514dfc2fbab6
Pranavchiku <goswami.4@iitj.ac.in>
2023-09-09 14:28:44 +0530

    add and register tests

---
+++ integration_tests/array_unbounded_01.f90
@@ -0,0 +1,12 @@
+subroutine sub(x, z)
+    real :: x(1:*), z(10)
+    x(1) = 4.29
+    print *, x(1)
+end subroutine
+    
+program main
+    real :: y(10), w(10)
+    call sub(y, w)
+    print *, y(1)
+    if (abs(y(1) - 4.29) > 1e-10) error stop
+end program
