ede65fdd3891b5e3b0ae8aa67a9593401e754a0e
Gagandeep Singh <gdp.1807@gmail.com>
2023-09-19 16:08:25 +0530

    TEST: Add test for passing expressions containing array slice to intrinsics

---
+++ integration_tests/intrinsics_75.f90
@@ -0,0 +1,11 @@
+program intrinsics_74
+implicit none
+
+real(8) :: lambda = 1.123
+real(8) :: x(10)
+integer :: i_max = 2
+x = 23.
+print *, exp(-lambda * (x(i_max:i_max+4)-x(1)))
+if( any(exp(-lambda * (x(i_max:i_max+4)-x(1))) /= 1.0) ) error stop
+
+end program
