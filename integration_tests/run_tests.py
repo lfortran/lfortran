a68c99b058b3466e723584dd686204c394799225
Thirumalai Shaktivel <thirumalaishaktivel@gmail.com>
2023-09-22 15:57:04 +0530

    Set `-DCMAKE_Fortran_FLAGS` to use `-g` option for Fortran backend

--- integration_tests/run_tests.py
+++ integration_tests/run_tests.py
@@ -30,6 +30,10 @@ def run_test(backend):
     elif backend == "cpp":
         run_cmd(f"FC=lfortran FFLAGS=\"--openmp\" cmake -DLFORTRAN_BACKEND={backend} -DFAST={fast_tests}" + common,
                 cwd=cwd)
+    elif backend == "fortran":
+        run_cmd(f"FC=lfortran cmake -DLFORTRAN_BACKEND={backend} "
+            "-DFAST={fast_tests} -DCMAKE_Fortran_FLAGS=\"-g\"" + common,
+                cwd=cwd)
     else:
         run_cmd(f"FC=lfortran cmake -DLFORTRAN_BACKEND={backend} -DFAST={fast_tests}" + common,
                 cwd=cwd)
