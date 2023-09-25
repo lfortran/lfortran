ac54906bdeefe448fca0676ac65403d261fe1aa6
Thirumalai Shaktivel <thirumalaishaktivel@gmail.com>
2023-09-25 15:32:51 +0530

    [LLVM] Support pointer types in read function

--- src/libasr/codegen/asr_to_llvm.cpp
+++ src/libasr/codegen/asr_to_llvm.cpp
@@ -6806,7 +6806,8 @@ public:
     }
 
     llvm::Function* get_read_function(ASR::ttype_t *type) {
-        type = ASRUtils::type_get_past_allocatable(type);
+        type = ASRUtils::type_get_past_allocatable(
+            ASRUtils::type_get_past_pointer(type));
         llvm::Function *fn = nullptr;
         switch (type->type) {
             case (ASR::ttypeType::Integer): {
@@ -6989,11 +6990,13 @@ public:
                 ASR::ttype_t* type = ASRUtils::expr_type(x.m_values[i]);
                 llvm::Function *fn = get_read_function(type);
                 if (ASRUtils::is_array(type)) {
-                    if (ASR::is_a<ASR::Allocatable_t>(*type)) {
+                    if (ASR::is_a<ASR::Allocatable_t>(*type)
+                        || ASR::is_a<ASR::Pointer_t>(*type)) {
                         tmp = CreateLoad(tmp);
                     }
                     tmp = arr_descr->get_pointer_to_data(tmp);
-                    if (ASR::is_a<ASR::Allocatable_t>(*type)) {
+                    if (ASR::is_a<ASR::Allocatable_t>(*type)
+                        || ASR::is_a<ASR::Pointer_t>(*type)) {
                         tmp = CreateLoad(tmp);
                     }
                     llvm::Value *arr = tmp;
