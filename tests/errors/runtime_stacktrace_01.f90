75cf64fd31080298b4cc46cb230f9a252d63ef91
Thirumalai Shaktivel <thirumalaishaktivel@gmail.com>
2023-09-29 21:53:27 +0530

    Test: Add a test for runtime stacktrace

---
+++ tests/errors/runtime_stacktrace_01.f90
@@ -0,0 +1,22 @@
+program expr2
+    implicit none
+    print *, main()
+
+contains
+
+    function main()
+        integer :: main
+        main = 1
+        print *, g()
+    end function main
+
+    subroutine f()
+        stop
+    end subroutine f
+
+    function g()
+        integer :: g
+        g = 1
+        call f()
+    end function g
+end program expr2
