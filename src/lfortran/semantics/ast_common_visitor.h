371ad4ca1329c9730105e1d01416dd94352de1df
arteevraina <arteevraina@gmail.com>
2023-10-07 21:01:10 +0530

    dev: check for asr_owner not equal to nullptr

--- src/lfortran/semantics/ast_common_visitor.h
+++ src/lfortran/semantics/ast_common_visitor.h
@@ -34,7 +34,7 @@ uint64_t static inline get_hash(ASR::asr_t *node)
 #define LFORTRAN_STMT_LABEL_TYPE(x) \
         case AST::stmtType::x: { return AST::down_cast<AST::x##_t>(f)->m_label; }
 
-#define ADD_ASR_DEPENDENCIES(current_scope, final_sym, current_function_dependencies) while(current_scope != nullptr) { \
+#define ADD_ASR_DEPENDENCIES(current_scope, final_sym, current_function_dependencies) while(current_scope->asr_owner != nullptr) { \
         if (ASR::is_a<ASR::AssociateBlock_t>(*ASR::down_cast<ASR::symbol_t>(current_scope->asr_owner)) || \
             ASR::is_a<ASR::Block_t>(*ASR::down_cast<ASR::symbol_t>(current_scope->asr_owner))) { \
                 current_scope = current_scope->parent; \
@@ -46,7 +46,7 @@ uint64_t static inline get_hash(ASR::asr_t *node)
             } \
     } \
 
-#define ADD_ASR_DEPENDENCIES_WITH_NAME(current_scope, final_sym, current_function_dependencies, dep_name) while(current_scope != nullptr) { \
+#define ADD_ASR_DEPENDENCIES_WITH_NAME(current_scope, final_sym, current_function_dependencies, dep_name) while(current_scope->asr_owner != nullptr) { \
         if (ASR::is_a<ASR::AssociateBlock_t>(*ASR::down_cast<ASR::symbol_t>(current_scope->asr_owner)) || \
             ASR::is_a<ASR::Block_t>(*ASR::down_cast<ASR::symbol_t>(current_scope->asr_owner))) { \
                 current_scope = current_scope->parent; \
