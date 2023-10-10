c9959d0eefd25b332e6faa68e04822e8689c770e
Shaikh Ubaid <shaikhubaid769@gmail.com>
2023-10-11 02:33:22 +0530

    CI: Support uploading tarball to release

---
+++ ci/upload_tarball_to_release.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+cd ./dist
+
+lfortran_version=$(<version)
+
+RELEASE_ID=$(\
+curl -s 'https://api.github.com/repos/lfortran/lfortran/releases/latest' | \
+python -c "import sys, json; print(json.load(sys.stdin)['id'], end='')")
+
+echo "RELEASE_ID=$RELEASE_ID"
+
+curl -L \
+-X POST \
+-H "Accept: application/vnd.github+json" \
+-H "Authorization: Bearer $GITHUB_TOKEN" \
+-H "X-GitHub-Api-Version: 2022-11-28" \
+-H "Content-Type: application/octet-stream" \
+"https://uploads.github.com/repos/lfortran/lfortran/releases/$RELEASE_ID/assets?name=lfortran-${lfortran_version}.tar.gz" \
+--data-binary "@lfortran-${lfortran_version}.tar.gz"
