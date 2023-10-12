5fc07822fbfbdcc7b774a156809a9868fd39f0b5
Khushi Agrawal <khushiagrawal411@gmail.com>
2023-10-12 18:01:52 +0530

    minor

--- integration_tests/arrays_reshape_15.f90
+++ integration_tests/arrays_reshape_15.f90
@@ -3,7 +3,7 @@ implicit none
 real, dimension(6) :: arr = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0]
 real :: s(2, 3)
 integer :: i, j
-real, parameter :: eps = 1.0
+real, parameter :: eps = 1e-5
 
 s = reshape(arr, shape(s))
 
