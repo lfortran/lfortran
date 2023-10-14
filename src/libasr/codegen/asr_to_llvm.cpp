c2163755ec42dae67daaeffe6d42fac38f4db103
Shaikh Ubaid <shaikhubaid769@gmail.com>
2023-10-14 14:41:50 +0530

    LLVM: Remove redundant code

--- src/libasr/codegen/asr_to_llvm.cpp
+++ src/libasr/codegen/asr_to_llvm.cpp
@@ -7578,17 +7578,9 @@ public:
     template <typename T>
     std::vector<llvm::Value*> convert_call_args(const T &x, bool is_method) {
         std::vector<llvm::Value *> args;
-        const ASR::symbol_t* func_subrout = symbol_get_past_external(x.m_name);
-        ASR::abiType x_abi = ASR::abiType::Source;
-        if( is_a<ASR::Function_t>(*func_subrout) ) {
-            ASR::Function_t* func = down_cast<ASR::Function_t>(func_subrout);
-            x_abi = ASRUtils::get_FunctionType(func)->m_abi;
-        }
-
         for (size_t i=0; i<x.n_args; i++) {
-
-            func_subrout = symbol_get_past_external(x.m_name);
-            x_abi = (ASR::abiType) 0;
+            ASR::symbol_t* func_subrout = symbol_get_past_external(x.m_name);
+            ASR::abiType x_abi = (ASR::abiType) 0;
             ASR::intentType orig_arg_intent = ASR::intentType::Unspecified;
             std::uint32_t m_h;
             ASR::Variable_t *orig_arg = nullptr;
