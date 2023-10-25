4fdcbc72867300c77cd3bd7bd3741c7cb96dc3ed
Ondřej Čertík <ondrej@certik.us>
2023-10-25 13:09:22 -0600

    equivalence_08: Test that equivalence works

--- integration_tests/equivalence_08.f90
+++ integration_tests/equivalence_08.f90
@@ -2,6 +2,9 @@ double precision function d1mach()
 integer small(2)
 double precision dmach(5)
 equivalence (dmach(1),small(1))
+dmach = 1.3d0
+if (small(1) /= -858993459) error stop
+if (small(2) /= 1073007820) error stop
 d1mach = 1.3d0
 end function
 
