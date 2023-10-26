9455109f2e3eaf8ab8ca2e72e20561f1c675d110
Pranavchiku <goswami.4@iitj.ac.in>
2023-10-26 22:51:13 +0530

    add compile time tests for selected_int_kind

--- integration_tests/selected_int_kind_01.f90
+++ integration_tests/selected_int_kind_01.f90
@@ -4,6 +4,31 @@ subroutine test_selected_int_kind(R, expected_value)
     if (selected_int_kind(R) /= expected_value) error stop
 end subroutine
 
+subroutine test_compile_time_selected_int_kind()
+    print *, selected_int_kind(1)
+    if (selected_int_kind(1) /= 1) error stop
+    print *, selected_int_kind(2)
+    if (selected_int_kind(2) /= 1) error stop
+    print *, selected_int_kind(3)
+    if (selected_int_kind(3) /= 2) error stop
+    print *, selected_int_kind(4)
+    if (selected_int_kind(4) /= 2) error stop
+    print *, selected_int_kind(5)
+    if (selected_int_kind(5) /= 4) error stop
+    print *, selected_int_kind(6)
+    if (selected_int_kind(6) /= 4) error stop
+    print *, selected_int_kind(7)
+    if (selected_int_kind(7) /= 4) error stop
+    print *, selected_int_kind(8)
+    if (selected_int_kind(8) /= 4) error stop
+    print *, selected_int_kind(9)
+    if (selected_int_kind(9) /= 4) error stop
+    print *, selected_int_kind(10)
+    if (selected_int_kind(10) /= 8) error stop
+    print *, selected_int_kind(11)
+    if (selected_int_kind(11) /= 8) error stop
+end subroutine
+
 program selected_int_kind_01
     integer :: x
     x = 1
@@ -23,4 +48,5 @@ program selected_int_kind_01
     end do
 
     call test_selected_int_kind(x, 8)
+    call test_compile_time_selected_int_kind()
 end  program
