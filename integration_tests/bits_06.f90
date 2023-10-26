9dfab58bd25e0b76b78056be1530d3f6fb2a6951
Khushi Agrawal <khushiagrawal411@gmail.com>
2023-10-27 01:18:14 +0530

    fix tests

--- integration_tests/bits_06.f90
+++ integration_tests/bits_06.f90
@@ -1,14 +1,14 @@
 program bits_06
 implicit none
 
+integer(1) :: int1 = 1
 integer(2) :: int2 = 4
 integer(4) :: int4 = 41
 integer(8) :: int8 = 411
-integer(16) :: int16 = 411111
 
-write(*, *) "Bit length of int2: ", bit_size(int2)
-write(*, *) "Bit length of int4: ", bit_size(int4)
-write(*, *) "Bit length of int8: ", bit_size(int8)
-write(*, *) "Bit length of int8: ", bit_size(int16)
+print *, bit_size(int1)
+print *, bit_size(int2)
+print *, bit_size(int4)
+print *, bit_size(int8)
 
 end program
