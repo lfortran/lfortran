d5bfed1a2c7fd45458156b6830a1b48388778d7e
Khushi Agrawal <khushiagrawal411@gmail.com>
2023-10-27 02:50:24 +0530

    refactor tests

--- integration_tests/bits_06.f90
+++ integration_tests/bits_06.f90
@@ -6,6 +6,20 @@ integer(2) :: int2 = 4
 integer(4) :: int4 = 41
 integer(8) :: int8 = 411
 
+integer :: size1, size2, size4, size8
+
+size1 = bit_size(int1)
+if (size1 /= 8) error stop
+
+size2 = bit_size(int2)
+if (size2 /= 16) error stop
+
+size4 = bit_size(int4)
+if (size4 /= 32) error stop
+
+size8 = bit_size(int8)
+if (size8 /= 64) error stop
+
 print *, bit_size(int1)
 print *, bit_size(int2)
 print *, bit_size(int4)
