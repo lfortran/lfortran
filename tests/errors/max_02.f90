f62b1283fcbd3d10ea9408249de960880c0f645f
HarshitaKalani <kalani.1@iitj.ac.in>
2023-11-07 20:33:16 +0530

    add and register tests

---
+++ tests/errors/max_02.f90
@@ -0,0 +1,5 @@
+program max_02
+    character(8) :: y
+    character :: z
+    print *, max(y, z)
+end program
---
+++ tests/errors/min_02.f90
@@ -0,0 +1,5 @@
+program min_02
+    character(8) :: y
+    character :: z
+    print *, min(y, z)
+end program
