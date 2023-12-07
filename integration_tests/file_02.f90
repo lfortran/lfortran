a48f40b3230c8cac643c1422e437b300903b76ad
Pranavchiku <goswami.4@iitj.ac.in>
2023-12-07 12:07:02 +0530

    test: update file_02.f90

--- integration_tests/file_02.f90
+++ integration_tests/file_02.f90
@@ -1,10 +1,10 @@
 program file_02
     implicit none
     integer :: iostat, unit=10, i
-    open(unit, file="file_01_data.txt")
+    open(UNIT=unit, file="file_01_data.txt")
 
     ! TODO: return iostat value
-    read(unit, *, iostat=iostat) i
+    read(UNIT, *, iostat=iostat) i
     if (is_iostat_end(iostat)) then
         print *, "End of file reached."
         stop
@@ -14,5 +14,5 @@ program file_02
     end if
     print *, i
     if (i /= 10130) error stop
-    close(unit)
+    close(UNIT=unit)
 end program file_02
