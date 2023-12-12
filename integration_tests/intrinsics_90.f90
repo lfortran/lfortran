2aa6b765874cf89b5477cc76ea13bdcbcfdd1949
HarshitaKalani <kalani.1@iitj.ac.in>
2023-12-12 11:13:00 +0530

    add and register test for shape intrinsic

--- integration_tests/intrinsics_90.f90
+++ integration_tests/intrinsics_90.f90
@@ -1,33 +1,9 @@
-module intrinsics_90_module
-
-contains
-   
-subroutine tester(arr)
-real(8) :: arr(:,:)
-
-integer :: n
-real(8) :: vec(size(arr,1))
-
-vec = huge(1.d0)
-
-do n = 1, size(arr,2)
-    vec = min(arr(:,n),vec)
-end do
-
-print *, vec
-if (any(abs(vec - (-13.9d0)) > 1d-9)) error stop
-    
-end subroutine tester
-
-end module intrinsics_90_module
-
 program intrinsics_90
-use intrinsics_90_module
-implicit none
-
-real(8) :: arr(3,3)
-
-arr = -13.9d0
-
-call tester(arr)
+integer :: a(3)
+a = [1, 2, 3]
+print *, shape(a)
+print *, size(a)
+print *, maxval(a)
+print *, minval(a)
+print *, sum(a)
 end program
