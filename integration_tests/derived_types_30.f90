59a0823de3717285fd420459f7dc7c2ff63aaf0e
Gagandeep Singh <gdp.1807@gmail.com>
2024-01-03 00:29:19 +0530

    TEST: Added MRE test for subprojects/test-drive/src/testdrive.F90

---
+++ integration_tests/derived_types_30.f90
@@ -0,0 +1,48 @@
+module testdrive
+    implicit none
+
+    public :: unittest_type
+
+    abstract interface
+        subroutine test_interface(error)
+        integer, intent(out) :: error
+        end subroutine test_interface
+    end interface
+
+
+    type :: unittest_type
+        procedure(test_interface), pointer, nopass :: test => null()
+        logical :: should_fail = .false.
+    end type unittest_type
+
+contains
+
+    subroutine run_unittest(test_var, error)
+        type(unittest_type), intent(in) :: test_var
+        integer, intent(inout) :: error
+
+        call test_var%test(error)
+    end subroutine run_unittest
+
+end module testdrive
+
+program derived_types_30
+use testdrive
+implicit none
+
+    type(unittest_type) :: var
+    integer :: error
+    var % test => test_impl
+
+    call run_unittest(var, error)
+
+    print *, error
+
+contains
+
+    subroutine test_impl(error)
+        integer, intent(out) :: error
+        error = 1
+    end subroutine test_impl
+
+end program
