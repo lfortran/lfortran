3ec68a2a6d95e56bfe30c32f4ff5d2fe1decfbc4
Julien Schueller <schueller@phimeca.com>
2024-01-03 08:37:54 +0100

    cli: Mention c backend

--- src/bin/lfortran.cpp
+++ src/bin/lfortran.cpp
@@ -2024,7 +2024,7 @@ int main(int argc, char *argv[])
         app.add_flag("--no-warnings", compiler_options.no_warnings, "Turn off all warnings");
         app.add_flag("--no-error-banner", compiler_options.no_error_banner, "Turn off error banner");
         app.add_option("--error-format", compiler_options.error_format, "Control how errors are produced (human, short)")->capture_default_str();
-        app.add_option("--backend", arg_backend, "Select a backend (llvm, cpp, x86, wasm, fortran)")->capture_default_str();
+        app.add_option("--backend", arg_backend, "Select a backend (llvm, c, cpp, x86, wasm, fortran)")->capture_default_str();
         app.add_flag("--openmp", compiler_options.openmp, "Enable openmp");
         app.add_flag("--generate-object-code", compiler_options.generate_object_code, "Generate object code into .o files");
         app.add_flag("--rtlib", compiler_options.rtlib, "Include the full runtime library in the LLVM output");
