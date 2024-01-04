46aaef8c967b94f369f82f9102d85eaa7b6e937b
HarshitaKalani <kalani.1@iitj.ac.in>
2024-01-04 15:41:11 +0530

    add and register tests

---
+++ integration_tests/intrinsics_101.f90
@@ -0,0 +1,12 @@
+program intrinsics_101
+    implicit none
+    character(len=5) :: s1
+    character(len=10) :: s2
+    s1 = "242"
+    s2 = repeat(s1, 2)
+    print *, repeat("hello", 0)
+    if (repeat("hello", 0) /= "") error stop
+    print *, repeat("hello", 2)
+    if (repeat("hello", 2) /= "hellohello") error stop
+    print *, '-', s2, '-'
+end program
