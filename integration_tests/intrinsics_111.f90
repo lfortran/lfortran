48d09ce3902da3d07ddc21f0270e2174b516b62f
Kishan-Ved <kishan.ved@iitgn.ac.in>
2024-01-18 00:28:24 +0530

    updated

--- integration_tests/intrinsics_111.f90
+++ integration_tests/intrinsics_111.f90
@@ -1,6 +1,6 @@
-program MaxExample
-  integer :: a, b, result
-  a = 10
-  b = 7
-  if ( .not. min(a1=a, a2=b) == 7) error stop
-end program MaxExample
+program intrinsics_111
+    integer :: a, b, result
+    a = 10
+    b = 7
+    if ( .not. min(a1=a, a2=b) == 7) error stop
+end program intrinsics_111
