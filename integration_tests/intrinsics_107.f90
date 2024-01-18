6f8d39c2bfb8d4f380f289cd763bb0a0250b2193
Kishan-Ved <kishan.ved@iitgn.ac.in>
2024-01-18 21:55:55 +0530

    conflicts resolved

--- integration_tests/intrinsics_107.f90
+++ integration_tests/intrinsics_107.f90
@@ -1,5 +1,13 @@
 program intrinsics_107
-    integer :: x(5) = [ 1, 2, 3, 4 ,5 ]
-    logical :: mask(5) = [ .TRUE., .FALSE., .TRUE., .FALSE., .TRUE. ]
-    if ( .not. sum( array = x, mask = mask ) == 9 ) error stop
-end
+    integer(4) :: x = 5
+    integer(8) :: y = 5
+    print *, trailz(0_4)
+    if ( trailz(0_4) /= 32 ) error stop
+    print *, trailz(0_8)
+    if ( trailz(0_8) /= 32 ) error stop
+    print *, trailz(x)
+    if ( trailz(x) /= 0 ) error stop
+    print *, trailz(y)
+    if ( trailz(y) /= 0 ) error stop
+end program
+
