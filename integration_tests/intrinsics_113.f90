505aa9d5f0cf902d76bd6f7f7dbe7845fb0d62d7
Kishan-Ved <kishan.ved@iitgn.ac.in>
2024-01-19 22:05:26 +0530

    updated

--- integration_tests/intrinsics_113.f90
+++ integration_tests/intrinsics_113.f90
@@ -4,6 +4,6 @@ program intrinsics_113
     print *, sign(a=-12.,b=-1.)
   
     if ( .not. sign( a = -12, b = 1 ) == 12 ) error stop
-    if ( .not. sign( -12, 0 ) == 12 ) error stop
+    if ( .not. sign( -12, b = 0 ) == 12 ) error stop
     if ( .not. sign( -12, -1 ) == -12 ) error stop
 end program intrinsics_113
