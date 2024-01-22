960b147a7210577ca15eb25cc230353056d4474f
Kishan-Ved <kishan.ved@iitgn.ac.in>
2024-01-22 23:35:34 +0530

    added an integration test for lshift

---
+++ integration_tests/intrinsics_123.f90
@@ -0,0 +1,18 @@
+program intrinsics_123
+
+    print*, lshift(10_4, 0)
+    if (.not. shiftr(10_4, 0) == 10) error stop
+
+    print*, lshift(-10_8, 1)
+    if (.not. lshift(-10_8, 1) == -20 ) error stop
+
+    print*, lshift(not(10_4), 0)
+    if (.not. lshift(not(10_4), 0) == -11 ) error stop
+
+    print*, lshift(not(10_4), 2)
+    if (.not. lshift(not(10_4), 2) == -44 ) error stop
+
+    print*, lshift(not(10_8), 3)
+    if (.not. lshift(not(10_8), 3) == -88 ) error stop
+
+end
