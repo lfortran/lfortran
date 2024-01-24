0d44e69c9af24917b196a75d530670e89dca7ce1
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2024-01-24 21:37:22 +0530

    fix: syntax error when (sub)module name doesn't match closing (sub)module name (#3179)
    Fixes #2183

--- src/lfortran/parser/semantics.h
+++ src/lfortran/parser/semantics.h
@@ -2136,18 +2136,18 @@ ast_t* COARRAY(Allocator &al, const ast_t *id,
 #define USE_READ(x, l) make_UseRead_t(p.m_a, l, name2char(x))
 
 
-#define MODULE(name, trivia, use, implicit, decl, contains, l) make_Module_t(p.m_a, l, \
-        name2char(name), \
+#define MODULE(name, trivia, use, implicit, decl, contains, name_opt, l) make_Module_t(p.m_a, l, \
+        name2char_with_check(name, name_opt, l, "module"), \
         trivia_cast(trivia), \
         /*unit_decl1_t** a_use*/ USES(use), /*size_t n_use*/ use.size(), \
         /*m_implicit*/ VEC_CAST(implicit, implicit_statement), \
         /*n_implicit*/ implicit.size(), \
         /*unit_decl2_t** a_decl*/ DECLS(decl), /*size_t n_decl*/ decl.size(), \
         /*program_unit_t** a_contains*/ CONTAINS(contains), /*size_t n_contains*/ contains.size())
-#define SUBMODULE(id ,name, trivia, use, implicit, decl, contains, l) make_Submodule_t(p.m_a, l, \
+#define SUBMODULE(id ,name, trivia, use, implicit, decl, contains, name_opt, l) make_Submodule_t(p.m_a, l, \
         name2char(id), \
         nullptr, \
-        name2char(name), \
+        name2char_with_check(name, name_opt, l, "submodule"), \
         trivia_cast(trivia), \
         /*unit_decl1_t** a_use*/ USES(use), /*size_t n_use*/ use.size(), \
         /*m_implicit*/ VEC_CAST(implicit, implicit_statement), \
@@ -2155,11 +2155,11 @@ ast_t* COARRAY(Allocator &al, const ast_t *id,
         /*unit_decl2_t** a_decl*/ DECLS(decl), /*size_t n_decl*/ decl.size(), \
         /*program_unit_t** a_contains*/ CONTAINS(contains), /*size_t n_contains*/ contains.size())
 
-#define SUBMODULE1(id, parent_name, name, trivia, use, implicit, decl, contains, l) \
+#define SUBMODULE1(id, parent_name, name, trivia, use, implicit, decl, contains, name_opt, l) \
         make_Submodule_t(p.m_a, l, \
         name2char(id), \
         name2char(parent_name), \
-        name2char(name), \
+        name2char_with_check(name, name_opt, l, "submodule"), \
         trivia_cast(trivia), \
         /*unit_decl1_t** a_use*/ USES(use), /*size_t n_use*/ use.size(), \
         /*m_implicit*/ VEC_CAST(implicit, implicit_statement), \
