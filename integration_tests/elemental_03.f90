ba80b939555862f0ce6345ff9c2ce9ed8d78ba79
Pranavchiku <goswami.4@iitj.ac.in>
2024-01-25 09:35:18 +0530

    test: improve test

--- integration_tests/elemental_03.f90
+++ integration_tests/elemental_03.f90
@@ -6,12 +6,22 @@ close = is_close(x, y, 1e-9)
 print *, close
 if (.not. close(1)) error stop
 if (close(2)) error stop
+close = is_close(x, y)
+print *, close
+if (.not. close(1)) error stop
+if (close(2)) error stop
+
 
 contains
 elemental logical function is_close(a, b, rel_tol) result(close)
 real, intent(in) :: a, b
 real, intent(in), optional :: rel_tol
-close = abs(a - b) <= rel_tol
+if (present(rel_tol)) then
+  close = abs(a - b) <= rel_tol
+else
+    close = abs(a - b) <= 1e-9
+end if
+
 end function is_close
 
 end program elemental_03
