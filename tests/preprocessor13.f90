ff3407173e934d9de86989e33108c297d86c6584
Ondřej Čertík <ondrej@certik.us>
2024-01-25 17:05:16 -0700

    Improve the test

--- tests/preprocessor13.f90
+++ tests/preprocessor13.f90
@@ -1,8 +1,95 @@
 #if defined(__GFORTRAN__)
-print *, "GFortran"
+print *, "1 GFortran"
 #elif defined(__LFORTRAN__)
-print *, "LFortran"
+print *, "1 LFortran"
 #else
-print *, "Other"
+print *, "1 Other"
 #endif
+
+#if defined(__GFORTRAN__)
+print *, "2 GFortran"
+#elif defined(__LFORTRAN__)
+print *, "2 LFortran 1"
+#elif defined(__LFORTRAN__)
+print *, "2 LFortran 2"
+#else
+print *, "2 Other"
+#endif
+
+#if defined(__GFORTRAN__)
+print *, "3 GFortran 1"
+#elif defined(__GFORTRAN__)
+print *, "3 GFortran 2"
+#elif defined(__LFORTRAN__)
+print *, "3 LFortran 1"
+#elif defined(__LFORTRAN__)
+print *, "3 LFortran 2"
+#else
+print *, "3 Other"
+#endif
+
+#if defined(__GFORTRAN__)
+print *, "4 GFortran 1"
+#elif defined(__GFORTRAN__)
+print *, "4 GFortran 2"
+
+#  if defined(__GFORTRAN__)
+    print *, "41 GFortran 1"
+#  elif defined(__GFORTRAN__)
+    print *, "41 GFortran 2"
+#  elif defined(__LFORTRAN__)
+    print *, "41 LFortran 1"
+#  elif defined(__LFORTRAN__)
+    print *, "41 LFortran 2"
+#  else
+    print *, "41 Other"
+#  endif
+
+#elif defined(__LFORTRAN__)
+print *, "4 LFortran 1"
+
+#  if defined(__GFORTRAN__)
+    print *, "42 GFortran 1"
+#  elif defined(__GFORTRAN__)
+    print *, "42 GFortran 2"
+#  elif defined(__LFORTRAN__)
+    print *, "42 LFortran 1"
+#  elif defined(__LFORTRAN__)
+    print *, "42 LFortran 2"
+#  else
+    print *, "42 Other"
+#  endif
+
+#elif defined(__LFORTRAN__)
+print *, "4 LFortran 2"
+
+#  if defined(__GFORTRAN__)
+    print *, "43 GFortran 1"
+#  elif defined(__GFORTRAN__)
+    print *, "43 GFortran 2"
+#  elif defined(__LFORTRAN__)
+    print *, "43 LFortran 1"
+#  elif defined(__LFORTRAN__)
+    print *, "43 LFortran 2"
+#  else
+    print *, "43 Other"
+#  endif
+
+#else
+print *, "4 Other"
+
+#  if defined(__GFORTRAN__)
+    print *, "44 GFortran 1"
+#  elif defined(__GFORTRAN__)
+    print *, "44 GFortran 2"
+#  elif defined(__LFORTRAN__)
+    print *, "44 LFortran 1"
+#  elif defined(__LFORTRAN__)
+    print *, "44 LFortran 2"
+#  else
+    print *, "44 Other"
+#  endif
+
+#endif
+
 end
