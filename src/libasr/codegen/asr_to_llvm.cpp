4af1ce3aefd5b8fd071bd7a294b82d1fd28f7fcc
Shaikh Ubaid <shaikhubaid769@gmail.com>
2024-01-26 07:17:00 +0530

    LLVM: Fix args size parameter

--- src/libasr/codegen/asr_to_llvm.cpp
+++ src/libasr/codegen/asr_to_llvm.cpp
@@ -9480,9 +9480,6 @@ public:
         // if (fmt_value) ...
         if (x.m_kind == ASR::string_format_kindType::FormatFortran) {
             std::vector<llvm::Value *> args;
-            int size = x.n_args;
-            llvm::Value *count = llvm::ConstantInt::get(llvm::Type::getInt32Ty(context), size);
-            args.push_back(count);
             visit_expr(*x.m_fmt);
             args.push_back(tmp);
 
@@ -9491,6 +9488,9 @@ public:
                 //  Use the function to compute the args, but ignore the format
                 compute_fmt_specifier_and_arg(fmt, args, x.m_args[i], x.base.base.loc);
             }
+            llvm::Value *args_cnt = llvm::ConstantInt::get(llvm::Type::getInt32Ty(context),
+                args.size() - 1);
+            args.insert(args.begin(), args_cnt);
             tmp = string_format_fortran(context, *module, *builder, args);
         } else {
             throw CodeGenError("Only FormatFortran string formatting implemented so far.");
