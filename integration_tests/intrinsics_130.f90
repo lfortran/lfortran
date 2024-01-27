2a0ba73c9f75b44d29be9a218e1c3115bc039862
parth121101 <parthmistry1211@gmail.com>
2024-01-27 22:08:53 +0530

    Added tests

--- integration_tests/intrinsics_130.f90
+++ integration_tests/intrinsics_130.f90
@@ -1,5 +1,11 @@
-program intrinsics_128
+program intrinsics_130
+    integer :: a, b
+    a = 10
+    b = 2
+    print *, ishftc(a, b)
+    if (ishftc(a, b) /= 40) error stop
+
     print*, ishftc(10, 2)
-    if (.not. ishftc(10, 2) == 40) error stop
+    if (ishftc(10, 2) /= 40) error stop
 
 end program
