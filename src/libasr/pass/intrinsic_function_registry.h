820fb1de488b1508a0949d31a5491d06bccfe6d9
Kishan-Ved <kishan.ved@iitgn.ac.in>
2024-01-27 15:09:14 +0530

    some work done

--- src/libasr/pass/intrinsic_function_registry.h
+++ src/libasr/pass/intrinsic_function_registry.h
@@ -1760,6 +1760,93 @@ namespace Shiftr {
 
 } // namespace Shiftr
 
+namespace Rshift {
+
+    static inline void verify_args(const ASR::IntrinsicScalarFunction_t& x, diag::Diagnostics& diagnostics) {
+        ASRUtils::require_impl(x.n_args == 2,
+            "Call to `rshift` must have exactly two arguments",
+            x.base.base.loc, diagnostics);
+        ASR::ttype_t *type1 = ASRUtils::expr_type(x.m_args[0]);
+        ASR::ttype_t *type2 = ASRUtils::expr_type(x.m_args[1]);
+        ASRUtils::require_impl((is_integer(*type1) && is_integer(*type2)),
+            "Arguments to `rshift` must be of integer type",
+            x.base.base.loc, diagnostics);
+    }
+
+    static ASR::expr_t *eval_Rshift(Allocator &al, const Location &loc,
+            ASR::ttype_t* t1, Vec<ASR::expr_t*> &args) {
+        int64_t val1 = ASR::down_cast<ASR::IntegerConstant_t>(args[0])->m_n;
+        int64_t val2 = ASR::down_cast<ASR::IntegerConstant_t>(args[1])->m_n;
+        int64_t val = val1 >> val2;
+
+        // Manually set leftmost bits based on the leftmost bit of val1
+        if (val1 < 0) {
+            val |= (-1LL << (8 * sizeof(int64_t) - val2));
+        } else {
+            val &= ~(-1LL << (8 * sizeof(int64_t) - val2));
+        }
+
+        return make_ConstantWithType(make_IntegerConstant_t, val, t1, loc);
+    }
+
+    static inline ASR::asr_t* create_Rshift(Allocator& al, const Location& loc,
+            Vec<ASR::expr_t*>& args,
+            const std::function<void (const std::string &, const Location &)> err) {
+        if (args.size() != 2) {
+            err("Intrinsic `rshift` function accepts exactly 2 arguments", loc);
+        }
+        ASR::ttype_t *type1 = ASRUtils::expr_type(args[0]);
+        ASR::ttype_t *type2 = ASRUtils::expr_type(args[1]);
+        if (!ASRUtils::is_integer(*type1) || !ASRUtils::is_integer(*type2)) {
+            err("Arguments of the `rshift` function must be Integer",
+                args[0]->base.loc);
+        }
+
+        ASR::expr_t *m_value = nullptr;
+        if (all_args_evaluated(args)) {
+            Vec<ASR::expr_t*> arg_values; arg_values.reserve(al, 2);
+            arg_values.push_back(al, expr_value(args[0]));
+            arg_values.push_back(al, expr_value(args[1]));
+            m_value = eval_Rshift(al, loc, expr_type(args[0]), arg_values);
+        }
+        return ASR::make_IntrinsicScalarFunction_t(al, loc,
+            static_cast<int64_t>(IntrinsicScalarFunctions::Rshift),
+            args.p, args.n, 0, ASRUtils::expr_type(args[0]), m_value);
+    }
+
+    static inline ASR::expr_t* instantiate_Rshift(Allocator &al, const Location &loc,
+            SymbolTable *scope, Vec<ASR::ttype_t*>& arg_types, ASR::ttype_t *return_type,
+            Vec<ASR::call_arg_t>& new_args, int64_t /*overload_id*/) {
+        declare_basic_variables("_lcompilers_sign_" + type_to_str_python(arg_types[0]));
+        fill_func_arg("x", arg_types[0]);
+        fill_func_arg("y", arg_types[1]);
+        auto result = declare(fn_name, return_type, ReturnVar);
+        /*
+        * r = rshift(x, y)
+        * r = (x >> y) | ((x < 0) ? (-1LL << (8 * sizeof(int64_t) - y)) : 0)
+        */
+        ASR::expr_t *two = i(2, arg_types[0]);
+        body.push_back(al, b.Assignment(result, i_tDiv(args[0], iPow(two, args[1], arg_types[0]), arg_types[0])));
+        body.push_back(al, b.BinaryOperation(BinaryOpType::BitwiseOr,
+            result,
+            b.Conditional(b.BinaryOperation(BinaryOpType::LessThan, args[0], i(0, arg_types[0])),
+                b.BinaryOperation(BinaryOpType::BitwiseOr,
+                    b.BinaryOperation(BinaryOpType::LeftShift,
+                        i(-1LL, arg_types[0]),
+                        i_tSub(b.SizeOf(arg_types[0]), args[1], arg_types[0]), arg_types[0]),
+                    result),
+                i(0, arg_types[0]))
+            )
+        );
+
+        ASR::symbol_t *f_sym = make_ASR_Function_t(fn_name, fn_symtab, dep, args,
+            body, result, ASR::abiType::Source, ASR::deftypeType::Implementation, nullptr);
+        scope->add_symbol(fn_name, f_sym);
+        return b.Call(f_sym, new_args, return_type, nullptr);
+
+    }
+
+} // namespace Rshift
 
 
 namespace Shiftl {
