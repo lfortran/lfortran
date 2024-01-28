a4b48c8cf04e1caf73f9fc9243cc316239d936bc
Kishan-Ved <kishan.ved@iitgn.ac.in>
2024-01-28 16:18:25 +0530

    updated

--- integration_tests/intrinsics_130.f90
+++ integration_tests/intrinsics_130.f90
@@ -1,9 +1,7 @@
 program intrinsics_130
 
-    integer(4) :: x1, x2, x3, y1, y2, y3
-    x1 = 10
-    x2 = 10
-    x3 = 10
+    integer(4) :: x, y1, y2, y3
+    x = 10
     y1 = 2
     y2 = 0
     y3 = -2
@@ -12,11 +10,11 @@ program intrinsics_130
     print*, ishft(10_4, 0)
     print*, ishft(10_4, -2)
 
-    print*, ishft(x1, y1)
-    ! if (ishft(x1, y1) /= 40) error stop
-    print*, ishft(x2, y2)
-    ! if (ishft(x2, y2) /= 10) error stop
-    print*, ishft(x3, y3)
-    ! if (ishft(x3, y3) /= 2) error stop
+    ! print*, ishft(x, y1) ! Floating point exception (core dumped)
+    ! if (ishft(x, y1) /= 40) error stop
+    print*, ishft(x, y2)
+    if (ishft(x, y2) /= 10) error stop
+    print*, ishft(x, y3)
+    if (ishft(x, y3) /= 2) error stop
    
 end
