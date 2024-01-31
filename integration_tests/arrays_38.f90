22af9aae5e12b733e0a0353078650ab06654f0ab
Pranavchiku <goswami.4@iitj.ac.in>
2024-01-31 23:18:24 +0530

    test: apply code review

--- integration_tests/arrays_38.f90
+++ integration_tests/arrays_38.f90
@@ -9,11 +9,12 @@ program arrays_38
 implicit none
 integer :: n, i
 complex :: res(5)
+complex :: res2(0)
 interface
-pure function linspace_n_1_cdp_cdp(n) result(res)
-integer, intent(in) :: n
-complex :: res(max(n, 0))
-end function linspace_n_1_cdp_cdp
+    pure function linspace_n_1_cdp_cdp(n) result(res)
+    integer, intent(in) :: n
+    complex :: res(max(n, 0))
+    end function linspace_n_1_cdp_cdp
 end interface
 
 res = linspace_n_1_cdp_cdp(5)
@@ -21,4 +22,6 @@ print *, res
 do i = 1, 5
     if (abs(res(i) - (1.0, 3.0)) > 1e-8) error stop
 end do
+res2 = linspace_n_1_cdp_cdp(-3)
+print *, res2
 end program
