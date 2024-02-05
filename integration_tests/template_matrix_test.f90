a03b9f093f97b442f2f629ec2f38031dca1b2851
Ondřej Čertík <ondrej@certik.us>
2024-02-05 09:00:23 -0700

    Consolidate template_matrix_real and integer
    This avoids compiling the modules twice.
    
    Towards #3211.

--- integration_tests/template_matrix_test.f90
+++ integration_tests/template_matrix_test.f90
@@ -16,9 +16,27 @@ contains
 
 end module
 
-program template_matrix_real
+module integer_func_m
 
+    public :: integer_zero, integer_one
+
+contains 
+
+    pure function integer_zero()
+        integer :: integer_zero
+        integer_zero = 0.
+    end function
+
+    pure function integer_one()
+        integer :: integer_one
+        integer_one = 1.
+    end function
+
+end module
+
+program template_matrix_test
     use matrix_m, only: matrix_tmpl
+    use integer_func_m
     use real_func_m
 
     implicit none
@@ -29,5 +47,10 @@ program template_matrix_real
             real_plus_matrix => plus_matrix, &
             real_times_matrix => times_matrix, &
             real_matrix_subtraction_tmpl => matrix_subtraction_tmpl
+    instantiate matrix_tmpl(integer, operator(+), integer_zero, operator(*), integer_one, n), only: &
+            integer_matrix => matrix, &
+            integer_plus_matrix => plus_matrix, &
+            integer_times_matrix => times_matrix, &
+            integer_matrix_subtraction_tmpl => matrix_subtraction_tmpl
 
 end program
