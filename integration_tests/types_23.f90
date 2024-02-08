54553fb46fc6d21ff57240423468aa5f2a55d25a
Kishan-Ved <kishan.ved@iitgn.ac.in>
2024-02-08 22:37:58 +0530

    merged tests into one file

--- integration_tests/types_23.f90
+++ integration_tests/types_23.f90
@@ -1,7 +1,20 @@
 program types_23
-    complex(8) :: B(3) = [1, 2, 3]
-    print*, B
+    complex(8) :: A(3) = [(1, 1), (2, 2), (3, 3)]
+    integer(8) :: B(3) = [(1, 1), (2, 2), (3, 3)]
+    real(8) :: C(3) = [(1, 1), (2, 2), (3, 3)]
+    complex(8) :: D(3) = [1, 2, 3]
 
-    if ( abs(real(B(1)) - 1.00) > 10e-5 ) error stop
-    if ( abs(imag(B(2)) - 0.00) > 10e-5 ) error stop
+    print*, A
+    if ( abs(real(A(1)) - 1.00) > 10e-5 ) error stop
+    if ( abs(imag(A(2)) - 2.00) > 10e-5 ) error stop
+    
+    print*, B
+    if ( abs(B(1) - 1.00) > 10e-5 ) error stop
+
+    print*, C
+    if ( abs(C(1) - 1.00) > 10e-5 ) error stop
+
+    print*, D
+    if ( abs(real(D(1)) - 1.00) > 10e-5 ) error stop
+    if ( abs(imag(D(2)) - 0.00) > 10e-5 ) error stop
 end
--- integration_tests/types_24.f90
+++
@@ -1,6 +0,0 @@
-program types_24
-    integer(8) :: B(3) = [(1, 1), (2, 2), (3, 3)]
-    print*, B
-
-    if ( abs(B(1) - 1.00) > 10e-5 ) error stop
-end
