48f56838ddf421ccb4bdee62e42d70ec78bd024e
HarshitaKalani <kalani.1@iitj.ac.in>
2024-02-09 19:46:52 +0530

    add test for runtime values

--- integration_tests/intrinsics_137.f90
+++ integration_tests/intrinsics_137.f90
@@ -1,15 +1,22 @@
 program intrinsics_137
-    
-    print *, huge(0_4)
-    if (huge(0_4) /= 2147483647) error stop
+    integer(4) :: i
+    integer(8) :: j
+    real(4) :: r
+    real(8) :: d
+    i = 0
+    j = 1
+    r = 0.0
+    d = 0.0d0
+    print *, huge(i)
+    if (huge(i) /= 2147483647) error stop
   
-    print*, huge(1_8)
-    ! if (huge(1_8) - 9223372036854775807 > 1e-5) error stop
+    print*, huge(j)
+    if (huge(j) - 9223372036854775807_8 > 1e-5) error stop
   
-    print*, huge(0.0)
-    ! if (abs(huge(0.0) - 3.40282347e+38) > 1e-5) error stop
+    print*, huge(r)
+    ! if (abs(huge(r) - 3.40282347e+38) > 1e-5) error stop
   
-    print*, huge(0.0d0)
-    ! if (abs(huge(0.0d0) - 1.79769313486231571e+308) > 1e-5) error stop
+    print*, huge(d)
+    ! if (abs(huge(d) - 1.79769313486231571e+308_8) > 1e-5) error stop
 
 end program intrinsics_137
