0d4f89095c243123eb797564422c76a7c34bdf2f
HarshitaKalani <kalani.1@iitj.ac.in>
2024-02-11 14:30:36 +0530

    add error for kind not supported in tiny and apply changes after code review

--- integration_tests/intrinsics_137.f90
+++ integration_tests/intrinsics_137.f90
@@ -1,19 +1,21 @@
 program intrinsics_137
+    use iso_fortran_env, only: int32, int64
     integer, parameter :: sp = kind(0.0)
     integer, parameter :: dp = kind(0.d0)
-    integer(sp) :: i
-    integer(dp) :: j
+    integer(int32) :: i
+    integer(int64) :: j
     real(sp) :: r
     real(dp) :: d
     i = 0
     j = 1
     r = 0.0_sp
     d = 0.0_dp
+    
     print *, huge(i)
-    if (huge(i) /= 2147483647) error stop
+    if (huge(i) /= 2147483647_int32) error stop
   
     print*, huge(j)
-    if (huge(j) - 9223372036854775807_dp > 1e-5_dp) error stop
+    if (huge(j) /= 9223372036854775807_int64) error stop
   
     print*, huge(r)
     ! if (abs(huge(r) - 3.40282347e+38_sp) > 1e-5_sp) error stop
