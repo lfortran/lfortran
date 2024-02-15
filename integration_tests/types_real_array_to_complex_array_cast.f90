e7842c18d4613eb033f432a5e68c7812d1187f21
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2024-02-16 01:52:01 +0530

    fix: real array cast to complex array (#3408)

---
+++ integration_tests/types_real_array_to_complex_array_cast.f90
@@ -0,0 +1,8 @@
+program types_real_array_to_complex_array_cast
+    integer, parameter :: dp = kind(0.d0)
+    complex(dp) :: D(3) = [1.0_dp, 2.0_dp, 3.0_dp]
+    if (any(D /= [(1.0_dp, 0.0_dp), &
+                  (2.0_dp, 0.0_dp), &
+                  (3.0_dp, 0.0_dp)])) &
+        error stop
+end program
