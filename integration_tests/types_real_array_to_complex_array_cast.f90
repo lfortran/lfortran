8d3f60cda4bb630af226fcb86d01a543ba0a4d12
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2024-02-15 14:11:20 +0530

    use double precision correctly in test cases

--- integration_tests/types_real_array_to_complex_array_cast.f90
+++ integration_tests/types_real_array_to_complex_array_cast.f90
@@ -1,4 +1,6 @@
 program types_real_array_to_complex_array_cast
-    complex(8) :: D(3) = [1.0, 2.0, 3.0]
-    if (any(D /= [(1.0, 0.0), (2.0, 0.0), (3.0, 0.0)])) error stop
+    integer, parameter :: dp = kind(0.d0)
+    complex(dp) :: D(3) = [1.0_dp, 2.0_dp, 3.0_dp]
+    if (any(D /= [(1.0_dp, 0.0_dp), (2.0_dp, 0.0_dp), (3.0_dp, 0.0_dp)])) \
+        error stop
 end program
--- integration_tests/types_real_to_complex_cast.f90
+++ integration_tests/types_real_to_complex_cast.f90
@@ -1,8 +1,9 @@
 program types_real_to_complex_cast
-    complex(8) :: c
-    real :: i
-    c = 1.0
-    i = 1.2
+    integer, parameter :: dp = kind(0.d0)
+    complex(dp) :: c
+    real(dp) :: i
+    c = 1.0_dp
+    i = 1.2_dp
     c = i
-    if (abs(c - 1.2) > 1e-6) error stop
+    if (abs(c - 1.2_dp) > 1e-8) error stop
 end program
