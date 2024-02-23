647def84d8a1b7ebe36582c86271f088b79f5f65
parth121101 <parthmistry1211@gmail.com>
2024-02-23 19:35:56 +0530

    further minimized module_23 file

--- a.f90
+++ a.f90
@@ -1,62 +1,23 @@
 module string_module
-    implicit none
-        type :: string_type
-        sequence
-        private
-        character(len=:), allocatable :: raw
-        end type string_type
     contains
-        elemental subroutine assign_char_to_string(lhs, rhs)
-            type(string_type), intent(inout) :: lhs
-            character(len=*), intent(in) :: rhs
-            lhs%raw = rhs
-        end subroutine assign_char_to_string
-
-        elemental function len_string(string3) result(length)
-            type(string_type), intent(in) :: string3
+        pure function len_string(string3) result(length)
+            character(len=:), allocatable, intent(in) :: string3
             integer :: length
-            if (allocated(string3%raw)) then
-                length = len(string3%raw)
-            else
-                length = 0
-            end if
         end function len_string
 
-        pure function maybe(string2) result(maybe_string)
-        type(string_type), intent(in) :: string2
-        character(len=len_string(string2)) :: maybe_string         
-        ! error in above line
-        if (allocated(string2%raw)) then
-            maybe_string = string2%raw
-        else
-            maybe_string = ''
-        end if
+        function maybe(string2) result(maybe_string)
+        character(len=:), allocatable, intent(in) :: string2
+        ! character(len=:), allocatable :: maybe_string
+        character(len=len_string(string2)) :: maybe_string
         end function maybe
 
-        elemental function adjustl_string(string1) result(adjusted_string)
-            type(string_type), intent(in) :: string1
-            type(string_type) :: adjusted_string
-            adjusted_string%raw = adjustl(maybe(string1))
+        function adjustl_string(string1) result(adjusted_string)
+            character(len=:), allocatable, intent(in) :: string1
+            character(len=:) , allocatable:: adjusted_string
+            print*, adjustl(maybe(string1))
         end function adjustl_string
-
-        
-
-
-    end module
-    
-    program main_program
-        use string_module
-        implicit none
-        
-        type(string_type) :: my_string
-        
-        ! Assigning a string to the user-defined type using assign_char_to_string subroutine
-        call assign_char_to_string(my_string, "      Hello, Fortran!")
-        
-        ! Accessing the string content using the adjustl_string function
-        print *, "Length of the string:", len_string(my_string)
-        print *, "String content:", maybe(my_string)
-        print *, "String content:", adjustl_string(my_string)
-        
-    end program main_program
-    
+end module
+program module_23
+    use string_module
+    implicit none
+end program
