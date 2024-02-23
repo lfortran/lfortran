48092dd9d059ed4008ad74db79c59b12137d19bd
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2024-02-23 12:09:46 +0530

    check for incorrect kinds (for 'real', 'integer' etc.) (#3455)
    * check for incorrect kinds (for 'real', 'integer' etc.)
    
    an incorrect kind could be of the following way:
    * incorrect data-type (e.g. string, float instead of integer)
    * unsupported type (e.g. kind=32 for integer)
    
    * changes
    
    * TEST: Update reference tests
    
    * refactor 'extract_kind'
    
    * add test cases
    
    * add more test cases and handle star as value of kind
    
    * refactor code and use correct value of kind for logical
    
    * restore 'kind' for creating logical's and put a comment for now
    
    * initialize a_kind to -1 and place comment at a better place

--- src/lfortran/semantics/ast_common_visitor.h
+++ src/lfortran/semantics/ast_common_visitor.h
@@ -2832,26 +2832,27 @@ public:
         type_declaration = nullptr;
 
         int a_kind = compiler_options.po.default_integer_kind;
+
+        // general assignments and checks except when it's a
+        // "Character" declaration
         if (sym_type->m_type != AST::decl_typeType::TypeCharacter &&
-            sym_type->m_kind != nullptr &&
-            sym_type->m_kind->m_value != nullptr) {
-            this->visit_expr(*sym_type->m_kind->m_value);
-            ASR::expr_t* kind_expr = ASRUtils::EXPR(tmp);
-            a_kind = ASRUtils::extract_kind<SemanticError>(kind_expr, loc);
-        }
-        if (sym_type->m_type == AST::decl_typeType::TypeReal) {
-            if(sym_type->m_kind) {
-                if (!sym_type->m_kind->m_value && sym_type->m_kind->m_type == AST::kind_item_typeType::Star) {
-                    throw SemanticError("Expected initialization expression for kind",
-                                    sym_type->m_kind->loc);
-                }
+            sym_type->m_kind != nullptr
+        ) {
+            if (sym_type->m_kind->m_value) {
                 this->visit_expr(*sym_type->m_kind->m_value);
                 ASR::expr_t* kind_expr = ASRUtils::EXPR(tmp);
-                int kind_value = ASRUtils::extract_kind<SemanticError>(kind_expr, loc);
-                if (kind_value != 4 && kind_value != 8) {
-                    throw SemanticError("Kind " + std::to_string(kind_value) + " is not supported for Real",
-                                    loc);
-                }
+                a_kind = ASRUtils::extract_kind<SemanticError>(kind_expr, sym_type->m_kind->loc);
+            }
+            // kind=* only allowed for "Character"
+            else if (sym_type->m_kind->m_type == AST::kind_item_typeType::Star) {
+                throw SemanticError("Expected initialization expression for kind",
+                                sym_type->m_kind->loc);
+            }
+        }
+        if (sym_type->m_type == AST::decl_typeType::TypeReal) {
+            if (a_kind != 4 && a_kind != 8) {
+                throw SemanticError("Kind " + std::to_string(a_kind) + " is not supported for Real",
+                                sym_type->m_kind->loc);
             }
             type = ASRUtils::TYPE(ASR::make_Real_t(al, loc, a_kind));
             type = ASRUtils::make_Array_t_util(al, loc, type, dims.p, dims.size(), abi, is_argument, ASR::array_physical_typeType::DescriptorArray, false, is_dimension_star);
@@ -2868,6 +2869,10 @@ public:
                     ASRUtils::type_get_past_allocatable(type)));
             }
         } else if (sym_type->m_type == AST::decl_typeType::TypeInteger) {
+            if (a_kind != 1 && a_kind != 2 && a_kind != 4 && a_kind != 8) {
+                throw SemanticError("Kind " + std::to_string(a_kind) + " is not supported for Integer",
+                                sym_type->m_kind->loc);
+            }
             type = ASRUtils::TYPE(ASR::make_Integer_t(al, loc, a_kind));
             type = ASRUtils::make_Array_t_util(
                 al, loc, type, dims.p, dims.size(), abi, is_argument, ASR::array_physical_typeType::DescriptorArray, false, is_dimension_star);
@@ -2876,6 +2881,13 @@ public:
                     ASRUtils::type_get_past_allocatable(type)));
             }
         } else if (sym_type->m_type == AST::decl_typeType::TypeLogical) {
+            if (a_kind != 1 && a_kind != 2 && a_kind != 4 && a_kind != 8) {
+                throw SemanticError("Kind " + std::to_string(a_kind) + " is not supported for Logical",
+                                sym_type->m_kind->loc);
+            }
+            // currently we change the kind's of all logical's to
+            // 'default_integer_kind'. GFortran support's logical's of
+            // different kind's, we need to think about this
             type = ASRUtils::TYPE(ASR::make_Logical_t(al, loc, compiler_options.po.default_integer_kind));
             type = ASRUtils::make_Array_t_util(
                 al, loc, type, dims.p, dims.size(), abi, is_argument, ASR::array_physical_typeType::DescriptorArray, false, is_dimension_star);
@@ -2884,6 +2896,10 @@ public:
                     ASRUtils::type_get_past_allocatable(type)));
             }
         } else if (sym_type->m_type == AST::decl_typeType::TypeComplex) {
+            if (a_kind != 4 && a_kind != 8) {
+                throw SemanticError("Kind " + std::to_string(a_kind) + " is not supported for Complex",
+                                sym_type->m_kind->loc);
+            }
             type = ASRUtils::TYPE(ASR::make_Complex_t(al, loc, a_kind));
             type = ASRUtils::make_Array_t_util(
                 al, loc, type, dims.p, dims.size(), abi, is_argument, ASR::array_physical_typeType::DescriptorArray, false, is_dimension_star);
@@ -2902,8 +2918,8 @@ public:
             }
         } else if (sym_type->m_type == AST::decl_typeType::TypeCharacter) {
             int a_len = -10;
-            int a_kind = 1;
             ASR::expr_t *len_expr = nullptr;
+            a_kind = 1;
             TypeMissingData* char_data = al.make_new<TypeMissingData>();
             char_data->sym_name = sym;
             LCOMPILERS_ASSERT(sym_type->n_kind < 3) // TODO
