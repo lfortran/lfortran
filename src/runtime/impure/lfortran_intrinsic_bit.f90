b16ca9bf2c69857712f586549dbe45de71f09863
HarshitaKalani <kalani.1@iitj.ac.in>
2024-02-26 18:08:25 +0530

    remove old runtime implementation

--- src/runtime/impure/lfortran_intrinsic_bit.f90
+++ src/runtime/impure/lfortran_intrinsic_bit.f90
@@ -2,18 +2,6 @@ module lfortran_intrinsic_bit
 use, intrinsic :: iso_fortran_env, only: int8, int16, int32, int64
 implicit none
 
-interface iand
-    module procedure iand16, iand32, iand64
-end interface
-
-interface ior
-    module procedure ior32, ior64
-end interface
-
-interface ieor
-    module procedure ieor32, ieor64
-end interface
-
 interface ibclr
     module procedure ibclr32, ibclr64
 end interface
@@ -35,103 +23,6 @@ interface ibits
 end interface
 
 contains
-
-! iand --------------------------------------------------------------------------
-
-elemental integer(int16) function iand16(x, y) result(r)
-integer(int16), intent(in) :: x
-integer(int16), intent(in) :: y
-interface
-    pure integer(int16) function c_iand16(x, y) bind(c, name="_lfortran_iand16")
-    import :: int16
-    integer(int16), intent(in), value :: x
-    integer(int16), intent(in), value :: y
-    end function
-end interface
-r = c_iand16(x, y)
-end function
-
-elemental integer(int32) function iand32(x, y) result(r)
-integer(int32), intent(in) :: x
-integer(int32), intent(in) :: y
-interface
-    pure integer(int32) function c_iand32(x, y) bind(c, name="_lfortran_iand32")
-    import :: int32
-    integer(int32), intent(in), value :: x
-    integer(int32), intent(in), value :: y
-    end function
-end interface
-r = c_iand32(x, y)
-end function
-
-elemental integer(int64) function iand64(x, y) result(r)
-integer(int64), intent(in) :: x
-integer(int64), intent(in) :: y
-interface
-    pure integer(int64) function c_iand64(x, y) bind(c, name="_lfortran_iand64")
-    import :: int64
-    integer(int64), intent(in), value :: x
-    integer(int64), intent(in), value :: y
-    end function
-end interface
-r = c_iand64(x, y)
-end function
-
-! ior --------------------------------------------------------------------------
-
-elemental integer(int32) function ior32(x, y) result(r)
-integer(int32), intent(in) :: x
-integer(int32), intent(in) :: y
-interface
-    pure integer(int32) function c_ior32(x, y) bind(c, name="_lfortran_ior32")
-    import :: int32
-    integer(int32), intent(in), value :: x
-    integer(int32), intent(in), value :: y
-    end function
-end interface
-r = c_ior32(x, y)
-end function
-
-elemental integer(int64) function ior64(x, y) result(r)
-integer(int64), intent(in) :: x
-integer(int64), intent(in) :: y
-interface
-    pure integer(int64) function c_ior64(x, y) bind(c, name="_lfortran_ior64")
-    import :: int64
-    integer(int64), intent(in), value :: x
-    integer(int64), intent(in), value :: y
-    end function
-end interface
-r = c_ior64(x, y)
-end function
-
-! ieor --------------------------------------------------------------------------
-
-elemental integer(int32) function ieor32(x, y) result(r)
-integer(int32), intent(in) :: x
-integer(int32), intent(in) :: y
-interface
-    pure integer(int32) function c_ieor32(x, y) bind(c, name="_lfortran_ieor32")
-    import :: int32
-    integer(int32), intent(in), value :: x
-    integer(int32), intent(in), value :: y
-    end function
-end interface
-r = c_ieor32(x, y)
-end function
-
-elemental integer(int64) function ieor64(x, y) result(r)
-integer(int64), intent(in) :: x
-integer(int64), intent(in) :: y
-interface
-    pure integer(int64) function c_ieor64(x, y) bind(c, name="_lfortran_ieor64")
-    import :: int64
-    integer(int64), intent(in), value :: x
-    integer(int64), intent(in), value :: y
-    end function
-end interface
-r = c_ieor64(x, y)
-end function
 
 ! ibclr --------------------------------------------------------------------------
 
