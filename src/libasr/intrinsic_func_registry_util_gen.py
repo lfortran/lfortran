4f31c83bfb5d3a6ddc06b9bae539f7329fd35716
Pranavchiku <goswami.4@iitj.ac.in>
2024-02-27 16:11:51 +0530

    chore: minor tweaks to fix integration tests

--- src/libasr/intrinsic_func_registry_util_gen.py
+++ src/libasr/intrinsic_func_registry_util_gen.py
@@ -119,7 +119,7 @@ intrinsic_funcs_args = {
     "Adjustl": [
         {
             "args": [("char",)],
-            "ret_type_arg_idx": 0
+            "return": "character(-1)"
         }
     ],
     "Aint": [
