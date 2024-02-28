d696c03a5618f07b01f183cde4e6fdc56a1752f5
HarshitaKalani <kalani.1@iitj.ac.in>
2024-02-28 20:32:01 +0530

    resolve conflicts

--- integration_tests/intrinsics_167.f90
+++ integration_tests/intrinsics_167.f90
@@ -5,19 +5,19 @@ program intrinsics_167
     integer :: a4 = -4
     integer :: a5 = -2
     integer :: a6 = -5
+
+    print*, btest(5, 8)
+    if (btest(5, 8) .neqv. .false.) error stop
+    print*, btest(-1, 5)
+    if (btest(-1, 5) .neqv. .true.) error stop
   
-    print*, ibclr(5, 8)
-    if (ibclr(5, 8) /= 5) error stop
-    print*, ibclr(-1, 5)
-    if (ibclr(-1, 5) /= -33) error stop
-  
-    print*, ibclr(a1, a2)
-    if (ibclr(a1, a2) /= 5) error stop
-    print*, ibclr(a3, a1)
-    if (ibclr(a3, a1) /= -33) error stop
-    print*, ibclr(a2, a4)
-    if (ibclr(a2, a4) /= 8) error stop
-    print*, ibclr(a5, a6)
-    if (ibclr(a5, a6) /= -134217730) error stop
+    print*, btest(a1, a2)
+    if (btest(a1, a2) .neqv. .false.) error stop
+    print*, btest(a3, a1)
+    if (btest(a3, a1) .neqv. .true.) error stop
+    print*, btest(a2, a4)
+    if (btest(a2, a4) .neqv. .false.) error stop
+    print*, btest(a5, a6)
+    if (btest(a5, a6) .neqv. .true.) error stop
   
 end program 
