749c0a9408cceca78fcf9672da77919c67d0e55a
HarshitaKalani <kalani.1@iitj.ac.in>
2024-03-01 15:03:40 +0530

    add and register test

---
+++ integration_tests/intrinsics_171.f90
@@ -0,0 +1,29 @@
+program intrinsics_171
+    print*, lgt("hello", "world")
+    if (lgt("hello", "world") .neqv. .false.) error stop 
+    print*, lgt("hello", "hello")
+    if (lgt("hello", "hello") .neqv. .false.) error stop
+    print*, lgt("lfortran", "fortran")
+    if (lgt("lfortran", "fortran") .neqv. .true.) error stop
+
+    print*, lge("hello", "world")
+    if (lge("hello", "world") .neqv. .false.) error stop 
+    print*, lge("hello", "hello")
+    if (lge("hello", "hello") .neqv. .true.) error stop
+    print*, lge("lfortran", "fortran")
+    if (lge("lfortran", "fortran") .neqv. .true.) error stop
+
+    print*, llt("hello", "world")
+    if (llt("hello", "world") .neqv. .true.) error stop 
+    print*, llt("hello", "hello")
+    if (llt("hello", "hello") .neqv. .false.) error stop
+    print*, llt("lfortran", "fortran")
+    if (llt("lfortran", "fortran") .neqv. .false.) error stop
+
+    print*, lle("hello", "world")
+    if (lle("hello", "world") .neqv. .true.) error stop 
+    print*, lle("hello", "hello")
+    if (lle("hello", "hello") .neqv. .true.) error stop
+    print*, lle("lfortran", "fortran")
+    if (lle("lfortran", "fortran") .neqv. .false.) error stop
+end
