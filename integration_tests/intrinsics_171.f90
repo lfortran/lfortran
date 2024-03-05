71e0c5789a7f587c3a4b56d15056c23cc11374fb
Shivi Mathur <mathur.7@iitj.ac.in>
2024-03-05 17:05:39 +0530

    changes after code review

--- integration_tests/intrinsics_171.f90
+++ integration_tests/intrinsics_171.f90
@@ -1,9 +1,7 @@
 program intrinsics_171
-    
     character(5):: name = "hELLo"
     print*, _lfortran_tolowercase("HeLo#")
     if (_lfortran_tolowercase(name) /= "hello") error stop
     print*, _lfortran_tolowercase(name)
     if (_lfortran_tolowercase("HeLo#") /= "helo#") error stop
-
 end
