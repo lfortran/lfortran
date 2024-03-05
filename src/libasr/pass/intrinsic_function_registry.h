9c50a1cc391886d54fd4a9c218bff696a7a3b695
Shivi Mathur <mathur.7@iitj.ac.in>
2024-03-05 16:42:10 +0530

    implement ibits

--- src/libasr/pass/intrinsic_function_registry.h
+++ src/libasr/pass/intrinsic_function_registry.h
@@ -67,6 +67,7 @@ inline std::string get_intrinsic_name(int x) {
         INTRINSIC_NAME_CASE(Ibclr)
         INTRINSIC_NAME_CASE(Ibset)
         INTRINSIC_NAME_CASE(Btest)
+        INTRINSIC_NAME_CASE(Ibits)
         INTRINSIC_NAME_CASE(Leadz)
         INTRINSIC_NAME_CASE(ToLowerCase)
         INTRINSIC_NAME_CASE(Digits)
@@ -243,6 +244,8 @@ namespace IntrinsicElementalFunctionRegistry {
             {&Btest::instantiate_Btest, &Btest::verify_args}},
         {static_cast<int64_t>(IntrinsicElementalFunctions::Ibset),
             {&Ibset::instantiate_Ibset, &Ibset::verify_args}},
+        {static_cast<int64_t>(IntrinsicElementalFunctions::Ibits),
+            {&Ibits::instantiate_Ibits, &Ibits::verify_args}},
         {static_cast<int64_t>(IntrinsicElementalFunctions::Leadz),
             {&Leadz::instantiate_Leadz, &Leadz::verify_args}},
         {static_cast<int64_t>(IntrinsicElementalFunctions::ToLowerCase),
@@ -485,7 +488,9 @@ namespace IntrinsicElementalFunctionRegistry {
         {static_cast<int64_t>(IntrinsicElementalFunctions::Ibset),
             "ibset"},     
         {static_cast<int64_t>(IntrinsicElementalFunctions::Btest),
-            "btest"},    
+            "btest"},  
+        {static_cast<int64_t>(IntrinsicElementalFunctions::Ibits),
+            "ibits"},  
         {static_cast<int64_t>(IntrinsicElementalFunctions::Leadz),
             "leadz"},
         {static_cast<int64_t>(IntrinsicElementalFunctions::ToLowerCase),
@@ -677,6 +682,7 @@ namespace IntrinsicElementalFunctionRegistry {
                 {"ibclr", {&Ibclr::create_Ibclr, &Ibclr::eval_Ibclr}},
                 {"ibset", {&Ibset::create_Ibset, &Ibset::eval_Ibset}},
                 {"btest", {&Btest::create_Btest, &Btest::eval_Btest}},
+                {"ibits", {&Ibits::create_Ibits, &Ibits::eval_Ibits}},
                 {"leadz", {&Leadz::create_Leadz, &Leadz::eval_Leadz}},
                 {"_lfortran_tolowercase", {&ToLowerCase::create_ToLowerCase, &ToLowerCase::eval_ToLowerCase}},
                 {"hypot", {&Hypot::create_Hypot, &Hypot::eval_Hypot}},
--- src/libasr/pass/intrinsic_functions.h
+++ src/libasr/pass/intrinsic_functions.h
@@ -67,6 +67,7 @@ enum class IntrinsicElementalFunctions : int64_t {
     Ibclr,
     Ibset,
     Btest,
+    Ibits,
     Leadz,
     ToLowerCase,
     Digits,
@@ -1529,6 +1530,44 @@ namespace Btest {
     }
 
 } // namespace Btest
+
+namespace Ibits {
+
+    static ASR::expr_t *eval_Ibits(Allocator &al, const Location &loc,
+            ASR::ttype_t* t1, Vec<ASR::expr_t*> &args, diag::Diagnostics& /*diag*/) {
+        int64_t val1 = ASR::down_cast<ASR::IntegerConstant_t>(args[0])->m_n;
+        int64_t val2 = ASR::down_cast<ASR::IntegerConstant_t>(args[1])->m_n;
+        int64_t val3 = ASR::down_cast<ASR::IntegerConstant_t>(args[2])->m_n;
+        int64_t result;
+        result = (val1 >> val2) & ((1 << val3) - 1);
+        return make_ConstantWithType(make_IntegerConstant_t, result, t1, loc);
+    }
+
+    static inline ASR::expr_t* instantiate_Ibits(Allocator &al, const Location &loc,
+            SymbolTable *scope, Vec<ASR::ttype_t*>& arg_types, ASR::ttype_t *return_type,
+            Vec<ASR::call_arg_t>& new_args, int64_t /*overload_id*/) {
+        declare_basic_variables("");
+        fill_func_arg("x", arg_types[0]);
+        fill_func_arg("y", arg_types[1]);
+        fill_func_arg("z", arg_types[2]);
+        auto result = declare(fn_name, return_type, ReturnVar);
+        
+        ASR::expr_t *val1 = args[0];
+        ASR::expr_t *val2 = args[1];
+        ASR::expr_t *val3 = args[2];
+        ASR::expr_t *one = i(1, arg_types[0]);
+
+        ASR::expr_t *cast_val2 = ASRUtils::EXPR(ASR::make_Cast_t(al, loc, val2, ASR::cast_kindType::IntegerToInteger, arg_types[0], nullptr));
+        ASR::expr_t *cast_val3 = ASRUtils::EXPR(ASR::make_Cast_t(al, loc, val3, ASR::cast_kindType::IntegerToInteger, arg_types[0], nullptr));
+        body.push_back(al, b.Assignment(result, i_BitAnd(i_BitRshift(val1, cast_val2, return_type), iSub(i_BitLshift(one, cast_val3, return_type), one), return_type)));
+        
+        ASR::symbol_t *f_sym = make_ASR_Function_t(fn_name, fn_symtab, dep, args,
+            body, result, ASR::abiType::Source, ASR::deftypeType::Implementation, nullptr);
+        scope->add_symbol(fn_name, f_sym);
+        return b.Call(f_sym, new_args, return_type, nullptr);
+    }
+
+} // namespace Ibits
 
 namespace Aint {
 
