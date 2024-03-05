f34c6bdc02395a434fcf3a26acb7bb65de13ec96
parth121101 <parthmistry1211@gmail.com>
2024-03-05 13:38:51 +0530

    Added missing tests for exponent function

--- integration_tests/intrinsics_170.f90
+++ integration_tests/intrinsics_170.f90
@@ -101,4 +101,125 @@ program intrinsics_170
     print *, exponent(0.0)
     if (exponent(0.0) /= 0) error stop
 
+    print *, exponent(3.99)
+    if (exponent(3.99) /= 2) error stop
+
+    print *, exponent(3.99_dp)
+    if (exponent(3.99_dp) /= 2) error stop
+
+    x = 3.99
+    y = 3.99_dp
+
+    print *, exponent(x)
+    if (exponent(x) /= 2) error stop
+
+    print *, exponent(y)
+    if (exponent(y) /= 2) error stop
+
+    print *, exponent(7.99)
+    if (exponent(7.99) /= 3) error stop
+
+    print *, exponent(7.99_dp)
+    if (exponent(7.99_dp) /= 3) error stop
+
+    x = 7.99
+    y = 7.99_dp
+
+    print *, exponent(x)
+    if (exponent(x) /= 3) error stop
+
+    print *, exponent(y)
+    if (exponent(y) /= 3) error stop
+
+    print *, exponent(8.1)
+    if (exponent(8.1) /= 4) error stop
+
+    print *, exponent(8.1_dp)
+    if (exponent(8.1_dp) /= 4) error stop
+
+    x = 8.1
+    y = 8.1_dp
+
+    print *, exponent(x)
+    if (exponent(x) /= 4) error stop
+
+    print *, exponent(y)
+    if (exponent(y) /= 4) error stop
+
+    print *, exponent(2.0)
+    if (exponent(2.0) /= 2) error stop
+
+    print *, exponent(2.0_dp)
+    if (exponent(2.0_dp) /= 2) error stop
+
+    x = 2.0
+    y = 2.0_dp
+
+    print *, exponent(x)
+    if (exponent(x) /= 2) error stop
+
+    print *, exponent(y)
+    if (exponent(y) /= 2) error stop
+
+    print *, exponent(8.0)
+    if (exponent(8.0) /= 4) error stop
+
+    print *, exponent(8.0_dp)
+    if (exponent(8.0_dp) /= 4) error stop
+
+    x = 8.0
+    y = 8.0_dp
+
+    print *, exponent(x)
+    if (exponent(x) /= 4) error stop
+
+    print *, exponent(y)
+    if (exponent(y) /= 4) error stop
+
+    print *, exponent(0.1)
+    if (exponent(0.1) /= -3) error stop
+
+    print *, exponent(0.1_dp)
+    if (exponent(0.1_dp) /= -3) error stop
+
+    x = 0.1
+    y = 0.1_dp
+
+    print *, exponent(x)
+    if (exponent(x) /= -3) error stop
+
+    print *, exponent(y)
+    if (exponent(y) /= -3) error stop
+
+    print *, exponent(0.01)
+    if (exponent(0.01) /= -6) error stop
+
+    print *, exponent(0.01_dp)
+    if (exponent(0.01_dp) /= -6) error stop
+
+    x = 0.01
+    y = 0.01_dp
+
+    print *, exponent(x)
+    if (exponent(x) /= -6) error stop
+
+    print *, exponent(y)
+    if (exponent(y) /= -6) error stop
+
+    print *, exponent(0.001)
+    if (exponent(0.001) /= -9) error stop
+
+    print *, exponent(0.001_dp)
+    if (exponent(0.001_dp) /= -9) error stop
+
+    x = 0.001
+    y = 0.001_dp
+
+    print *, exponent(x)
+    if (exponent(x) /= -9) error stop
+
+    print *, exponent(y)    
+    if (exponent(y) /= -9) error stop
+
+
 end program
