17d4198427481e1d6039c8e53b6bf9bae845c0ca
Pranavchiku <goswami.4@iitj.ac.in>
2024-03-08 10:50:54 +0530

    test: add and register

--- integration_tests/array_01_pack.f90
+++ integration_tests/array_01_pack.f90
@@ -1,5 +1,9 @@
 program array_01_pack
   integer :: m(6), p(2)
+  integer :: s(1)
   m = [ 1, 0, 0, 0, 5, 0 ]
   p = pack(m, m /= 0)
+  s = shape(pack(m, m /= 0))
+  print *, s(1)
+  if (s(1) /= 2) error stop
 end program
