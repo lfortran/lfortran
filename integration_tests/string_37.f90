00c2e8c629bd60d95b3df460f2babaf780dfc048
HarshitaKalani <kalani.1@iitj.ac.in>
2024-03-17 15:54:25 +0530

    update test with error stop condition

--- integration_tests/string_37.f90
+++ integration_tests/string_37.f90
@@ -1,6 +1,8 @@
 program string_37
     character(5) :: string
     print*, max(maybe(string), 'works')
+    if (max(maybe(string), 'works') /= 'works') error stop 
+    
     contains
 
     function maybe(string) result(maybe_string)
@@ -8,4 +10,3 @@ program string_37
         character(len=len(string)) :: maybe_string
     end function maybe
 end
-
