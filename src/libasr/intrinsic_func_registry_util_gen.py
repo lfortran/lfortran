167ecb6a86c1c150b6d9a1b43d5928622e4e6781
HarshitaKalani <kalani.1@iitj.ac.in>
2024-03-18 22:58:47 +0530

    implement index

--- src/libasr/intrinsic_func_registry_util_gen.py
+++ src/libasr/intrinsic_func_registry_util_gen.py
@@ -104,6 +104,12 @@ intrinsic_funcs_args = {
             "ret_type_arg_idx": 3
         }
     ],
+    "SubstrIndex": [
+        {
+            "args": [("char", "char", "bool", "int")],
+            "ret_type_arg_idx": 3
+        }
+    ],
     "MinExponent": [
         {
             "args": [("real",)],
