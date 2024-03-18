b3b75e75229edb7d59a4633e543003ca3cf08671
HarshitaKalani <kalani.1@iitj.ac.in>
2024-03-18 22:58:57 +0530

    update test

--- integration_tests/intrinsics_91.f90
+++ integration_tests/intrinsics_91.f90
@@ -1,7 +1,32 @@
 program intrinsics_91
-    if ( index('abc', 'b') /= 2 ) error stop
-    if ( index("elephant", "an") /= 6 ) error stop
+    character(3) :: abc = "abc"
+    character(8) :: elephant = "elephant"
+    character(6) :: potato = "potato"
+    character(1) :: b = "b"
+    character(5) :: an = "@a!n#"
+    character(1) :: t = "t"
+    character(2) :: ta = "ta"
+
+    print*, index("abc", "b")
+    if ( index("abc", "b") /= 2 ) error stop
+    print*, index("elephant", "@a!n#")
+    if ( index("elephant", "@a!n#") /= 0 ) error stop
+    print*, index("potato", "t", back=.true.)
     if ( index("potato", "t", back=.true.) /= 5 ) error stop
+    print*, index("potato", "t", back=.false.)
     if ( index("potato", "t", back=.false.) /= 3 ) error stop
+    print*, index("potato", "ta", back=.true.)
     if ( index("potato", "ta", back=.true.) /= 3 ) error stop
-end
+
+    print*, index(abc, b)
+    if ( index(abc, b) /= 2 ) error stop
+    print*, index(elephant, an)
+    if ( index(elephant, an) /= 0 ) error stop
+    print*, index(potato, t, back=.true.)
+    if ( index(potato, t, back=.true.) /= 5 ) error stop
+    print*, index(potato, t, back=.false.)
+    if ( index(potato, t, back=.false.) /= 3 ) error stop
+    print*, index(potato, ta, back=.true.)
+    if ( index(potato, ta, back=.true.) /= 3 ) error stop
+
+end program
