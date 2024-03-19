9686f15fb78181c172ea31dfe359784427e255d1
HarshitaKalani <kalani.1@iitj.ac.in>
2024-03-19 13:22:10 +0530

    remove old runtime implementation

--- src/runtime/pure/lfortran_intrinsic_string.f90
+++ src/runtime/pure/lfortran_intrinsic_string.f90
@@ -4,16 +4,6 @@ implicit none
 
 contains
 
-integer elemental function len_trim(string) result(r)
-    character(len=*), intent(in) :: string
-    r = len(string)
-    if (r == 0) return
-    do while(string(r:r) == " ")
-        r = r - 1
-        if (r == 0) exit
-    end do
-end function
-
 function trim(x) result(r)
     character(len=*),intent(in) :: x
     character(len=len_trim(x)) :: r
