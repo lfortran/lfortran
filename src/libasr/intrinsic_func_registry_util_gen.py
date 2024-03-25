63a3946fe049523f9d815a681f87a121f204fccc
parth121101 <parthmistry1211@gmail.com>
2024-03-25 18:55:40 +0530

    dev: implemented intrinsic bessely0

--- src/libasr/intrinsic_func_registry_util_gen.py
+++ src/libasr/intrinsic_func_registry_util_gen.py
@@ -50,6 +50,12 @@ intrinsic_funcs_args = {
             "ret_type_arg_idx": 0
         },
     ],
+    "BesselY0": [
+        {
+            "args": [("real",)],
+            "ret_type_arg_idx": 0
+        },
+    ],
     "Mvbits": [
         {
             "args": [("int", "int", "int", "int", "int")],
