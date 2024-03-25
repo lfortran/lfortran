ecc1c637dc820f007e5d22e952b082306de6d21b
HarshitaKalani <kalani.1@iitj.ac.in>
2024-03-25 13:21:06 +0530

    remove lfortran_intrinsic_math2 file

--- src/lfortran/semantics/ast_to_asr.cpp
+++ src/lfortran/semantics/ast_to_asr.cpp
@@ -53,13 +53,12 @@ void load_rtlib(Allocator &al, ASR::TranslationUnit_t &tu, CompilerOptions &comp
     SymbolTable *tu_symtab = tu.m_symtab;
     const std::string m_builtin = "lfortran_intrinsic_builtin";
     const std::string m_math = "lfortran_intrinsic_math";
-    const std::string m_math2 = "lfortran_intrinsic_math2";
     const std::string m_math3 = "lfortran_intrinsic_math3";
     const std::string m_string = "lfortran_intrinsic_string";
     const std::string m_bit = "lfortran_intrinsic_bit";
     const std::string m_ieee_arithmetic = "lfortran_intrinsic_ieee_arithmetic";
     std::vector<std::string> intrinsic_modules = {
-        m_math2,
+        //
     };
     for (auto &module_name : intrinsic_modules) {
         Location loc;
