10a3621184bfbcf69872833c2168248687fd48cf
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2024-03-28 12:51:18 +0530

    update tests

--- integration_tests/module_function_with_nopass.f90
+++ integration_tests/module_function_with_nopass.f90
@@ -6,6 +6,7 @@ module modules_module_function_with_nopass
     type :: calculator
     contains
         procedure, nopass :: SQUARE  ! Associate SQUARE with the calculator type
+        procedure, nopass :: AREA
     end type calculator
 
 contains
@@ -22,16 +23,37 @@ contains
         end if
     end function SQUARE
 
+    function AREA(x, y) result(area_result)
+        integer, optional, intent(in) :: x
+        integer, optional, intent(in) :: y
+        integer :: area_result
+
+        if (present(x) .and. present(y)) then
+            area_result = x * y
+        else
+            area_result = 1
+        end if
+    end function
+
 end module modules_module_function_with_nopass
 
 program module_function_with_nopass
     use modules_module_function_with_nopass
     implicit none
     type(calculator) :: calc
-    integer :: number, result
+    integer :: number, result, area1, area2, area3
 
     number = 4
 
     result = calc%SQUARE(number)
-    if (abs(result) /= 16) error stop
+    if (result /= 16) error stop
+
+    area1 = calc%AREA()
+    if (area1 /= 1) error stop
+
+    area2 = calc%AREA(1, 2)
+    if (area2 /= 2) error stop
+
+    area3 = calc%AREA(1)
+    if (area3 /= 1) error stop
 end program module_function_with_nopass
