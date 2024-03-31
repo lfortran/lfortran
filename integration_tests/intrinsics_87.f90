d30200c61c69dddd0d6a4183c5022db68ad1e0dc
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2024-04-01 01:06:44 +0530

    fix: assign "m_type" for complex array argument in Abs correctly (#3750)

--- integration_tests/intrinsics_87.f90
+++ integration_tests/intrinsics_87.f90
@@ -1,10 +1,15 @@
 program intrinsics_87
-implicit none
-integer, allocatable :: C(:, :)
-allocate(C(5, 10))
-C = -10
-C = abs(C)
-print *, C
-if (any(C /= 10)) error stop
-end program
+    implicit none
+    integer, allocatable :: C(:, :)
+    complex(4) :: arr(3)
+    allocate(C(5, 10))
+    C = -10
+    C = abs(C)
+    print *, C
+    if (any(C /= 10)) error stop
 
+    ! test if 'abs' of complex array results in a real array
+    arr = [(3, 4), (5, 6), (7, 8)]
+    print *, sum(abs(arr))
+    if (sum(abs(arr)) - 23.4403954 > 1e-7) error stop
+end program
