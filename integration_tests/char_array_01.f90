93b4ff6203bae45df147a1b66c225ac420aac779
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2024-04-09 16:21:19 +0530

    assign "m_end" as UpperBound when an array of character
    Fixes #3039

---
+++ integration_tests/char_array_01.f90
@@ -0,0 +1,9 @@
+program char_array_01
+    character char_arr1(4)
+    character char_arr2(2)
+    char_arr1(:) = "A"
+    char_arr2(:) = " "
+
+    print *, char_arr1
+    print *, char_arr2
+end program char_array_01
