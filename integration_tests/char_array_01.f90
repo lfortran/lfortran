91e6fe6b62f5babf056c2f275d96b5f0db2cf921
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2024-04-10 11:41:53 +0530

    add ASR file and check with "if ... error stop" condition

--- integration_tests/char_array_01.f90
+++ integration_tests/char_array_01.f90
@@ -5,5 +5,12 @@ program char_array_01
     char_arr2(:) = " "
 
     print *, char_arr1
+    if (char_arr1(1) /= "A") error stop
+    if (char_arr1(2) /= "A") error stop
+    if (char_arr1(3) /= "A") error stop
+    if (char_arr1(4) /= "A") error stop
+
     print *, char_arr2
+    if (char_arr2(1) /= " ") error stop
+    if (char_arr2(2) /= " ") error stop
 end program char_array_01
---
+++ tests/char_arr_assignment.f90
@@ -0,0 +1,4 @@
+program char_arr_assignment
+    character char_arr(4)
+    char_arr(:) = "A"
+end program char_arr_assignment
