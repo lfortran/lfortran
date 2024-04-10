1c535819a2baa65a7eb769a350d0080f9eb0f0e0
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2024-04-10 18:20:02 +0530

    update docs

--- integration_tests/character_01.f90
+++ integration_tests/character_01.f90
@@ -1,6 +1,8 @@
 program character_01
    CHARACTER A*4, B*4, C(2)*3
    CHARACTER, parameter :: D*3 = "ape"
+   CHARACTER(len=3) :: E(2) = "apel"
+
    A = 'ABCD'
    B = 'EFGH'
    C(1) = 'IJK'
@@ -8,4 +10,5 @@ program character_01
 
    print *, A, B, C(1), C(2)
    print *, D
+   print *, E
 end program
