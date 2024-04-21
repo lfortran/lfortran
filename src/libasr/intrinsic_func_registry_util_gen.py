36a9613d78b5b9c264ffbad4173780af92175882
Mistry Parth <parth1211@Parths-Laptop.local>
2024-04-21 14:19:37 +0530

    enh: create macros for math functions

--- src/libasr/intrinsic_func_registry_util_gen.py
+++ src/libasr/intrinsic_func_registry_util_gen.py
@@ -44,34 +44,10 @@ intrinsic_funcs_args = {
             "ret_type_arg_idx": 0
         },
     ],
-    "BesselJ0": [
-        {
-            "args": [("real",)],
-            "ret_type_arg_idx": 0
-        },
-    ],
-    "BesselJ1": [
-        {
-            "args": [("real",)],
-            "ret_type_arg_idx": 0
-        },
-    ],
     "BesselJN": [
         {
             "args": [("int", "real")],
             "ret_type_arg_idx": 1
-        },
-    ],
-    "BesselY0": [
-        {
-            "args": [("real",)],
-            "ret_type_arg_idx": 0
-        },
-    ],
-    "BesselY1": [
-        {
-            "args": [("real",)],
-            "ret_type_arg_idx": 0
         },
     ],
     "BesselYN": [
@@ -239,24 +215,6 @@ intrinsic_funcs_args = {
             "args": [("real",)],
             "return": "int32",
             "kind_arg": True
-        }
-    ],
-    "Asind": [
-        {
-            "args": [("real",)],
-            "ret_type_arg_idx": 0
-        }
-    ],
-    "Acosd": [
-        {
-            "args": [("real",)],
-            "ret_type_arg_idx": 0
-        }
-    ],
-    "Atand": [
-        {
-            "args": [("real",)],
-            "ret_type_arg_idx": 0
         }
     ],
     "Sqrt": [
