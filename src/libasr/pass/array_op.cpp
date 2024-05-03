4ee756119849c660b21f92ea97fe6a986c48b103
Gagandeep Singh <gdp.1807@gmail.com>
2024-05-04 00:00:10 +0530

    DEV: Replace Var with ArrayItem

--- src/libasr/pass/array_op.cpp
+++ src/libasr/pass/array_op.cpp
@@ -13,6 +13,42 @@
 
 namespace LCompilers {
 
+class ArrayVarAddressReplacer: public ASR::BaseExprReplacer<ArrayVarAddressReplacer> {
+
+    public:
+
+    Allocator& al;
+    Vec<ASR::expr_t**>& vars;
+
+    ArrayVarAddressReplacer(Allocator& al_, Vec<ASR::expr_t**>& vars_):
+        al(al_), vars(vars_) {}
+
+    void replace_Var(ASR::Var_t* x) {
+        if( ASRUtils::is_array(ASRUtils::symbol_type(x->m_v)) ) {
+            vars.push_back(al, current_expr);
+        }
+    }
+
+};
+
+class ArrayVarAddressCollector: public ASR::CallReplacerOnExpressionsVisitor<ArrayVarAddressCollector> {
+
+    private:
+
+    ArrayVarAddressReplacer replacer;
+
+    public:
+
+    void call_replacer() {
+        replacer.current_expr = current_expr;
+        replacer.replace_expr(*current_expr);
+    }
+
+    ArrayVarAddressCollector(Allocator& al_, Vec<ASR::expr_t**>& vars_):
+        replacer(al_, vars_) {}
+
+};
+
 class ReplaceArrayOp: public ASR::BaseExprReplacer<ReplaceArrayOp> {
 
     private:
@@ -91,13 +127,79 @@ class ArrayOpVisitor: public ASR::CallReplacerOnExpressionsVisitor<ArrayOpVisito
         n_body = body.size();
     }
 
+    bool call_replace_on_expr(ASR::exprType expr_type) {
+        switch( expr_type ) {
+            case ASR::exprType::ArrayConstant: {
+                return true;
+            }
+            default: {
+                return false;
+            }
+        }
+    }
+
     void visit_Assignment(const ASR::Assignment_t& x) {
-        replacer.result_expr = x.m_target;
-        ASR::expr_t** current_expr_copy = current_expr;
-        current_expr = const_cast<ASR::expr_t**>(&x.m_value);
-        this->call_replacer();
-        current_expr = current_expr_copy;
-        replacer.result_expr = nullptr;
+        const Location loc = x.base.base.loc;
+        if( call_replace_on_expr(x.m_value->type) ) {
+            replacer.result_expr = x.m_target;
+            ASR::expr_t** current_expr_copy = current_expr;
+            current_expr = const_cast<ASR::expr_t**>(&x.m_value);
+            this->call_replacer();
+            current_expr = current_expr_copy;
+            replacer.result_expr = nullptr;
+            return ;
+        }
+
+        Vec<ASR::expr_t**> vars;
+        vars.reserve(al, 1);
+        vars.push_back(al, const_cast<ASR::expr_t**>(&(x.m_target)));
+        ArrayVarAddressCollector var_collector(al, vars);
+        var_collector.current_expr = const_cast<ASR::expr_t**>(&(x.m_value));
+        var_collector.call_replacer();
+
+        Vec<size_t> var_ranks;
+        var_ranks.reserve(al, vars.size());
+        for( size_t i = 0; i < vars.size(); i++ ) {
+            ASR::expr_t* expr = *vars[i];
+            ASR::ttype_t* type = ASRUtils::expr_type(expr);
+            var_ranks.push_back(al, ASRUtils::extract_n_dims_from_ttype(type));
+        }
+
+        std::unordered_map<size_t, Vec<ASR::expr_t*>> var2indices;
+        ASR::ttype_t* int32_type = ASRUtils::TYPE(ASR::make_Integer_t(al, loc, 4));
+        for( size_t i = 0; i < vars.size(); i++ ) {
+            Vec<ASR::expr_t*> indices;
+            indices.reserve(al, var_ranks[i]);
+            for( size_t j = 0; j < var_ranks[i]; j++ ) {
+                std::string index_var_name = current_scope->get_unique_name(
+                    "__libasr_index_" + std::to_string(j) + "_");
+                ASR::symbol_t* index = ASR::down_cast<ASR::symbol_t>(ASR::make_Variable_t(
+                    al, loc, current_scope, s2c(al, index_var_name), nullptr, 0, ASR::intentType::Local,
+                    nullptr, nullptr, ASR::storage_typeType::Default, int32_type, nullptr,
+                    ASR::abiType::Source, ASR::accessType::Public, ASR::presenceType::Required, false));
+                current_scope->add_symbol(index_var_name, index);
+                ASR::expr_t* index_expr = ASRUtils::EXPR(ASR::make_Var_t(al, loc, index));
+                indices.push_back(al, index_expr);
+            }
+            var2indices[i] = indices;
+        }
+
+        for( size_t i = 0; i < vars.size(); i++ ) {
+            Vec<ASR::array_index_t> indices;
+            indices.reserve(al, var_ranks[i]);
+            for( size_t j = 0; j < var_ranks[i]; j++ ) {
+                ASR::array_index_t array_index;
+                array_index.loc = loc;
+                array_index.m_left = nullptr;
+                array_index.m_right = var2indices[i][j];
+                array_index.m_step = nullptr;
+                indices.push_back(al, array_index);
+            }
+            ASR::ttype_t* var_i_type = ASRUtils::type_get_past_array_pointer_allocatable(
+                ASRUtils::expr_type(*vars[i]));
+            *vars[i] = ASRUtils::EXPR(ASR::make_ArrayItem_t(al, loc, *vars[i], indices.p,
+                indices.size(), var_i_type, ASR::arraystorageType::ColMajor, nullptr));
+        }
     }
 
 };
