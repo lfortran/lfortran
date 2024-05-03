24a2ed2011ae1320c8fba8d2a400e72a69bea761
Shivi Mathur <mathur.7@iitj.ac.in>
2024-05-03 22:36:28 +0530

    Added double precision

--- integration_tests/cos_01.f90
+++ integration_tests/cos_01.f90
@@ -4,6 +4,6 @@ implicit none
 real(dp) :: x
 x = cos(1.5_dp)
 print *, x
-if (abs(x - 7.07372016677029064e-02) > 1e-6) error stop
+if (abs(x - 7.07372016677029064e-02_dp) > 1e-6_dp) error stop
 end program
 
