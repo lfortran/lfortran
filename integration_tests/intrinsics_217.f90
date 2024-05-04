6b962a1ed3c081cde3561a201dbe39dd1d367c96
HarshitaKalani <kalani.1@iitj.ac.in>
2024-05-04 19:55:49 +0530

    update test

--- integration_tests/intrinsics_217.f90
+++ integration_tests/intrinsics_217.f90
@@ -1,15 +1,36 @@
 program intrinsics_217
-    logical l
+    implicit none
+    logical :: l
+    logical :: a(3) = [.true., .true., .false.]
+    logical :: x(2, 2)
+
+    print *, all(a)
+    if (all(a)) error stop
+
+    a = [.true., .true., .true.]
+    print *, all(a)
+    if (.not. all(a)) error stop
+    x(1, 1) = .true.
+    x(1, 2) = .true.
+    x(2, 1) = .true.
+    x(2, 2) = .false.
+
+    print *, all(x)
+    if (all(x)) error stop
+
     l = all([.true., .true., .true.])
     print *, l
     call section
-    contains
+
+contains
     subroutine section
-    integer a(2,3), b(2,3)
-    a = 1
-    b = 1
-    b(2,2) = 2
-    print *, all(a == b, 1)
-    print *, all(a == b, 2)
+        integer a(2,3), b(2,3)
+        a = 1
+        b = 1
+        b(2,2) = 2
+        print *, all(a == b, 1)
+        if (all(all(a == b, 1))) error stop
+        print *, all(a == b, 2)
+        if (.not. any(all(a == b, 2))) error stop
     end subroutine section
 end program
