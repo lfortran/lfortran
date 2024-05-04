586c3727510f40388e5398e1e4708899842f9430
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2024-05-04 16:40:31 +0530

    don't allow initialization without "::"
    Fixes #3945

---
+++ tests/errors/missing_colons_init.f90
@@ -0,0 +1,7 @@
+program missing_colons_init
+    implicit none
+    integer :: p, q = 1, r
+    integer :: a = 1, b, c = 1
+    integer :: d, e, f = 1
+    integer n = 1 ! bad syntax
+end program missing_colons_init
