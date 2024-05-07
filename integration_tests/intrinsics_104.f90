8f6a7eeff0662fd6f7b4edaac402d94d456cb229
HarshitaKalani <kalani.1@iitj.ac.in>
2024-05-07 15:41:06 +0530

    update test

--- integration_tests/intrinsics_104.f90
+++ integration_tests/intrinsics_104.f90
@@ -1,9 +1,13 @@
 program intrinsics_104
-implicit none
-
-integer, parameter :: new_len = len(new_line('a'))
-
-print *, new_len
-if (new_len /= 1) error stop
-
+    implicit none
+    
+    integer, parameter :: new_len = len(new_line('a'))
+    integer, parameter :: new_len2 = len(adjustl('a'))
+    
+    print *, new_len
+    if (new_len /= 1) error stop
+    
+    print*, new_len2
+    if (new_len2 /= 1) error stop
+    
 end program
