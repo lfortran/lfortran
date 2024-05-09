5211c978034e67327dddb4f3249cf19c23e36f25
parth121101 <parthmistry1211@gmail.com>
2024-05-10 02:45:54 +0530

    forced compile time evaluation using integer, parameter :: res

--- integration_tests/intrinsics_220.f90
+++ integration_tests/intrinsics_220.f90
@@ -1,6 +1,29 @@
 program intrinsics_220
     integer :: a, b, i
     integer(8) :: c, d, e
+    integer(4) :: expected_1(3)
+    integer(8) :: expected_2(3)
+    integer :: iterator
+
+    integer(4), parameter :: result_1(3) = merge_bits([-10, 10, -10], [-15, 15, 15], [-2, 2, -2])
+
+    integer(8), parameter :: result_2(3) = merge_bits([1005627, -1005627827, 92748272], &
+    [1027293, 1026271812, -82927282], [10098, -1628291, -10098])
+
+    expected_1 = [-9, 15, -9]
+    expected_2 = [1018047_8, -1005613491_8, 92748224_8]
+
+
+    do iterator = 1, size(result_1)
+        print *, result_1(iterator)
+        if (result_1(iterator) /= expected_1(iterator)) error stop
+    end do
+
+    do iterator = 1, size(result_2)
+        print *, result_2(iterator)
+        if (result_2(iterator) /= expected_2(iterator)) error stop
+    end do
+
     a = -10
     b = -15
     i = -2
@@ -14,12 +37,6 @@ program intrinsics_220
 
     print *, merge_bits(c, d, e)
     if(merge_bits(c, d, e) /= 1018047) error stop
-
-    print *, merge_bits(-10, -15, -2)
-    if(merge_bits(-10, -15, -2) /= -9) error stop
-
-    print *, merge_bits(1005627, 1027293, 10098)
-    if(merge_bits(1005627, 1027293, 10098) /= 1018047) error stop
 
     a = 10
     b = 15
@@ -35,12 +52,6 @@ program intrinsics_220
     print *, merge_bits(c, d, e)
     if(merge_bits(c, d, e) /= -1005613491) error stop
 
-    print *, merge_bits(10, 15, 2)
-    if(merge_bits(10, 15, 2) /= 15) error stop
-
-    print *, merge_bits(-1005627827, 1026271812, -1628291)
-    if(merge_bits(-1005627827, 1026271812, -1628291) /= -1005613491) error stop
-
     a = -10
     b = 15
     i = -2
@@ -54,11 +65,5 @@ program intrinsics_220
 
     print *, merge_bits(c, d, e)
     if(merge_bits(c, d, e) /= 92748224) error stop
-
-    print *, merge_bits(-10, 15, -2)
-    if(merge_bits(-10, 15, -2) /= -9) error stop
-
-    print *, merge_bits(92748272, -82927282, -10098)
-    if(merge_bits(92748272, -82927282, -10098) /= 92748224) error stop
 
 end program
