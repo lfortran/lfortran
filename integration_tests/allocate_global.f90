05cbce65c375b68edbd8c4ed8021bb02402dee2f
Saurabh Kumar <developer.saurabh@outlook.com>
2024-05-13 19:10:00 +0530

    TESTS: Add test

---
+++ integration_tests/allocate_global.f90
@@ -0,0 +1,14 @@
+module a
+    implicit none
+    integer, allocatable, dimension(:) :: f
+ end module a
+ 
+ program mm
+    use a
+    implicit none
+    allocate(f(4))
+    f = [1, 2, 3, 4]
+    print *,f
+    
+ end program mm
+ 
