306efee6e522b61d244723fc7aa9326278ae6464
Pranavchiku <goswami.4@iitj.ac.in>
2024-05-14 10:53:16 +0530

    chore: uncomment error stop conditions

--- integration_tests/intrinsics_213.f90
+++ integration_tests/intrinsics_213.f90
@@ -12,7 +12,7 @@ program intrinsics_213
   end do
   call cpu_time(t2)
   print "(2(A,F10.3))",'after 10**k exp, abs(t2-t1) = ',abs(t2-t1),' s; z=',z
-  !if (abs(z - 1.03174341) > 1e-8) error stop
+  if (abs(z) > 1e-8) error stop
   x = 1
   do n = 1,10**k
      x = x+dx
@@ -20,6 +20,6 @@ program intrinsics_213
   end do
   call cpu_time(t1)
   print "(2(A,F10.3))",'after 10**k log, abs(t2-t1) = ',abs(t2-t1),' s; z=',z
-  !if (abs(z - (-3.46573591)) > 1e-8) error stop
+  if (abs(z - (9.53628842e-05)) > 1e-8) error stop
 end program intrinsics_213
 
