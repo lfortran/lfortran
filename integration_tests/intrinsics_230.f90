03d4cc212c7c1b50c62d006d8d19a582ea0159de
HarshitaKalani <kalani.1@iitj.ac.in>
2024-05-26 10:30:01 +0530

    rectify mistakes in test

--- integration_tests/intrinsics_230.f90
+++ integration_tests/intrinsics_230.f90
@@ -23,51 +23,51 @@ program intrinsic_230
     res1_yn = bessel_yn(1, [3.3398640543782485_dp, 5.039184326269492_dp])
 
     print*, res0_j0
-    if (abs(res0_j0(1) - (-0.35276533724012676)) > 1e6) error stop
-    if (abs(res0_j0(2) - (-0.35276533724012676)) > 1e6) error stop
+    if (abs(res0_j0(1) - (-0.35276533724012676)) > 1e-6) error stop
+    if (abs(res0_j0(2) - (-0.35276533724012676)) > 1e-6) error stop
 
     print*, res0_j1
-    if (abs(res0_j1(1) - 0.581545886001298) > 1e6) error stop
-    if (abs(res0_j1(2) - 0.581545886001298) > 1e6) error stop
+    if (abs(res0_j1(1) - 0.20421621687655642) > 1e-6) error stop
+    if (abs(res0_j1(2) - (-0.33171193864355270)) > 1e-6) error stop
 
     print*, res0_jn
-    if (abs(res0_jn(1) - 0.581545886001298) > 1e6) error stop
-    if (abs(res0_jn(2) - 0.581545886001298) > 1e6) error stop
+    if (abs(res0_jn(1) - 0.20421621687655642) > 1e-6) error stop
+    if (abs(res0_jn(2) - (-0.33171193864355270)) > 1e-6) error stop
 
     print*, res0_y0
-    if (abs(res0_y0(1) - 0.281323269014833) > 1e6) error stop
-    if (abs(res0_y0(2) - 0.281323269014833) > 1e6) error stop
+    if (abs(res0_y0(1) - 0.25351438512601238) > 1e-6) error stop
+    if (abs(res0_y0(2) - 0.25351438512601238) > 1e-6) error stop
 
     print*, res0_y1
-    if (abs(res0_y1(1) - 0.506133477927775) > 1e6) error stop
-    if (abs(res0_y1(2) - 0.506133477927775) > 1e6) error stop
+    if (abs(res0_y1(1) - 0.39357815895597936) > 1e-6) error stop
+    if (abs(res0_y1(2) - 0.13456148643731775) > 1e-6) error stop
 
     print*, res0_yn
-    if (abs(res0_yn(1) - 0.506133477927775) > 1e6) error stop
-    if (abs(res0_yn(2) - 0.506133477927775) > 1e6) error stop
+    if (abs(res0_yn(1) - 0.39357815895597936) > 1e-6) error stop
+    if (abs(res0_yn(2) - 0.13456148643731775) > 1e-6) error stop
     
     print*, res1_j0
-    if (abs(res1_j0(1) - (-0.35276533724012676)) > 1e6) error stop
-    if (abs(res1_j0(2) - (-0.35276533724012676)) > 1e6) error stop
+    if (abs(res1_j0(1) - (-0.35276533724012676)) > 1e-6) error stop
+    if (abs(res1_j0(2) - (-0.35276533724012676)) > 1e-6) error stop
 
     print*, res1_j1
-    if (abs(res1_j1(1) - 0.581545886001298) > 1e6) error stop
-    if (abs(res1_j1(2) - 0.581545886001298) > 1e6) error stop
+    if (abs(res1_j1(1) - 0.20421621687655642) > 1e-6) error stop
+    if (abs(res1_j1(2) - (-0.33171193864355270)) > 1e-6) error stop
 
     print*, res1_jn
-    if (abs(res1_jn(1) - 0.581545886001298) > 1e6) error stop
-    if (abs(res1_jn(2) - 0.581545886001298) > 1e6) error stop
+    if (abs(res1_jn(1) - 0.20421621687655642) > 1e-6) error stop
+    if (abs(res1_jn(2) - (-0.33171193864355270)) > 1e-6) error stop
 
     print*, res1_y0
-    if (abs(res1_y0(1) - 0.281323269014833) > 1e6) error stop
-    if (abs(res1_y0(2) - 0.281323269014833) > 1e6) error stop
+    if (abs(res1_y0(1) - 0.25351438512601238) > 1e-6) error stop
+    if (abs(res1_y0(2) - 0.25351438512601238) > 1e-6) error stop
 
     print*, res1_y1
-    if (abs(res1_y1(1) - 0.506133477927775) > 1e6) error stop
-    if (abs(res1_y1(2) - 0.506133477927775) > 1e6) error stop
+    if (abs(res1_y1(1) - 0.39357815895597936) > 1e-6) error stop
+    if (abs(res1_y1(2) - 0.13456148643731775) > 1e-6) error stop
 
     print*, res1_yn
-    if (abs(res1_yn(1) - 0.506133477927775) > 1e6) error stop
-    if (abs(res1_yn(2) - 0.506133477927775) > 1e6) error stop
+    if (abs(res1_yn(1) - 0.39357815895597936) > 1e-6) error stop
+    if (abs(res1_yn(2) - 0.13456148643731775) > 1e-6) error stop
 
 end
