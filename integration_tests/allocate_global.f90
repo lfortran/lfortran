e4e81a1a53e1e582d5e4ca2b18b227d7be41011e
Saurabh Kumar <developer.saurabh@outlook.com>
2024-05-27 14:13:36 +0530

    TESTS: Comment out failing test

--- integration_tests/allocate_global.f90
+++ integration_tests/allocate_global.f90
@@ -1,14 +1,14 @@
-module a
-    implicit none
-    integer, allocatable, dimension(:) :: f
- end module a
+! module a
+!     implicit none
+!     integer, allocatable, dimension(:) :: f
+!  end module a
  
- program mm
-    use a
-    implicit none
-    allocate(f(4))
-    f = [1, 2, 3, 4]
-    print *,f
+!  program mm
+!     use a
+!     implicit none
+!     allocate(f(4))
+!     f = [1, 2, 3, 4]
+!     print *,f
     
- end program mm
+!  end program mm
  
