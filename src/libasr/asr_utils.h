72967ec3e5387164d5e7b21e7582fc5a87e3a1cc
Pranavchiku <goswami.4@iitj.ac.in>
2024-05-28 18:48:51 +0530

    chore: reduce precision for real ArrayConstant values in fortran backend while converting to string

--- src/libasr/asr_utils.h
+++ src/libasr/asr_utils.h
@@ -5120,7 +5120,9 @@ inline std::string fetch_ArrayConstant_value(void *data, ASR::ttype_t* type, int
         }
         case ASR::ttypeType::Real: {
             switch (kind) {
-                case 4: return to_string_with_precision(((float*)data)[i], 8);
+                // Precision reduced to 6 for float, to avoid line truncation issues
+                // in the generated Fortran code, revert to 8 once the issue is resolved.
+                case 4: return to_string_with_precision(((float*)data)[i], 6);
                 case 8: return to_string_with_precision(((double*)data)[i], 16);
                 default:
                     throw LCompilersException("Unsupported kind for real array constant.");
