bb94caf05be293173a4cfdfce119322267e65f72
Assem <assem2002@gmail.com>
2024-05-29 22:51:33 +0300

    minor fix in compatibility with gfortran

--- integration_tests/global_allocatable_01.f90
+++ integration_tests/global_allocatable_01.f90
@@ -7,11 +7,11 @@ module mod_test_allocatable_01
  program mm
     use mod_test_allocatable_01
     implicit none
+    logical :: ret
     allocate(f(4))
     f = [1, 2, 3, 4]
     print *,f
-    logical :: ret
     ret = all( f == [1,2,3,4]) 
-    if(ret == .false.) error stop
+    if(ret .eqv. .false.) error stop
 
  end program mm
--- integration_tests/global_allocatable_02.f90
+++ integration_tests/global_allocatable_02.f90
@@ -9,7 +9,7 @@ subroutine r
     implicit none
     logical :: ret
     ret  = all (f(1,:) == [1,2,3,4])
-    if (ret == .false.) error stop
+    if (ret .eqv. .false.) error stop
     deallocate(f)
     allocate(f(6,6))
     f(2,:) = [10,10,10,10,10,10]
@@ -18,13 +18,13 @@ end subroutine r
  program mm
     use mod_test_allocatable_02
     implicit none
+    logical :: ret
     allocate(f(4,4))
     f(1,:) = [1, 2, 3, 4]
     print *,f
     call r
     print *,f
-    logical :: ret
     ret = all (f(2,:) == [10,10,10,10,10,10])
-    if(ret == .false.) error stop
+    if(ret .eqv. .false.) error stop
 
  end program mm
