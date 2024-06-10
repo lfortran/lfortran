bb6ce861474d0416a6f1dcc63fb34a29ee9ac5f2
Saurabh Kumar <developer.saurabh@outlook.com>
2024-06-11 00:44:07 +0530

    TESTS: Update test to check for loop variable being an `ExternalSymbol_t`

--- integration_tests/implied_do_loops5.f90
+++ integration_tests/implied_do_loops5.f90
@@ -1,12 +1,12 @@
 MODULE implied_do_loops5
    INTEGER, DIMENSION(10) :: fixup_counter
+   INTEGER :: n
 END MODULE implied_do_loops5
 
 PROGRAM test
    USE implied_do_loops5
    IMPLICIT NONE
 
-   INTEGER :: n
 
    PRINT *, (fixup_counter(n) + n, n=1, 4)
 END PROGRAM test
