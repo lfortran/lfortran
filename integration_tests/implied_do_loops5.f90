c40da518a66f35c15518f5a77419040063dd2334
Saurabh Kumar <developer.saurabh@outlook.com>
2024-06-11 09:20:45 +0530

    TESTS: Update test to add check on array value

--- integration_tests/implied_do_loops5.f90
+++ integration_tests/implied_do_loops5.f90
@@ -1,12 +1,16 @@
 MODULE implied_do_loops5
-   INTEGER, DIMENSION(10) :: fixup_counter
+   INTEGER, DIMENSION(5) :: fixup_counter
    INTEGER :: n
 END MODULE implied_do_loops5
 
 PROGRAM test
    USE implied_do_loops5
    IMPLICIT NONE
+   INTEGER, DIMENSION(5) :: result_array
 
+   PRINT *, (fixup_counter(n) + n, n = 1, 5)
+   result_array = (/ (fixup_counter(n) + n, n = 1, 5) /)
 
-   PRINT *, (fixup_counter(n) + n, n=1, 4)
+   if (all(result_array /= [1, 2, 3, 4, 5])) ERROR STOP
+
 END PROGRAM test
