0619798a9852255f4caf803df5800be57ae823b7
HarshitaKalani <kalani.1@iitj.ac.in>
2024-06-12 11:15:53 +0530

    test: add and regsiter test

---
+++ integration_tests/intrinsics_249.f90
@@ -0,0 +1,20 @@
+program intrinsics_249
+    use iso_fortran_env, only: dp => real64
+    integer, parameter :: n = 1000
+    integer, parameter :: m = 1000
+    integer, dimension(n,m) :: x
+    real(dp) :: res
+    x = 18
+    res = var_2_iint8_dp(x, 1)
+    print *, res
+    if (abs(res - 18000000.0_dp) > 1e-12) error stop
+    contains
+    function var_2_iint8_dp(x, dim) result(res)
+    use iso_fortran_env, only: dp => real64
+    integer, intent(in) :: x(:,:)
+    integer, intent(in) :: dim
+    real(dp) :: res
+    res = sum(sum(abs(x), dim))
+    end function var_2_iint8_dp
+end program
+    
---
+++ integration_tests/intrinsics_250.f90
@@ -0,0 +1,17 @@
+program intrinsics_50
+integer, dimension(2,2) :: x
+real(8) :: res(2)
+x = 2
+res = func(x, 1)
+contains
+
+function func(x, dim) result(res)
+integer, intent(in) :: x(:,:)
+integer, intent(in) :: dim
+real(8) :: res(2)
+print*, sum(abs(x), dim)
+res = sum(abs(x), dim)
+if (any(res /= [4, 4])) error stop
+end function func
+end program
+    
