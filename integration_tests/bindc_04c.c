10ed610704554f3f327c03526fed450b39594983
Pranavchiku <goswami.4@iitj.ac.in>
2024-06-12 23:53:12 +0530

    test: add and register

--- integration_tests/bindc_04c.c
+++ integration_tests/bindc_04c.c
@@ -7,8 +7,32 @@ void a_func(void *func(void*)) {
     }
     struct thread_data {
         float* a;
+        int n;
     } data;
     
     data.a = array;
+    // print &array as integer
+    printf("Array address in C: %ld\n", (long int) array);
+    printf("Array[0] address in C: %ld\n", (long int) &array[0]);
+    data.n = 5;
     func(&data);
+}
+
+void b_func(void *func(void*)) {
+    float array[5];
+    for (int i = 0; i < 5; i++) {
+        array[i] = i;
+    }
+    float *data = array;
+    // print &array as integer
+    printf("Array address in C for b_func: %ld\n", (long int) array);
+    func(data);
+}
+
+void c_func(void *array) {
+    printf("Printing array in C for c_func\n");
+    float *data = array;
+    for (int i = 0; i < 5; i++) {
+        printf("%f\n", data[i]);
+    }
 }
