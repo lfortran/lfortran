28ebf8440b46e69263dbc1d1df8d77ca6741cd80
HarshitaKalani <kalani.1@iitj.ac.in>
2024-06-18 12:56:02 +0530

    update integration test for blt, ble and bge

--- integration_tests/intrinsics_156.f90
+++ integration_tests/intrinsics_156.f90
@@ -1,4 +1,6 @@
 program intrinsics_156
+    logical, parameter :: l1 = blt(10, 5)
+    logical, parameter :: l2 = blt(10_8, -5_8)
     integer:: k1 = 10
     integer:: k2 = 5
     integer:: k3 = -5
@@ -6,6 +8,27 @@ program intrinsics_156
     integer:: k5 = 0
     integer:: k6 = -15
     logical:: f = .false.
+    logical, parameter :: ar1(4) = blt([1, 2, 4, 5], 3)
+    logical, parameter :: ar2(4) = blt([-1, 3, -7, 5], -3)
+
+    integer :: arr1(4)
+    integer :: arr2(3)
+    logical :: res(3)
+
+    arr1 = [11, 2, 13, 4]
+    arr2 = [-5_8, 7_8, 0_8]
+
+    print *, l1
+    if (l1 .neqv. .false.) error stop
+
+    print *, l2
+    if (l2 .neqv. .true.) error stop
+
+    print *, ar1
+    if (any(ar1) .neqv. .true.) error stop
+
+    print *, ar2
+    if (any(ar2) .neqv. .true.) error stop
 
     print*, blt(10, 5)
     if (blt(10, 5) .neqv. .false.) error stop
@@ -48,4 +71,12 @@ program intrinsics_156
     if (blt(k3, k5) .neqv. .false.) error stop
     print*, blt(k2, k5)
     if (blt(k2, k5) .neqv. f) error stop
-end
+
+    print *, blt(arr1, 5)
+    if (any(blt(arr1, 5) .neqv. .true.) .neqv. .true.) error stop
+
+    res = blt(arr2, k5)
+    print *, res
+    if (any(res .neqv. .true.) .neqv. .true.) error stop
+
+end program
--- integration_tests/intrinsics_158.f90
+++ integration_tests/intrinsics_158.f90
@@ -1,4 +1,6 @@
-program intrinsics_155
+program intrinsics_158
+    logical, parameter :: l1 = bge(10, 5)
+    logical, parameter :: l2 = bge(10_8, -5_8)
     integer:: k1 = 10
     integer:: k2 = 5
     integer:: k3 = -5
@@ -6,6 +8,27 @@ program intrinsics_155
     integer:: k5 = 0
     integer:: k6 = -15
     logical:: t = .true.
+    logical, parameter :: ar1(4) = bge([1, 2, 4, 5], 3)
+    logical, parameter :: ar2(4) = bge([-1, 3, -7, 5], -3)
+
+    integer :: arr1(4)
+    integer :: arr2(3)
+    logical :: res(3)
+
+    arr1 = [11, 2, 13, 4]
+    arr2 = [-5_8, 7_8, 0_8]
+
+    print *, l1
+    if (l1 .neqv. .true.) error stop
+
+    print *, l2
+    if (l2 .neqv. .false.) error stop
+
+    print *, ar1
+    if (any(ar1) .neqv. .true.) error stop
+
+    print *, ar2
+    if (any(ar2) .neqv. .true.) error stop
 
     print*, bge(10, 5)
     if (bge(10, 5) .neqv. .true.) error stop
@@ -48,4 +71,12 @@ program intrinsics_155
     if (bge(k3, k5) .neqv. .true.) error stop
     print*, bge(k2, k5)
     if (bge(k2, k5) .neqv. t) error stop
-end
+
+    print *, bge(arr1, 5)
+    if (any(bge(arr1, 5) .neqv. .true.) .neqv. .true.) error stop
+
+    res = bge(arr2, k5)
+    print *, res
+    if (any(res .neqv. .true.) .neqv. .false.) error stop
+
+end program
