b8d0bf468d24fd9f8b37a6e90799964abd136e55
parth121101 <parthmistry1211@gmail.com>
2024-06-19 12:38:53 +0530

    tests: creation of new files for ceil and floor intrinsics

--- integration_tests/intrinsics_133.f90
+++ integration_tests/intrinsics_133.f90
@@ -1,41 +1,11 @@
 program intrinsics_133
-    real, parameter :: a(4) = [11.012, -21.125, 31.210, -41.0]
-    real, parameter :: b(4) = [33.9, 91.2, 37.4, 19.1]
-    integer, parameter :: c(8) = Ceiling([1.2, 3.3, 5.0, 6.8, -5.7, &
-    -8.9, 0.0, -412.124])
     real :: x = 5.8
     real :: y = 6.0
-    integer, parameter :: array_size = 6
-    integer :: i
+    integer, parameter :: array_size = 3
     integer(kind = 4) :: res_4
     integer(kind = 8) :: res_8
     integer(kind=4) :: res_4_arr(array_size)
     integer(kind=8) :: res_8_arr(array_size)
-    integer(kind=4) :: res_4_arr_2(8)
-    integer :: expected(8) = [2, 4, 5, 7, -5, -8, 0, -412]
-
-    print *, Ceiling(a)
-    if (any(Ceiling(a) /= [12, -21, 32, -41])) error stop
-
-    print *, Ceiling(b)
-    if (any(Ceiling(b) /= [34, 92, 38, 20])) error stop
-
-    do i = 1, size(c)
-    print *, c(i)
-    if (c(i) /= expected(i)) error stop
-    end do
-
-    res_4_arr = Ceiling([real:: 1.2, 3.3, 5, 6.8, -5.7, -8.9])
-    print *, res_4_arr
-    if (any(res_4_arr /= [2, 4, 5, 7, -5, -8])) error stop
-
-    res_8_arr = Ceiling([real(8) :: 1.2, 3.3, 5, 101.768, -121.321, 65.4], kind=8)
-    print *, res_8_arr
-    if (any(res_8_arr /= [2, 4, 5, 102, -121, 66])) error stop
-
-    res_4_arr_2 = Ceiling([1.2, 3.3, 5.0, 6.8, -5.7, -8.9, 0.0, -412.124], kind=4)
-    print *, res_4_arr_2
-    if (any(res_4_arr_2 /= [2, 4, 5, 7, -5, -8, 0, -412])) error stop
     
     res_4 = Ceiling(x)
     print *, res_4
@@ -100,5 +70,34 @@ program intrinsics_133
     res_8 = Ceiling(-412.00, 8)
     print *, res_8
     if (res_8 /= -412) error stop
+
+     ! Compile time broadcasting
+    res_4_arr = Ceiling([real :: 1.2, 3.3, 5])
+    print *, res_4_arr(1)
+    if (res_4_arr(1) /= 2) error stop
+    print *, res_4_arr(2)
+    if (res_4_arr(2) /= 4) error stop
+    print *, res_4_arr(3)
+    if (res_4_arr(3) /= 5) error stop
+
+     ! Compile time broadcasting
+    res_4_arr = Ceiling([real :: 1.2, 3.3, 5])
+    print *, res_4_arr(1)
+    if (res_4_arr(1) /= 2) error stop
+    print *, res_4_arr(2)
+    if (res_4_arr(2) /= 4) error stop
+    print *, res_4_arr(3)
+    if (res_4_arr(3) /= 5) error stop
+
+    res_8_arr = Ceiling([real(8) :: 1.2, 3.3, 5], kind=8)
+    print *, res_8_arr(1)
+    if (res_8_arr(1) /= 2) error stop
+    if (kind(res_8_arr(1)) /= 8) error stop
+    print *, res_8_arr(2)
+    if (res_8_arr(2) /= 4) error stop
+    if (kind(res_8_arr(2)) /= 8) error stop
+    print *, res_8_arr(3)
+    if (res_8_arr(3) /= 5) error stop
+    if (kind(res_8_arr(3)) /= 8) error stop
 
 end program
