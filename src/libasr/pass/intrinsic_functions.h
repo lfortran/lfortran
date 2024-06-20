d898c2f360a32f852ffbbf616911d0866f665540
parth121101 <parthmistry1211@gmail.com>
2024-06-20 11:42:36 +0530

    cleaned up the code and used preimplemented macro

--- src/libasr/pass/intrinsic_functions.h
+++ src/libasr/pass/intrinsic_functions.h
@@ -657,68 +657,47 @@ namespace Atan2 {
             Vec<ASR::ttype_t*>& arg_types, ASR::ttype_t *return_type,
             Vec<ASR::call_arg_t>& new_args,int64_t /*overload_id*/) {
         ASR::ttype_t* arg_type = arg_types[0];
-
-
         std::string c_func_name;
         std::string new_name = "atan2";
-    switch (arg_type->type) {
-        case ASR::ttypeType::Complex : {
-            if (ASRUtils::extract_kind_from_ttype_t(arg_type) == 4) {
-                c_func_name = "_lfortran_c" + new_name;
-            } else {
-                c_func_name = "_lfortran_z" + new_name;
+        switch (arg_type->type) {
+            case ASR::ttypeType::Complex : {
+                if (ASRUtils::extract_kind_from_ttype_t(arg_type) == 4) {
+                    c_func_name = "_lfortran_c" + new_name;
+                } else {
+                    c_func_name = "_lfortran_z" + new_name;
+                }
+                break;
             }
-            break;
-        }
-        default : {
-            if (ASRUtils::extract_kind_from_ttype_t(arg_type) == 4) {
-                c_func_name = "_lfortran_s" + new_name;
-            } else {
-                c_func_name = "_lfortran_d" + new_name;
+            default : {
+                if (ASRUtils::extract_kind_from_ttype_t(arg_type) == 4) {
+                    c_func_name = "_lfortran_s" + new_name;
+                } else {
+                    c_func_name = "_lfortran_d" + new_name;
+                }
             }
         }
-    }
-    new_name = "_lcompilers_" + new_name + "_" + type_to_str_python(arg_type);
+        new_name = "_lcompilers_" + new_name + "_" + type_to_str_python(arg_type);
 
-    declare_basic_variables(new_name);
-    if (scope->get_symbol(new_name)) {
-        ASR::symbol_t *s = scope->get_symbol(new_name);
-        ASR::Function_t *f = ASR::down_cast<ASR::Function_t>(s);
-        return b.Call(s, new_args, expr_type(f->m_return_var));
-    }
-    fill_func_arg("x", arg_type);
-    fill_func_arg("y", arg_type)
-    auto result = declare(new_name, return_type, ReturnVar);
-
-    {
-        SymbolTable *fn_symtab_1 = al.make_new<SymbolTable>(fn_symtab);
-        Vec<ASR::expr_t*> args_1;
+        declare_basic_variables(new_name);
+        if (scope->get_symbol(new_name)) {
+            ASR::symbol_t *s = scope->get_symbol(new_name);
+            ASR::Function_t *f = ASR::down_cast<ASR::Function_t>(s);
+            return b.Call(s, new_args, expr_type(f->m_return_var));
+        }
+        fill_func_arg("x", arg_type);
+        fill_func_arg("y", arg_type);
+        auto result = declare(new_name, return_type, ReturnVar);
         {
-            args_1.reserve(al, 2);
-            ASR::expr_t *arg_1 = b.Variable(fn_symtab_1, "x", arg_type,
-                ASR::intentType::In, ASR::abiType::BindC, true);
-            ASR::expr_t *arg_2 = b.Variable(fn_symtab_1, "y", arg_type,
-                ASR::intentType::In, ASR::abiType::BindC, true);
-            args_1.push_back(al, arg_1);
-            args_1.push_back(al, arg_2);
+            ASR::symbol_t *s = b.create_c_func(c_func_name, fn_symtab, return_type, arg_types.size(), arg_types);
+            fn_symtab->add_symbol(c_func_name, s);
+            dep.push_back(al, s2c(al, c_func_name));
+            body.push_back(al, b.Assignment(result, b.Call(s, args, return_type)));
         }
 
-        ASR::expr_t *return_var_1 = b.Variable(fn_symtab_1, c_func_name,
-            arg_type, ASRUtils::intent_return_var, ASR::abiType::BindC, false);
-
-        SetChar dep_1; dep_1.reserve(al, 1);
-        Vec<ASR::stmt_t*> body_1; body_1.reserve(al, 1);
-        ASR::symbol_t *s = make_ASR_Function_t(c_func_name, fn_symtab_1, dep_1, args_1,
-            body_1, return_var_1, ASR::abiType::BindC, ASR::deftypeType::Interface, s2c(al, c_func_name));
-        fn_symtab->add_symbol(c_func_name, s);
-        dep.push_back(al, s2c(al, c_func_name));
-        body.push_back(al, b.Assignment(result, b.Call(s, args, arg_type)));
-    }
-
-    ASR::symbol_t *new_symbol = make_ASR_Function_t(fn_name, fn_symtab, dep, args,
+        ASR::symbol_t *new_symbol = make_ASR_Function_t(fn_name, fn_symtab, dep, args,
         body, result, ASR::abiType::Source, ASR::deftypeType::Implementation, nullptr);
-    scope->add_symbol(fn_name, new_symbol);
-    return b.Call(new_symbol, new_args, return_type);
+        scope->add_symbol(fn_name, new_symbol);
+        return b.Call(new_symbol, new_args, return_type);
     }
 }
 
