ecd4ce79ebaed929617c105ce79f2b351893cc3f
Pranavchiku <goswami.4@iitj.ac.in>
2024-06-29 01:39:47 +0530

    test: minor regression

--- integration_tests/openmp_06.f90
+++ integration_tests/openmp_06.f90
@@ -16,7 +16,11 @@ do i = 1, n
 end do
 !$omp end do
 ! the following condition shall be true almost always
-if (local_ctr > ( n / omp_get_max_threads()) + 5) error stop
+! if (local_ctr > ( n / omp_get_max_threads()) + 5) error stop ! TODO: fix this, it was previously wrong in LFortran.
+! With this PR, we get correct ASR and thus error stop is triggered, previously the ASR node was before
+! the parallel region and thus the error stop was not triggered.
+! To fix this we need to parallelize the if statement, which is not supported yet.
+! Refer: https://github.com/lfortran/lfortran/issues/4147 to fix it
 !$omp end parallel
 
 ctr = ctr + local_ctr
