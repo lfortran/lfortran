6d03d18309d0474a5b040b6fe9ac8e34d4738dc8
parth121101 <parthmistry1211@gmail.com>
2024-06-30 20:12:45 +0530

    implement compiler_version

--- src/runtime/pure/lfortran_intrinsic_iso_fortran_env.f90
+++ src/runtime/pure/lfortran_intrinsic_iso_fortran_env.f90
@@ -1,23 +1,29 @@
 module lfortran_intrinsic_iso_fortran_env
-implicit none
+    implicit none
 
-integer, parameter :: int8 = 1
-integer, parameter :: int16 = 2
-integer, parameter :: int32 = 4
-integer, parameter :: int64 = 8
-integer, parameter :: real32 = 4
-integer, parameter :: real64 = 8
-integer, parameter :: real128 = -1
+    integer, parameter :: int8 = 1
+    integer, parameter :: int16 = 2
+    integer, parameter :: int32 = 4
+    integer, parameter :: int64 = 8
+    integer, parameter :: real32 = 4
+    integer, parameter :: real64 = 8
+    integer, parameter :: real128 = -1
 
-integer, parameter :: input_unit = 5
-integer, parameter :: output_unit = 6
-integer, parameter :: error_unit = 0
+    integer, parameter :: input_unit = 5
+    integer, parameter :: output_unit = 6
+    integer, parameter :: error_unit = 0
 
-integer, parameter :: integer_kinds(2) = [4, 8]
-integer, parameter :: real_kinds(2) = [4, 8]
-integer, parameter :: character_kinds(1) = [1]
-integer, parameter :: logical_kinds(1) = [4]
+    integer, parameter :: integer_kinds(2) = [4, 8]
+    integer, parameter :: real_kinds(2) = [4, 8]
+    integer, parameter :: character_kinds(1) = [1]
+    integer, parameter :: logical_kinds(1) = [4]
 
-integer, parameter :: iostat_end = -1
+    integer, parameter :: iostat_end = -1
+
+contains
+    function compiler_version() result(version)
+        character(len=23) :: version
+        version = "LFortran version 0.37.0"
+    end function compiler_version
 
 end module
