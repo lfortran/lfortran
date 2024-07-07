9d6d7512984c8dd0ce65e0cfa9b75f5939d9d7d1
HarshitaKalani <kalani.1@iitj.ac.in>
2024-07-07 17:16:38 +0530

    test: add and register test for ishftc, bit_size, conjg

--- integration_tests/complex_08.f90
+++ integration_tests/complex_08.f90
@@ -1,12 +1,36 @@
 program complex_08
+    use iso_fortran_env, only: sp => real32, dp => real64
     implicit none
-    complex :: x = conjg((0, 0))
-    complex :: y = conjg((2.0, -5.5))
-    complex :: z = conjg((42, 3.14))
+    complex(sp), parameter :: c1 = conjg((1.0_sp, 2.0_sp))
+    complex(dp), parameter :: c2 = conjg((-3.0_dp, -4.0_dp))
+    complex(sp), parameter :: ar1(2) = conjg([(1.0_sp, 2.0_sp), (-3.0_sp, 4.0_sp)])
+    complex(dp), parameter :: ar2(2) = conjg([(-1.0_dp, -2.0_dp), (3.0_dp, -4.0_dp)])
 
+    complex(sp) :: x = conjg((0, 0))
+    complex(dp) :: y = conjg((2.0, -5.5))
+    complex(dp) :: z = (42, 3.14)
+    complex(sp) :: arr1(3) = [(0, 0), (2.0, 5.5), (42, -3.14)]
+    complex(dp) :: arr2(3) = [(-1.0, -2.0), (3.0, -4.0), (42, -3.14)]
+
+    print *, c1 
+    if (abs(c1 - (1.0_sp, -2.0_sp)) > 1e-5) error stop
+    print *, c2
+    if (abs(c2 - (-3.0_dp, 4.0_dp)) > 1e-5) error stop
+    print *, ar1
+    if (any(abs(ar1 - [(1.0_sp, -2.0_sp), (-3.0_sp, -4.0_sp)]) > 1e-5)) error stop
+    print *, ar2
+    if (any(abs(ar2 - [(-1.0_dp, 2.0_dp), (3.0_dp, 4.0_dp)]) > 1e-5)) error stop
+
+    print *, x
     if (abs(x - (0, 0)) > 1e-5) error stop
+    print *, y
     if (abs(y - (2.0, 5.5)) > 1e-5) error stop
-    if (abs(z - (42, -3.14)) > 1e-5) error stop
+    print *, conjg(z)
+    if (abs(conjg(z) - (42, -3.14)) > 1e-5) error stop
+    
+    print *, conjg(arr1)
+    if (any(abs(conjg(arr1) - [(0, 0), (2.0, -5.5), (42, 3.14)]) > 1e-5)) error stop
+    print *, conjg(arr2)
+    if (any(abs(conjg(arr2) - [(-1.0, 2.0), (3.0, 4.0), (42, 3.14)]) > 1e-5)) error stop
 
-    print *, x, y, z
 end program
--- integration_tests/intrinsics_208.f90
+++ integration_tests/intrinsics_208.f90
@@ -4,6 +4,20 @@ program intrinsics_208
     integer :: i = 123
     integer(8) :: j = 1234567
     integer :: size
+    integer :: arr1(3) = [11, 121, 1234]
+    integer, parameter :: i1 = bit_size(11)
+    integer(8), parameter :: i2 = bit_size(11)
+    integer, parameter :: ar1 = bit_size([1,2,3])
+    integer(8), parameter :: ar2 = bit_size([1,2,3])
+
+    print *, i1
+    if( i1 /= 32 ) error stop
+    print *, i2
+    if( i2 /= 32 ) error stop
+    print *, ar1
+    if( ar1 /= 32 ) error stop
+    print *, ar2
+    if( ar2 /= 32 ) error stop
   
     print*, bit_size(1)
     if (bit_size(1) /= 32) error stop
@@ -17,5 +31,11 @@ program intrinsics_208
   
     print*, bit_size(j)
     if( bit_size(j) /= 64 ) error stop
+
+    print *, bit_size(arr1)
+    if( bit_size(arr1) /= 32 ) error stop
+
+    print *, kind(bit_size(1_8))
+    if( kind(bit_size(1_8)) /= 8 ) error stop
   
 end program
