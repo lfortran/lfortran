593b0153209e33bb2243470de97791786e2d0c83
Assem <assem2002@gmail.com>
2024-07-13 11:30:28 +0300

    Add and register tests

---
+++ integration_tests/arrays_51.f90
@@ -0,0 +1,19 @@
+! Check that lcompilers_get_i() returns mutated value of i not just hard coded value of 4
+module arrays_51_mod
+    IMPLICIT NONE
+    integer :: i = 4
+    integer :: arr_mod(4,4)
+    contains
+    subroutine sub (arr)
+        integer , INTENT(OUT):: arr(4,i)
+        print *,size(arr)
+        if(size(arr) /= 20000) error stop
+    end subroutine sub
+end module arrays_51_mod
+
+
+program arrays_51
+    use arrays_51_mod
+    i = 5000
+    call sub(arr_mod)
+end program arrays_51
