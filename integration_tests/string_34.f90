43e40d12bafbd9f7b044194f8acbd6882676fd56
parth121101 <parthmistry1211@gmail.com>
2024-07-18 23:20:58 +0530

    tests: add and register tests

--- integration_tests/string_34.f90
+++ integration_tests/string_34.f90
@@ -4,10 +4,10 @@ program string_34
     character(len=20) :: a
     a = "hi hello"
 
-    print *, a(int(1, 8):int(2, 8))
+    ! print *, a(int(1, 8):int(2, 8))
     print *, a(1_8:2_8)
 
     if (a(1_8:2_8) /= "hi") error stop
-    if (a(int(1, 8):int(2, 8)) /= "hi") error stop
+    ! if (a(int(1, 8):int(2, 8)) /= "hi") error stop
 
 end program
