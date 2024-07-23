796e50510cdf84072437c4ec1a2d99563862f036
Gagandeep Singh <gdp.1807@gmail.com>
2024-07-23 20:55:42 +0530

    wip

--- src/libasr/pass/pass_manager.h
+++ src/libasr/pass/pass_manager.h
@@ -221,6 +221,7 @@ namespace LCompilers {
                 "symbolic",
                 "intrinsic_function", // To be re-written without creating allocotables and auxiliary variables
                 "intrinsic_subroutine", // To be re-written without creating allocotables and auxiliary variables
+                "array_op",
                 // "subroutine_from_function", There should be no need to apply this twice
                 // "array_op", There should be no need to apply this twice
                 "pass_array_by_data",
