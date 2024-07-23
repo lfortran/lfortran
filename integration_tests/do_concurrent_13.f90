6164761596e412420211f1effad81616cdc6b01c
Pranavchiku <goswami.4@iitj.ac.in>
2024-07-24 00:22:36 +0530

    test: add do concurrent test for mandelbrot example

--- integration_tests/do_concurrent_13.f90
+++ integration_tests/do_concurrent_13.f90
@@ -1,16 +1,11 @@
-program openmp_33
-    use omp_lib
+program do_concurrent_13
     integer, parameter :: Nx = 600, Ny = 450, n_max = 255, dp=kind(0.d0)
     real(dp), parameter :: xcenter = -0.5_dp, ycenter = 0.0_dp, &
         width = 4, height = 3, dx_di = width/Nx, dy_dj = -height/Ny, &
         x_offset = xcenter - (Nx+1)*dx_di/2, y_offset = ycenter - (Ny+1)*dy_dj/2
     real(dp) :: x, y, x_0, y_0, x_sqr, y_sqr, wtime
     integer :: i, j, n, image(Nx, Ny)
-    call omp_set_num_threads(4)
-    wtime = omp_get_wtime()
-    !$omp parallel shared(image) private(i, j, x, y, x_0, y_0, x_sqr, y_sqr, n)
-    !$omp do
-    do j = 1, Ny
+    do concurrent (j = 1:Ny) shared(image) local(i, j, x, y, x_0, y_0, x_sqr, y_sqr, n)
         y_0 = y_offset + dy_dj * j
         do i = 1, Nx
             x_0 = x_offset + dx_di * i
@@ -27,10 +22,6 @@ program openmp_33
             end do
         end do
     end do
-    !$omp end do
-    !$omp end parallel
-    wtime = omp_get_wtime() - wtime
-    print *, 'Time = ', wtime, "(s)"
     print *, sum(image)
     if ( sum(image) /= 59157126 ) error stop
 end program
