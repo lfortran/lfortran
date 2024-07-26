a68258da0972fb97bd1edc5029d10ff3c332c909
Gagandeep Singh <gdp.1807@gmail.com>
2024-07-26 20:40:32 +0530

    wip

--- src/libasr/pass/intrinsic_array_function_registry.h
+++ src/libasr/pass/intrinsic_array_function_registry.h
@@ -4102,42 +4102,10 @@ namespace Pack {
         }
         ASR::ttype_t* ret_type = return_type;
         if (overload_id == 2) {
-            ret_type = ASRUtils::duplicate_type(al, ASRUtils::type_get_past_allocatable(return_type), nullptr, ASRUtils::extract_physical_type(return_type), true);
-            LCOMPILERS_ASSERT(ASR::is_a<ASR::Array_t>(*ret_type));
-            ASR::Array_t *ret_type_array = ASR::down_cast<ASR::Array_t>(ret_type);
-            if (ret_type_array->m_dims[0].m_length &&
-                ASR::is_a<ASR::FunctionCall_t>(*ret_type_array->m_dims[0].m_length)
-            ) {
-                ASR::FunctionCall_t *func_call = ASR::down_cast<ASR::FunctionCall_t>(ret_type_array->m_dims[0].m_length);
-                if (ASR::is_a<ASR::ArrayPhysicalCast_t>(*func_call->m_args[0].m_value)) {
-                    ASR::ArrayPhysicalCast_t *array_cast = ASR::down_cast<ASR::ArrayPhysicalCast_t>(func_call->m_args[0].m_value);
-                    array_cast->m_arg = args[1];
-                    array_cast->m_old = ASRUtils::extract_physical_type(arg_types[1]);
-                    array_cast->m_type = ASRUtils::duplicate_type_with_empty_dims(al, array_cast->m_type);
-
-                    ret_type = TYPE(ASR::make_Array_t(al, loc, ret_type_array->m_type, ret_type_array->m_dims,
-                                ret_type_array->n_dims, ret_type_array->m_physical_type));
-                } else {
-                    ret_type = return_type;
-                }
-            } else if (ret_type_array->m_dims[0].m_length &&
-                ASR::is_a<ASR::IntrinsicArrayFunction_t>(*ret_type_array->m_dims[0].m_length)
-            ) {
-                ASR::IntrinsicArrayFunction_t *intrinsic_array = ASR::down_cast<ASR::IntrinsicArrayFunction_t>(ret_type_array->m_dims[0].m_length);
-                if (ASR::is_a<ASR::ArrayPhysicalCast_t>(*intrinsic_array->m_args[0])) {
-                    ASR::ArrayPhysicalCast_t *array_cast = ASR::down_cast<ASR::ArrayPhysicalCast_t>(intrinsic_array->m_args[0]);
-                    array_cast->m_arg = args[1];
-                    array_cast->m_old = ASRUtils::extract_physical_type(arg_types[1]);
-                    array_cast->m_type = ASRUtils::duplicate_type_with_empty_dims(al, array_cast->m_type);
-
-                    ret_type = TYPE(ASR::make_Array_t(al, loc, ret_type_array->m_type, ret_type_array->m_dims,
-                                ret_type_array->n_dims, ret_type_array->m_physical_type));
-                } else {
-                    ret_type = return_type;
-                }
-            } else {
-                ret_type = return_type;
-            }
+            ret_type = ASRUtils::duplicate_type_with_empty_dims(
+                al, ASRUtils::type_get_past_pointer(
+                        ASRUtils::type_get_past_allocatable(return_type)),
+                ASR::array_physical_typeType::DescriptorArray, true);
         }
         ASR::expr_t *result = declare("result", ret_type, Out);
         args.push_back(al, result);
