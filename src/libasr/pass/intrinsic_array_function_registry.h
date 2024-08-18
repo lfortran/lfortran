5dd78b86a2b250149477a7064db19388037b7fc8
Shubhneet <shubhneetgrover@gmail.com>
2024-08-18 19:00:19 +0530

    added test, and suggested changes

--- src/libasr/pass/intrinsic_array_function_registry.h
+++ src/libasr/pass/intrinsic_array_function_registry.h
@@ -588,27 +588,26 @@ static inline ASR::asr_t* create_ArrIntrinsic(
     if (args[1]) {
         if (is_integer(*ASRUtils::expr_type(args[1]))) {
             dim = args[1];
-            if (args[2]) {
+            if (args[2] && is_logical(*ASRUtils::expr_type(args[2]))) {
                 mask = args[2];
-            }
+            } else if (args[2]) {
+                append_error(diag, "`mask` argument to `" + intrinsic_func_name + "` must be a scalar or array of logical type",
+                    args[2]->base.loc);
+                return nullptr;
+            } 
         } else if (is_logical(*ASRUtils::expr_type(args[1]))) {
             mask = args[1];
-            if (args[2]) {
+            if (args[2] && is_integer(*ASRUtils::expr_type(args[2]))) {
                 dim = args[2];
-            }
-        } else{
-            append_error(diag, "`dim` argument to `" + intrinsic_func_name + "` intrinsic at (1) must be INTEGER",
+            } else if (args[2]) {
+                append_error(diag, "`dim` argument to `" + intrinsic_func_name + "` must be a scalar and of integer type",
+                    args[2]->base.loc);
+                return nullptr;
+            } 
+        } else {
+            append_error(diag, "Invalid argument type for `dim` or `mask`",
                 args[1]->base.loc);
             return nullptr;
-
-        }
-    } else if (args[2]) {
-        if (is_integer(*ASRUtils::expr_type(args[2]))) {
-            dim = args[2];
-            mask = args[1];
-        } else if (is_logical(*ASRUtils::expr_type(args[2]))) {
-            mask = args[2];
-            dim = args[1];
         }
     }
     if (dim) {
