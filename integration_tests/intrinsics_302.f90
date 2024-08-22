6b499b7528854ec89e2e0773cfcd92c540edbaaf
HarshitaKalani <kalani.1@iitj.ac.in>
2024-08-22 17:40:19 +0530

    add and regsiter test

---
+++ integration_tests/intrinsics_302.f90
@@ -0,0 +1,11 @@
+program intrinsics_302
+    implicit none
+    integer :: x(4) = [1,2,3,4]
+    
+    print *, is_contiguous(x)
+    if (.not. is_contiguous(x)) error stop
+    
+    print *, is_contiguous(x(::2))
+    if (is_contiguous(x(::2))) error stop
+end program
+  
