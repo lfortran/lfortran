7d98f1d42667cefc422ad900bec8c2b221227eb5
HarshitaKalani <kalani.1@iitj.ac.in>
2024-08-23 21:10:09 +0530

    throw error for arg types not yet handled in is_contiguous

--- integration_tests/intrinsics_302.f90
+++ integration_tests/intrinsics_302.f90
@@ -1,6 +1,9 @@
 program intrinsics_302
     implicit none
     integer :: x(4) = [1,2,3,4]
+
+    print *, is_contiguous([1,2,3,4])
+    if (.not. is_contiguous([1,2,3,4])) error stop
     
     print *, is_contiguous(x)
     if (.not. is_contiguous(x)) error stop
