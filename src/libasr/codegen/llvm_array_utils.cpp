42fd97698575444509f8f2e8e8cf3977a0963ce8
Assem <assem2002@gmail.com>
2024-08-24 10:15:23 +0300

    Minor Fix : LLVM version function issue

--- src/libasr/codegen/llvm_array_utils.cpp
+++ src/libasr/codegen/llvm_array_utils.cpp
@@ -391,7 +391,7 @@ namespace LCompilers {
                 std::vector<llvm::Value*> idx_vec = {
                     llvm::ConstantInt::get(context, llvm::APInt(32, 1))};
                     llvm::Value* null_dim_des_ptr = llvm::ConstantPointerNull::get(dim_des->getPointerTo());
-                    llvm::Value* size_of_dim_des_struct = builder->CreateGEP(null_dim_des_ptr, idx_vec);
+                    llvm::Value* size_of_dim_des_struct = LLVM::CreateGEP(*builder,null_dim_des_ptr, idx_vec);
                     llvm::Value* size_of_dim_des_struct_casted = builder->CreatePtrToInt(size_of_dim_des_struct, llvm::Type::getInt32Ty(context)); //cast to int32
                     llvm::Value* size_mul_ndim = builder->CreateMul(size_of_dim_des_struct_casted, llvm::ConstantInt::get(context, llvm::APInt(32, n_dims)));
                     llvm::Value* struct_ptr = LLVMArrUtils::lfortran_malloc(
