d6ae56509f00168cbedcd8c937c9994fc93a2d7f
Thirumalai-Shaktivel <thirumalaishaktivel@gmail.com>
2024-08-28 21:33:57 +0530

    [CMake] Add MLIR testing support and enable a test

--- integration_tests/run_tests.py
+++ integration_tests/run_tests.py
@@ -8,7 +8,8 @@ import os
 NO_OF_THREADS = 8 # default no of threads is 8
 SUPPORTED_BACKENDS = ['llvm', 'llvm2', 'llvm_rtlib', 'c', 'cpp', 'x86', 'wasm',
                       'gfortran', 'llvmImplicit', 'llvmStackArray', 'fortran',
-                      'c_nopragma', 'llvm_nopragma', 'llvm_wasm', 'llvm_wasm_emcc', 'llvm_omp']
+                      'c_nopragma', 'llvm_nopragma', 'llvm_wasm', 'llvm_wasm_emcc',
+                      'llvm_omp', 'mlir']
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 LFORTRAN_PATH = f"{BASE_DIR}/../src/bin:$PATH"
 
