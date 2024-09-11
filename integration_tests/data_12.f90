39e7c05fe4ba8db725bae7c7ffba253a383b029e
Gagandeep Singh <gdp.1807@gmail.com>
2024-09-11 20:35:00 +0530

    wip

--- integration_tests/data_12.f90
+++ integration_tests/data_12.f90
@@ -12,30 +12,30 @@ end module data_12_module
 program data_12_program
     use data_12_module
     implicit none
-    ! print *, x
-    ! if (x /= 1) error stop
+    print *, x
+    if (x /= 1) error stop
 
-    ! print *, y
-    ! if (abs(y-2.0) > 1e-5) error stop
+    print *, y
+    if (abs(y-2.0) > 1e-5) error stop
 
-    ! print *, z
-    ! if (abs(z-3.0) > 1e-5) error stop
+    print *, z
+    if (abs(z-3.0) > 1e-5) error stop
 
-    ! print *, c1(1)
-    ! if (abs(c1(1)-0.0) > 1e-5) error stop
+    print *, c1(1)
+    if (abs(c1(1)-0.0) > 1e-5) error stop
 
     print *, c1
     if (abs(c1(2)-0.22) > 1e-5) error stop
 
-    ! print *, c1(4)
-    ! if (abs(c1(4)+0.21) > 1e-5) error stop
+    print *, c1(4)
+    if (abs(c1(4)+0.21) > 1e-5) error stop
 
-    ! print *, bf1
-    ! if (abs(bf1-0.8) > 1e-5) error stop
+    print *, bf1
+    if (abs(bf1-0.8) > 1e-5) error stop
 
-    ! print *, xx90
-    ! if (abs(xx90-0.55) > 1e-5) error stop
+    print *, xx90
+    if (abs(xx90-0.55) > 1e-5) error stop
 
-    ! print *, xx95
-    ! if (abs(xx95-0.62) > 1e-5) error stop
+    print *, xx95
+    if (abs(xx95-0.62) > 1e-5) error stop
 end program data_12_program
