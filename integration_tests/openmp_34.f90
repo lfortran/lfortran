c90975ae22741559b76125e325c52a79ababb5bb
Aditya Trivedi <adit4443ya@gmail.com>
2024-09-15 00:48:35 +0530

    Created new test For ensuring efficient Thread Load Balanacing
    The Output will include each thread_id twice which is expected too, as iterations per thred would be 10/5=2

--- integration_tests/openmp_34.f90
+++ integration_tests/openmp_34.f90
@@ -1,22 +1,25 @@
 subroutine omp_func(n)
+    use omp_lib
 implicit none
 integer, intent(in) :: n
 integer :: i
-
-!$omp parallel private(i) shared(n)
+call omp_set_num_threads(5)
+!$omp parallel  private(i) shared(n)
 !$omp do
 do i = 1, n
+    !omp critical
+    print *,omp_get_thread_num()
     print *, "xyz"
 end do
 !$omp end do
 !$omp end parallel
 
 print *, "n = ", n
-if (n /= 100) error stop
+if (n /= 10) error stop
 end subroutine
 
 program openmp_01
-integer, parameter :: n = 100
+integer, parameter :: n = 10
 call omp_func(n)
 print *, "Done for n = ", n
 end program
