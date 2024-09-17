714dfea6f319834297c6f74cec7cc1d51da961df
Saurabh Kumar <developer.saurabh@outlook.com>
2024-09-17 23:52:23 +0530

    tests: update output value to test

--- integration_tests/parameter_13.f90
+++ integration_tests/parameter_13.f90
@@ -1,6 +1,7 @@
 program parameter_13
 implicit none
-integer, parameter :: N = 100           ! TODO: Set N = 65535
+! FIXME: Set N = 65535
+integer, parameter :: N = 100
 integer, parameter :: sp = kind(1.0)
 integer, parameter :: dp = kind(1.d0)
 real(dp), parameter :: pi = 2*asin(1._dp)
@@ -13,5 +14,7 @@ logical, parameter :: l = S < 2
 real(kind=merge(sp, dp, l)) :: y
 if (kind(y) /= sp) error stop
 print *, S
-if (abs(S - 1.9999999996170159_dp) > 1e-15_dp) error stop
+if (abs(S - 1.99983550388744358_dp) > 1e-15_dp) error stop
+! FIXME: Uncomment the below line
+! if (abs(S - 1.9999999996170159_dp) > 1e-15_dp) error stop
 end program
