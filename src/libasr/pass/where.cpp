bce2a86a52d863f70c6d8f9dbc430903ccd21b94
Saurabh Kumar <developer.saurabh@outlook.com>
2024-09-18 22:27:42 +0530

    chore: do stricter type checking for arrays of ttype `Logical_t`

--- src/libasr/pass/where.cpp
+++ src/libasr/pass/where.cpp
@@ -433,20 +433,24 @@ public:
 
         // We initially handle this case for logical arrays inside the AST node visitor. We need to handle it here
         // again to work with the changes introduced during the ASR passes before this.
-        if (ASR::is_a<ASR::Logical_t>(*ASRUtils::type_get_past_array_pointer_allocatable(ASRUtils::expr_type(test)))) {
-            ASR::expr_t* logical_true = ASRUtils::EXPR(
-                                            ASR::make_LogicalConstant_t(
-                                                al,
-                                                x.base.base.loc,
-                                                true,
-                                                ASRUtils::TYPE(ASR::make_Logical_t(al, x.base.base.loc, 4))));
-            test = ASRUtils::EXPR(ASR::make_LogicalBinOp_t(al,
-                                                           x.base.base.loc,
-                                                           test,
-                                                           ASR::logicalbinopType::Eqv,
-                                                           logical_true,
-                                                           ASRUtils::expr_type(test),
-                                                           nullptr));
+        if (ASRUtils::is_array(ASRUtils::expr_type(test))
+            && ASR::is_a<ASR::Logical_t>(
+                *ASRUtils::type_get_past_array_pointer_allocatable(ASRUtils::expr_type(test)))) {
+            if (!ASR::is_a<ASR::IntegerCompare_t>(*test) && !ASR::is_a<ASR::RealCompare_t>(*test)
+                && !ASR::is_a<ASR::LogicalBinOp_t>(*test)) {
+                ASR::expr_t* logical_true = ASRUtils::EXPR(ASR::make_LogicalConstant_t(
+                    al,
+                    x.base.base.loc,
+                    true,
+                    ASRUtils::TYPE(ASR::make_Logical_t(al, x.base.base.loc, 4))));
+                test = ASRUtils::EXPR(ASR::make_LogicalBinOp_t(al,
+                                                               x.base.base.loc,
+                                                               test,
+                                                               ASR::logicalbinopType::Eqv,
+                                                               logical_true,
+                                                               ASRUtils::expr_type(test),
+                                                               nullptr));
+            }
         }
 
         if (ASR::is_a<ASR::IntegerCompare_t>(*test)) {
