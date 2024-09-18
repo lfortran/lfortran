ecf713289a31ba9e1e8dce4d184a29de653148b0
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2024-09-18 17:51:06 +0530

    remove unnecessary reference test and corresponding files

--- integration_tests/array_section_is_non_allocatable.f90
+++ integration_tests/array_section_is_non_allocatable.f90
@@ -1,12 +1,12 @@
 PROGRAM array_section_is_non_allocatable
     IMPLICIT NONE
- 
+
     REAL, ALLOCATABLE, DIMENSION(:, :) :: fixup_counter
- 
+
     CALL dim1_sweep ( fixup_counter (:, 1))
- 
+
 CONTAINS
- 
+
     SUBROUTINE dim1_sweep ( fixup_counter )
        REAL, DIMENSION(4), INTENT(INOUT) :: fixup_counter
     END SUBROUTINE dim1_sweep
