63db8b9af5c44f31274dc9d64a4f83bf69affc0d
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2024-09-18 18:05:48 +0530

    pass another argument to subroutine and print it

--- integration_tests/array_section_is_non_allocatable.f90
+++ integration_tests/array_section_is_non_allocatable.f90
@@ -2,12 +2,17 @@ PROGRAM array_section_is_non_allocatable
     IMPLICIT NONE
 
     REAL, ALLOCATABLE, DIMENSION(:, :) :: fixup_counter
+    INTEGER, PARAMETER :: x = 1
 
-    CALL dim1_sweep ( fixup_counter (:, 1))
+    CALL dim1_sweep ( x, fixup_counter (:, 1))
 
 CONTAINS
 
-    SUBROUTINE dim1_sweep ( fixup_counter )
-       REAL, DIMENSION(4), INTENT(INOUT) :: fixup_counter
+    SUBROUTINE dim1_sweep ( x_param, fixup_counter )
+        REAL, DIMENSION(4), INTENT(INOUT) :: fixup_counter
+        INTEGER, INTENT(IN) :: x_param
+        
+        PRINT *, x_param
+        if (x_param /= 1) error stop
     END SUBROUTINE dim1_sweep
 END PROGRAM array_section_is_non_allocatable
