3ac301fc6603bef6c3017d7f44444136c838d457
HarshitaKalani <kalani.1@iitj.ac.in>
2024-09-23 20:27:31 +0530

    remove old implementation of command_argument_count

--- src/lfortran/semantics/comptime_eval.h
+++ src/lfortran/semantics/comptime_eval.h
@@ -76,7 +76,6 @@ struct IntrinsicProcedures {
             {"is_iostat_eor", {m_builtin, &not_implemented, false}},
             {"is_iostat_end", {m_builtin, &not_implemented, false}},
             {"get_command_argument", {m_builtin, &not_implemented, false}},
-            {"command_argument_count", {m_builtin, &not_implemented, false}},
             {"newunit", {m_custom, &not_implemented, false}},
 
             // These will fail if used in symbol table visitor, but will be
