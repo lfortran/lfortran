a3fb36c8118d31a9fab6480480ee55d0c8e32a23
advik <advikkabra25@gmail.com>
2024-09-25 08:39:08 +0530

    Alter type saving logic in CreateLoad

--- src/libasr/codegen/llvm_utils.cpp
+++ src/libasr/codegen/llvm_utils.cpp
@@ -1634,17 +1634,19 @@ namespace LCompilers {
 #else
         llvm::Type *type = nullptr, *type_copy = nullptr;
         if (ptr_type.find(x) != ptr_type.end()) {
-            type_copy = type = ptr_type[x];
+            type = ptr_type[x];
         }
         LCOMPILERS_ASSERT(type);
         // getPointerTo() is used for allocatable or pointer
         if (type != character_type && (llvm::isa<llvm::AllocaInst>(x) &&
                 llvm::dyn_cast<llvm::AllocaInst>(x)->getAllocatedType()->isPointerTy())) {
             // AllocaInst
+            type_copy = type;
             type = type->getPointerTo();
         } else if (llvm::StructType *arr_type = llvm::dyn_cast<llvm::StructType>(type)) {
             // Function arguments
             if (arr_type->getName() == "array") {
+                type_copy = type;
                 type = type->getPointerTo();
             }
         }
@@ -1657,12 +1659,13 @@ namespace LCompilers {
             std::string s_name = std::string(llvm::dyn_cast<llvm::StructType>(
                 gep->getSourceElementType())->getName());
             if ( name2dertype.find(s_name) != name2dertype.end() ) {
+                type_copy = type;
                 type = type->getPointerTo();
             }
         }
 
         llvm::Value *load = builder->CreateLoad(type, x);
-        if (type != type_copy) {
+        if (type_copy) {
             ptr_type[load] = type_copy;
         }
         return load;
