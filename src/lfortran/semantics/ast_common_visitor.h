92c70f715c02999163edded63a8746b0074e315e
HarshitaKalani <kalani.1@iitj.ac.in>
2024-10-06 11:49:14 +0530

    implement date_and_time

--- src/lfortran/semantics/ast_common_visitor.h
+++ src/lfortran/semantics/ast_common_visitor.h
@@ -852,6 +852,7 @@ public:
         {"get_command", {IntrinsicSignature({"command", "length", "status"}, 0, 3)}},
         {"get_command_argument", {IntrinsicSignature({"number", "value", "length", "status"}, 1, 4)}},
         {"system_clock", {IntrinsicSignature({"count", "count_rate", "count_max"}, 0, 3)}},
+        {"date_and_time", {IntrinsicSignature({"date", "time", "zone", "values"}, 0, 4)}},
         {"get_environment_variable", {IntrinsicSignature({"name", "value", "length", "status", "trim_name"}, 1, 5)}},
         {"execute_command_line", {IntrinsicSignature({"command", "wait", "exitstat", "cmdstat", "cmdmsg"}, 1, 5)}},
         {"move_alloc", {IntrinsicSignature({"from", "to"}, 2, 2)}},
