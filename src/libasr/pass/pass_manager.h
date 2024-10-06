94fd2fd4d02d2d08eac31ffe4047c7e68c9752bb
Pranavchiku <goswami.4@iitj.ac.in>
2024-10-06 10:54:54 +0530

    chore: run implied_do_loops past with --fast

--- src/libasr/pass/pass_manager.h
+++ src/libasr/pass/pass_manager.h
@@ -245,13 +245,13 @@ namespace LCompilers {
             _with_optimization_passes = {
                 "global_stmts",
                 "function_call_in_declaration",
+                "implied_do_loops", // Should be implemented when optimisations for ImpliedDoLoop are possible in LFortran, until then not needed.
                 "simplifier", /* Verification checks to be implemented in this pass - 1. No array, user defined type variable should have a symbolic value. 2. Print, SubroutineCall, FileWrite, IntrinsicImpureSubroutine nodes shouldn't have non-Var arguments. 3. All expressions which need a temporary should be directly linked to a target via an assignment. 4. Sizes of auxiliary allocatables should be calculated using only Var nodes (with non-array symbols), or FunctionCall returning scalars. */
                 "nested_vars",
                 "transform_optional_argument_functions",
                 // "init_expr", This pass shouldn't be needed.
                 "forall",
                 "openmp",
-                // "implied_do_loops", Should be implemented when optimisations for ImpliedDoLoop are possible in LFortran, until then not needed.
                 "class_constructor",
                 "pass_list_expr",
                 "where",
