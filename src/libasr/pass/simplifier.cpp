ca01ae736c5030e8e89ad466c14e383406d61232
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2024-10-08 17:00:57 +0530

    force replace a FunctionCall when original target and --realloc-lhs is true

--- src/libasr/pass/simplifier.cpp
+++ src/libasr/pass/simplifier.cpp
@@ -1507,7 +1507,7 @@ class ReplaceExprWithTemporary: public ASR::BaseExprReplacer<ReplaceExprWithTemp
 
         if( is_current_expr_linked_to_target && ASRUtils::is_array(x->m_type) ) {
             targetType target_Type = exprs_with_target[*current_expr].second;
-            if( target_Type == targetType::OriginalTarget ) {
+            if( target_Type == targetType::OriginalTarget && realloc_lhs ) {
                 force_replace_current_expr_for_array(std::string("_function_call_") +
                                            ASRUtils::symbol_name(x->m_name))
                 return ;
