3d62ecc671cf836db8d8259ae1737e9e4769c9db
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2024-10-17 18:41:32 +0530

    uncomment stdlib CI job and fix return type of generic procedure

--- integration_tests/elemental_function_scalar_array_arg.f90
+++ integration_tests/elemental_function_scalar_array_arg.f90
@@ -1,5 +1,9 @@
 module mod_elemental_function_scalar_array_arg
     implicit none
+    interface count
+        module procedure count_char_char
+    end interface
+
     contains
 
     !> Returns an integer
@@ -17,10 +21,15 @@ program elemental_function_scalar_array_arg
     use mod_elemental_function_scalar_array_arg
     implicit none
     character(len=128) :: string
-    integer :: count(3)
+    integer :: count_value1(3)
+    integer :: count_value2(3)
 
     string = "How much wood would a woodchuck chuck if a woodchuck could chuck wood?"
     print *, count_char_char(string, ["would", "chuck", "could"])
-    count = count_char_char(string, ["would", "chuck", "could"])
-    if (any(count /= [1, 1, 1])) error stop
+    count_value1 = count_char_char(string, ["would", "chuck", "could"])
+    if (any(count_value1 /= [1, 1, 1])) error stop
+
+    print *, count(string, ["would", "chuck", "could"])
+    count_value2 = count(string, ["would", "chuck", "could"])
+    if (any(count_value2 /= [1, 1, 1])) error stop
 end program
