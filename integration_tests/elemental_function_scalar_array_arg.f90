4a64f814115836e91aa0ccb3681d6e5bb0e040bc
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2024-10-17 17:33:04 +0530

    minor change

--- integration_tests/elemental_function_scalar_array_arg.f90
+++ integration_tests/elemental_function_scalar_array_arg.f90
@@ -18,9 +18,9 @@ program elemental_function_scalar_array_arg
     implicit none
     character(len=128) :: string
     integer :: count(3)
-  
+
     string = "How much wood would a woodchuck chuck if a woodchuck could chuck wood?"
     print *, count_char_char(string, ["would", "chuck", "could"])
     count = count_char_char(string, ["would", "chuck", "could"])
     if (any(count /= [1, 1, 1])) error stop
-end program 
+end program
