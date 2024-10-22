f8ef4327e257a9d459ddfa4ac16a8624728c1374
Assem <assem2002@gmail.com>
2024-10-22 06:07:49 +0300

    port asr_util.h

--- src/libasr/asr_utils.h
+++ src/libasr/asr_utils.h
@@ -5723,6 +5723,37 @@ static inline ASR::asr_t* make_FunctionCall_t_util(
 
     Call_t_body(al, a_name, a_args, n_args, a_dt, nullptr, false, nopass);
 
+    if( ASRUtils::is_array(a_type) && ASRUtils::is_elemental(a_name) &&
+        !ASRUtils::is_fixed_size_array(a_type) &&
+        !ASRUtils::is_dimension_dependent_only_on_arguments(a_type) ) {
+        ASR::ttype_t* type_ = ASRUtils::type_get_past_array_pointer_allocatable(a_type);
+        ASR::expr_t* i32one = ASRUtils::EXPR(ASR::make_IntegerConstant_t(al, a_loc, 1,
+            ASRUtils::TYPE(ASR::make_Integer_t(al, a_loc, 4))));
+        for( size_t i = 0; i < n_args; i++ ) {
+            ASR::ttype_t* type = ASRUtils::expr_type(a_args[i].m_value);
+            if (ASRUtils::is_array(type)) {
+                ASR::dimension_t* m_dims = nullptr;
+                size_t n_dims = ASRUtils::extract_dimensions_from_ttype(type, m_dims);
+                if( ASRUtils::is_dimension_empty(m_dims, n_dims) ) {
+                    Vec<ASR::dimension_t> m_dims_vec; m_dims_vec.reserve(al, n_dims);
+                    for( size_t j = 0; j < n_dims; j++ ) {
+                        ASR::dimension_t m_dim_vec;
+                        m_dim_vec.loc = m_dims[j].loc;
+                        m_dim_vec.m_start = i32one;
+                        m_dim_vec.m_length = ASRUtils::EXPR(ASR::make_ArraySize_t(al, m_dims[j].loc,
+                            a_args[i].m_value, i32one, ASRUtils::expr_type(i32one), nullptr));
+                        m_dims_vec.push_back(al, m_dim_vec);
+                    }
+                    m_dims = m_dims_vec.p;
+                    n_dims = m_dims_vec.size();
+                }
+                a_type = ASRUtils::make_Array_t_util(al, type->base.loc, type_, m_dims, n_dims,
+                    ASR::abiType::Source, false, ASR::array_physical_typeType::DescriptorArray, true);
+                break;
+            }
+        }
+    }
+
     return ASR::make_FunctionCall_t(al, a_loc, a_name, a_original_name,
             a_args, n_args, a_type, a_value, a_dt);
 }
