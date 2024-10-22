d2b8c0431fd4335a99831cea2c768fd74f137a23
jinangshah21 <jinang2110@gmail.com>
2024-10-23 02:31:18 +0530

    result of size and func is verified

--- integration_tests/array_section_03.f90
+++ integration_tests/array_section_03.f90
@@ -1,9 +1,14 @@
 program array_section_03
    implicit none
    integer :: y(2, 3)
+   integer :: x(2)
    y = reshape([6, 3, 8, 5, 2, 7], shape(y))
+   x = func(y(:,1))
    print *, size(y(:, 1))
    print *, func(y(:,1))
+
+   if(size(y(:,1)) /= 2) error stop
+   if(x(1) /= 6 .or. x(2) /= 3) error stop
 
 contains
 
