08af94301b1bdfb121a3a0b9c65dcc086655638f
Aditya Trivedi <adit4443ya@gmail.com>
2024-10-23 12:20:20 +0530

    changed to collapse(2)

--- integration_tests/openmp_39.f90
+++ integration_tests/openmp_39.f90
@@ -7,7 +7,7 @@ program main
     nx=4
     nz=5
 
-!$omp parallel do collapse(3) private(iy, ix, iz)
+!$omp parallel do collapse(2) private(iy, ix, iz)
 do iy = 1, ny
     do ix = 1, nx
         print *,"iy->", iy,"ix->", ix
