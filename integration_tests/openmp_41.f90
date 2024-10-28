b10cf22aa2eccc9e4457dc91225578eff7c06ff7
Aditya Trivedi <adit4443ya@gmail.com>
2024-10-28 20:11:42 +0530

    Added another testcase

---
+++ integration_tests/openmp_41.f90
@@ -0,0 +1,24 @@
+program main
+   use omp_lib
+! declare variables as required
+   integer :: ny, nx, nz,nk
+   integer :: iy, ix, iz,ik
+   ny=3
+   nx=4
+   nz=5
+   nk=6
+
+!$omp parallel do collapse(3) private(iy, ix, iz)
+   do iy = 1, ny
+      do ix = 1, nx
+         print *,"iy->", iy,"ix->", ix
+         do iz = 1, nz
+            do ik = 1, nk
+               print *,"iy->", iy,"ix->", ix,"iz->", iz,"ik->", ik
+            end do
+         end do
+      end do
+   end do
+!$omp end parallel do
+
+end program
