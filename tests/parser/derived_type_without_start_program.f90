35b4a3a4e8d63c9cd8e595f30a74c5ea3c150ce5
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2024-11-06 20:45:30 +0530

    use the original reported program in issue

--- tests/parser/derived_type_without_start_program.f90
+++ tests/parser/derived_type_without_start_program.f90
@@ -1,7 +1,3 @@
-! Test to ensure that 'type' declaration without
-! a 'program' statement at the beginning parses
-! correctly
-    implicit none
     type :: test_t
         real :: a
     end type
