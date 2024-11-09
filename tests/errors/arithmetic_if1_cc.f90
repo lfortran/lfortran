7d88aaa228bd7853f1a73c30756562706a32aead
parth121101 <parthmistry1211@gmail.com>
2024-11-09 19:44:18 +0530

    tests: add and register error test for continue compilation

---
+++ tests/errors/arithmetic_if1_cc.f90
@@ -0,0 +1,13 @@
+program arithmetic_if1
+    integer :: x, c
+    x = -3
+    c = 0
+    if ("yy") 1, 2, 3
+    1 c = c + 1
+    2 c = c + 2
+    3 c = c + 4
+    print *, c
+    print *, "continued compilation"
+    if (c /= 7) error stop
+end program
+    
