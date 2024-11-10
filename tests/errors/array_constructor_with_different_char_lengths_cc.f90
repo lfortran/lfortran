1f887cba89f841862bd05d0294a4e09326b7bf44
parth121101 <parthmistry1211@gmail.com>
2024-11-10 14:52:06 +0530

    tests: add and register test for continued compilation

---
+++ tests/errors/array_constructor_with_different_char_lengths_cc.f90
@@ -0,0 +1,5 @@
+program array_constructor_with_different_char_lengths
+    print *, ["a", "b", "ball", "cat"]
+    print *, ["a1", "b1", "ball1", "cat1"]
+    print *, "compilation continued despite errors"
+end program
