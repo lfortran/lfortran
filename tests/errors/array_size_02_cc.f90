57be88621e2050a3141a8f82677aa711c9eaaa8c
HarshitaKalani <kalani.1@iitj.ac.in>
2024-11-10 12:38:46 +0530

    test: add and register test

---
+++ tests/errors/array_size_02_cc.f90
@@ -0,0 +1,8 @@
+program arrays_02
+    implicit none
+    integer :: a(3)
+    integer :: size_a
+    size_a = size(a, 1, dim=1)
+    size_a = size(a, dim = 1, 1)
+    print *, "compilation continued despite errors"
+end program
