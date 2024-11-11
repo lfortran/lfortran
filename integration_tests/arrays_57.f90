88becb78bb0cb7b3f9ffa9d05b5c607d241dbd3a
Pranavchiku <goswami.4@iitj.ac.in>
2024-11-11 19:48:35 +0530

    test: add and register

---
+++ integration_tests/arrays_57.f90
@@ -0,0 +1,21 @@
+program arrays_57
+
+real :: A(2,2) = reshape([1.0,2.0,3.0,4.0], [2,2])
+call trans(A)
+print *, A
+if ( any(abs( A - 12.91 ) > 1e-6) ) error stop 
+
+contains
+
+subroutine trans(A)
+real, intent(inout) :: A(2, 2)
+A = matprod(transpose(A))
+end subroutine
+
+function matprod(x) result(k)
+real,intent(in) :: x(:,:)
+real :: k(size(x, 1), size(x, 2))
+k = 12.91
+end function
+
+end program 
