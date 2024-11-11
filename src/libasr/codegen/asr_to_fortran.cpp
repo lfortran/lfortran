fb107a5d901231c713978ac6e63d73e7fe4612fd
Aditya Trivedi <adit4443ya@gmail.com>
2024-11-11 17:52:47 +0530

    overrided visit_PointerToCPtr

--- src/libasr/codegen/asr_to_fortran.cpp
+++ src/libasr/codegen/asr_to_fortran.cpp
@@ -1832,7 +1832,10 @@ public:
 
     // void visit_CLoc(const ASR::CLoc_t &x) {}
 
-    // void visit_PointerToCPtr(const ASR::PointerToCPtr_t &x) {}
+    void visit_PointerToCPtr(const ASR::PointerToCPtr_t &x) {
+        visit_expr(*x.m_arg);
+        src = "c_loc(" + src + ")";
+    }
 
     // void visit_GetPointer(const ASR::GetPointer_t &x) {}
 
