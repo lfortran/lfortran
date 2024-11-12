adc4a76ccedff5194d4caffae24590a4dda99e3c
Ansh Mehta <ansh.mehta522007@gmail.com>
2024-11-12 21:36:13 +0530

    removed error messages

--- tests/errors/array_xx_cc.f90
+++ tests/errors/array_xx_cc.f90
@@ -73,14 +73,11 @@ print *, "compilation continued despite errors"
 
 ! data_implied_do1
 data(iarx(i), i=1, k) / 1, 2, 3 /
-print *, "compilation continued despite errors"
 
 ! data_implied_do2
 data(iarx(i), i=1, 3, k) / 1, 2, 3 /
-print *, "compilation continued despite errors"
 
 ! data_implied_do3
 data(iarx(i), i=k, 3) / 1, 2, 3 /
-print *, "compilation continued despite errors"
 
 end program
