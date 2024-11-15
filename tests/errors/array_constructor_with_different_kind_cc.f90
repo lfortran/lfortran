e649ef2b3096cb289b4d0260705f4959ecc20b10
parth121101 <parthmistry1211@gmail.com>
2024-11-16 01:07:22 +0530

    tests: update tests refs

--- tests/errors/array_constructor_with_different_kind_cc.f90
+++
@@ -1,6 +0,0 @@
-program array_constructor_with_different_kind
-    real(8), allocatable :: x(:)
-    allocate(x(4))
-    print *, [x, [1., 2.]]
-    print *, "compilation continued despite errors"
-end program
