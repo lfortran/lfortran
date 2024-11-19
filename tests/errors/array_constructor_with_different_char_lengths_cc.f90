6b3063e4e1294090703dfe2330514f7fd91ab3b8
parth121101 <parthmistry1211@gmail.com>
2024-11-19 22:46:35 +0530

    tests: update test refs

--- tests/errors/array_constructor_with_different_char_lengths_cc.f90
+++
@@ -1,5 +0,0 @@
-program array_constructor_with_different_char_lengths
-    print *, ["a", "b", "ball", "cat"]
-    print *, ["a1", "b1", "ball1", "cat1"]
-    print *, "compilation continued despite errors"
-end program
