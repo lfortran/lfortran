e9cb976be9d7e298cad8b94c062cb7e30dd3faf1
Tanay <manerikartanay@gmail.com>
2024-11-19 06:15:55 +0000

    Moved the test from tests to integration tests

--- integration_tests/multiple_objects_args.f90
+++ integration_tests/multiple_objects_args.f90
@@ -5,13 +5,20 @@ program main
         real :: radius = 1.0
     end type Circle
     
+    logical :: was_called = .false.
+    
     call print_radius(Circle(1.5), Circle())
+
+    if (.not. was_called) then
+        error stop 
+    end if
 
 contains
 
     subroutine print_radius(m, n)
         type(Circle), intent(in) :: m, n
         print *, m%radius, n%radius
+        was_called = .true.
     end subroutine
 
 end program main
