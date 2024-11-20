a9f20746f3455d99ead57c0cd50c6a0303ec83ce
Assem <assem2002@gmail.com>
2024-11-20 05:20:23 +0200

    Add and register test

---
+++ integration_tests/arrays_59.f90
@@ -0,0 +1,20 @@
+program arrays_59
+    real :: A(2,2) = reshape([1.0,2.0,3.0,4.0], [2,2])
+    call trans(A)
+    print *, A 
+
+    contains
+    
+    subroutine trans(A)
+    real, intent(inout) :: A(:, :)
+    real :: B(size(A,1), size(A,2))
+    B = A
+    A = matprod(transpose(B))
+    end subroutine
+
+    function matprod(x) result(k)
+    real,intent(in) :: x(:,:)
+    real :: k(size(x, 1), size(x, 2))
+    k = 12.91
+    end function
+ end program 
