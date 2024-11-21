8c7b58ddaa6947f9bc22c0b74d1d67abd2403805
Pranavchiku <goswami.4@iitj.ac.in>
2024-11-21 15:04:07 +0530

    enh: create a new function that returns json string

--- src/libasr/lsp.cpp
+++ src/libasr/lsp.cpp
@@ -326,6 +326,105 @@ int get_definitions(const std::string &infile, LCompilers::CompilerOptions &comp
 
     return 0;
 }
+
+std::string get_definitions_source_code(std::string input, LCompilers::CompilerOptions &compiler_options)
+{
+    LCompilers::FortranEvaluator fe(compiler_options);
+    std::vector<LCompilers::document_symbols> symbol_lists;
+
+    LCompilers::LocationManager lm;
+    {
+        LCompilers::LocationManager::FileLocations fl;
+        fl.in_filename = "xx.f90";
+        lm.files.push_back(fl);
+        lm.file_ends.push_back(input.size());
+    }
+    {
+        LCompilers::diag::Diagnostics diagnostics;
+        LCompilers::Result<LCompilers::ASR::TranslationUnit_t*>
+            x = fe.get_asr2(input, lm, diagnostics);
+        if (x.ok) {
+            // populate_symbol_lists(x.result, lm, symbol_lists);
+            uint16_t l = std::stoi(compiler_options.line);
+            uint16_t c = std::stoi(compiler_options.column);
+            uint64_t pos = lm.linecol_to_pos(l, c);
+            LCompilers::ASR::asr_t* asr = fe.handle_lookup_name(x.result, pos);
+            LCompilers::document_symbols loc;
+            if (!ASR::is_a<ASR::symbol_t>(*asr)) {
+                return "{}";
+            }
+            ASR::symbol_t* s = ASR::down_cast<ASR::symbol_t>(asr);
+            std::string symbol_name = ASRUtils::symbol_name( s );
+            uint32_t first_line;
+            uint32_t last_line;
+            uint32_t first_column;
+            uint32_t last_column;
+            std::string filename;
+            lm.pos_to_linecol(asr->loc.first, first_line,
+                first_column, filename);
+            lm.pos_to_linecol(asr->loc.last, last_line,
+                last_column, filename);
+            loc.first_column = first_column;
+            loc.last_column = last_column;
+            loc.first_line = first_line-1;
+            loc.last_line = last_line-1;
+            loc.symbol_name = symbol_name;
+            loc.filename = filename;
+            symbol_lists.push_back(loc);
+        } else {
+            return "{}";
+        }
+    }
+
+    rapidjson::Document test_output(rapidjson::kArrayType);
+    rapidjson::Document range_object(rapidjson::kObjectType);
+    rapidjson::Document start_detail(rapidjson::kObjectType);
+    rapidjson::Document end_detail(rapidjson::kObjectType);
+    rapidjson::Document location_object(rapidjson::kObjectType);
+    rapidjson::Document test_capture(rapidjson::kObjectType);
+
+    test_output.SetArray();
+
+    for (auto symbol : symbol_lists) {
+        uint32_t start_character = symbol.first_column;
+        uint32_t start_line = symbol.first_line;
+        uint32_t end_character = symbol.last_column;
+        uint32_t end_line = symbol.last_line;
+        std::string name = symbol.symbol_name;
+
+        range_object.SetObject();
+        rapidjson::Document::AllocatorType &allocator = range_object.GetAllocator();
+
+        start_detail.SetObject();
+        start_detail.AddMember("character", rapidjson::Value().SetInt(start_character), allocator);
+        start_detail.AddMember("line", rapidjson::Value().SetInt(start_line), allocator);
+        range_object.AddMember("start", start_detail, allocator);
+
+        end_detail.SetObject();
+        end_detail.AddMember("character", rapidjson::Value().SetInt(end_character), allocator);
+        end_detail.AddMember("line", rapidjson::Value().SetInt(end_line), allocator);
+        range_object.AddMember("end", end_detail, allocator);
+
+        location_object.SetObject();
+        location_object.AddMember("range", range_object, allocator);
+        location_object.AddMember("uri", rapidjson::Value().SetString("uri", allocator), allocator);
+
+        test_capture.SetObject();
+        test_capture.AddMember("kind", rapidjson::Value().SetInt(1), allocator);
+        test_capture.AddMember("location", location_object, allocator);
+        test_capture.AddMember("name", rapidjson::Value().SetString(name.c_str(), allocator), allocator);
+        test_output.PushBack(test_capture, test_output.GetAllocator());
+    }
+    rapidjson::StringBuffer buffer;
+    buffer.Clear();
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+    test_output.Accept(writer);
+    std::string resp_str( buffer.GetString() );
+
+    return resp_str;
+}
 #endif
 }
+
+
 
