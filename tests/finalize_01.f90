ac8179230bd9d8f19ac81a56992272135ae78161
Assem <assem2002@gmail.com>
2024-11-25 07:52:09 +0200

    Modify test and update

--- tests/finalize_01.f90
+++ tests/finalize_01.f90
@@ -2,12 +2,22 @@ module finalize_01_mod
     contains
     subroutine test_01() ! Test that we deallocate allocatable characters
         character(:), allocatable :: str
+        integer :: i
         allocate(character(10) :: str)
+        return ! This will have `impliciteDeallocate` inserted before it.
+        do i=1, 2
+            return ! This won't have `impliciteDeallocate` inserted before it as we already have dead end. 
+        end do
     end subroutine test_01
 
     subroutine ss() ! Test deallocation on arrays.
         integer, allocatable :: arr(:)
         allocate(arr(10))
+        if(.true.) then
+            return ! This will have `impliciteDeallocate` inserted before it.
+        else 
+            return ! This will have `impliciteDeallocate` inserted before it.
+        end if
     end subroutine ss
 end module finalize_01_mod
 
@@ -25,6 +35,14 @@ program finalize_01
 
     subroutine internal_sub()
         character(:),allocatable :: str 
-        real, allocatable :: arr_real(:,:)  
+        real, allocatable :: arr_real(:,:)
+        bl : block
+            integer, allocatable :: arr_in_block(:)
+        exit bl ! This will have `impliciteDeallocate` inserted before it.
+        return ! This won't have `impliciteDeallocate` inserted before it as we already have dead end. 
+        end block bl 
+        do while(.true.)
+            return ! This will have `impliciteDeallocate` inserted before it.
+        end do
     end subroutine internal_sub
 end program finalize_01
