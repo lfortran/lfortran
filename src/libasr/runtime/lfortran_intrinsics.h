f8edc65081d8d5256b2701c6138b5c47e13c3d13
Saurabh Kumar <developer.saurabh@outlook.com>
2024-11-25 22:06:44 +0530

    enh: free memory allocated to command-line arguments at program termination
    Added `_lpython_free_argv()` to deallocate memory allocated by `_lpython_set_argv()` when program ends.

--- src/libasr/runtime/lfortran_intrinsics.h
+++ src/libasr/runtime/lfortran_intrinsics.h
@@ -258,6 +258,7 @@ LFORTRAN_API void _lfortran_close(int32_t unit_num);
 LFORTRAN_API int32_t _lfortran_ichar(char *c);
 LFORTRAN_API int32_t _lfortran_iachar(char *c);
 LFORTRAN_API void _lpython_set_argv(int32_t argc_1, char *argv_1[]);
+LFORTRAN_API void _lpython_free_argv();
 LFORTRAN_API int32_t _lpython_get_argc();
 LFORTRAN_API char *_lpython_get_argv(int32_t index);
 LFORTRAN_API void _lpython_call_initial_functions(int32_t argc_1, char *argv_1[]);
