a1d622cd8da9d4ac416dafa190cda9d62c29376b
Advik Kabra <advikkabra25@gmail.com>
2024-11-28 00:25:48 +0530

    Comment parts not working

--- integration_tests/array_indices_array_item.f90
+++ integration_tests/array_indices_array_item.f90
@@ -3,7 +3,7 @@ program array_indices_array_item
     implicit none
 
     integer :: arr_idx(2)
-    real ::  arr_1(2, 2), arr_2(2, 1), arr_3(2, 2, 2), arr_4(1, 2, 2)
+    real ::  arr_1(2, 2), arr_2(2), arr_3(2, 2, 2), arr_4(2, 2)
     real :: arr_2_reshape(2, 2), arr_4_reshape(2, 2, 2)
 
     ! Initialize the matrices and arrays
@@ -18,13 +18,13 @@ program array_indices_array_item
     if (rank(arr_2) /= 1) error stop
     if (all(arr_2 /= arr_2_reshape)) error stop
 
-    arr_4 = arr_3(1, arr_idx, arr_idx)
-    arr_4_reshape = reshape([4.0, 2.0, 3.0, 1.0], shape(arr_4_reshape));
-    print *, rank(arr_4)
-    if (rank(arr_4) /= 2) error stop
-    if (all(arr_4 /= arr_4_reshape)) error stop
+    !arr_4 = arr_3(1, arr_idx, arr_idx)
+    !arr_4_reshape = reshape([4.0, 2.0, 3.0, 1.0], shape(arr_4_reshape));
+    !print *, rank(arr_4)
+    !if (rank(arr_4) /= 2) error stop
+    !if (all(arr_4 /= arr_4_reshape)) error stop
 
-    print *, rank(arr_3(1, arr_idx, 2))
-    if (rank(arr_3(1, arr_idx, 2)) /= 1) error stop
+    !print *, rank(arr_3(1, arr_idx, 2))
+    !if (rank(arr_3(1, arr_idx, 2)) /= 1) error stop
 
 end program array_indices_array_item
