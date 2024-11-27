cc893518139f55c551a67d8bff2fd084e0d430be
Saurabh Kumar <developer.saurabh@outlook.com>
2024-11-28 00:04:42 +0530

    tests: update test and references

--- tests/finalize_02.f90
+++ tests/finalize_02.f90
@@ -1,30 +1,42 @@
-program deallocate_01
-    implicit none
- 
-    integer, allocatable :: my_int(:)
- 
-    type my_type
-       integer, allocatable :: int(:)
-    end type
- 
-    type(my_type) :: x
- 
-    allocate(x%int(2))
-    allocate(my_int(2))
- 
-    call sub
-    call sub
- 
- contains
- 
-    subroutine sub
-       type my_type_sub
-          integer, allocatable :: int(:)
-       end type
- 
-       type(my_type) :: x_sub
- 
-       allocate(x_sub%int(2))
-    end subroutine sub
- end program
- 
+module finalize_module
+
+   type :: Circle
+      real, allocatable :: radius(:)
+   end type Circle
+
+end module finalize_module
+
+program finalize_02
+   use finalize_module
+   implicit none
+
+   integer, allocatable :: int(:)
+
+   type my_int
+      integer, allocatable :: int(:)
+   end type
+   
+   type(my_int) :: x_int
+   type(Circle) :: c
+
+   allocate(c%radius(2))
+
+   allocate(x_int%int(2))
+   allocate(int(2))
+
+   call sub
+   call sub
+
+contains
+
+   subroutine sub
+      type my_int_sub
+         integer, allocatable :: int(:)
+      end type
+
+      type(my_int) :: x_sub
+
+      allocate(x_sub%int(2))
+   end subroutine sub
+end program
+
