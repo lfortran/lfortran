37823a694c7d6407343cd3bdf4e700f43d493dd1
Advik Kabra <advikkabra25@gmail.com>
2024-12-02 15:52:16 +0530

    DEBUG: removed segfault causing code

--- integration_tests/array_indices_array_item.f90
+++ integration_tests/array_indices_array_item.f90
@@ -12,40 +12,40 @@ program array_indices_array_item
     !arr_1 = reshape([1.0, 2.0, 3.0, 4.0], shape(arr_1))
     arr_3 = reshape([1.0, 2.0, 3.0, 4.0, 1.0, 2.0, 3.0, 4.0, 5.0], shape(arr_3))
     arr_idx = [2, 1]
-    !allocate(arr_idx2(2))
-    !arr_idx2 = [2, 1]
+    allocate(arr_idx2(2))
+    arr_idx2 = [2, 1]
 
-     !Slicing
-    !arr_2 = arr_1(arr_idx, 1)
-    !arr_2_reshape = reshape([3.0, 1.0], shape(arr_2_reshape));
-    !print *, rank(arr_2)
-    !if (rank(arr_2) /= 1) error stop
-    !if (all(arr_2 /= arr_2_reshape)) error stop
+     Slicing
+    arr_2 = arr_1(arr_idx, 1)
+    arr_2_reshape = reshape([3.0, 1.0], shape(arr_2_reshape));
+    print *, rank(arr_2)
+    if (rank(arr_2) /= 1) error stop
+    if (all(arr_2 /= arr_2_reshape)) error stop
 
-    !arr_4 = arr_3(1, arr_idx, arr_idx)
-    !arr_4_reshape = reshape([4.0, 2.0, 3.0, 1.0], shape(arr_4_reshape));
-    !print *, rank(arr_4)
-    !if (rank(arr_4) /= 2) error stop
-    !if (all(arr_4 /= arr_4_reshape)) error stop
+    arr_4 = arr_3(1, arr_idx, arr_idx)
+    arr_4_reshape = reshape([4.0, 2.0, 3.0, 1.0], shape(arr_4_reshape));
+    print *, rank(arr_4)
+    if (rank(arr_4) /= 2) error stop
+    if (all(arr_4 /= arr_4_reshape)) error stop
 
     rank_val = rank(arr_3(1, arr_idx, 2))
-    !print *, rank_val
-    !if (rank_val /= 1) error stop
+    print *, rank_val
+    if (rank_val /= 1) error stop
 
-    !arr_2 = arr_1(arr_idx2, 1)
-    !arr_2_reshape = reshape([3.0, 1.0], shape(arr_2_reshape));
-    !print *, rank(arr_2)
-    !if (rank(arr_2) /= 1) error stop
-    !if (all(arr_2 /= arr_2_reshape)) error stop
+    arr_2 = arr_1(arr_idx2, 1)
+    arr_2_reshape = reshape([3.0, 1.0], shape(arr_2_reshape));
+    print *, rank(arr_2)
+    if (rank(arr_2) /= 1) error stop
+    if (all(arr_2 /= arr_2_reshape)) error stop
 
-    !arr_4 = arr_3(1, arr_idx2, arr_idx2)
-    !arr_4_reshape = reshape([4.0, 2.0, 3.0, 1.0], shape(arr_4_reshape));
-    !print *, rank(arr_4)
-    !if (rank(arr_4) /= 2) error stop
-    !if (all(arr_4 /= arr_4_reshape)) error stop
+    arr_4 = arr_3(1, arr_idx2, arr_idx2)
+    arr_4_reshape = reshape([4.0, 2.0, 3.0, 1.0], shape(arr_4_reshape));
+    print *, rank(arr_4)
+    if (rank(arr_4) /= 2) error stop
+    if (all(arr_4 /= arr_4_reshape)) error stop
 
-    !rank_val = rank(arr_3(1, arr_idx2, 2))
-    !print *, rank_val
-    !if (rank_val /= 1) error stop
+    rank_val = rank(arr_3(1, arr_idx2, 2))
+    print *, rank_val
+    if (rank_val /= 1) error stop
 
 end program array_indices_array_item
