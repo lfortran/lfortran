f4b97e10e19c367250593f78752f31152605068e
Aditya Trivedi <adit4443ya@gmail.com>
2024-12-03 16:02:13 +0530

    test: apply suggested review

--- integration_tests/openmp_40.f90
+++ integration_tests/openmp_40.f90
@@ -1,6 +1,8 @@
 program openmp_40
-integer :: i, j
-do concurrent ( i =1:4, j = 1:3 )
+integer :: i, j, res=0
+do concurrent ( i =1:4, j = 1:3 ) reduce(+:res)
     print *,i
+    res=res+i
 end do 
+print *,"res =",res
 end program
