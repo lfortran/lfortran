ef0ef3847f1a71b3d801ec2bce7b5946b2adf061
Advik Kabra <64316822+advikkabra@users.noreply.github.com>
2024-12-04 10:48:42 +0530

    Allow for allocatable file names in file inquire (#5565)

--- integration_tests/inquire_01.f90
+++ integration_tests/inquire_01.f90
@@ -7,7 +7,11 @@ contains
 
     subroutine check_inquire_01()
         logical :: file_exists
+        character(len=:), allocatable :: fname
+        fname = "this/will/fail.always"
         inquire (file='/this/will/fail.always', exist=file_exists)
+        print *, file_exists
+        inquire (file=fname, exist=file_exists)
         print *, file_exists
     end subroutine check_inquire_01
 
