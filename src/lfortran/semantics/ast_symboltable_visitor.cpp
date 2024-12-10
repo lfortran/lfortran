1979edd06c16dd5e69b135ff99aca635da01ccf0
parth121101 <parthmistry1211@gmail.com>
2024-12-11 00:53:29 +0530

    fix: skipped errors for common1.f90 file provided continue-compilation flag

--- src/lfortran/semantics/ast_symboltable_visitor.cpp
+++ src/lfortran/semantics/ast_symboltable_visitor.cpp
@@ -1033,7 +1033,11 @@ public:
         for (size_t i=0; i<x.n_decl; i++) {
             is_Function = true;
             if (!AST::is_a<AST::Require_t>(*x.m_decl[i])) {
-                visit_unit_decl2(*x.m_decl[i]);
+                try {
+                    visit_unit_decl2(*x.m_decl[i]);
+                } catch (SemanticAbort &e) {
+                    if ( !compiler_options.continue_compilation ) throw e;
+                }
             }
             is_Function = false;
         }
