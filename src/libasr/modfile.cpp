e96858c867dedff168a2c488a772cfd70114acf2
saikarna913 <pardheevsai913@gmail.com>
2024-12-11 15:05:05 +0530

    Update modfile.cpp

--- src/libasr/modfile.cpp
+++ src/libasr/modfile.cpp
@@ -69,7 +69,7 @@ std::string save_pycfile(const ASR::TranslationUnit_t &m) {
     return asr_string;
 }
 
-inline void load_serialised_asr(const std::string &s, std::string& asr_binary) {
+inline void load_serialised_asr(const std::string &s, std::string& asr_binary, const std::string &filename) {
 #ifdef WITH_LFORTRAN_BINARY_MODFILES
     BinaryReader b(s);
 #else
@@ -77,14 +77,25 @@ inline void load_serialised_asr(const std::string &s, std::string& asr_binary) {
 #endif
     std::string file_type = b.read_string();
     if (file_type != lfortran_modfile_type_string) {
-        throw LCompilersException("LCompilers Modfile format not recognized");
+        // Print the error and communicate the issue back to the main program
+        std::cerr << "Error: LCompilers Modfile format not recognized for file: " << filename << std::endl;
+        throw LCompilersException("LCompilers Modfile format not recognized for file: " + filename);
     }
+    
     std::string version = b.read_string();
     if (version != LFORTRAN_VERSION) {
-        throw LCompilersException("Incompatible format: LFortran Modfile was generated using version '" + version + "', but current LFortran version is '" + LFORTRAN_VERSION + "'");
+        // Print the error and communicate the issue back to the main program
+        std::cerr << "Error: Incompatible format for file: " << filename 
+                  << ". LFortran Modfile was generated using version '" 
+                  << version << "', but current LFortran version is '" 
+                  << LFORTRAN_VERSION << "'" << std::endl;
+        throw LCompilersException("Incompatible format for file: " + filename + ". LFortran Modfile was generated using version '" + version + "', but current LFortran version is '" + LFORTRAN_VERSION + "'");
     }
+    
+    // Read the binary string and set the output
     asr_binary = b.read_string();
 }
+
 
 ASR::TranslationUnit_t* load_modfile(Allocator &al, const std::string &s,
         bool load_symtab_id, SymbolTable &symtab) {
