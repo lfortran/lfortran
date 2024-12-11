c40d5c0fa05734cb190ce8446a54f3797bd02a04
Aditya Trivedi <adit4443ya@gmail.com>
2024-12-11 23:18:33 +0530

    test: add and register

---
+++ integration_tests/nullify_03.f90
@@ -0,0 +1,25 @@
+module p
+    implicit none
+    integer, parameter, private :: mxdim = 3
+    type :: rp1d
+        real(8), dimension(:), pointer :: f
+    end type
+
+    type :: sds
+        integer :: ndim
+        integer, dimension(mxdim) :: dims
+        logical :: hdf32
+        type(rp1d), dimension(mxdim) :: scales
+        real(8), dimension(:,:,:), pointer :: f
+    end type
+end module
+
+program main
+    use p
+    implicit none
+    type(sds) :: s
+    ! allocate(s%scales(1)%f(2))
+    ! s%scales(1)%f=[1,2]
+    ! print *, 'Scale 0:', s%scales(1)%f
+    nullify(s%scales(1)%f)
+end program
