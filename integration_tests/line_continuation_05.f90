43fbdd14b729eea52a3fd44e0301dd6b31a98197
saikarna913 <pardheevsai913@gmail.com>
2024-12-14 10:26:29 +0530

    Update line_continuation_05.f90

--- integration_tests/line_continuation_05.f90
+++ integration_tests/line_continuation_05.f90
@@ -1,14 +1,21 @@
 program main
   implicit none
-  integer, parameter :: str_len = 256 
-  character(len=str_len), allocatable :: textblock(:)
-  textblock = [character(len=str_len) :: &
-      '!', &
-      's=&', &
-      '& !', &
-      ' & ! 225', &
-      '( 0.0,-1.0 )]   ! 270', &
-      '       do i=1,size(vals)', &
-       '' ]
+  character(len=28),allocatable :: textblock(:)
+  integer :: i, n
+  n = 7
+  allocate(textblock(n))
+  textblock = [character(len=28) :: &
+     '!'//repeat(' ', 27), &
+     's=&'//repeat(' ', 24), &
+     '& !'//repeat(' ', 24), &
+     ' & ! 225'//repeat(' ', 19), &
+     '( 0.0,-1.0 ) ! 270'//repeat(' ', 13), &
+     '       do i=1,size(vals)'//repeat(' ', 3), &
+     ''//repeat(' ', 28) ]
+
+  do i = 1, n
+     print *, textblock(i)
+  end do
+  deallocate(textblock)
 
 end program main
