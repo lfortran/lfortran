2562fe70cf309a83bcd3e60c5d1788697cddd4b7
Aditya Trivedi <adit4443ya@gmail.com>
2024-12-15 16:59:37 +0530

    test:add and register

---
+++ integration_tests/array_init_with_scalar.f90
@@ -0,0 +1,9 @@
+module array_init_with_scalar_0
+    implicit none
+    integer, dimension(2) :: drratio=0
+    real, dimension(2) :: drratior=0.0
+end module
+
+program array_init_with_scalar_main
+implicit none
+end program array_init_with_scalar_main
