437689eb89a524133e427ba8b3e297292b5e7a1a
Assem <assem2002@gmail.com>
2024-12-18 12:20:02 +0200

    Fix : Remove dependency on lcompiler_assert to avoid failures in release mode

--- src/libasr/pass/array_op.cpp
+++ src/libasr/pass/array_op.cpp
@@ -11,6 +11,7 @@
 
 #include <vector>
 #include <utility>
+#include<iostream>
 
 /*
 This ASR pass replaces operations over arrays with do loops.
@@ -908,9 +909,9 @@ class ReplaceArrayOp: public ASR::BaseExprReplacer<ReplaceArrayOp> {
                                 m_step), i32_one);
                         } else if (ASRUtils::is_array(ASRUtils::expr_type(x->m_args[j].m_right))) {
                             ASR::dimension_t* arg_dim = nullptr;
-                            LCOMPILERS_ASSERT(
-                                ASRUtils::extract_dimensions_from_ttype(
-                                    ASRUtils::expr_type(x->m_args[j].m_right), arg_dim) == 1);
+                            [[maybe_unused]] size_t right_index_size = ASRUtils::extract_dimensions_from_ttype(
+                                    ASRUtils::expr_type(x->m_args[j].m_right), arg_dim);
+                            LCOMPILERS_ASSERT( right_index_size == 1);
                             res_dims_vec.p[j] = arg_dim[0];
                         } else if (x->m_args[j].m_right != nullptr || x->m_args[j].m_left != nullptr) {
                             ASR::expr_t* m_left = x->m_args[j].m_left;
