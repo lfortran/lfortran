3fa26e78d0d3d357f00aa4f7e1f8c20f28edd326
Assem <assem2002@gmail.com>
2024-12-19 11:27:47 +0200

    Update test string_43 as it has invalid fortran assignment statement

--- integration_tests/string_43.f90
+++ integration_tests/string_43.f90
@@ -2,12 +2,10 @@ program string_43
 
     implicit none
     character(:), allocatable :: a
-    character(:), allocatable :: b
 
     a = "Hello"
-    a  =  b
 
     print *, a
-    if(a /= "") error stop
+    if(a /= "Hello") error stop
 
 end program string_43
