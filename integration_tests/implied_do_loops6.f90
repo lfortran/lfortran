8dea21d092676d4551ee90155508f2690e101dc1
Gagandeep Singh <gdp.1807@gmail.com>
2024-12-20 21:50:44 +0530

    TEST: Added checks in implied_do_loops6

--- integration_tests/implied_do_loops6.f90
+++ integration_tests/implied_do_loops6.f90
@@ -11,9 +11,11 @@ Q([1, 3]) = -4.0
 print *, Q
 R = [abs(Q), 0.0_4]
 print *, R
+if( any(R /= [4.0, 5.0, 4.0, 5.0, 0.0]) ) error stop
 
 rank = maxval([0_4, trueloc(Q > 0)])
 print *, rank
+if( rank /= 4 ) error stop
 
 contains
 
