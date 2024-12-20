53078e7e7e508ee092e6e05983383871fca455d8
Pranavchiku <goswami.4@iitj.ac.in>
2024-12-20 12:48:08 +0530

    test: add, register and support --show-errors

--- run_tests.py
+++ run_tests.py
@@ -54,6 +54,7 @@ def single_test(test: Dict, verbose: bool, no_llvm: bool, skip_run_with_dbg: boo
     asr_implicit_interface_and_typing_with_llvm = is_included("asr_implicit_interface_and_typing_with_llvm")
     continue_compilation = is_included("continue_compilation")
     semantics_only_cc = is_included("semantics_only_cc")
+    show_errors = is_included("show_errors")
     syntax_only_cc = is_included("syntax_only_cc")
     show_asr_with_cc = is_included("show_asr_with_cc")
     asr_use_loop_variable_after_loop = is_included("asr_use_loop_variable_after_loop")
@@ -339,6 +340,13 @@ def single_test(test: Dict, verbose: bool, no_llvm: bool, skip_run_with_dbg: boo
 
     if semantics_only_cc:
         run_test(filename, "asr", "lfortran --semantics-only --continue-compilation --no-color {infile}",
+            filename,
+            update_reference,
+            verify_hash,
+            extra_args)
+    
+    if show_errors:
+        run_test(filename, "asr", "lfortran --show-errors --continue-compilation --no-color {infile}",
             filename,
             update_reference,
             verify_hash,
