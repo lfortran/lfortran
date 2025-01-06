1288bf75b2f0df39906ca45fed1df26f0f8e7f35
Assem <assem2002@gmail.com>
2025-01-06 23:49:59 +0200

    Add and register integration test

---
+++ integration_tests/arrays_reshape_19.f90
@@ -0,0 +1,19 @@
+module arrays_reshape_19_mod
+    contains 
+    subroutine another_func(q)
+      integer, intent(in) :: q(:,:)
+      integer,allocatable :: anew(:)
+      allocate(anew(size(q) +2))
+      print *, size(anew)
+      anew = reshape([0,q,17], [18])
+      print *, anew
+      if(any(anew /= [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17])) error stop 
+    end subroutine another_func
+  end module
+  
+  program arrays_reshape_19
+    use arrays_reshape_19_mod
+    integer :: q(4,4)
+    q = reshape([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16], [4,4])
+    call another_func(q)
+  end program arrays_reshape_19
