4cbded8c187d09e486c5a40766225780c5820c04
Assem <assem2002@gmail.com>
2025-01-07 00:06:17 +0200

    Update test

--- integration_tests/arrays_reshape_19.f90
+++ integration_tests/arrays_reshape_19.f90
@@ -2,9 +2,7 @@ module arrays_reshape_19_mod
     contains 
     subroutine another_func(q)
       integer, intent(in) :: q(:,:)
-      integer,allocatable :: anew(:)
-      allocate(anew(size(q) +2))
-      print *, size(anew)
+      integer :: anew(18)
       anew = reshape([0,q,17], [18])
       print *, anew
       if(any(anew /= [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17])) error stop 
