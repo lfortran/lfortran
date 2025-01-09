44b16911a20aaa582e3d2d16506ab77bdb313870
Saurabh Kumar <developer.saurabh@outlook.com>
2025-01-09 14:55:40 +0530

    tests: add test case with `procedure` pointer

--- integration_tests/derived_types_38.f90
+++ integration_tests/derived_types_38.f90
@@ -6,9 +6,13 @@ end module derived_types_38_mod
 
 program derived_types_38
    use derived_types_38_mod
+   ! Uncomment below test case after supporting procedure
+   ! declaration using subroutine, inside a derived type declared 
+   ! in `program`.
 
    type :: t
       integer :: i
+      !   procedure(f), nopass, pointer :: proc
    end type t
 
    type(m) :: m_var
@@ -18,8 +22,12 @@ program derived_types_38
    t_var%i = 2
 
    call f()
+   call g()
 
-   print *, t_var
+!    t_var%proc => g
+!    call t_var%proc()
+
+   print *, t_var%i
    if (t_var%i /= 3) error stop
 
 contains
@@ -27,6 +35,10 @@ contains
    subroutine f()
       print *, m_var
       if (m_var%i /= 1) error stop
-      t_var%i = t_var%i + 1
    end subroutine f
+
+   subroutine g()
+      t_var%i = t_var%i + 1
+   end subroutine g
+
 end program derived_types_38
