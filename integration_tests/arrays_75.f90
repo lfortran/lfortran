28c0ee1ff68e8040ecf488d321615fde716394bc
AnshMehta1 <ansh.mehta522007@gmail.com>
2025-01-12 15:55:19 +0530

    Handled Initialization and modified integration test as for Gfortran

--- integration_tests/arrays_75.f90
+++ integration_tests/arrays_75.f90
@@ -1,12 +1,18 @@
 program arrays_75
-integer :: score(4) = [-1,1,3,2]
+    integer :: score(4) = [-1,1,3,2]
 
-print * , [score, 1] > 0
-if (([score,1] > 0) /= [.false. , .true. , .true. , .true. , .true.]) error stop
+    print *, [score, 1] > 0
+    if (ALL(([score, 1] > 0) .NEQV. [.false., .true., .true., .true., .true.])) then
+        error stop
+    end if
 
-print * , 0 < [score , -1]
-if ((0 < [score , -1]) /= [.false. , .true. , .true. , .true. , .false.]) error stop
+    print *, 0 < [score, -1]
+    if (ALL((0 < [score, -1]) .NEQV. [.false., .true., .true., .true., .false.])) then
+        error stop
+    end if
 
-print * , [score , 1] <= [score , -1]
-if (([score , 1] <= [score , -1]) /= [.true. , .true. , .true. , .true. , .false.]) error stop
+    print *, [score, 1] <= [score, -1]
+    if (ALL(([score, 1] <= [score, -1]) .NEQV. [.true., .true., .true., .true., .false.])) then
+        error stop
+    end if
 end program
