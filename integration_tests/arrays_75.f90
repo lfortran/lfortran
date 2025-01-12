99725ebb790f4cce1cd342927f0f77ab26192101
AnshMehta1 <ansh.mehta522007@gmail.com>
2025-01-12 15:10:08 +0530

    Adding more integration tests

--- integration_tests/arrays_75.f90
+++ integration_tests/arrays_75.f90
@@ -3,4 +3,10 @@ integer :: score(4) = [-1,1,3,2]
 
 print * , [score, 1] > 0
 if (([score,1] > 0) /= [.false. , .true. , .true. , .true. , .true.]) error stop
+
+print * , 0 < [score , -1]
+if ((0 < [score , -1]) /= [.false. , .true. , .true. , .true. , .false.]) error stop
+
+print * , [score , 1] <= [score , -1]
+if (([score , 1] <= [score , -1]) /= [.true. , .true. , .true. , .true. , .false.]) error stop
 end program
