92f0e97798ddafa50003aba8c16e4acb94acfe0a
Aditya Trivedi <adit4443ya@gmail.com>
2025-01-15 00:57:44 +0530

    Test: improve indent

--- integration_tests/openmp_43.f90
+++ integration_tests/openmp_43.f90
@@ -2,21 +2,21 @@ program openmp_43
    implicit none
 
    integer :: i,sum_fun,sum_sub
-
+   
    do concurrent (i = 1:4) reduce(+:sum_fun)
       sum_fun = sum_fun + FUN(i)
    end do
-print *,"FUN sum->", sum_fun
+
+   print *,"FUN sum->", sum_fun
 
    do concurrent (i = 1:4) reduce(+:sum_sub)
       call SUB(i)
       sum_sub = sum_sub + i
    end do
 
-print *,"SUB sum->", sum_sub
-if(sum_sub /= 30) error stop
-
-if(sum_fun /= 30) error stop
+   print *,"SUB sum->", sum_sub
+   if(sum_sub /= 30) error stop
+   if(sum_fun /= 30) error stop
 
 contains
 
