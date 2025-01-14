8d4f0e6f2b0a5d42d92fa04be80212fe686184ec
Aditya Trivedi <adit4443ya@gmail.com>
2025-01-14 19:38:25 +0530

    Test: Added Subroutine and error check

--- integration_tests/openmp_43.f90
+++ integration_tests/openmp_43.f90
@@ -1,13 +1,29 @@
 program openmp_43
    implicit none
 
-   integer :: i
+   integer :: i,sum_fun,sum_sub
 
-   do concurrent (i = 1:4)
-      print *,"Fun->", FUN(i)
+   do concurrent (i = 1:4) reduce(+:sum_fun)
+      sum_fun = sum_fun + FUN(i)
    end do
-print *,"Fun->", FUN(2)
+print *,"FUN sum->", sum_fun
+
+   do concurrent (i = 1:4) reduce(+:sum_sub)
+      call SUB(i)
+      sum_sub = sum_sub + i
+   end do
+
+print *,"SUB sum->", sum_sub
+if(sum_sub /= 30) error stop
+
+if(sum_fun /= 30) error stop
+
 contains
+
+   pure subroutine SUB(x)
+      integer, intent(inout) :: x
+       x = x * x
+   end subroutine SUB
 
    PURE integer function FUN(x)
       integer, intent(in) :: x
