fa4f6dc052220bba74cda7830cdd0f61305d97ab
HarshilShah1804 <hkshah1978@gmail.com>
2025-01-16 14:51:11 +0530

    Initialize variable to values

--- integration_tests/matrix_02_matmul.f90
+++ integration_tests/matrix_02_matmul.f90
@@ -2,6 +2,9 @@ program matrix_02_matmul
 implicit none
 integer :: i, j, k, a(3, 4)
 real :: b(4, 3), cmat(3, 3), centry
+
+a = reshape([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], shape(a))
+b = reshape([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0], shape(b))
 cmat = matmul(a, b)
 
 do i = 1, 3
