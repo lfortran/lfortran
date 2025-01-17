145e33c812ab36bbae8875fa8410bcc372c81188
Jaysukh Makvana <111515433+Jaysukh-409@users.noreply.github.com>
2025-01-17 15:57:53 +0530

    chore: update program name

--- integration_tests/test_ieee_arithmetic_02.f90
+++ integration_tests/test_ieee_arithmetic_02.f90
@@ -1,6 +1,6 @@
-program dpcheck
+program test_ieee_arithmetic_02
   use , intrinsic:: ieee_arithmetic
   integer , parameter:: dp = kind(1d0)
   real:: expected = 1/3.0
   if (abs(1/3.0_dp - expected) > 1e-6) error stop
-end program dpcheck
+end program test_ieee_arithmetic_02
