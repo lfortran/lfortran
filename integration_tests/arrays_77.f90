f3d47993cbddecd6a19379b2f964b336e125a0d1
AnshMehta1 <ansh.mehta522007@gmail.com>
2025-01-17 21:13:08 +0530

    Modified module name in Integration test

--- integration_tests/arrays_77.f90
+++ integration_tests/arrays_77.f90
@@ -1,14 +1,14 @@
-module a 
+module a_arrays_77
   contains 
   function f2() result(res)
     integer(8), allocatable :: res(:)
     allocate(res(10))
     res = [1,2,3,4,5,6,7,8,9,10]
   end function f2
-end module a 
+end module a_arrays_77
 
 program arrays_77
-  use a
+  use a_arrays_77
   integer :: i
   integer :: arr(10)
   integer,allocatable :: arr_res(:)
