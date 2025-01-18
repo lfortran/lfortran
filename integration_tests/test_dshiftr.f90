f7d0f774bcce888010a4d895a95cf6a9954c5220
Pranav <85227306+Pranavchiku@users.noreply.github.com>
2025-01-18 10:25:28 +0530

    chore: apply suggestion from code review

--- integration_tests/test_dshiftr.f90
+++ integration_tests/test_dshiftr.f90
@@ -2,5 +2,5 @@ program test_dshiftr
   integer, parameter:: ik1 = selected_int_kind(1)
   integer(ik1), parameter:: i0 = 0, i1 = 1
   integer(ik1):: expected = 64
-  if (abs(dshiftr(i1, i0, 2) - expected) > 1e-6) error stop
+  if (dshiftr(i1, i0, 2) /= expected) error stop
 end program test_dshiftr
