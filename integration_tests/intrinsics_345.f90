40b8d2482cd44af8343dd3c4d4b8081d99de409a
HarshilShah1804 <hkshah1978@gmail.com>
2025-01-20 00:42:58 +0530

    Update test

--- integration_tests/intrinsics_345.f90
+++ integration_tests/intrinsics_345.f90
@@ -1,6 +1,9 @@
 program intrinsics_345
     complex, dimension(2) :: dc = [(1.4, 1.1), (2.3, 2.2)]
     complex :: x = (1.4, 1.1)
-    print *, findloc(dc, x)
-    if (findloc(dc, x) /= 1) error stop
-end program
+    integer, dimension(1) :: loc
+
+    loc = findloc(dc, x)  
+    print *, loc
+    if (loc(1) /= 1) error stop
+end program intrinsics_345
