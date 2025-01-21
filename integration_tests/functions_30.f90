28857e339d2405f8696ff0a9bf6ecaae6cccc23c
jinangshah21 <jinang2110@gmail.com>
2025-01-21 12:36:20 +0530

    test: add integration test

--- integration_tests/functions_30.f90
+++ integration_tests/functions_30.f90
@@ -4,10 +4,25 @@ program functions_30
     if(maxval([1.0,x]) /= 4.0) error stop
     print *, max_func([1.0,x])
     if(max_func([1.0,x]) /= 4.0) error stop
+    !! Assignment with function on rhs having lhs var in its arg
+    x = x / norm(x)
+    print *, x
+    if(any(abs(x - [0.182574183, 0.365148365, 0.547722518, 0.730296731]) > 10e-12)) error stop
 contains 
     function max_func(x) result(y)
       real, intent(in) :: x(:)
       real :: y
       y = maxval(x)
     end function
+    function norm(vec) result(res)
+      implicit none
+      real, intent(in) :: vec(:)   
+      real :: res                  
+      integer :: i                 
+      res = 0.0                    
+      do i = 1, size(vec)
+          res = res + vec(i)**2   
+      end do
+      res = sqrt(res)             
+end function norm
 end program
