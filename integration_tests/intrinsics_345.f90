2a86ee55458eac0cd1212d530a0af1cc2283987a
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2025-01-26 17:49:56 +0530

    add tests for 'findloc'

--- integration_tests/intrinsics_345.f90
+++ integration_tests/intrinsics_345.f90
@@ -5,16 +5,31 @@ program intrinsics_345
     integer, dimension(2, 3) :: array3 = reshape([2, 2, 0, 1, 5, 1], [2, 3])
     integer, dimension(2, 3) :: array4 = reshape([1, 1, 1, 1, 1, 1], [2, 3])
     integer, dimension(2, 3) :: array5 = reshape([2, 2, 1, 0, 1, 0], [2, 3])
+    integer, dimension(2, 3) :: array6 = reshape([0, 1, 1, 1, 1, 2], [2, 3])
     integer :: value = 1
 
-    print *, findloc(array1, value, dim=1)
     if (any(findloc(array1, value, dim=1) /= [1, 2, 2])) error stop
-    print *, findloc(array2, value, dim=1)
+    if (any(findloc(array1, value, dim=1, back=.true.) /= [2, 2, 2])) error stop
+    if (any(findloc(array1, value, dim=2) /= [1, 1])) error stop
+    if (any(findloc(array1, value, dim=2, back=.true.) /= [1, 3])) error stop
     if (any(findloc(array2, value, dim=1) /= [2, 2, 2])) error stop
-    print *, findloc(array3, value, dim=1)
+    if (any(findloc(array2, value, dim=1, back=.true.) /= [2, 2, 2])) error stop
+    if (any(findloc(array2, value, dim=2) /= [0, 1])) error stop
+    if (any(findloc(array2, value, dim=2, back=.true.) /= [0, 3])) error stop
     if (any(findloc(array3, value, dim=1) /= [0, 2, 2])) error stop
-    print *, findloc(array4, value, dim=1)
+    if (any(findloc(array3, value, dim=1, back=.true.) /= [0, 2, 2])) error stop
+    if (any(findloc(array3, value, dim=2) /= [0, 2])) error stop
+    if (any(findloc(array3, value, dim=2, back=.true.) /= [0, 3])) error stop
     if (any(findloc(array4, value, dim=1) /= [1, 1, 1])) error stop
-    print *, findloc(array5, value, dim=1)
+    if (any(findloc(array4, value, dim=1, back=.true.) /= [2, 2, 2])) error stop
+    if (any(findloc(array4, value, dim=2) /= [1, 1])) error stop
+    if (any(findloc(array4, value, dim=2, back=.true.) /= [3, 3])) error stop
     if (any(findloc(array5, value, dim=1) /= [0, 1, 1])) error stop
+    if (any(findloc(array5, value, dim=1, back=.true.) /= [0, 1, 1])) error stop
+    if (any(findloc(array5, value, dim=2) /= [2, 0])) error stop
+    if (any(findloc(array5, value, dim=2, back=.true.) /= [3, 0])) error stop
+    if (any(findloc(array6, value, dim=1) /= [2, 1, 1])) error stop
+    if (any(findloc(array6, value, dim=1, back=.true.) /= [2, 2, 1])) error stop
+    if (any(findloc(array6, value, dim=2) /= [2, 1])) error stop
+    if (any(findloc(array6, value, dim=2, back=.true.) /= [3, 2])) error stop
 end program
