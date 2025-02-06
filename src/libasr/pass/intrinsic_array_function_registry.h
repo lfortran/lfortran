9b946d920d812c2ea3362f57f9a778ffd46dbd6d
Chaitanya140904 <chaitanya094257@gmail.com>
2025-02-06 22:26:08 +0530

    Fix issue in count function

--- src/libasr/pass/intrinsic_array_function_registry.h
+++ src/libasr/pass/intrinsic_array_function_registry.h
@@ -3941,7 +3941,10 @@ namespace Count {
             Vec<ASR::expr_t*>& args, diag::Diagnostics& diag) {
         int64_t id_mask = 0, id_mask_dim = 1;
         int64_t overload_id = id_mask;
-
+        if (!is_array(expr_type(args[0])) || !is_logical(*expr_type(args[0]))){
+            append_error(diag, "`mask` argument to `count` intrinsic must be a logical array",
+                args[0]->base.loc);
+        }
         ASR::expr_t *mask = args[0], *dim_ = nullptr, *kind = nullptr;
 
         if (args.size() == 2) {
