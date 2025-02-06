c24642918bd1e55ef151aa087b6085532323fdd3
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2025-02-06 11:48:23 +0530

    fix: separate runtime functions to read string into array values

--- integration_tests/read_01.f90
+++ integration_tests/read_01.f90
@@ -6,12 +6,14 @@ program read_01
     integer(8) :: i64
     real :: f32
     real(8) :: f64
+    integer, save, dimension(3) :: i32_arr_size3 = [-1,-1,-1]
+    character(20) :: key
  
     character(5) :: val
     character(len=:), allocatable :: al_val
-    
+
     character(len=:), allocatable :: al_str
-    
+
     read(str, *) c
     print *, c
     if (c /= "42") error stop
@@ -19,7 +21,7 @@ program read_01
     read(str, *) i32
     print *, i32
     if (i32 /= 42) error stop
- 
+
     read(str, *) i64
     print *, i64
     if (i64 /= 42) error stop
@@ -27,7 +29,7 @@ program read_01
     read(str, *) f32
     print *, f32
     if (f32 /= 42.0) error stop
- 
+
     read(str, *) f64
     print *, f64
     if (f64 /= 42.0) error stop
@@ -40,7 +42,6 @@ program read_01
     print *, al_str
     if (al_str /= "ab") error stop
 
-    
     allocate(character(len=2) :: al_val)
 
     str = "cd"
@@ -48,6 +49,14 @@ program read_01
     read(str, *) al_val
     print *, al_val
     if (al_val /= "cd") error stop
- 
- end program
- 
+
+    key = "int_read"
+    select case(key)
+        case ("int_read")
+            read(str, *) i32
+        case ("int_arr_size3_read")
+            read(str, *) i32_arr_size3
+    end select
+    print *, i32
+    if (i32 /= 42) error stop
+end program
