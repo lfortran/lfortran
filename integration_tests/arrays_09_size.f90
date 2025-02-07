3098023326bfe94634a512ef904310d3b41fb764
AnshMehta1 <ansh.mehta522007@gmail.com>
2025-02-07 19:23:02 +0530

    Change module name in integration_test

--- integration_tests/arrays_09_size.f90
+++ integration_tests/arrays_09_size.f90
@@ -1,13 +1,13 @@
-module moddd
+module mod_arrays_09_size
   contains 
   subroutine sub(xx)
     real :: xx(5)
     real :: buggy(count(xx > 0.0))
     if (size(buggy) /= 3) error stop
   end subroutine sub
-end module moddd
+end module mod_arrays_09_size
 program main
-  use moddd
+  use mod_arrays_09_size
   real :: xx(5) = [1.1, 2.1, -1.3, 3.4, 0.0]
   call sub(xx)
 end program main
