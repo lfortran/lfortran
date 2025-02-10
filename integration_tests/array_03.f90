8487bfebd4633bec2f7330030b903031a299c63e
HarshitaKalani <kalani.1@iitj.ac.in>
2025-02-10 23:28:34 +0530

    test: add test for passing array indices in array reference

---
+++ integration_tests/array_03.f90
@@ -0,0 +1,23 @@
+program array_03
+    integer, parameter :: RP = kind(0.0D0)
+    integer, parameter :: IK = kind(0)
+    real(RP) :: G(2, 2)
+    integer(IK) :: knew
+    real(RP):: zmat(1:1, 1:1) 
+    knew = 1
+    zmat = reshape([1.0_RP], [1, 1])
+    G = planerot(zmat(knew, [1_IK, 2]))
+    print *, G
+    if (any(G /= reshape([1.0_RP, -1.0_RP, 1.0_RP, 1.0_RP], [2, 2]))) error stop
+    contains
+    
+    function planerot(x) result(G)
+        real(RP), intent(in) :: x(:)
+        real(RP) :: c, s, r
+        real(RP) :: G(2, 2)
+        r = 1
+        s = 1
+        c = x(1) / r
+        G = reshape([c, -s, s, c], [2, 2])
+    end function
+end program
