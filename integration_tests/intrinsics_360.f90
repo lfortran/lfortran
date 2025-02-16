3f79352008f8c5841f38ccd746c0674e67344ac0
HarshilShah1804 <hkshah1978@gmail.com>
2025-02-16 23:41:41 +0530

    test: Update test

--- integration_tests/intrinsics_360.f90
+++ integration_tests/intrinsics_360.f90
@@ -1,11 +1,11 @@
 program intrinsics_360
-    real(8), dimension(2) :: dc_r = [1.3, 2.3]
-    real :: x_r = 1.3
-    print *, findloc(dc_r, x_r)
-    if (any(findloc(dc_r, x_r) /= 1)) error stop
-
-    integer, dimension(2) :: dc_i = [1, 2]
-    integer(8) :: x_i = 2
-    print *, findloc(dc_i, x_i)
-    if (any(findloc(dc_i, x_i) /= 2)) error stop
-end program
+    implicit none  
+    real(8), dimension(2) :: dc_r = [1.3, 2.3]  
+    real :: x_r = 1.3  
+    integer, dimension(2) :: dc_i = [1, 2]  
+    integer(8) :: x_i = 2  
+    print *, findloc(dc_r, x_r) 
+    if (any(findloc(dc_r, x_r) /= 1)) error stop  
+    print *, findloc(dc_i, x_i)  
+    if (any(findloc(dc_i, x_i) /= 2)) error stop 
+end program intrinsics_360
