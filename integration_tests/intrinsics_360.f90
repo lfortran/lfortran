ed73e7482a9e5ad4a6f82cab92d52044255574c7
HarshilShah1804 <hkshah1978@gmail.com>
2025-02-17 01:37:13 +0530

    test: add test

--- integration_tests/intrinsics_360.f90
+++ integration_tests/intrinsics_360.f90
@@ -8,4 +8,8 @@ program intrinsics_360
     if (any(findloc(dc_r, x_r) /= 1)) error stop  
     print *, findloc(dc_i, x_i)  
     if (any(findloc(dc_i, x_i) /= 2)) error stop 
+    print *, findloc(dc_r, x_i)
+    if (any(findloc(dc_r, x_i) /= 0)) error stop
+    print *, findloc(dc_i, x_r)
+    if (any(findloc(dc_i, x_r) /= 1)) error stop
 end program intrinsics_360
