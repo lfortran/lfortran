de62e9d71eb9cb7589c3414c00ba71f86fe30f53
Assem <assem2002@gmail.com>
2025-02-17 07:52:23 +0200

    Test : Revert removed test ( Adding it back )

--- integration_tests/sign_from_value.f90
+++ integration_tests/sign_from_value.f90
@@ -2,7 +2,9 @@ program flip_sign
     implicit none
     real :: rxsp = 5.5, epsrsp = 1e-6
     real(8) :: rxdp = 5.5, epsrdp = 1e-6
+    integer :: ixsp = 5, epsisp = 16
     integer(8) :: ixdp = 5, epsidp = 16
+    integer :: a=2, b=-3, c
     real :: x=-2, y=-3, z, arr(5)
 
     rxsp = rxsp * sign(1._4, epsrsp)
@@ -17,6 +19,10 @@ program flip_sign
     print *, rxdp
     if (abs(rxdp - 5.5) > epsrdp) error stop
 
+    ixsp = ixsp * sign(1_4, epsisp)
+    print *, ixsp
+    if (ixsp /= 5) error stop
+
     rxdp = rxdp * sign(1._8, -epsrdp)
     print *, rxdp
     if (abs(rxdp + 5.5) > epsrdp) error stop
@@ -25,6 +31,9 @@ program flip_sign
     print *, ixdp
     if (ixdp /= 5) error stop
 
+    c = a*sign(1, b) ! Test that we don't apply sign opt. on integers.
+    print *, c
+    if(c /= -2) error stop
 
     arr = [1.0, 2.0, 3.0, 4.0, 5.0]
     z = arr(int(x*sign(1.0,y), 4)) ! Test nested `sign` expression
