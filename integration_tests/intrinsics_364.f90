2a3c2a278828f1a5266f9fcac35191502528dd30
Pranavchiku <goswami.4@iitj.ac.in>
2025-02-20 17:47:54 +0530

    test: add and register

---
+++ integration_tests/intrinsics_364.f90
@@ -0,0 +1,7 @@
+program intrinsics_364
+implicit none
+real, allocatable :: array(:, :)
+allocate(array(2:5, 5:10))
+print *, is_contiguous(array)
+if ( .not. is_contiguous(array) ) error stop
+end program
