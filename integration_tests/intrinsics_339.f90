ea6202a5569da2dc943cea4280b863819d6fd94c
HarshitaKalani <kalani.1@iitj.ac.in>
2025-02-26 14:20:10 +0530

    test: update test

--- integration_tests/intrinsics_339.f90
+++ integration_tests/intrinsics_339.f90
@@ -6,4 +6,6 @@ program intrinsics_339
     if (any(minloc(score, mask=(score > 0)) /= 2)) error stop
     print *, minloc(score, 1, [.false., .true., .true., .true.])
     if (minloc(score, 1, [.false., .true., .true., .true.]) /= 2) error stop
+    print*, minloc([1, 2, 3], 1, .true.)
+    if (minloc([1, 2, 3], 1, .true.) /= 1) error stop
 end program
