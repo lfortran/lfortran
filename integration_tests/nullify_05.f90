e09018c3ac634bc4f6c76281ee7a891ba3fab76d
jinangshah21 <jinang2110@gmail.com>
2025-02-27 11:41:55 +0530

    test: add integration tests

--- integration_tests/nullify_05.f90
+++ integration_tests/nullify_05.f90
@@ -1,11 +1,23 @@
-program nullify_05
-   procedure(OBJ), pointer :: calfun
-   nullify(calfun)
-
-   if (associated(calfun)) error stop
-
+module nullify_05_type
 contains
-
    subroutine OBJ()
    end subroutine
+end module
+
+module nullify_05_mod
+   use nullify_05_type
+   procedure(OBJ), pointer :: calfun_ext
+end module
+
+program nullify_05
+   use nullify_05_mod
+   use nullify_05_type
+   procedure(OBJ), pointer :: calfun
+   calfun => OBJ
+   nullify(calfun)
+   calfun_ext => OBJ
+   nullify(calfun_ext)
+
+   if (associated(calfun)) error stop
+   if (associated(calfun_ext)) error stop
 end program
