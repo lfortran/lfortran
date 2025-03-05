aca84c55d36c1d58ae5b2b5023bdd7756d344434
jinangshah21 <jinang2110@gmail.com>
2025-03-05 11:33:07 +0530

    enh: apply same logic to subroutineCall

--- integration_tests/pass_array_by_data_11.f90
+++ integration_tests/pass_array_by_data_11.f90
@@ -2,22 +2,27 @@ program pass_array_by_data_11
 
     implicit none
     real :: y0(5)
-    if (AB5(func, y0(:)) /= 1.0) error stop 
+    y0 = AB5(func, y0(:))
+    if (any(y0 /= [1.0,1.0,1.0,1.0,0.0])) error stop 
 
 contains
 
-    real function AB5(f, y0)
+    function AB5(f, y0)
         interface
             real function f(y)
                 real, intent(in) :: y(:)
             end function
         end interface
         real, intent(in) :: y0(:)
-        AB5 = 1.0
+        real, allocatable :: AB5(:)
+        allocate(AB5(size(y0)))
+        AB5(1:size(y0)-1) = func(y0(1:3))
+        AB5(size(y0)) = 0.0 
     end function
 
     real function func(y)
         real, intent(in) :: y(:)
+        func = 1.0
     end function
 
 end program
