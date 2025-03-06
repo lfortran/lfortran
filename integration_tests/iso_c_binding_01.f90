3409b4654f1a9a84210b391a64be3970146b3fd3
Jaysukh-409 <jaysukhmakvana2004@gmail.com>
2025-03-07 00:09:19 +0530

    update test

--- integration_tests/iso_c_binding_01.f90
+++ integration_tests/iso_c_binding_01.f90
@@ -1,4 +1,6 @@
 program iso_c_binding_01
       use iso_c_binding
       character(len=2, kind=c_char), parameter :: a = 'a'
+      if (a /= 'a') then error stop
+      if (len(a) /= 2) then error stop
 end program iso_c_binding_01
