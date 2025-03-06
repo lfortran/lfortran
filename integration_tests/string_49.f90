7d7b90c9de49885dd9aed329ee36cb8a22b4fefb
jinangshah21 <jinang2110@gmail.com>
2025-03-07 01:20:14 +0530

    update test

--- integration_tests/string_49.f90
+++ integration_tests/string_49.f90
@@ -1,17 +1,13 @@
 module string_49_mod
    implicit none
    private
-   public getopt, str
    character(len=128) :: str = ''
+   public getopt, str
    contains
       subroutine getopt() 
         character(len=10) :: status 
-        if (str(1:128) == '-') then
-            status =  "fail"
-        else
-            status = "success"
-        end if
-        if (status /= "success") error stop
+        if (str(1:1) == '-') error stop
+        if (str(1:128) == '-') error stop
       end subroutine getopt
 
 end module string_49_mod
@@ -22,16 +18,8 @@ program string_49
     character(len=10) :: status 
     character(len=128) :: str2 = '' 
     call getopt()
-    if (str(1:128) == '-') then
-        status =  "fail"
-    else
-        status = "success"
-    end if
-    if (status /= "success") error stop
-    if (str2(1:128) == '-') then
-        status =  "fail"
-    else
-        status = "success"
-    end if
-    if (status /= "success") error stop
+    if (str(1:1) == '-') error stop
+    if (str2(1:1) == '-') error stop
+    if (str(1:128) == '-') error stop
+    if (str2(1:128) == '-') error stop
 end program 
