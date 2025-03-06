ef7d7cc5acf5c400befd1d5e7bfd6fb888b1fd98
Jaysukh-409 <jaysukhmakvana2004@gmail.com>
2025-03-07 00:17:20 +0530

    update stop condition

--- integration_tests/iso_c_binding_01.f90
+++ integration_tests/iso_c_binding_01.f90
@@ -1,7 +1,7 @@
 program iso_c_binding_01
       use iso_c_binding
       character(len=2, kind=c_char), parameter :: a = 'a'
-      if (a /= 'a') then error stop
-      if (len(a) /= 2) then error stop
-      if (kind(a) /= c_char) then error stop
+      if (a /= 'a') error stop
+      if (len(a) /= 2) error stop
+      if (kind(a) /= c_char) error stop
 end program iso_c_binding_01
