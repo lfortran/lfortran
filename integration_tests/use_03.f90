751066d187a59950311b29eaf0a6ff4db677fb47
Pranavchiku <goswami.4@iitj.ac.in>
2025-03-13 18:49:48 +0530

    test: apply suggestion for use_03 integration test from code review

--- integration_tests/use_03.f90
+++ integration_tests/use_03.f90
@@ -1,13 +1,19 @@
 module test_use_03
     use iso_fortran_env, ip => int32
-    ! use iso_fortran_env, only: ip => int32
     implicit none
     integer,parameter :: i = real64
+    contains
+    subroutine test_ip_working()
+        print *, ip
+        if ( ip /= 4 ) error stop
+    end subroutine
 end module test_use_03
 
 program use_03
     use test_use_03
     implicit none
-    print *, i
+    print *, i, ip
     if ( i /= 8 ) error stop
+    if ( ip /= 4 ) error stop
+    call test_ip_working()
 end program
