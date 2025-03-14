bfac0f2ad0f0b5bc2b3d98df0fc922ca31235f48
AnshMehta1 <ansh.mehta522007@gmail.com>
2025-03-14 19:31:44 +0530

    tests : Add integration tests with explicit typing for attribute dimension

--- integration_tests/attr_dim_01.f90
+++ integration_tests/attr_dim_01.f90
@@ -3,8 +3,7 @@ subroutine sub (array)
     double precision array
 end subroutine
 
-program subroutines_19
-    implicit none
+program attr_dim_01
     double precision , dimension(3) :: arr
     call sub(arr)
 end program
