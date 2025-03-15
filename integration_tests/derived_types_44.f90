c56807f285ca173a1134a7d1e90e2f4baf5334b9
Assem <assem2002@gmail.com>
2025-03-15 11:58:29 +0200

    Test : Enhance test

--- integration_tests/derived_types_44.f90
+++ integration_tests/derived_types_44.f90
@@ -11,16 +11,22 @@ program derived_types_44
     print *, "ubound(x): ", ubound(x)
     if ( abs(x(1) - 1.0) > 1e-8 ) error stop
     if ( abs(x(2) - 2.0) > 1e-8 ) error stop
-    print *, "x in sub = ", x
+    if ( abs(x(100000) - 88.0) > 1e-8 ) error stop
+    print *, "x(1) = ", x(1)
+    print *, "x(2) = ", x(2)
+    print *, "x(100000) = ", x(100000) ! Use big size to make sure about correct memory.
     deallocate(x)
 contains
     subroutine sub_to_allocate(x)
         real, dimension(:), pointer, intent(inout) :: x
         type(sds) :: s
-        allocate(s%f(2))
+        allocate(s%f(100000))
         x => s%f
         x(1)=1.0
         x(2)=2.0
-        print *, "x = ", x
+        x(100000)=88.0
+        print *, "x(1) = ", x(1)
+        print *, "x(2) = ", x(2)
+        print *, "x(100000) = ", x(100000)
     end subroutine
 end program
