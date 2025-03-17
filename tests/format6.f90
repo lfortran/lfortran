1a1b47642b9e3581134537c155b3b257786c69d8
Assem <assem2002@gmail.com>
2025-03-17 12:23:19 +0200

    Minor : Edit test

--- tests/format6.f90
+++ tests/format6.f90
@@ -9,7 +9,7 @@ program format6
     print '(SP,I3.2)', 1111
     print '(SP,ss,I0.2)', 11
     PRINT '(SP,D25.10)', 12345.6789
-    print "(SP,E67.60)", 1.23456789101112e-62_8
+    print "(SP,E60.50)", 1.23456789101112e-62_8
     PRINT '(SP,EN12.4, EN12.1)', 12345.6789 , 12845.6789 
     print '(SP,F5.1)', 1.0 
 end program
