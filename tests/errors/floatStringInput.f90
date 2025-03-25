1d6601b760ed1b4f00bb178a62b59390ba150b6e
Th3C0d3Mast3r <acharyadevesh1105@gmail.com>
2025-03-25 18:57:53 +0530

    File Changes and Tests Added

---
+++ tests/errors/floatStringInput.f90
@@ -0,0 +1,9 @@
+program floatStringInput
+    implicit none
+    real::x
+
+
+    print *, "Enter Float: "
+    read(*,*) x
+    print *, "You Entered: ", x
+end program floatStringInput
---
+++ tests/errors/integerStringInput.f90
@@ -0,0 +1,9 @@
+program integerStringInput
+    implicit none
+    integer::x
+
+
+    print *, "Enter Integer: "
+    read(*,*) x
+    print *, "You Entered: ", x
+end program integerStringInput
