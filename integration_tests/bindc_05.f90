251ada62dded60badf096e893f567d4cabb20bba
Aditya Trivedi <adit4443ya@gmail.com>
2025-03-27 17:17:17 +0530

    Test: Update Test

--- integration_tests/bindc_05.f90
+++ integration_tests/bindc_05.f90
@@ -1,28 +1,55 @@
-module mpi_barr
-    contains
+module mpi
+    use iso_c_binding, only: c_int, c_ptr, c_f_pointer, c_null_ptr, c_associated
+    implicit none
 
-    function c_mpi_comm_f2c(comm_f) bind(C, name="MPI_Comm_f2c")
-        use iso_c_binding, only: c_int, c_ptr
-        integer(c_int), value :: comm_f
-        type(c_ptr) :: c_mpi_comm_f2c  ! MPI_Comm as pointer
-    end function c_mpi_comm_f2c
+    interface
+        function ax(comm_f) bind(C, name="ax")
+            import :: c_int, c_ptr
+            integer(c_int), value :: comm_f
+            type(c_ptr) :: ax
+        end function ax
+    end interface
+
+contains
 
     subroutine MPI_Barrier(comm, ierror)
-        use iso_c_binding, only: c_int, c_ptr
         integer, intent(in) :: comm
         integer, intent(out), optional :: ierror
         type(c_ptr) :: c_comm
+        integer, pointer :: fortran_ptr
 
-        c_comm = c_mpi_comm_f2c(comm)
+        ! Call the C function
+        c_comm = ax(comm)
+        if (.not. c_associated(c_comm)) then
+            print *, "Error: Null pointer returned from C function"
+            if (present(ierror)) ierror = 1
+            return
+        end if
+
+        ! Convert C pointer to Fortran pointer
+        call c_f_pointer(c_comm, fortran_ptr)
+        print *, "Fortran received value:", fortran_ptr
+
+        ! Verify the value matches what was passed
+        if (fortran_ptr /= comm) then
+            print *, "Error: Value mismatch"
+            if (present(ierror)) ierror = 1
+        else
+            if (present(ierror)) ierror = 0
+        end if
     end subroutine MPI_Barrier
-end module
-
-
+end module mpi
 
 program bindc_05
-    use mpi_barr
+    use mpi
     implicit none
-    integer, parameter :: MPI_COMM_WORLD = 0
+    integer, parameter :: MPI_COMM_WORLD = 42  ! Non-zero value
     integer :: ierr
+
     call MPI_Barrier(MPI_COMM_WORLD, ierr)
-end program
+    if (ierr == 0) then
+        print *, "Test passed successfully"
+    else
+        print *, "Test failed"
+    end if
+end program bindc_05
