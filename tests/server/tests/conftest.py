2613186ac386b724f43e7b6d920ac6508f97b16c
Dylon Edwards <dylon.devo@gmail.com>
2025-03-29 11:20:34 -0400

    Restores shutdown logic for LSP server; sigkills the server if it is not terminated cleanly during the test; prints the server log on failure

--- tests/server/tests/conftest.py
+++ tests/server/tests/conftest.py
@@ -1,5 +1,7 @@
 import os
 import shutil
+import signal
+import sys
 from pathlib import Path
 from typing import Iterator
 
@@ -59,11 +61,26 @@ def client(request: pytest.FixtureRequest) -> Iterator[LFortranLspTestClient]:
         }
     })
 
-    with client.serve():
-        # Steps abstracted by context manager:
-        # 1. Send request: initialize
-        # 2. Send notification: initialized
-        # 3. <yield client>
-        # 4. Send request: shutdown
-        # 5. Send notification: exit
-        yield client
+    log_printed = False
+    try:
+        with client.serve():
+            # Steps abstracted by context manager:
+            # 1. Send request: initialize
+            # 2. Send notification: initialized
+            # 3. <yield client>
+            # 4. Send request: shutdown
+            # 5. Send notification: exit
+            yield client
+    except Exception as e:
+        with open(log_path) as f:
+            print(f.read(), file=sys.stderr)
+            log_printed = True
+        raise e
+    finally:
+        if not client.server.poll():
+            print('Server did not terminate cleanly, terminating it forcefully ...', file=sys.stderr)
+            os.kill(client.server.pid, signal.SIGKILL)
+            if not log_printed:
+                with open(log_path) as f:
+                    print(f.read(), file=sys.stderr)
+                    log_printed = True
