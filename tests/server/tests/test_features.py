2c7864550240a0fa2ded8617a13b30fd4131c6d9
Dylon Edwards <dylon.devo@gmail.com>
2025-04-03 14:52:07 -0400

    Restores disabled tests

--- tests/server/tests/test_features.py
+++ tests/server/tests/test_features.py
@@ -1,8 +1,6 @@
 from pathlib import Path
 from tempfile import NamedTemporaryFile
 
-import pytest
-
 from lsprotocol.types import DidChangeConfigurationParams
 
 from llanguage_test_client.lsp_test_client import IncomingEvent
@@ -19,7 +17,6 @@ def test_goto_definition(client: LFortranLspTestClient) -> None:
     assert doc.line == 8
     assert doc.column == 5
 
-@pytest.mark.skip("Timeout")
 def test_diagnostics(client: LFortranLspTestClient) -> None:
     path = Path(__file__).absolute().parent.parent.parent / "function_call1.f90"
     doc = client.open_document("fortran", path)
@@ -57,7 +54,6 @@ def test_diagnostics(client: LFortranLspTestClient) -> None:
     diagnostics = validation["params"]["diagnostics"]
     assert len(diagnostics) == 0
 
-@pytest.mark.skip("Timeout")
 def test_configuration_caching(client: LFortranLspTestClient) -> None:
     with NamedTemporaryFile(
             prefix="test_configuration_caching-",
