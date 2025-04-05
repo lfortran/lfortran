4f74e493a558819e32e557b9adc2bab08b024656
Ondřej Čertík <ondrej@certik.us>
2025-04-05 17:50:09 -0600

    Test on Windows

--- ci/test.sh
+++ ci/test.sh
@@ -53,12 +53,7 @@ if [[ $WIN != "1" ]]; then
     ./run_tests.py -b llvm2 llvm_rtlib llvm_nopragma -f
     ./run_tests.py -b llvm -f -nf16
     cd ..
-
-    pip install src/server/tests tests/server
-    # NOTE: `--full-trace` tends to print excessively long stack traces. Please
-    # re-enable it if needed:
-    # pytest -vv --showlocals --full-trace --capture=no --timeout=5 tests/server
-#    pytest -vv --showlocals --capture=no --timeout=5 tests/server
-    pytest tests/server/tests/test_lsp.py
-    pytest tests/server/tests/test_lsp.py -s
 fi
+
+pytest tests/server/tests/test_lsp.py
+pytest tests/server/tests/test_lsp.py -s
