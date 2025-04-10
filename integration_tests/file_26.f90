a1d887116cabe4bfca7b23aae3d2b9222f6c7821
jinangshah21 <jinang2110@gmail.com>
2025-04-11 02:35:34 +0530

    tests: add integration test

--- integration_tests/file_26.f90
+++ integration_tests/file_26.f90
@@ -3,7 +3,8 @@ program file_26
   integer, parameter :: n = 5
   integer :: data_out(n), data_in(n)
   integer :: i, write_unit, read_unit, ios
-  character(len=5) :: tmp
+  character(len=2) :: tmp
+  character(len=3) :: tmp2
   data_out = (/ 1, 2, 3, 4, 5 /)
 
   open(newunit=write_unit, file='data.dat', form='unformatted', status='replace')
@@ -28,14 +29,21 @@ program file_26
   print *, data_in
   if (any(data_out /= data_in)) error stop
 
-! TODO: Support stream for character type
-!   open(newunit=write_unit, file='data2.dat', form='unformatted', status='replace', access="stream")
-!   write(write_unit) "Hello"
-!   close(write_unit)
-!   open(newunit=read_unit, file='data2.dat', form='unformatted', status='old', access="stream")
-!   read(read_unit) tmp
-!   close(read_unit)
-!   print *, tmp
-!   if (trim(tmp) /= "Hello") error stop
+  open(newunit=write_unit, file='data2.dat', form='unformatted', status='replace')
+  write(write_unit) "Hello"
+  close(write_unit)
+  open(newunit=read_unit, file='data2.dat', form='unformatted', status='old')
+  read(read_unit) tmp, tmp2
+  close(read_unit)
+  print *, tmp, " ", tmp2
+  if (tmp /= "He" .or. tmp2 /= "llo") error stop
+  open(newunit=write_unit, file='data2.dat', form='unformatted', status='replace', access="stream")
+  write(write_unit) "Hello"
+  close(write_unit)
+  open(newunit=read_unit, file='data2.dat', form='unformatted', status='old', access="stream")
+  read(read_unit) tmp, tmp2
+  close(read_unit)
+  print *, tmp, " ", tmp2
+  if (tmp /= "He" .or. tmp2 /= "llo") error stop
 
 end program
