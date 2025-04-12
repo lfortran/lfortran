e5eaa8e0f562694536a3231758091316aa4ccebf
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2025-04-12 15:20:59 +0530

    improve test file

--- tests/preprocessor20.f90
+++ tests/preprocessor20.f90
@@ -1,10 +1,15 @@
-#if !defined PRIMA_PPF_H
-#define PI 1
-#endif /* include guard to avoid double inclusion
+#if !defined pi_constant
+#define PI 3.14
+#endif /* multi-line comment on a two separate lines
 */
 
+#if !defined negative_constant
+#define negative_one -1
+#endif /* multi-line comment on a single line */
 
-program main
+
+program preprocessor20
     implicit none
     print *, "PI: ", PI
-end program main
+    print *, "negative one: ", negative_one
+end program preprocessor20
