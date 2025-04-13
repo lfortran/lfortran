3829bb209ca40c4b14cc7d9711d54549471f040a
HarshitaKalani <kalani.1@iitj.ac.in>
2025-04-13 19:19:59 +0530

    test: add and register test

---
+++ integration_tests/intrinsics_376.f90
@@ -0,0 +1,22 @@
+module stdlib_logger_intrinsics_376    
+    implicit none
+contains
+subroutine add_log_unit(stat )
+        integer, intent(out), optional    :: stat
+        call validate_unit()
+    contains
+        subroutine validate_unit()
+                if ( present(stat) ) then
+                    stat = 5
+                end if
+        end subroutine validate_unit
+    end subroutine add_log_unit
+end module
+program intrinsics_376
+    use stdlib_logger_intrinsics_376
+    implicit none
+!   integer :: stat
+!   call add_log_unit(stat)
+!   print *, stat
+!   if (stat /= 5) error stop
+end program
