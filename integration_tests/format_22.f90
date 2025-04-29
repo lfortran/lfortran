2789bb53c21cdf8ad3cdf32ccf3859377d44301e
HarshitaKalani <kalani.1@iitj.ac.in>
2025-04-29 19:51:03 +0530

    test: update and register test

--- integration_tests/format_22.f90
+++ integration_tests/format_22.f90
@@ -1,7 +1,9 @@
 program format_22
   implicit none
-  integer :: x
+  integer :: x, y, z
   x = 21
+  y = 100
+  z = 0
   print '(B0)', x
   print '(B1)', x
   print '(B2)', x
@@ -15,4 +17,12 @@ program format_22
   print '(B10)', x
   print "(a,b32,a)",'-1 in b32 format: "', -1,'"'
   print "(a,b0 ,a)",'-1 in b0  format: "', -1,'"'
+  print "(a,b32 ,a)",'10 in b32  format: "', 10,'"'
+  print "(a,b0 ,a)",'11 in b0  format: "', 11,'"'
+  print '(B0)', y
+  print '(B4)', y
+  print '(B5)', y
+  print '(B10)', y
+  print '(B0)', z
+  print '(B4)', z
 end program format_22
