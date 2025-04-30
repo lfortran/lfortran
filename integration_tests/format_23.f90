1666d3387626fbb6565f05b7ce1037e530c08228
HarshitaKalani <kalani.1@iitj.ac.in>
2025-04-30 16:42:23 +0530

    test: udpate test to consider more cases

--- integration_tests/format_23.f90
+++ integration_tests/format_23.f90
@@ -1,23 +1,38 @@
 program format_23
-use,intrinsic :: iso_fortran_env, only : dp=>real64,sp=>real32
-implicit none
-real(kind=dp) :: t
-real(kind=dp) :: my_dp_eps
+  implicit none
+  real(4) :: x1, y1, z1
+  real(8) :: x8, y8, z8
 
-   ! starting with a value of 1, keep dividing the value
-   ! by 2 until no change is detected. Note that with
-   ! infinite precision this would be an infinite loop.
-   ! 1/2 of a non-zero number is always smaller than the number, right?
-   my_dp_eps = 1.0d0
-   INFINITE: do
-               my_dp_eps = my_dp_eps/2.0d0
-               t = 1.0d0 + my_dp_eps
-               if (t <= 1.0d0) exit
-   enddo INFINITE
-   my_dp_eps = 2.0d0*my_dp_eps
-   print *, my_dp_eps
-   print *, 'weird coincidence, that is the same as ',epsilon(0.0d0)
-   print *, 'congratulations! You calculated the epsilon value of a machine the hard way!'
-   print '(b64.64)',my_dp_eps
+  x1 = 3.14
+  y1 = -2.5
+  z1 = 0.0
+
+  x8 = 3.14_8
+  y8 = -2.5_8
+  z8 = 0.0_8
+
+  print *, "Binary format for real(4):"
+  print '(a, b0)', "x1 (3.14): ", x1
+  print '(a, b10)', "x1 (3.14): ", x1
+  print '(a, b32)', "x1 (3.14): ", x1
+
+  print '(a, b0)', "y1 (-2.5): ", y1
+  print '(a, b10)', "y1 (-2.5): ", y1
+  print '(a, b32)', "y1 (-2.5): ", y1
+
+  print '(a, b0)', "z1 (0.0): ", z1
+  print '(a, b32)', "z1 (0.0): ", z1
+
+  print *, "Binary format for real(8):"
+  print '(a, b0)', "x8 (3.14): ", x8
+  print '(a, b10)', "x8 (3.14): ", x8
+  print '(a, b64)', "x8 (3.14): ", x8
+
+  print '(a, b0)', "y8 (-2.5): ", y8
+  print '(a, b10)', "y8 (-2.5): ", y8
+  print '(a, b64)', "y8 (-2.5): ", y8
+
+  print '(a, b0)', "z8 (0.0): ", z8
+  print '(a, b64)', "z8 (0.0): ", z8
 
 end program format_23
