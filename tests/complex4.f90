37dc88279d4e1dbd44fc572314d084a9500c038f
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2025-04-30 12:46:40 +0530

    remove the added test case, depends on platform
    removed the added test case because the reference output for LLVM IR
    depends on the platform macOS vs Linux, and hence isn't the best of
    the idea to use platform dependent reference tests without proper
    infrastructure for that, which we currently don't have

--- tests/complex4.f90
+++
@@ -1,9 +0,0 @@
-program complex4
-    implicit none
-    complex(4) :: x_4
-    complex(8) :: x_8
-    x_4 = (0.5, 0.5)
-    x_8 = (0.5, 0.5)
-    print *, sin(x_4)
-    print *, sin(x_8)
-end program complex4
