232dd86d84f8293baa4d1c1ddaf8efeed99b3574
Pranavchiku <goswami.4@iitj.ac.in>
2025-04-30 17:25:20 +0530

    ci: bump to latest stdlib commit with bitsets

--- ci/test_third_party_codes.sh
+++ ci/test_third_party_codes.sh
@@ -54,8 +54,8 @@ time_section "🧪 Testing stdlib (Less Workarounds)" '
   cd stdlib
   export PATH="$(pwd)/../src/bin:$PATH"
 
-  git checkout n-lf-3
-  git checkout 1de43b51a7c3c67baca65da66ee7e5d14edf6121
+  git checkout n-lf-4
+  git checkout 56d2c75e2bd491475de663692ad37801349919fc
   micromamba install -c conda-forge fypp
 
   git clean -fdx
