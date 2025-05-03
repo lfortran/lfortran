ebf216a0cb41c9b1b984e11cca3cfd719c9fcf2a
Hitesh Ghanchi <hiteshghanchi87@gmail.com>
2025-05-03 10:09:13 +0530

    Test case added

---
+++ tests/iostat_test.f90
@@ -0,0 +1,23 @@
+program read_from_file
+    implicit none
+    character(len=20) :: s 
+    integer :: ios
+    character(len=100) ::sa
+
+    open(unit=10, file="../codes/input.txt", status="old", iostat=ios)
+    if (ios /= 0) then
+        print *, "Error opening the file."
+        stop
+    end if
+
+    print * , ios
+    read(10, *, iostat=ios) s
+    print * , ios
+
+
+
+    print *, trim(s)
+
+    close(10)
+
+end program read_from_file
