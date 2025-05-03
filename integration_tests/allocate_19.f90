7e8bfc8d66163ea2c091c7ce77242ffd4165fa90
HarshilShah1804 <hkshah1978@gmail.com>
2025-05-03 23:03:22 +0530

    test: update test

--- integration_tests/allocate_19.f90
+++ integration_tests/allocate_19.f90
@@ -1,7 +1,29 @@
 program expr2
     integer, allocatable :: a(:), b(:)
+    allocate(a(5))
     a = [1, 2, 3, 4, 5]
+    ! When target size is greater than source
+
     allocate(b(10), source = a)
     print *, b
     if ( any( b(1:5) /= [1, 2, 3, 4, 5])) error stop
+    deallocate(b)
+
+    ! When target size is less than source
+    allocate(b(2), source = a)
+    print *, b
+    if ( any( b /= [1, 2])) error stop
+    deallocate(b)
+
+    ! When target size is same as source
+    allocate(b(5), source = a)
+    print *, b
+    if ( any(b /= [1, 2, 3, 4, 5])) error stop
+    deallocate(b)
+
+    ! When target size is not specified
+    allocate(b, source = a)
+    print *, b
+    if ( any(b /= [1, 2, 3, 4, 5])) error stop
+    deallocate(b)
 end program
