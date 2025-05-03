9ac141dd648b33fb0db4c765956b3d2105390754
Pranavchiku <goswami.4@iitj.ac.in>
2025-05-03 21:03:21 +0530

    fix: test formatting

--- integration_tests/separate_compilation_04a.f90
+++ integration_tests/separate_compilation_04a.f90
@@ -1,19 +1,16 @@
 module separate_compilation_04a_module
 
-    implicit none
-    
-    contains
-    
-    subroutine lu()
-    end subroutine
-    
-    
-    subroutine resol_lu(L)
-    real, allocatable :: L(:,:)
-    print *, sum(L)
-    if ( abs(sum(L) - 2.08361094e+04 ) > 1e-8 ) error stop
-    end subroutine
-    
-    end module
-    
-    
+implicit none
+
+contains
+
+subroutine lu()
+end subroutine
+
+subroutine resol_lu(L)
+real, allocatable :: L(:,:)
+print *, sum(L)
+if ( abs(sum(L) - 2.08361094e+04 ) > 1e-8 ) error stop
+end subroutine
+
+end module
