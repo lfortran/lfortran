ade30b646304b98e55e81b78b1dfd9cca0089cf6
Hitesh Ghanchi <hiteshghanchi87@gmail.com>
2025-05-05 01:03:54 +0530

    Added Testcases

---
+++ integration_tests/formatted_read_01.f90
@@ -0,0 +1,21 @@
+program formatted_read_01
+  character(len=10) :: str1
+  character(len=15) :: str2
+  character(len=5) :: str3
+
+  open(unit=10, file="formatted_read_input_01.txt", status="old")
+
+  read(unit=10, fmt='(a10)') str1
+  print *, str1
+  print *, len(str1)
+
+  read(unit=10, fmt='(a10)') str2
+  print *, str2
+  print *, len(str2)
+
+  read(unit=10, fmt='(a10)') str3
+  print *, str3
+  print *, len(str3)
+
+  close(10)
+end program formatted_read_01
