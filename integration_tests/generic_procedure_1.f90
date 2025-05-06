c37d06431d1ac94b08e2ecd27c8a539a32eeb85a
Aditya Trivedi <adit4443ya@gmail.com>
2025-05-06 19:12:30 +0530

    Test: Check pass and nopass attributes

--- integration_tests/generic_procedure_1.f90
+++ integration_tests/generic_procedure_1.f90
@@ -1,37 +1,48 @@
-module custom_deque
+module derived_types
   implicit none
-  type :: Complex32Deque
-    integer :: size_ = 0
+
+  type :: type_pass
+    integer :: value
   contains
-    procedure :: Complex32Deque_resize_default
-    generic :: resize => Complex32Deque_resize_default
-  end type Complex32Deque
+    procedure :: set_value_pass
+    generic :: set_value => set_value_pass
+  end type
+
+  type :: type_nopass
+    integer :: value
+  contains
+    procedure, nopass :: set_value_nopass
+    generic :: set_value => set_value_nopass
+  end type
 
 contains
 
-  subroutine Complex32Deque_resize_default(this, count, value)
-    class(Complex32Deque), intent(inout) :: this
-    integer, intent(in) :: count
-    integer, optional, intent(in) :: value
+  subroutine set_value_pass(this, value)
+    class(type_pass), intent(inout) :: this
+    integer, intent(in) :: value
+    this%value = 2 * value
+  end subroutine
 
-    ! Simplified implementation to avoid undefined dependencies
-    if (count >= 0) then
-      this%size_ = count
-      if (present(value)) then
-        ! Simulate setting value (not implemented)
-      end if
-    end if
-  end subroutine Complex32Deque_resize_default
+  subroutine set_value_nopass(obj, value)
+    class(type_nopass), intent(inout) :: obj
+    integer, intent(in) :: value
+    obj%value = value
+  end subroutine
 
-end module custom_deque
+end module
 
-program generic_procedure_1
-  use custom_deque
+program main
+  use derived_types
   implicit none
 
-  type(Complex32Deque) :: d
-  integer :: rc=0
+  type(type_pass) :: obj_pass
+  type(type_nopass) :: obj_nopass
 
-  call d%resize(5, value=5)
-  print *, "Size:", d%size_, "RC:", rc
-end program generic_procedure_1
+  obj_pass%value = 42
+  call obj_pass%set_value(value=5)
+  if (obj_pass%value /= 10) error stop "Error in set_value_pass"
+
+  obj_nopass%value = 42
+  call obj_nopass%set_value(obj_nopass, value=5)
+  if (obj_nopass%value /= 5) error stop "Error in set_value_nopass"
+end program
