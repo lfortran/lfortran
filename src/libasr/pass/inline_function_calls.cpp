2da7a22857d2ade37aaee2c0fd75909c79b3627c
Gagandeep Singh <gdp.1807@gmail.com>
2025-05-06 18:43:14 +0530

    DEV: Additional criteria and bug fixes for inline_function_calls

--- src/libasr/pass/inline_function_calls.cpp
+++ src/libasr/pass/inline_function_calls.cpp
@@ -73,6 +73,11 @@ class InlineFunctionCalls: public ASR::BaseExprReplacer<InlineFunctionCalls> {
     InlineFunctionCalls(Allocator& al_): al(al_) {}
 
     bool check_inline_possibility(ASR::symbol_t* func, ASR::FunctionCall_t* func_call) {
+        if( current_body == nullptr ) {
+            // Inlining not possible because the function call is not
+            // inside the body of a Function, Program
+            return false;
+        }
         // Symbol should be a ASR::Function_t
         func = ASRUtils::symbol_get_past_external(func);
         if( !ASR::is_a<ASR::Function_t>(*func) ) {
@@ -80,6 +85,12 @@ class InlineFunctionCalls: public ASR::BaseExprReplacer<InlineFunctionCalls> {
         }
 
         ASR::Function_t* function = ASR::down_cast<ASR::Function_t>(func);
+
+        if( ASRUtils::symbol_abi(func) != ASR::abiType::Source ||
+            ASR::down_cast<ASR::FunctionType_t>(function->m_function_signature)->m_deftype
+                != ASR::deftypeType::Implementation ) {
+            return false;
+        }
 
         // Number of arguments in function call
         // must match the number of arguments
@@ -91,7 +102,14 @@ class InlineFunctionCalls: public ASR::BaseExprReplacer<InlineFunctionCalls> {
         // ⁠Function should have only Variable symbols in its symtab.
         // The type of those Variable symbols shouldn’t be FunctionType.
         for( auto sym: function->m_symtab->get_scope() ) {
-            if( !ASR::is_a<ASR::Variable_t>(*sym.second) ) {
+            if( !ASR::is_a<ASR::Variable_t>(*sym.second) ||
+                (ASR::down_cast<ASR::Variable_t>(sym.second)->m_intent != ASRUtils::intent_in && // TODO: Remove these intent checks
+                ASR::down_cast<ASR::Variable_t>(sym.second)->m_intent != ASRUtils::intent_local && // IntentOut can be accomodated
+                ASR::down_cast<ASR::Variable_t>(sym.second)->m_intent != ASRUtils::intent_unspecified && // via pointer variables
+                ASR::down_cast<ASR::Variable_t>(sym.second)->m_intent != ASRUtils::intent_return_var) ||
+                ASRUtils::is_array(ASR::down_cast<ASR::Variable_t>(sym.second)->m_type) ||
+                ASR::is_a<ASR::String_t>(
+                    *ASRUtils::extract_type(ASR::down_cast<ASR::Variable_t>(sym.second)->m_type)) ) { // TODO: Remove this check as well, use pointers for arrays
                 return false;
             }
 
@@ -154,7 +172,7 @@ class InlineFunctionCalls: public ASR::BaseExprReplacer<InlineFunctionCalls> {
             ASR::ttype_t* local_ttype_copy = type_duplicator.duplicate_ttype(variable->m_type);
             ASR::symbol_t* local_sym = ASR::down_cast<ASR::symbol_t>(
                 ASRUtils::make_Variable_t_util(al, loc, current_scope, s2c(al, local_sym_unique_name),
-                nullptr, 0, variable->m_intent, nullptr, nullptr, variable->m_storage,
+                nullptr, 0, ASRUtils::intent_local, nullptr, nullptr, variable->m_storage,
                 local_ttype_copy, variable->m_type_declaration, variable->m_abi, variable->m_access,
                 variable->m_presence, variable->m_value_attr, variable->m_target_attr, variable->m_contiguous_attr));
             current_scope->add_symbol(local_sym_unique_name, local_sym);
@@ -169,6 +187,11 @@ class InlineFunctionCalls: public ASR::BaseExprReplacer<InlineFunctionCalls> {
 
             function2currentscope[sym.second] = local_sym;
             currentscope2function[local_sym] = sym.second;
+        }
+
+        FixSymbols fix_symbols(function2currentscope);
+        for( auto sym: currentscope2function ) {
+            fix_symbols.visit_symbol(*sym.first);
         }
 
         for( size_t i = 0; i < function->n_args; i++ ) {
@@ -206,7 +229,6 @@ class InlineFunctionCalls: public ASR::BaseExprReplacer<InlineFunctionCalls> {
         // Step 3 - Replace symbols in the duplicated body
         // with their local copies
         ASRUtils::ExprStmtDuplicator stmt_duplicator(al);
-        FixSymbols fix_symbols(function2currentscope);
         for( size_t i = 0; i < function->n_body; i++ ) {
             ASR::stmt_t* stmt_copy = stmt_duplicator.duplicate_stmt(function->m_body[i]);
             fix_symbols.visit_stmt(*stmt_copy);
@@ -214,6 +236,9 @@ class InlineFunctionCalls: public ASR::BaseExprReplacer<InlineFunctionCalls> {
         }
 
         *current_expr = ASRUtils::EXPR(ASR::make_Var_t(al, loc, return_variable));
+    }
+
+    void replace_OverloadedCompare(ASR::OverloadedCompare_t* /*x*/) {
     }
 
 };
@@ -304,6 +329,26 @@ class InlineFunctionCallsVisitor: public ASR::CallReplacerOnExpressionsVisitor<I
         }
         transform_stmts(xx.m_body, xx.n_body);
         current_scope = current_scope_copy;
+    }
+
+    void visit_OverloadedCompare(const ASR::OverloadedCompare_t& /*x*/) {
+    }
+
+    void visit_WhileLoop(const ASR::WhileLoop_t &x) {
+        ASR::WhileLoop_t& xx = const_cast<ASR::WhileLoop_t&>(x);
+        transform_stmts(xx.m_body, xx.n_body);
+        transform_stmts(xx.m_orelse, xx.n_orelse);
+    }
+
+    void visit_DoLoop(const ASR::DoLoop_t &x) {
+        ASR::DoLoop_t& xx = const_cast<ASR::DoLoop_t&>(x);
+        transform_stmts(xx.m_body, xx.n_body);
+        transform_stmts(xx.m_orelse, xx.n_orelse);
+    }
+
+    void visit_DoConcurrentLoop(const ASR::DoConcurrentLoop_t &x) {
+        ASR::DoConcurrentLoop_t& xx = const_cast<ASR::DoConcurrentLoop_t&>(x);
+        transform_stmts(xx.m_body, xx.n_body);
     }
 
 };
