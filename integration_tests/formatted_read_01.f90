bb015f3ef8fd46a486fe101c336159a122903106
Hitesh Ghanchi <hiteshghanchi87@gmail.com>
2025-05-07 20:51:29 +0530

    Changes asked final

--- integration_tests/formatted_read_01.f90
+++ integration_tests/formatted_read_01.f90
@@ -2,6 +2,7 @@ program formatted_read_01
   character(len=10) :: str1
   character(len=15) :: str2
   character(len=5) :: str3
+  character(len=10) :: str4
 
   open(unit=10, file="formatted_read_input_01.txt", status="old")
 
@@ -16,6 +17,10 @@ program formatted_read_01
   read(unit=10, fmt='(a10)') str3
   print *, str3
   print *, len(str3)
+
+  read(unit=10, fmt='(a5)') str1
+  print *, str1
+  print *, len(str1)
 
   close(10)
 end program formatted_read_01
