573d61e78e9aced998ffddf6cc64e157ff6ea23f
akramhany <akramhany65@gmail.com>
2025-05-13 18:20:16 +0300

    tests: add a test for using allocate with a class

---
+++ integration_tests/class_allocate_01.f90
@@ -0,0 +1,43 @@
+module shapes_module
+    implicit none
+
+    type, abstract :: Shape
+    contains
+        procedure(area_interface), deferred :: area
+    end type Shape
+
+    abstract interface
+        function area_interface(self) result(res)
+            import :: Shape
+            class(Shape), intent(in) :: self
+            real :: res
+        end function area_interface
+    end interface
+
+    type, extends(Shape) :: Circle
+        real :: radius
+    contains
+        procedure :: area => circle_area
+    end type Circle
+
+contains
+
+    function circle_area(self) result(res)
+        class(Circle), intent(in) :: self
+        real :: res
+        res = 3.14159 * self%radius**2
+    end function circle_area
+
+end module shapes_module
+
+
+program main
+    use shapes_module
+    implicit none
+
+    type(Shape), allocatable :: s
+    real :: a
+
+    allocate(Circle :: s)
+
+end program main
