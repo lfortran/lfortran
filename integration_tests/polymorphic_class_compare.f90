b64c1fa90a06193688aee2507ce59b853bf93e12
Aditya Trivedi <adit4443ya@gmail.com>
2025-05-14 21:47:47 +0530

    Test: Add Program and Add GFortran Check

--- integration_tests/polymorphic_class_compare.f90
+++ integration_tests/polymorphic_class_compare.f90
@@ -2,6 +2,7 @@ module gFTL2_Integer32Complex32Map
    implicit none
 
    type :: map_SetIterator
+      integer::i
    end type map_SetIterator
 
    interface operator(==)
@@ -16,12 +17,27 @@ module gFTL2_Integer32Complex32Map
    logical function map_s_iter_equal(a, b) result(eq)
       type(map_SetIterator), intent(in) :: a
       type(map_SetIterator), intent(in) :: b
-      eq = .true.
+      eq = a%i == b%i
    end function map_s_iter_equal
 
    logical function map_s_iter_not_equal(a, b) result(ne)
       implicit none
       class(map_SetIterator), intent(in) :: a, b
       ne = a == b
+      ne = .not. ne
    end function map_s_iter_not_equal
 end module gFTL2_Integer32Complex32Map
+
+program polymorphic_class_compare
+  use gFTL2_Integer32Complex32Map
+  implicit none
+
+  type(map_SetIterator) :: iter1, iter2
+
+   ! Initialize iterators
+   iter1%i = 1
+   iter2%i = 1
+
+  if (iter1 /= iter2) error stop "Error: iter1 and iter2 should be equal"
+
+end program polymorphic_class_compare
