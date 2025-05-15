0bcb316d4e62d173a7d7443cfc2a12cb8d467555
Gagandeep Singh <gdp.1807@gmail.com>
2025-05-15 11:59:55 +0530

    DEV: Fix logical pointer in test expression of if statement

--- src/lfortran/semantics/ast_body_visitor.cpp
+++ src/lfortran/semantics/ast_body_visitor.cpp
@@ -4335,7 +4335,7 @@ public:
         visit_expr(*x.m_test);
         ASR::expr_t *test = ASRUtils::EXPR(tmp);
         ASR::ttype_t *test_type = ASRUtils::expr_type(test);
-        if (!ASR::is_a<ASR::Logical_t>(*test_type)) {
+        if (!ASR::is_a<ASR::Logical_t>(*ASRUtils::type_get_past_allocatable_pointer(test_type))) {
             diag.add(diag::Diagnostic("Expected logical expression in if statement, but recieved " +
                 ASRUtils::type_to_str_fortran(test_type) + " instead",
                 diag::Level::Error, diag::Stage::Semantic, {
