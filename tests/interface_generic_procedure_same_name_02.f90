cd2e0a643dc563f31517021c543265921dbc4b3b
Saurabh Kumar <developer.saurabh@outlook.com>
2025-05-17 12:35:34 +0530

    tests: add test case
    Signed-off-by: Saurabh Kumar <developer.saurabh@outlook.com>

--- tests/interface_generic_procedure_same_name_02.f90
+++ tests/interface_generic_procedure_same_name_02.f90
@@ -1,6 +1,10 @@
 module interface_generic_procedure_same_name_02
    implicit none
 
+
+   ! Test case 1:
+   ! Here, the interface, a module subroutine inside it and it's
+   ! associated implementation function have the same name.
    interface first_interface
       module subroutine first_interface()
       end subroutine first_interface
@@ -8,12 +12,23 @@ module interface_generic_procedure_same_name_02
       module procedure first_interface_2
    end interface first_interface
 
+   ! Test case 2:
+   ! Here, the interface and a module function inside it
+   ! have the same name.
    interface second_interface
-      integer module function second_interface()
+      integer module function second_interface(x)
+         integer :: x
       end function second_interface
 
       module procedure second_interface_2
    end interface second_interface
+
+   ! Test case 3:
+   ! Here, the interface, a module procedure inside it and it's
+   ! associated implementation function have the same name.
+   interface third_interface
+      module procedure third_interface
+   end interface third_interface
 
 contains
 
@@ -28,4 +43,9 @@ contains
    integer module function second_interface_2(i)
       integer :: i
    end function second_interface_2
+
+   subroutine third_interface(i)
+      integer :: i
+      i = 1
+   end subroutine third_interface
 end module interface_generic_procedure_same_name_02
