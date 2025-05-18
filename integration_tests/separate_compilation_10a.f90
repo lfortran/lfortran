75ff8ceaa3fe02f05bb5618daaffc31b48c6affc
Pranavchiku <goswami.4@iitj.ac.in>
2025-05-18 20:40:15 +0530

    fix: integration test

--- integration_tests/separate_compilation_10a.f90
+++ integration_tests/separate_compilation_10a.f90
@@ -2,7 +2,9 @@ module history_mod_separate_compilation_10a
 contains
 subroutine rangehist(xhist)
         real, intent(inout) :: xhist(:, :)
-        xhist = reshape([xhist(:, 5), xhist(:, 5)], shape(xhist))
+        real :: x(25)
+        x = 12.912
+        xhist = reshape(x, shape(xhist))
         xhist = 99.124
 end subroutine
 end module
