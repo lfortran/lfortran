3c65c0ebf30b84f8f1ab0e07619996becb97cfcd
HarshitaKalani <kalani.1@iitj.ac.in>
2025-05-20 21:14:23 +0530

    test: add and register test

---
+++ integration_tests/format_30.f90
@@ -0,0 +1,7 @@
+program format_30
+print "(2E13.3)", 1.0, 2.0
+print "(2ES13.3)", epsilon(1e0), epsilon(3e0)
+print "(3F10.4)", 1.2345, 2.3456, 3.4567
+print "(2EN12.3)", 123.0, 456.0
+print "(2ES13.3)", epsilon(1e0), epsilon(3e0)
+end program format_30
