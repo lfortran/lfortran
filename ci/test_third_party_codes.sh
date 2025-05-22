81b07b66dbff24d9ef45f22d1345b15709c0c3ce
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2025-05-22 16:03:22 +0530

    CI: test numeric-method-fortran with --std=f23 (#7385)
    * CI: test numeric-method-fortran with --std=f23
    
    Towards: https://github.com/lfortran/lfortran/issues/6020
    
    * CI: test numeric methods fortran with separate compilation and std=f23

--- ci/test_third_party_codes.sh
+++ ci/test_third_party_codes.sh
@@ -171,6 +171,27 @@ time_section "🧪 Testing Numerical Methods Fortran" '
   run_test plot_pendulum.exe
   run_test plot_transes_iso.exe
 
+  git clean -dfx
+  print_subsection "Building Numerical Methods Fortran with f23 standard"
+
+  FC="$FC --std=f23" make
+  run_test test_fix_point.exe
+  run_test test_integrate_one.exe
+  run_test test_linear.exe
+  run_test test_newton.exe
+  run_test test_ode.exe
+  run_test test_probability_distribution.exe
+  run_test test_sde.exe
+
+  run_test plot_bogdanov_takens.exe
+  run_test plot_bruinsma.exe
+  run_test plot_fun1.exe
+  run_test plot_lorenz.exe
+  run_test plot_lotka_volterra1.exe
+  run_test plot_lotka_volterra2.exe
+  run_test plot_pendulum.exe
+  run_test plot_transes_iso.exe
+
 
   git clean -dfx
   print_subsection "Building Numerical Methods Fortran with separate compilation"
@@ -193,10 +214,32 @@ time_section "🧪 Testing Numerical Methods Fortran" '
   run_test plot_pendulum.exe
   run_test plot_transes_iso.exe
 
+  git clean -dfx
+  print_subsection "Building Numerical Methods Fortran with separate compilation and f23 standard"
+
+  FC="$FC --generate-object-code --std=f23" make
+  run_test test_fix_point.exe
+  run_test test_integrate_one.exe
+  run_test test_linear.exe
+  run_test test_newton.exe
+  run_test test_ode.exe
+  run_test test_probability_distribution.exe
+  run_test test_sde.exe
+
+  run_test plot_bogdanov_takens.exe
+  run_test plot_bruinsma.exe
+  run_test plot_fun1.exe
+  run_test plot_lorenz.exe
+  run_test plot_lotka_volterra1.exe
+  run_test plot_lotka_volterra2.exe
+  run_test plot_pendulum.exe
+  run_test plot_transes_iso.exe
+
 
   print_success "Done with Numerical Methods Fortran"
 
   cd ..
+  rm -rf numerical-methods-fortran
 '
 
 ######################
