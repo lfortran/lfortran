9031a07a7ec5451ac5bba960d0e080d62244df29
Sri Ganesh Thota <sriganeshthota12345@gmail.com>
2025-05-24 00:44:41 +0530

    Moved test from tests/ to integration_tests/

--- integration_tests/allocatble_c_ptr.f90
+++ integration_tests/allocatble_c_ptr.f90
@@ -1,7 +1,7 @@
-program main
+program allocatble_c_ptr
     use iso_c_binding, only: c_ptr
     implicit none
     type(c_ptr), allocatable :: c_requests(:)
     allocate(c_requests(4))
     deallocate(c_requests)
-end program main
+end program allocatble_c_ptr
