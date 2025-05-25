c5b6782c2cbefd1300c209a7f8cd64e2ab3b1a30
swamishiju <swaminathshiju24@gmail.com>
2025-05-25 10:30:56 +0530

    Testing using allocate

--- integration_tests/lp_list_test_01.f90
+++ integration_tests/lp_list_test_01.f90
@@ -1,15 +1,17 @@
 program lp_list_test_01
 
     ! placeholder integer to add entry to ptr_type
-    integer :: _placeholder = 15
+    integer :: _placeholder
 
     type(_lfortran_list_integer) :: test_list 
     call _lfortran_list_append(test_list, 1)
     call _lfortran_list_append(test_list, 10)
-    if (_lfortran_list_len(test_list) /= 2) error stop
+    _placeholder = _lfortran_list_len(test_list)
+    if (_placeholder /= 2) error stop
     
     call _lfortran_list_append(test_list, -50)
-    if (_lfortran_list_len(test_list) /= 3) error stop
+    _placeholder = _lfortran_list_len(test_list)
+    if (_placeholder /= 3) error stop
     
     ! Add other intrinsics later
 end program
