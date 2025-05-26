198480c9142fd5d51a349b643fcb567ca253d890
swamishiju <swaminathshiju24@gmail.com>
2025-05-26 19:30:04 +0530

    TEST: Added test for list constants

--- integration_tests/lp_list_test_01.f90
+++ integration_tests/lp_list_test_01.f90
@@ -6,18 +6,24 @@ program lp_list_test_01
     call _lfortran_list_append(test_list, 10)
     x = _lfortran_list_len(test_list)
     if (x /= 2) error stop
+
+    test_list = _lfortran_list_constant(1, 2, 3, 4)
+    if (_lfortran_list_len(test_list) /= 4) error stop
     
     call _lfortran_list_append(test_list, -50)
-    if (_lfortran_list_len(test_list) /= 3) error stop
+    if (_lfortran_list_len(test_list) /= 5) error stop
     !
-    type(_lfortran_list_real) :: test_list_r
-    if (_lfortran_list_len(test_list_r) /= 0) error stop
+    type(_lfortran_list_real) :: test_list_r  = _lfortran_list_constant(1.0, 2.0, 3.0, 4.0)
+    if (_lfortran_list_len(test_list_r) /= 4) error stop
     call _lfortran_list_append(test_list_r, 1.11)
     call _lfortran_list_append(test_list_r, -10.12)
     call _lfortran_list_append(test_list_r, 12.0)
     call _lfortran_list_append(test_list_r, -111.0)
     call _lfortran_list_append(test_list_r, -110.12)
 
-    if (_lfortran_list_len(test_list_r) /= 5) error stop
+    if (_lfortran_list_len(test_list_r) /= 9) error stop
+
+    type(_lfortran_list_real) :: test_list_r1  
+    if (_lfortran_list_len(test_list_r1) /= 0) error stop
     ! Add other intrinsics later
 end program
