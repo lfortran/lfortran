d295a2e237eee93255f495c1840a76769e91dcce
swamishiju <swaminathshiju24@gmail.com>
2025-05-26 14:48:43 +0530

    Removed real type still needs fixes

--- integration_tests/lp_list_test_01.f90
+++ integration_tests/lp_list_test_01.f90
@@ -10,15 +10,5 @@ program lp_list_test_01
     call _lfortran_list_append(test_list, -50)
     if (_lfortran_list_len(test_list) /= 3) error stop
 
-
-    type(_lfortran_list_integer) :: test_list_r
-    if (_lfortran_list_len(test_list_r) /= 0) error stop
-    call _lfortran_list_append(test_list_r, 1.11)
-    call _lfortran_list_append(test_list_r, -10.12)
-    call _lfortran_list_append(test_list_r, 12)
-    call _lfortran_list_append(test_list_r, -111)
-    call _lfortran_list_append(test_list_r, -110.12)
-    
-    if (_lfortran_list_len(test_list_r) /= 5) error stop
     ! Add other intrinsics later
 end program
