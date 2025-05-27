c2854ea238e7b75c1075d681e66f64466edda146
AnshMehta1 <ansh.mehta522007@gmail.com>
2025-05-27 18:02:46 +0530

    Handle LLVM Type of NullPointer for LLVM Versions > 16 for Polymorphic variable

--- src/libasr/codegen/asr_to_llvm.cpp
+++ src/libasr/codegen/asr_to_llvm.cpp
@@ -3641,12 +3641,22 @@ public:
                 llvm_utils->create_gep2(string_descriptor, ptr, 2));\
             } else if (ASR::is_a<ASR::ClassType_t>(*ASRUtils::type_get_past_allocatable_pointer(v->m_type))) { \
                 ASR::symbol_t* struct_sym = ASRUtils::symbol_get_past_external(ASR::down_cast<ASR::ClassType_t>(ASRUtils::extract_type(v->m_type))->m_class_type); \
+                ASR::Struct_t* st = ASR::down_cast<ASR::Struct_t>(struct_sym); \
+                std::string struct_name = st->m_name; \
+                ASR::ttype_t* wrapped_struct_type = ASRUtils::TYPE( \
+                            ASRUtils::make_StructType_t_util(al, v->m_type->base.loc, \
+                                struct_sym)); \
+                llvm::Type* wrapper_struct_llvm_type = llvm_utils->get_type_from_ttype_t_util(wrapped_struct_type, module.get()); \
                 llvm::Value* struct_hash = llvm::ConstantInt::get(llvm_utils->getIntType(8), \
                                         llvm::APInt(64, get_class_hash(struct_sym))); \
                 llvm::Value* hash_ptr = llvm_utils->create_gep2(v->m_type, ptr, 0); \
                 builder->CreateStore(struct_hash, hash_ptr); \
                 llvm::Value* struct_ptr = llvm_utils->create_gep2(v->m_type, ptr, 1); \
-                builder->CreateStore(llvm::ConstantPointerNull::getNullValue(llvm_utils->CreateLoad(struct_ptr)->getType()), struct_ptr); \
+                if (struct_name == "~abstract_type") { \
+                    builder->CreateStore(llvm::ConstantPointerNull::getNullValue(llvm::Type::getVoidTy(context)->getPointerTo()), struct_ptr); \
+                } else { \
+                    builder->CreateStore(llvm::ConstantPointerNull::getNullValue(wrapper_struct_llvm_type->getPointerTo()), struct_ptr); \
+                } \
             } else { \
                 builder->CreateStore(null_value, ptr); \
             }\
