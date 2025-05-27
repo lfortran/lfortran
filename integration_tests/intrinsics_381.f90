67a1b10980b5cc6e3ea20cd30ac03580681bf46a
Pranavchiku <goswami.4@iitj.ac.in>
2025-05-27 17:06:07 +0530

    test: add and register

---
+++ integration_tests/intrinsics_381.f90
@@ -0,0 +1,9 @@
+PROGRAM intrinsics_381
+CHARACTER(len=255) :: test_env_var
+integer :: len
+CALL get_environment_variable("LFORTRAN_TEST_ENV_VAR", test_env_var, len)
+WRITE (*,*) TRIM(test_env_var)
+if (trim(test_env_var) /= "STATUS OK!") error stop
+print *, len
+if (len /= 9) error stop
+END PROGRAM
