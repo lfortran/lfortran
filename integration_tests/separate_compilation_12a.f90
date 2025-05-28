f7cb21eceeba803f0a456ca6ff2dbbc156b98dff
Pranavchiku <goswami.4@iitj.ac.in>
2025-05-28 12:50:36 +0530

    test: add and register

--- integration_tests/separate_compilation_12a.f90
+++ integration_tests/separate_compilation_12a.f90
@@ -1,4 +1,4 @@
-module separate_compilation_11a_module
+module separate_compilation_12a_module
 contains
 subroutine parent_of_nested_subroutine(a, b)
     integer, intent(inout) :: a, b
@@ -8,9 +8,9 @@ subroutine parent_of_nested_subroutine(a, b)
     contains
     subroutine nested_subroutine(c, d)
         integer, intent(inout) :: c, d
-        c = c + 1
-        d = d + 2
+        a = a + c
+        b = b + d
     end subroutine nested_subroutine
 end subroutine parent_of_nested_subroutine
 
-end module separate_compilation_11a_module
+end module separate_compilation_12a_module
