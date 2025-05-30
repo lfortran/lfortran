2c8ae094fd4cd2c21340ebd2bcb9cd999bf2d2b3
HarshilShah1804 <hkshah1978@gmail.com>
2025-05-30 12:20:59 +0530

    test: update test

--- integration_tests/allocate_01.f90
+++ integration_tests/allocate_01.f90
@@ -12,6 +12,10 @@ n = 10
 allocate(e, mold=d)
 if (size(e) /= 5) error stop
 
+allocate(a, mold=e)
+if (size(a) /= 5) error stop
+deallocate(a)
+
 allocate(a(5:n + 5))
 allocate(b(n:2*n, n:3*n), stat=ierr)
 if( size(a) /= n + 1 ) error stop
