9f008c55fc9f04b49616877cd08b3732281e0108
jinangshah21 <jinang2110@gmail.com>
2025-05-30 23:25:58 +0530

    test: add integration test

--- integration_tests/class_21.f90
+++ integration_tests/class_21.f90
@@ -4,8 +4,11 @@ program class_21
     end type
     integer :: stat
     class(val_type), allocatable :: val
+    logical :: temp = .false.
+    type(val_type), parameter :: val_par = val_type()
     allocate(val)
     stat = merge(val%origin, stat, .true.)
-
     if (stat /= 3) error stop
+    stat = merge(val_par%origin, stat - 1, temp)
+    if (stat /= 2) error stop
 end program
