9c2e9c21b283a95527d15d4f3f089ca1db6a3977
swamishiju <swaminathshiju24@gmail.com>
2025-05-31 12:46:44 +0530

    DEV: Implemented lfortran_set_item for lists

--- src/lfortran/semantics/ast_common_visitor.h
+++ src/lfortran/semantics/ast_common_visitor.h
@@ -1287,6 +1287,7 @@ public:
 
 
         // LFortran-specific intrinsics
+        {"_lfortran_set_item", IntrinsicSignature({"iterable", "index", "element"}, 3, 3)},
         {"_lfortran_list_append", IntrinsicSignature({"list", "element"}, 2, 2)},
         {"_lfortran_set_add", IntrinsicSignature({"set", "element"}, 2, 2)},
     };
