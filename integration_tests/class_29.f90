c84c510c9d560b79687287e588dc9c7131b9dcab
jinangshah21 <jinang2110@gmail.com>
2025-05-31 12:56:01 +0530

    fix: get type pointer for `allocated` also

--- integration_tests/class_29.f90
+++ integration_tests/class_29.f90
@@ -4,10 +4,13 @@ program class_29
     end type
     class(toml_table), pointer :: temp
     class(toml_table), pointer :: temp2
+    class(toml_table), allocatable :: temp3
     type(toml_table), target :: temp_table
     temp_table%x = 42
     temp => temp_table
-    print *, associated(temp)
-    print *, associated(temp2)
     if (associated(temp2) .or. .not. associated(temp)) error stop
+    
+    if (allocated(temp3)) error stop
+    allocate(temp3)
+    if (.not. allocated(temp3)) error stop
 end program
