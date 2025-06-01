f7527855ba1caa998119c86c201f523eccadceba
jinangshah21 <jinang2110@gmail.com>
2025-06-01 16:55:16 +0530

    tests: add test and update test references

--- integration_tests/string_02.f90
+++ integration_tests/string_02.f90
@@ -4,16 +4,13 @@ implicit none
 character(len = 15) :: surname, firstname
 character(len = 6) :: title
 character(len = 25)::greetings
-integer, parameter :: SCK = selected_char_kind("ascii")
-character(kind=SCK) :: one_len
 
 title = 'Mr. '
 firstname = 'Rowan '
 surname = 'Atkinson'
 greetings = 'A big hello from Mr. Bean'
-one_len = "hello"
+
 print *, 'Here is ', title, firstname, surname
 print *, greetings
 
-if (one_len /= "h") error stop
 end program
