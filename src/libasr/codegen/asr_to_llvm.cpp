32aa8c79226f379805d00d8f5cc74a3a2fd7ae3a
swamishiju <swaminathshiju24@gmail.com>
2025-06-05 11:01:48 +0530

    FIX: Fixed list type in len

--- src/libasr/codegen/asr_to_llvm.cpp
+++ src/libasr/codegen/asr_to_llvm.cpp
@@ -1794,7 +1794,8 @@ public:
             ptr_loads = ptr_loads_copy;
             llvm::Value* plist = tmp;
 
-            std::string type_code = ASRUtils::get_type_code(x.m_type);
+            std::string type_code = ASRUtils::get_type_code(
+                ASRUtils::get_contained_type(ASRUtils::expr_type(x.m_arg)));
             llvm::Type* list_type = list_api->get_list_type(nullptr, type_code, 0);
             tmp = list_api->len2(list_type, plist);
         }
