6d321c648173808cdecd087dcb74f49400bde694
parth121101 <parthmistry1211@gmail.com>
2025-06-06 23:39:16 +0530

    fix: addition of undefined checks

--- src/libasr/pass/intrinsic_functions.h
+++ src/libasr/pass/intrinsic_functions.h
@@ -4995,15 +4995,23 @@ namespace Ichar {
         SymbolTable *scope, Vec<ASR::ttype_t*>& arg_types, ASR::ttype_t *return_type,
         Vec<ASR::call_arg_t>& new_args, int64_t /*overload_id*/) {
         declare_basic_variables("_lcompilers_ichar_" + type_to_str_python(arg_types[0]));
-        fill_func_arg("str", ASRUtils::TYPE(ASR::make_String_t(al, loc, 1, nullptr, ASR::string_length_kindType::AssumedLength, ASR::string_physical_typeType::PointerString)));
+        fill_func_arg("str",arg_types[0]);
         auto result = declare("result", return_type, ReturnVar);
         auto itr = declare("i", int32, Local);
 
-        ASR::expr_t* len_expr = ASR::down_cast<ASR::String_t>(ASR::down_cast<ASR::Variable_t>(ASR::down_cast<ASR::Var_t>(args[0])->m_v)->m_type)->m_len;
-        if (ASR::is_a<ASR::IntegerConstant_t>(*len_expr) ) {
-            int64_t len = ASR::down_cast<ASR::IntegerConstant_t>(len_expr)->m_n;
-            if (len != 1) {
-                throw LCompilersException("Argument to Ichar must have length one");
+        if (ASR::is_a<ASR::Var_t>(*args[0])) {
+            ASR::symbol_t* sym_t = ASR::down_cast<ASR::Var_t>(args[0])->m_v;
+            if (ASR::is_a<ASR::Variable_t>(*sym_t)) {
+                ASR::ttype_t* type_t = ASR::down_cast<ASR::Variable_t>(sym_t)->m_type;
+                if (ASR::is_a<ASR::String_t>(*type_t)) {
+                    ASR::expr_t* len_expr = ASR::down_cast<ASR::String_t>(type_t)->m_len;
+                    if (len_expr && ASR::is_a<ASR::IntegerConstant_t>(*len_expr) ) {
+                        int64_t len = ASR::down_cast<ASR::IntegerConstant_t>(len_expr)->m_n;
+                        if (len != 1) {
+                            throw LCompilersException("Argument to Ichar must have length one");
+                        }
+                    }
+                }
             }
         }
 
