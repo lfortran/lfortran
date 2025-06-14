bfbfbc7253ad7833ecdbb9d1bcd934651e4df298
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2025-06-14 20:14:48 +0530

    correct modulen and program name

--- integration_tests/arrays_92.f90
+++ integration_tests/arrays_92.f90
@@ -2,12 +2,12 @@ module arrays_92_mod
     implicit none
     integer, parameter :: ints(0:9) = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
     integer :: e = 3
-end module stdlib_ansi
+end module arrays_92_mod
 
 program arrays_92
-    use arrays_92, only : e, ints
+    use arrays_92_mod, only : e, ints
     integer :: x
     x = ints(e)
     print *, "x: ", x
     if (x /= 14) error stop
-end program test_colors
+end program arrays_92
