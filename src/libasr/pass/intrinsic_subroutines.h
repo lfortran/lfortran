640deeb4c65a85b28df3dcf0c0fa041c9ed097ec
Pranavchiku <goswami.4@iitj.ac.in>
2025-06-15 15:38:02 +0530

    fix: using correct string type in execute_command_line intrinsic

--- src/libasr/pass/intrinsic_subroutines.h
+++ src/libasr/pass/intrinsic_subroutines.h
@@ -819,12 +819,13 @@ namespace ExecuteCommandLine {
         std::string c_func_name = "_lfortran_exec_command";
         std::string new_name = "_lcompilers_execute_command_line_";
         ASR::symbol_t* s = scope->get_symbol(new_name);
+        ASR::ttype_t* str_type = ASRUtils::TYPE(ASR::make_String_t(al, loc, 1, nullptr, ASR::string_length_kindType::AssumedLength, ASR::string_physical_typeType::PointerString));
         if (s) {
             ASRBuilder b(al, loc);
             return b.SubroutineCall(s, new_args);
         } else {
             declare_basic_variables(new_name);
-            fill_func_arg_sub("command", arg_types[0], InOut);
+            fill_func_arg_sub("command", str_type, InOut);
             if (arg_types.size() == 2) {
                 fill_func_arg_sub("wait", arg_types[1], InOut);
             } else if (arg_types.size() == 3) {
@@ -837,12 +838,12 @@ namespace ExecuteCommandLine {
 
             SymbolTable *fn_symtab_1 = al.make_new<SymbolTable>(fn_symtab);
             Vec<ASR::expr_t*> args_1; args_1.reserve(al, 1);
-            ASR::expr_t *arg = b.Variable(fn_symtab_1, "n", arg_types[0],
+            ASR::expr_t *arg = b.Variable(fn_symtab_1, "n", str_type,
                 ASR::intentType::InOut, ASR::abiType::BindC, true);
             args_1.push_back(al, arg);
 
             ASR::expr_t *return_var_1 = b.Variable(fn_symtab_1, c_func_name,
-            ASRUtils::type_get_past_array(ASRUtils::type_get_past_allocatable(arg_types[0])),
+            str_type,
             ASRUtils::intent_return_var, ASR::abiType::BindC, false);
 
             SetChar dep_1; dep_1.reserve(al, 1);
@@ -854,7 +855,7 @@ namespace ExecuteCommandLine {
 
             Vec<ASR::expr_t*> call_args; call_args.reserve(al, 1);
             call_args.push_back(al, args[0]);
-            body.push_back(al, b.Assignment(args[0], b.Call(s, call_args, arg_types[0])));
+            body.push_back(al, b.Assignment(args[0], b.Call(s, call_args, str_type)));
             ASR::symbol_t *new_symbol = make_ASR_Function_t(fn_name, fn_symtab, dep, args,
                 body, nullptr, ASR::abiType::Intrinsic, ASR::deftypeType::Implementation, nullptr);
             scope->add_symbol(fn_name, new_symbol);
