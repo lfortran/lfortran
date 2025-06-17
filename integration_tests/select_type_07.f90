b5cfe06d98d342f322cf8bc780ad86741c8cf4e6
jinangshah21 <jinang2110@gmail.com>
2025-06-18 00:22:26 +0530

    test: add integration tests

---
+++ integration_tests/select_type_07.f90
@@ -0,0 +1,54 @@
+program select_type_07
+    implicit none
+
+    class(*), allocatable :: obj
+
+    type :: structType
+        integer :: code
+        character(len=20) :: message
+    end type structType
+
+    type(structType) :: err
+
+    allocate(obj, source=42)
+    call print_obj(obj)
+
+    deallocate(obj)
+    allocate(obj, source=3.14)
+    call print_obj(obj)
+
+    err%code = 500
+    err%message = "Server Error"
+    deallocate(obj)
+    allocate(obj, source=err)
+    call print_obj(obj)
+
+contains
+
+    subroutine print_obj(x)
+        class(*), intent(in) :: x
+        integer :: val1
+        real :: val2
+        ! character(len=:), allocatable :: val3  ! TODO
+        type(structType) :: val4
+
+        !! TODO: for integer and real type is not matching correctly
+        !! TODO: For structType message is not deep copied
+        select type(x)
+        type is (integer)
+            val1 = x
+            ! print *, val1
+            ! if (val1 /= 42) error stop
+        type is (real)
+            val2 = x
+            ! print *, val2
+            ! if (val2 /= 3.14) error stop
+        type is (structType)
+            val4 = x
+            ! print *, val4%code
+            ! print *, val4%message
+            ! if (val4%code /= 500) error stop
+        end select
+    end subroutine print_obj
+
+end program select_type_07
