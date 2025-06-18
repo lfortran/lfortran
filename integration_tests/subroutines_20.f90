dee44849812700e82c2222a95b6766b023380bb1
Gauravsingh Sisodia <xaerru@gmail.com>
2025-06-18 09:31:13 +0000

    fix: don't create temporary variable when intent is inout or out

---
+++ integration_tests/subroutines_20.f90
@@ -0,0 +1,19 @@
+program subroutines_20
+     type :: regex_token
+        character(len=:), allocatable :: ccl
+    end type regex_token
+
+    type :: regex_pattern
+        type(regex_token) :: pattern
+    end type regex_pattern
+
+    type(regex_pattern) :: my_pattern
+    call temp(my_pattern%pattern)
+
+    if (my_pattern%pattern%ccl /= "abc") error stop
+contains
+    subroutine temp(pattern)
+        type(regex_token), intent(out) :: pattern
+        pattern%ccl = 'abc'
+    end subroutine temp
+end program
