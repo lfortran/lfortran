0d598851302950dd212baeeb6b3d6c1c9a0d8429
Pranavchiku <goswami.4@iitj.ac.in>
2025-06-20 14:13:16 +0530

    test: add and register

--- integration_tests/iso_c_binding_01.f90
+++ integration_tests/iso_c_binding_01.f90
@@ -4,4 +4,6 @@ program iso_c_binding_01
       if (a /= 'a') error stop
       if (len(a) /= 2) error stop
       if (kind(a) /= c_char) error stop
+      print *, "c_intptr_t:", c_intptr_t
+      if (c_intptr_t /= 8) error stop
 end program iso_c_binding_01
