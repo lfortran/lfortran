438019bafc72ee71bb7f47bc1e6e46d3a98a67c1
Sri Ganesh Thota <sriganeshthota12345@gmail.com>
2025-06-20 13:14:19 +0530

    Changed this test to be compile time

--- integration_tests/call_subroutine_without_type.f90
+++ integration_tests/call_subroutine_without_type.f90
@@ -9,13 +9,12 @@ module module_call_subroutine_without_type
 contains
     subroutine get_i(self)
         class(myType), intent(inout) :: self
-        call get_r(self)  ! pass self explicitly
-        
+        call get_r()
+
         contains
-            subroutine get_r(s)
-                class(myType), intent(inout) :: s
-                print *, "r = ", s % r
-                if (s%r /= 1.0) error stop
+            subroutine get_r()
+                print *, "r = ", self % r
+                if (self%r /= 1.0) error stop
             end subroutine get_r
     end subroutine get_i
 
@@ -23,8 +22,4 @@ end module module_call_subroutine_without_type
 
 program call_subroutine_without_type
     use module_call_subroutine_without_type
-    class(myType), allocatable :: obj
-    allocate(obj)
-    obj%r = 1.0
-    call obj%get_i()
 end program call_subroutine_without_type
