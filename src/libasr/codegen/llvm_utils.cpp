76f286cf56c84de38296958c32be28487fbb6e6e
Saurabh Kumar <developer.saurabh@outlook.com>
2025-06-22 16:26:21 +0530

    fix: more working tests
    Signed-off-by: Saurabh Kumar <developer.saurabh@outlook.com>

--- src/libasr/codegen/llvm_utils.cpp
+++ src/libasr/codegen/llvm_utils.cpp
@@ -684,8 +684,7 @@ namespace LCompilers {
                         type = getStructType(
                             ASR::down_cast<ASR::Struct_t>(type_declaration), module, true);
                     } else {
-                        type = getClassType(ASR::down_cast<ASR::Struct_t>(type_declaration), true)
-                                   ->getPointerTo();
+                        type = getClassType(ASR::down_cast<ASR::Struct_t>(type_declaration), true);
                     }
                 } else {
                     if (ASR::down_cast<ASR::StructType_t>(asr_type)->m_is_cstruct) {
@@ -698,8 +697,7 @@ namespace LCompilers {
                         type
                             = getClassType(ASR::down_cast<ASR::Struct_t>(
                                                ASRUtils::get_struct_sym_from_struct_expr(arg_expr)),
-                                           true)
-                                  ->getPointerTo();
+                                           true);
                     }
                 }
                 break;
