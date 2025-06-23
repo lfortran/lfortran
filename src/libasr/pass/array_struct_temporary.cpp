ff06a4435f024fd2d0ebdf7cad601e2e7884634a
HarshitaKalani <kalani.1@iitj.ac.in>
2025-06-24 00:28:04 +0530

    enh: get more tests working

--- src/libasr/pass/array_struct_temporary.cpp
+++ src/libasr/pass/array_struct_temporary.cpp
@@ -112,7 +112,7 @@ ASR::expr_t* create_temporary_variable_for_scalar(Allocator& al,
     std::string var_name = scope->get_unique_name("__libasr_created_" + name_hint);
     ASR::symbol_t* temporary_variable = ASR::down_cast<ASR::symbol_t>(ASRUtils::make_Variable_t_util(
         al, value->base.loc, scope, s2c(al, var_name), nullptr, 0, ASR::intentType::Local,
-        nullptr, nullptr, ASR::storage_typeType::Default, var_type, nullptr, ASR::abiType::Source,
+        nullptr, nullptr, ASR::storage_typeType::Default, var_type, ASRUtils::get_struct_sym_from_struct_expr(value), ASR::abiType::Source,
         ASR::accessType::Public, ASR::presenceType::Required, false));
     scope->add_symbol(var_name, temporary_variable);
 
