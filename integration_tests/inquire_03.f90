f05e0c5bba4ba4ae1622192eb744d60bf30dec3f
HarshilShah1804 <hkshah1978@gmail.com>
2025-06-25 22:18:01 +0530

    test: update test

--- integration_tests/inquire_03.f90
+++ integration_tests/inquire_03.f90
@@ -1,8 +1,31 @@
 program inquire_03
     integer :: unit = 2
-    character(12) :: filename, specifier
+    character(12) :: filename, specifier_r, specifier_w, specifier_rw
     filename = "dummy.txt"
+
+    open(unit, file=filename, action='write')
+    inquire(unit, write=specifier_w, read=specifier_r, readwrite=specifier_rw)
+    print *, specifier_r, specifier_w, specifier_rw
+    if (specifier_r /= "NO" .or. specifier_w /= "YES ".or. specifier_rw /= "NO") error stop
+
+    close(unit)
+
+    open(unit, file=filename, action='read')
+    inquire(unit, write=specifier_w, read=specifier_r, readwrite=specifier_rw)
+    print *, specifier_r, specifier_w, specifier_rw
+    if (specifier_r /= "YES" .or. specifier_w /= "NO  " .or. specifier_rw /= "NO") error stop
+    close(unit)
+
     open(unit, file=filename, action='readwrite')
-    inquire(unit, write=specifier)
-    if (specifier(1:1) /= 'Y' .and. specifier(1:1) /= 'y') error stop
+    inquire(unit, write=specifier_w, read=specifier_r, readwrite=specifier_rw)
+    print *, specifier_r, specifier_w, specifier_rw
+    if (specifier_r /= "YES" .or. specifier_w /= "YES " .or. specifier_rw /= "YES") error stop
+    close(unit)
+
+    open(unit, file=filename)
+    inquire(unit, write=specifier_w, read=specifier_r, readwrite=specifier_rw)
+    print *, specifier_r, specifier_w, specifier_rw
+    if (specifier_r /= "YES" .or. specifier_w /= "YES " .or. specifier_rw /= "YES") error stop
+    close(unit)
+
 end program inquire_03
