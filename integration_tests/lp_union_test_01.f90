16390a5f411cf7560305bdfcc462f4c22667cb70
swamishiju <swaminathshiju24@gmail.com>
2025-06-30 13:29:39 +0530

    TEST: Updated union tests

--- integration_tests/lp_union_test_01.f90
+++ integration_tests/lp_union_test_01.f90
@@ -1,27 +1,34 @@
 program lp_union_test_01
-
-   type:: test_type
-      integer :: x
-      real    :: y
-   end type
-
-   _lfortran_union_type :: test_type1 ! this is a test comment
-   integer :: x
-   real    :: i
-   type(test_type) :: y
-   end _lfortran_union_type
-
-
-   _lfortran_union_type :: test_type2 ! this is a test comment
-   integer :: x
-   real    :: i
-   type(test_type) :: y
-   end _lfortran_union_type
-
-
-   type(test_type) :: t_ty
-   type(test_type1) :: t_ty1
-   type(test_type2) :: t_ty2
+    
+    type:: test_type
+        integer :: x
+        real    :: y
+    end type
+    
+    _lfortran_union_type :: test_type1 ! this is a test comment
+        integer :: x
+        real    :: i
+        type(test_type) :: y
+    end _lfortran_union_type
+    
+    
+    _lfortran_union_type :: test_type2 ! this is a test comment
+        integer :: x
+        real    :: i
+        type(test_type1) :: y
+    end _lfortran_union_type
+    
+    type :: test_type3 ! this is a test comment
+        integer :: x
+        real    :: i
+        type(test_type) :: y
+        type(test_type1) :: z
+        type(test_type2) :: w
+    end type
+    
+    type(test_type) :: t_ty
+    type(test_type1) :: t_ty1
+    type(test_type2) :: t_ty2
 
 end program
 
