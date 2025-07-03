895016aafedb3554fd5213e52e235f60d95a1921
Aditya Trivedi <adit4443ya@gmail.com>
2025-07-04 00:33:40 +0530

    FEAT: Generate ASR for Distribute cosntruct

--- src/lfortran/semantics/ast_body_visitor.cpp
+++ src/lfortran/semantics/ast_body_visitor.cpp
@@ -5386,6 +5386,8 @@ public:
                     collect_omp_body(ASR::omp_region_typeType::Critical);
                 } else if (LCompilers::startswith(x.m_construct_name, "teams")) {
                     collect_omp_body(ASR::omp_region_typeType::Teams);
+                } else if (LCompilers::startswith(x.m_construct_name, "distribute")) {
+                    collect_omp_body(ASR::omp_region_typeType::Distribute);
                 }
                 pragma_in_do_loop=false;
                 if((pragma_nesting_level_2 == 0 && omp_region_body.size()==1) || all_blocks_nesting>0) {
@@ -5524,6 +5526,14 @@ public:
                 body.reserve(al, 0);
                 omp_region_body.push_back(ASRUtils::STMT(
                     ASR::make_OMPRegion_t(al, loc, ASR::omp_region_typeType::Teams, clauses.p, clauses.n, body.p, body.n)));
+            } else if (to_lower(x.m_construct_name) == "distribute") {
+                pragma_nesting_level_2++;
+                Vec<ASR::omp_clause_t*> clauses;
+                clauses = get_clauses(x);
+                Vec<ASR::stmt_t*> body;
+                body.reserve(al, 0);
+                omp_region_body.push_back(ASRUtils::STMT(
+                    ASR::make_OMPRegion_t(al, loc, ASR::omp_region_typeType::Distribute, clauses.p, clauses.n, body.p, body.n)));
             } else {
                 diag.add(Diagnostic(
                     "The construct "+ std::string(x.m_construct_name)
