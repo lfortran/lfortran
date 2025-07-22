f97e2df9578c3c7ce0d923bcbfebef6003e22845
jinangshah21 <jinang2110@gmail.com>
2025-07-23 00:06:41 +0530

    test: add integration test

--- integration_tests/class_56.f90
+++ integration_tests/class_56.f90
@@ -6,6 +6,10 @@ module class_56_mod1
     procedure :: accept
   end type
 
+  type :: string_t
+    character(len=:), allocatable :: s
+  end type string_t
+
 contains
 
   subroutine accept(self)
@@ -51,6 +55,13 @@ contains
     end select
   end subroutine
 
+  subroutine pkgcfg_list_all(descriptions)  
+    type(string_t), optional, allocatable, intent(out) :: descriptions(:)
+    allocate(descriptions(2))
+    descriptions(1)%s = "Package 1"
+    descriptions(2)%s = "Package 2"
+  end subroutine
+
 end module
 
 program class_56
@@ -59,10 +70,14 @@ program class_56
 
   type(ser_config) :: cfg
   type(toml_value) :: v
+  type(string_t), allocatable :: descriptions(:)
 
   v%x = 1
   call cfg%temp(v)
   if (v%x /= 2) error stop
   call cfg%temp2(v)
   if (v%x /= 3) error stop
+  call pkgcfg_list_all(descriptions)
+  if (descriptions(1)%s /= "Package 1") error stop
+  if (descriptions(2)%s /= "Package 2") error stop
 end program
