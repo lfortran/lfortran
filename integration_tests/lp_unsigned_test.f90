18cc7b0bef2ecef206ffb09c62ca55daec6e5eef
swamishiju <swaminathshiju24@gmail.com>
2025-07-27 19:16:56 +0530

    TEST: Added test

---
+++ integration_tests/lp_unsigned_test.f90
@@ -0,0 +1,33 @@
+program lp_unsigned_test
+
+    implicit none
+
+    ! 4 byte unsigned int 
+    type(uint) :: x
+    integer(kind=4)    :: y
+    integer :: i
+    x = 1
+    y = 1
+
+    do i = 1, 30
+        x = x * _lfortran_uint(2)
+        y = y * 2
+        if ( x < _lfortran_uint(0) ) error stop 
+        if ( y < 0 )  error stop 
+    end do
+
+    x = x * _lfortran_uint(2)
+    y = y * 2
+    if ( x < _lfortran_uint(0) ) error stop 
+    if ( y > 0 )  error stop 
+
+
+    x = x * _lfortran_uint(2)
+    y = y * 2
+    if ( x /= _lfortran_uint(0) ) error stop 
+    if ( y /= 0 )  error stop 
+
+    
+
+end program lp_unsigned_test
+
