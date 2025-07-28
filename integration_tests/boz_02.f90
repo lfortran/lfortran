71af918913f0f8c7b20350bc2c7c3194240974f4
Yash <yashnagda17@gmail.com>
2025-07-28 22:55:56 +0530

    Add better comments

--- integration_tests/boz_02.f90
+++ integration_tests/boz_02.f90
@@ -1,20 +1,25 @@
 program real_boz
 implicit none
 
-real:: boz_4
-real :: matrix1(5)
-real:: matrix2(5)
-real :: arr1(2)
-integer:: arr2(2)
-integer::i
+integer :: i
+real :: boz_1, matrix1(5), matrix2(2), arr1(2)
+integer :: arr2(2)
 
-data boz_4 /b'01011101'/
-data matrix1/ b'10', 2*o'100', 3.12, z'ABC' /
-data matrix2/ 5*b'10'/
+!Check Scalar Assigment
+data boz_1 /b'01011101'/  
+
+!Check Multiple Assigments of Real array with BOZ Values
+data matrix1 / b'10', 2*o'100', 3.12, z'ABC' / 
+
+!Check Data Broadcasting to all elements of array
+data matrix2 / 2*b'10' /
+
+!Check Implied Do loop Assignments, with real assigments along with integer assignments
 data (arr1(i), arr2(i), i=1,2) / 1.2, z'02', z'01', 3 /
 
-print *, boz_4
+print *, boz_1
 print *, matrix1
 print *, matrix2
-print *, arr1,arr2
+print *, arr1, arr2
+
 end program
