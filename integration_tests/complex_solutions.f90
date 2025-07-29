109d7b3678f14d99e6f170829ba006ebd526312b
Yuvraj Singh <qomfortzone@gmail.com>
2025-07-29 17:41:40 +0530

    more aligned to fortran

--- integration_tests/complex_solutions.f90
+++ integration_tests/complex_solutions.f90
@@ -1,26 +1,29 @@
 program complex_solutions
-   use, intrinsic :: iso_fortran_env, only: wp => real64
+
+   ! print some complex solutions to the equation: 1**x = rhs.
+   
+   use, intrinsic :: iso_fortran_env, only: wp=>real64
    implicit none
-   complex(wp) :: arg, x, val
+   complex(wp) :: z1, arg, x
    real(wp), parameter :: rhs = 4.0_wp, twopi = 8 * atan(1.0_wp), lnrhs = log(rhs), tol = 1.0e-12_wp
    integer :: k
+   character(*), parameter :: cfmth='("Complex solutions to 1**x=",g0.4/a2,*(4x,a11,5x))', &
+        &  cfmt='(i2,*(" (",es0.2,",",es0.2,")":))'
+
+   write(*,cfmth) rhs, 'k', 'arg=i2Pik', 'exp(arg)', 'x', 'exp(arg)**x', 'exp(arg*x)'
 
    do k = 1, 10
       arg = cmplx(0.0_wp, twopi * k, kind=wp)
+      z1  = exp(arg)
       x   = cmplx(0.0_wp, -lnrhs/(twopi*k), kind=wp)
-      val = exp(arg*x)
 
-      if (abs(real(val) - rhs) > tol .or. abs(aimag(val)) > tol) then
+      ! Check for correctness:
+      if (abs(real(exp(arg*x)) - rhs) > tol .or. abs(aimag(exp(arg*x))) > tol) then
          print *, "Integration test failed for k=", k
          error stop 1
-
       end if
+
+      write(*,cfmt) k, arg, z1, x, z1**x, exp(arg*x)
    end do
 
-contains
-   pure function itoa(i) result(str)
-      integer, intent(in) :: i
-      character(len=32) :: str
-      write(str,'(I0)') i
-   end function itoa
 end program complex_solutions
