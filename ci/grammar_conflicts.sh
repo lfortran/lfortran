a5af3058015836973c45f50184f0ebd9855a3a8d
Ondřej Čertík <ondrej@certik.us>
2025-08-03 09:21:15 -0600

    Remove .bak on Linux

--- ci/grammar_conflicts.sh
+++ ci/grammar_conflicts.sh
@@ -59,9 +59,9 @@
 set -ex
 
 patch -p1 < ci/parser.yy.patch
-sed -i '.bak' '/^%expect-rr/d' src/lfortran/parser/parser.yy
-sed -i '.bak' 's/^%expect .*/%expect 0/' src/lfortran/parser/parser.yy
-sed -i '.bak' '/^%glr-parser/d' src/lfortran/parser/parser.yy
+sed -i '/^%expect-rr/d' src/lfortran/parser/parser.yy
+sed -i 's/^%expect .*/%expect 0/' src/lfortran/parser/parser.yy
+sed -i '/^%glr-parser/d' src/lfortran/parser/parser.yy
 (cd src/lfortran/parser && bison -Wall -d -r all parser.yy)
 
 echo "Patched grammar is LALR(1), no essential conflicts in GLR."
