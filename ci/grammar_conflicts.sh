4286f72be7af5383b8dca3ad84254ceeeea36e6a
Ondřej Čertík <ondrej@certik.us>
2025-08-03 08:25:15 -0600

    Add .bak

--- ci/grammar_conflicts.sh
+++ ci/grammar_conflicts.sh
@@ -3,9 +3,9 @@
 set -ex
 
 patch -p1 < ci/parser.yy.patch
-sed -i '/^%expect-rr/d' src/lfortran/parser/parser.yy
-sed -i 's/^%expect .*/%expect 0/' src/lfortran/parser/parser.yy
-sed -i '/^%glr-parser/d' src/lfortran/parser/parser.yy
+sed -i '.bak' '/^%expect-rr/d' src/lfortran/parser/parser.yy
+sed -i '.bak' 's/^%expect .*/%expect 0/' src/lfortran/parser/parser.yy
+sed -i '.bak' '/^%glr-parser/d' src/lfortran/parser/parser.yy
 (cd src/lfortran/parser && bison -Wall -d -r all parser.yy)
 
 echo "Grammar is LALR(1), no conflicts"
