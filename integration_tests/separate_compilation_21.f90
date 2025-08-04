c10a97c35943a8b29d3fe844721e7f1ae0bc521c
AnshMehta1 <ansh.mehta522007@gmail.com>
2025-08-04 18:30:29 +0530

    Add Integration Tests

--- integration_tests/separate_compilation_21.f90
+++ integration_tests/separate_compilation_21.f90
@@ -4,10 +4,15 @@ program separate_compilation_21
 
     integer, parameter :: n = 2
     integer, parameter :: base = 1
-    real :: result(n)
 
-    result = func(n, base)
+    real :: result1(n)
+    integer :: result2(n)
 
-    print *, result
-    if (.not. all(result == [1.0, 2.0])) error stop
+    result1 = logspace(n, base)
+    result2 = logspace(n, base)
+
+    print *, result1
+    print *, result2
+    if (.not. all(result1 == [1.0, 2.0])) error stop
+    if (.not. all(result2 == [1, 2])) error stop
 end program
