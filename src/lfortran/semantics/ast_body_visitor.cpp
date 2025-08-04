8d3181789ba37430f5fb47e26c23a8351f4bbc6f
Saurabh Kumar <developer.saurabh@outlook.com>
2025-08-04 17:28:59 +0530

    fix: support `complex` type in statement functions
    Signed-off-by: Saurabh Kumar <developer.saurabh@outlook.com>

--- src/lfortran/semantics/ast_body_visitor.cpp
+++ src/lfortran/semantics/ast_body_visitor.cpp
@@ -2977,7 +2977,8 @@ public:
             } else {
                 if (ASR::is_a<ASR::Variable_t>(*sym)) {
                     auto v = ASR::down_cast<ASR::Variable_t>(sym);
-                    if (ASR::is_a<ASR::Integer_t>(*v->m_type) || ASR::is_a<ASR::Real_t>(*v->m_type) || ASR::is_a<ASR::Logical_t>(*v->m_type)) {
+                    if (ASR::is_a<ASR::Integer_t>(*v->m_type) || ASR::is_a<ASR::Real_t>(*v->m_type) || ASR::is_a<ASR::Logical_t>(*v->m_type)
+                        || ASR::is_a<ASR::Complex_t>(*v->m_type)) {
                         if (ASRUtils::is_array(v->m_type)) {
                             return false;
                         } else {
