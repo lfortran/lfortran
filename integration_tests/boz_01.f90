67d0ef51bca3329a7a2d75cff99b6c8574572825
Yash <yashnagda17@gmail.com>
2025-08-10 10:08:13 +0530

    Resolve Comments

--- integration_tests/boz_01.f90
+++ integration_tests/boz_01.f90
@@ -9,7 +9,13 @@ boz_3 = int(z'ABC')
 !Testing Truncation of BOZ
 boz_4 = int(Z'234567890abcdef1')
 boz_5 = int(Z'2234567890abcdef1')
-if (boz_4 /= boz_5) stop
+!Check with Integer Equivalent Values
+if (boz_4 /= boz_5) error stop
+if (boz_1 /= 93) error stop
+if (boz_2 /= 1255) error stop
+if (boz_3 /= 2748) error stop
+if (boz_4 /= 180150001) error stop
+if (boz_5 /= 180150001) error stop 
 
-print *, boz_1, boz_2, boz_3
+print *, boz_1, boz_2, boz_3, boz_4, boz_5
 end program
