e24cf745a2e23717f2e0d00c8503fb82919f6441
Pranavchiku <goswami.4@iitj.ac.in>
2025-08-14 23:52:10 +0530

    test: register and update references

---
+++ tests/classes1.f90
@@ -0,0 +1,23 @@
+module xx
+
+    type :: base
+        integer :: x
+    contains
+        procedure, non_overridable :: show_x
+    end type
+
+    contains
+        subroutine show_x(this)
+            class(base), intent(inout) :: this
+            this%x = 12
+        end subroutine show_x
+end module
+
+! program main
+!     use xx
+!     implicit none
+
+!     type(base) :: b
+!     b%x = 10
+!     call b%show_x()
+! end program
