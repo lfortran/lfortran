317c219546d5a977c3f01cc0cf790a8de64c9e79
parth121101 <96720723+parth121101@users.noreply.github.com>
2025-08-15 07:11:13 +0530

    Add enable style compiler option (#8336)

--- run_tests.py
+++ run_tests.py
@@ -54,6 +54,7 @@ def single_test(test: Dict, verbose: bool, no_llvm: bool, skip_run_with_dbg: boo
     asr_implicit_interface_and_typing_with_llvm = is_included("asr_implicit_interface_and_typing_with_llvm")
     asr_no_warnings = is_included("asr_no_warnings")
     asr_disable_style_and_warnings = is_included("asr_disable_style_and_warnings")
+    asr_enable_style_suggestion = is_included("asr_enable_style_suggestion")
     continue_compilation = is_included("continue_compilation")
     fixed_form_cc_asr = is_included("fixed_form_cc_asr")
     semantics_only_cc = is_included("semantics_only_cc")
@@ -258,6 +259,19 @@ def single_test(test: Dict, verbose: bool, no_llvm: bool, skip_run_with_dbg: boo
             update_reference,
             verify_hash,
             extra_args)
+    
+    if asr_enable_style_suggestion:
+        if no_llvm:
+            log.info(f"{filename} * obj    SKIPPED as requested")
+        else:
+            run_test(
+                filename,
+                "asr_enable_style_suggestion",
+                "lfortran --std=f23 --style-warnings --no-color {infile}",
+                filename,
+                update_reference,
+                verify_hash,
+                extra_args)
 
     if asr:
         # run fixed form
