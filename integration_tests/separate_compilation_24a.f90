ecc77cedf140cb00b9fe802e66c73d3e11d16ef4
AnshMehta1 <ansh.mehta522007@gmail.com>
2025-08-17 03:01:08 +0530

    Add and Register Integration Tests

---
+++ integration_tests/separate_compilation_24a.f90
@@ -0,0 +1,10 @@
+module sorting_separate_compilation_24
+    implicit none
+
+    interface
+        module subroutine sort(n)
+            integer, intent(inout) :: n
+        end subroutine
+    end interface
+    
+end module sorting_separate_compilation_24
