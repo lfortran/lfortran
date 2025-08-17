d6b861d4b228403acdf3bf53edc2cee8288d7c94
jinangshah21 <jinang2110@gmail.com>
2025-08-17 17:40:50 +0530

    fix: create vtabs for pointer variable also

--- src/libasr/codegen/asr_to_llvm.cpp
+++ src/libasr/codegen/asr_to_llvm.cpp
@@ -4896,7 +4896,8 @@ public:
                     ASR::Variable_t* var = ASR::down_cast<ASR::Variable_t>(
                         ASRUtils::symbol_get_past_external(x.m_symtab->resolve_symbol(variable_type_name)));
                     ASR::symbol_t* class_sym = ASRUtils::symbol_get_past_external(var->m_type_declaration);
-                    if (ASRUtils::is_allocatable(var->m_type) && ASRUtils::is_class_type(ASRUtils::type_get_past_allocatable(var->m_type))) {
+                    if (ASRUtils::is_allocatable_or_pointer(var->m_type) &&
+                          ASRUtils::is_class_type(ASRUtils::extract_type(var->m_type))) {
                         create_vtab = true;
                         break;
                     }
