390b7a2983b350d2fd67493c80970d4588329ccb
Christopher Albert <albert@tugraz.at>
2025-09-27 20:28:49 +0200

    fix: keep fast optimizations for legacy array sections

--- src/libasr/utils.h
+++ src/libasr/utils.h
@@ -67,6 +67,7 @@ struct PassOptions {
     bool time_report = false;
     bool skip_removal_of_unused_procedures_in_pass_array_by_data = false;
     bool bounds_checking = true;
+    bool legacy_array_sections = false;
     std::vector<std::string> vector_of_time_report;
 };
 
