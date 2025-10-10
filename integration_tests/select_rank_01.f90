f36309c5a2a1820edde3b25d4e89cb85530a8776
HarshilShah1804 <hkshah1978@gmail.com>
2025-10-10 19:00:17 +0530

    test: update test

--- integration_tests/select_rank_01.f90
+++ integration_tests/select_rank_01.f90
@@ -15,15 +15,25 @@ contains
     subroutine check(x, expected)
         integer, intent(in) :: x(..)
         integer, intent(in) :: expected
+        integer :: i
 
         select rank(x)
             rank(0)
                 if (expected /= 0) error stop
             rank(1)
+                i = x(2)
+                print *, i
+                if (i /= 2) error stop
                 if (expected /= 1) error stop
             rank(2)
+                i = x(1,2)
+                print *, i
+                if (i /= 3) error stop
                 if (expected /= 2) error stop
             rank(3)
+                i = x(2,2,1)
+                print *, i
+                if (i /= 4) error stop
                 if (expected /= 3) error stop
 
         end select
