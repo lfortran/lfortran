b972b76dd9e1bb53ea102d838032c63a73149ab4
opixdown <atharvawaghmare34@gmail.com>
2025-11-04 01:13:27 +0530

    Add polymorphic_select_type_01 test and fix printing issue

--- integration_tests/polymorphic_select_type_01.f90
+++ integration_tests/polymorphic_select_type_01.f90
@@ -1,17 +1,20 @@
 program main
-   type :: type_a
-      integer :: a
-   end type type_a
+    type :: type_a
+       integer :: a
+    end type type_a
 
-   class(type_a), allocatable :: x
+    class(type_a), allocatable :: x
+    character(100) :: ss
 
-   allocate(x)
-   x%a = 10
+    allocate(x)
+    x%a = 10
 
-   select type(x)
-    type is(type_a)
-        if (x%a /= 10) error stop "FAILED: wrong value"
-    class default
-        error stop "FAILED: wrong type in select type"
-   end select
+    select type(x)
+     type is (type_a)
+       print *, x
+       write(ss, "(i0)") x
+       if (trim(ss) /= "10") error stop "FAILED: wrong value from write"
+     class default
+       error stop "FAILED: wrong type"
+    end select
 end program
