cc065b9c54db51f30fd2c479d698df028c5bf00b
Christopher Albert <albert@tugraz.at>
2025-11-26 22:58:01 +0100

    Fix lapack_04: add stub implementations and main program
    Make lapack_04 a complete runnable test like lapack_02:
    - Add stub implementations for SCOPY and SGEMV
    - Add main PROGRAM that calls SLALS0 and prints PASS
    - Test now compiles and executes successfully
    
    This changes lapack_04 from a compile-only MRE to a full integration test.

--- integration_tests/lapack_04.f90
+++ integration_tests/lapack_04.f90
@@ -13,3 +13,27 @@ C MRE: implicit-interface array physical type mismatch from LAPACK SLALS0
    90 CONTINUE
 
       END
+
+      SUBROUTINE SCOPY( N, X, INCX, Y, INCY )
+      INTEGER N, INCX, INCY
+      REAL X(*), Y(*)
+C Stub implementation - just to make test compile and run
+      END
+
+      SUBROUTINE SGEMV( TRANS, M, N, ALPHA, A, LDA, X, INCX,
+     $                  BETA, Y, LDY )
+      CHARACTER TRANS
+      INTEGER M, N, LDA, INCX, LDY
+      REAL ALPHA, BETA
+      REAL A(LDA,*), X(*), Y(*)
+C Stub implementation - just to make test compile and run
+      END
+
+      PROGRAM LAPACK_04
+      INTEGER PERM(2)
+      REAL B(3,2), BX(3,2), WORK(2)
+      PERM(1) = 1
+      PERM(2) = 2
+      CALL SLALS0( 2, B, 3, BX, 3, PERM, 2, WORK )
+      PRINT *, 'PASS'
+      END
