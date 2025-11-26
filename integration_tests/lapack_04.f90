586e9b642e298235b19b4075eaa70dd023343884
Christopher Albert <albert@tugraz.at>
2025-11-26 22:36:15 +0100

    Fix lapack_04: use minimal 14-line MRE
    Rebase restored the old elaborate version. Replace with minimal reproducer
    that triggers: ASRUtils::check_equal_type(i32[:], r32[:, :]) crash.

--- integration_tests/lapack_04.f90
+++ integration_tests/lapack_04.f90
@@ -1,57 +1,15 @@
 C MRE: implicit-interface array physical type mismatch from LAPACK SLALS0
-      PROGRAM LAPACK_04
-      CALL TEST
-      PRINT *, 'PASS'
-      END
-
-      SUBROUTINE TEST
-      INTEGER PERM(2)
-      REAL B(3,1), BX(3,1), WORK(2)
-      PERM(1) = 1
-      PERM(2) = 2
-      CALL SLALS0_MRE(1, B, 3, BX, 3, PERM, 2, WORK, 1, 0)
-      END
-
-      SUBROUTINE SLALS0_MRE(ICOMPQ, B, LDB, BX, LDBX, PERM, K, WORK,
-     &     NRHS, INFO)
-      IMPLICIT NONE
-      INTEGER ICOMPQ, LDB, LDBX, K, NRHS, INFO
-      INTEGER PERM(*)
+      SUBROUTINE SLALS0( NRHS, B, LDB, BX, LDBX, PERM, K, WORK )
+      INTEGER K, LDB, LDBX, NRHS, I, PERM(*)
       REAL B(LDB,*), BX(LDBX,*), WORK(*)
-      REAL TEMP
-      IF (ICOMPQ .EQ. 0) THEN
-         WORK(1) = -1.0
-         TEMP = SNRM2(K, WORK, 1)
-         CALL SGEMV('T', K, NRHS, 1.0, BX, LDBX, WORK, 1, 0.0,
-     &        B(1,1), LDB)
-         CALL SLASCL('G', 0, 0, TEMP, 1.0, 1, NRHS, B(1,1), LDB, INFO)
-      ELSE
-         CALL SGEMV('T', K, NRHS, 1.0, B, LDB, WORK, 1, 0.0,
-     &        BX(1,1), LDBX)
-         CALL SCOPY(NRHS, BX(1,1), LDBX, B(PERM(1),1), LDB)
-      END IF
-      END
+      EXTERNAL SCOPY, SGEMV
 
-      REAL FUNCTION SNRM2(N, X, INCX)
-      INTEGER N, INCX
-      REAL X(*)
-      SNRM2 = 0.0
-      END
+      CALL SCOPY( NRHS, B, LDB, BX, LDBX )
+      CALL SGEMV( 'T', K, NRHS, 1.0, B, LDB, WORK, 1, 0.0,
+     $            BX( 1, 1 ), LDBX )
 
-      SUBROUTINE SGEMV(TRANS, M, N, ALPHA, A, LDA, X, INCX, BETA, Y, LDY)
-      CHARACTER*(*) TRANS
-      INTEGER M, N, LDA, INCX, LDY
-      REAL ALPHA, BETA
-      REAL A(LDA,*), X(*), Y(LDY,*)
-      END
+      DO 90 I = 1, K
+         CALL SCOPY( NRHS, BX( I, 1 ), LDBX, B( PERM( I ), 1 ), LDB )
+   90 CONTINUE
 
-      SUBROUTINE SLASCL(TYPE, KL, KU, CFROM, CTO, M, N, A, LDA, INFO)
-      CHARACTER*(*) TYPE
-      INTEGER KL, KU, M, N, LDA, INFO
-      REAL CFROM, CTO, A(LDA,*)
-      END
-
-      SUBROUTINE SCOPY(N, X, INCX, Y, INCY)
-      INTEGER N, INCX, INCY
-      REAL X(*), Y(*)
       END
