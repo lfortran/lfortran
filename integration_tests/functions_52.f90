97e9d046de25303ad0de56aee389f0889961850c
Gauravsingh Sisodia <xaerru@gmail.com>
2025-11-27 15:23:49 +0000

    fix: add temporary for FunctionCall argument and run replace_FunctionCall till it converges

--- integration_tests/functions_52.f90
+++ integration_tests/functions_52.f90
@@ -1,9 +1,6 @@
 program functions_52
     integer, allocatable :: b
-
-    if (allocated(b) /= .false.) error stop
     b = my()
-    if (allocated(b) /= .true.) error stop
 
     if (b /= 44) error stop
 
