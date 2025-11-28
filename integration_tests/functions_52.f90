b41a7b3571f69ce6d7d5afa01199cb3d97046771
Gauravsingh Sisodia <xaerru@gmail.com>
2025-11-28 16:34:46 +0000

    fix: improve functions_52 test

--- integration_tests/functions_52.f90
+++ integration_tests/functions_52.f90
@@ -1,6 +1,9 @@
 program functions_52
     integer, allocatable :: b
+
+    if (allocated(b) /= .false.) error stop
     b = my()
+    if (allocated(b) /= .true.) error stop
 
     if (b /= 44) error stop
 
