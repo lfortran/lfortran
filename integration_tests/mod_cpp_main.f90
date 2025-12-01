93c27ea8e8cee3ca5adca0d2b330548bea70ec20
Christopher Albert <albert@tugraz.at>
2025-12-01 08:23:45 +0100

    Remove mod_cpp_modfile_crash integration test (class_63 still exercises bug)

--- integration_tests/mod_cpp_main.f90
+++
@@ -1,5 +0,0 @@
-program mod_cpp_main
-    use mod_cpp_mod
-    real, allocatable :: a(:)
-    call boom(a)
-end program mod_cpp_main
--- integration_tests/mod_cpp_mod.f90
+++
@@ -1,8 +0,0 @@
-module mod_cpp_mod
-contains
-    subroutine boom(a)
-        real, allocatable, intent(inout) :: a(:)
-        ! Force runtime path that needs preprocessor location remap
-        a = 1.0
-    end subroutine boom
-end module mod_cpp_mod
