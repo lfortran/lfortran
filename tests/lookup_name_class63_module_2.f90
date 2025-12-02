9ef8aaf5f8d77824cec12a1ff79e0249cb2c3ef9
Christopher Albert <albert@tugraz.at>
2025-12-02 01:44:01 +0100

    test: add lookup-name coverage for abstract type modules

---
+++ tests/lookup_name_class63_module_2.f90
@@ -0,0 +1,20 @@
+module lookup_name_class63_module_2
+
+   use lookup_name_class63_module_1, only: AbsType
+   
+   type :: MyType
+      class(AbsType), allocatable :: arr(:)
+   contains
+      procedure :: method
+   end type MyType
+
+contains
+   
+   subroutine method(self)
+      class(MyType), intent(inout) :: self
+      associate ( element => self%arr(size(self%arr)) )
+      end associate
+   end subroutine method
+
+end module
+
