107887461f61543c0c3cdd03f225764bb19997bc
Christopher Albert <albert@tugraz.at>
2025-12-11 20:24:48 +0100

    Convert lapack_04 MRE to free-form Fortran
    The bug being tested has nothing to do with fixed-form, so convert
    to free-form to simplify the test. This removes the --fixed-form
    flag requirement.

--- integration_tests/lapack_04.f90
+++ integration_tests/lapack_04.f90
@@ -1,22 +1,22 @@
-C MRE from LAPACK slals0.f: integer array index into real array
-C causes type check assertion: check_equal_type(i32[:], r32[:])
-C Pattern: arr(idx(i)) passed to implicit-interface subroutine
-      SUBROUTINE TEST
-      INTEGER IDX(2)
-      REAL ARR(4)
+! MRE from LAPACK slals0.f: integer array index into real array
+! causes type check assertion: check_equal_type(i32[:], r32[:])
+! Pattern: arr(idx(i)) passed to implicit-interface subroutine
+subroutine test
+    integer :: idx(2)
+    real :: arr(4)
 
-      IDX(1) = 1
-      ARR = 1.0
+    idx(1) = 1
+    arr = 1.0
 
-      CALL SUB(ARR(IDX(1)))
-      END
+    call sub(arr(idx(1)))
+end subroutine test
 
-      SUBROUTINE SUB(X)
-      REAL X(*)
-      IF (X(1) .NE. 1.0) STOP 1
-      END
+subroutine sub(x)
+    real :: x(*)
+    if (x(1) /= 1.0) stop 1
+end subroutine sub
 
-      PROGRAM LAPACK_04
-      CALL TEST
-      PRINT *, 'PASS'
-      END
+program lapack_04
+    call test
+    print *, 'PASS'
+end program lapack_04
