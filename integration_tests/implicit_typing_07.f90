d104d5c563eee6b557ba33e587707886e0ff5073
opixdown <atharvawaghmare34@gmail.com>
2025-12-17 01:32:01 +0530

    Combine implicit do tests into single program

--- integration_tests/implicit_typing_07.f90
+++ integration_tests/implicit_typing_07.f90
@@ -1,8 +1,16 @@
-program implicit_do_1
-    integer :: n(3)
-    n = [(42, i = 1, size(n))]
-    if (n(1) /= 42) error stop 
-    if (n(2) /= 42) error stop
-    if (n(3) /= 42) error stop
+program implicit_do_combined
+    integer :: n(3), m(3)
+    integer :: p(3) = [(42, i = 1, 3)]  ! declaration initialization
+    
+    n = [(42, i = 1, size(n))]  ! array constructor with size()
+    
+    do i = 1, size(m)  ! explicit do loop
+        m(i) = 42
+    end do
+    
+    if (any(n /= 42)) error stop "n failed"
+    if (any(m /= 42)) error stop "m failed"
+    if (any(p /= 42)) error stop "p failed"
+    
     print "(3I3)", n
 end program
