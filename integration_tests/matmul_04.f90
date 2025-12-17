c15fa6341b6392614a5aaab33086c1c0e377d1e7
Christopher Albert <albert@tugraz.at>
2025-12-17 12:25:59 +0100

    test: add matmul_04 for unallocated result with --realloc-lhs-arrays
    Addresses review comment: test that matmul works when C is not
    pre-allocated, using --realloc-lhs-arrays flag.
    
    Without the flag, runtime error is raised as expected:
      Runtime Error: Array 'result' is indexed but not allocated.

---
+++ integration_tests/matmul_04.f90
@@ -0,0 +1,20 @@
+program matmul_04
+implicit none
+real, allocatable :: A(:,:), B(:,:), C(:,:)
+real :: expected
+
+allocate(A(3, 4), B(4, 2))
+A = 1.0
+B = 1.0
+
+! C is not allocated - should be auto-allocated with --realloc-lhs-arrays
+C = matmul(A, B)
+
+expected = 4.0
+if (abs(C(1,1) - expected) > 1e-6) error stop
+if (abs(C(3,2) - expected) > 1e-6) error stop
+if (size(C, 1) /= 3) error stop
+if (size(C, 2) /= 2) error stop
+print *, "OK: matmul with auto-allocated result"
+
+end program
