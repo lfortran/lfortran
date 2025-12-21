1293555787f78562aa54abb36fb2a68563b8a843
PrakharGoyal28 <prakhar28goyal@gmail.com>
2025-12-21 23:34:16 +0530

    minor

--- integration_tests/character_deferred_length_01.f90
+++ integration_tests/character_deferred_length_01.f90
@@ -4,21 +4,16 @@ program character_deferred_length_01
     character(2) :: x(200)
     character(:), allocatable :: y(:)
     
-    ! This should work - both are deferred-length allocatable
     allocate(character(2) :: y(10))
     call ss(y)
     if (.not. allocated(y)) error stop
     if (size(y) /= 10) error stop
     if (len(y) /= 2) error stop
     
-    ! Note: call ss(x) should fail with a type mismatch error
-    ! because x is fixed-length and ss expects deferred-length
-    ! This is tested separately to verify the error is caught
     
 contains
     subroutine ss(xx)
         character(:), allocatable :: xx(:)
-        ! This subroutine expects deferred-length allocatable strings
     end subroutine ss
 end program
 
--- tests/errors/character_deferred_length_mismatch.f90
+++ tests/errors/character_deferred_length_mismatch.f90
@@ -3,11 +3,9 @@ program character_deferred_length_mismatch
     character(2) :: x(200)
     character(:), allocatable :: y(:)
     
-    ! This should work - both are deferred-length allocatable
     allocate(character(2) :: y(10))
     call ss(y)
     
-    ! This should ERROR - x is fixed-length, but ss expects deferred-length
     call ss(x)
     
 contains
