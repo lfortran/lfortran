b2cc600e179e70f17d7b28e591c29ea130b36dfa
AtharvSharmaAI <b24cm1013@iitj.ac.in>
2025-12-22 21:04:56 +0530

    test added

---
+++ integration_tests/nested_19.f90
@@ -0,0 +1,24 @@
+program pp
+    type t1
+        integer, allocatable :: ll
+    end type
+
+    type t2
+        type(t1),allocatable :: arr(:)
+    end type t2
+
+    type t3
+        type(t2) ,allocatable :: z(:)
+    end type
+
+    call ss
+
+    contains 
+    subroutine ss
+        type(t3), allocatable :: t2
+        allocate(t2)
+        allocate(t2%z(10))
+        allocate(t2%z(1)%arr(5))
+    end subroutine
+
+end program
