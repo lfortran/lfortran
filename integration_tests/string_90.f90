6ca48902a61ef77bf26f6b1a97e9699b5717ab4f
Atharva Waghmare <102764510+opixdown@users.noreply.github.com>
2025-12-23 04:49:40 +0530

    Apply suggestion from @assem2002
    Co-authored-by: Assem Medhat <assem2002@gmail.com>

--- integration_tests/string_90.f90
+++ integration_tests/string_90.f90
@@ -3,10 +3,9 @@ program string_test
     character(len=2) :: c(6) = 'a'
     
     !  c(1) should be 'a ' not 'a'
-    if (len(c(1)) /= 2) then
-        print *, "Error: length mismatch"
-        stop 1
-    end if
+    if (len(c(1)) /= 2) error stop
+    if ((c(1)(1:1) /= "a")) error stop
+    if ((c(1)(2:2) /= " ")) error stop
     
     print *, c
     
