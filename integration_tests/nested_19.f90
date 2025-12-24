bd3a0215800d469571d546829e53a919d861fde7
Jainam-not-a-robot <b24cs1071@iitj.ac.in>
2025-12-24 23:58:10 +0530

    added tests in cmakelists.txt and changed code format

--- integration_tests/nested_19.f90
+++ integration_tests/nested_19.f90
@@ -1,18 +1,21 @@
 module nested_19_mod
+    implicit none
+
     type t
         integer :: i = 1
         integer :: arr(2) = [10, 20]
-    end type
+    end type t
 
-    type(t), parameter :: t_param = t() 
-    contains
-    
-
-end module 
+    type(t), parameter :: t_param = t()
+end module nested_19_mod
 
 program nested_19
     use nested_19_mod
-    integer :: i
-    i  = 1
-    print *, t_param%arr(i)
-end program
+    implicit none
+
+    ! Check scalar component
+    if (t_param%i /= 1) error stop
+
+    ! Check array component
+    if (any(t_param%arr /= [10, 20])) error stop
+end program nested_19
