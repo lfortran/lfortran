6f1d85a316364e983b606e0c699b7b67295e01d3
Harshita Kalani <harshitakalani02@gmail.com>
2025-12-26 14:47:20 +0530

    test: udpate test name

--- integration_tests/format_39.f90
+++ integration_tests/format_39.f90
@@ -1,4 +1,4 @@
-program testen0
+program format_39
   implicit none
   write(*,"(EN0.0E0)") 3.14159
   write(*,"(EN0.0E0)") 0.0
@@ -10,4 +10,4 @@ program testen0
   write(*,"(EN0.0E0)") -5.0
   write(*,"(EN0.0E0)") 1.23456E-5
   write(*,"(EN0.0E0)") 9.99E+10
-end program testen0
+end program format_39
