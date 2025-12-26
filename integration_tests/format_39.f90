9de0714a383398895bdb714b26c2bd64dccc5a4f
Harshita Kalani <harshitakalani02@gmail.com>
2025-12-26 14:43:45 +0530

    test: add and register test

---
+++ integration_tests/format_39.f90
@@ -0,0 +1,13 @@
+program testen0
+  implicit none
+  write(*,"(EN0.0E0)") 3.14159
+  write(*,"(EN0.0E0)") 0.0
+  write(*,"(EN0.0E0)") 10.0
+  write(*,"(EN0.0E0)") 3.14E+03
+  write(*,"(EN0.0E0)") 30.14159
+  write(*,"(EN0.0E0)") 391.14
+  write(*,"(EN0.0E0)") 3.14E+04
+  write(*,"(EN0.0E0)") -5.0
+  write(*,"(EN0.0E0)") 1.23456E-5
+  write(*,"(EN0.0E0)") 9.99E+10
+end program testen0
