49cf7322a428a30f0bdafb561f165a2aa2d02149
Anas <anas23khan083@gmail.com>
2025-12-27 14:13:50 +0530

    fix: add nullptr check to prevent crash in visit_SubmoduleModuleCommon

--- src/lfortran/semantics/ast_body_visitor.cpp
+++ src/lfortran/semantics/ast_body_visitor.cpp
@@ -2481,6 +2481,9 @@ public:
         }
         SymbolTable *old_scope = current_scope;
         ASR::symbol_t *t = current_scope->get_symbol(to_lower(x.m_name));
+        if (t == nullptr) {
+            return;
+        }
         ASR::Program_t *v = ASR::down_cast<ASR::Program_t>(t);
         current_scope = v->m_symtab;
         starting_m_body = x.m_body;
@@ -2876,6 +2879,9 @@ public:
     void visit_Procedure(const AST::Procedure_t &x) {
         SymbolTable* old_scope = current_scope;
         ASR::symbol_t* t = current_scope->get_symbol(to_lower(x.m_name));
+        if (t == nullptr) {
+            return;
+        }
         starting_m_body = x.m_body;
         starting_n_body = x.n_body;
 
@@ -2919,6 +2925,9 @@ public:
     // TODO: add SymbolTable::get_symbol(), which will only check in Debug mode
         SymbolTable *old_scope = current_scope;
         ASR::symbol_t *t = current_scope->get_symbol(to_lower(x.m_name));
+        if (t == nullptr) {
+            return;
+        }
         starting_m_body = x.m_body;
         starting_n_body = x.n_body;
         if( t->type == ASR::symbolType::GenericProcedure ) {
@@ -6120,6 +6129,9 @@ public:
 
         SymbolTable* old_scope = current_scope;
         ASR::symbol_t* t = current_scope->get_symbol(to_lower(x.m_name));
+        if (t == nullptr) {
+            return;
+        }
         ASR::Template_t* v = ASR::down_cast<ASR::Template_t>(t);
         current_scope = v->m_symtab;
         for (size_t i=0; i<x.n_decl; i++) {
