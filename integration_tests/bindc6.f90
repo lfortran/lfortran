6c0d27e72a31626d41ed0602e7852fd4882d0730
Christopher Albert <albert@tugraz.at>
2025-12-28 20:57:10 +0100

    llvm: fix bind(c) interface array argument ABI

---
+++ integration_tests/bindc6.f90
@@ -0,0 +1,21 @@
+program bindc6
+    use iso_c_binding, only: c_double
+    implicit none
+    real(c_double) :: arr(3)
+    real(c_double) :: result
+
+    arr = [1.0d0, 2.0d0, 3.0d0]
+
+    interface
+        function sum_arr(a, n) bind(c)
+            import :: c_double
+            real(c_double), intent(in) :: a(*)
+            integer, value :: n
+            real(c_double) :: sum_arr
+        end function
+    end interface
+
+    result = sum_arr(arr, 3)
+    if (abs(result - 6.0d0) > 1.0d-10) error stop "sum should be 6.0"
+    print *, "PASS"
+end program
