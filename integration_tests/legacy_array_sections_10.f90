5eae4a359f912982c8b4d30e15b1ff85010cc101
Christopher Albert <albert@tugraz.at>
2025-12-28 17:06:48 +0100

    integration_tests: make legacy_array_sections_10 standard-conforming

--- integration_tests/legacy_array_sections_10.f90
+++ integration_tests/legacy_array_sections_10.f90
@@ -1,18 +1,19 @@
 program legacy_array_sections_10
     implicit none
 
-    real, target :: a(4)
+    real :: a(4)
 
     a = [1.0, 2.0, 3.0, 4.0]
     call sub(a(2))
 
-contains
-
-    subroutine sub(x)
-        real, pointer :: x(:)
-
-        if (size(x) /= 3) error stop
-        if (abs(sum(x) - 9.0) > 1e-6) error stop
-    end subroutine sub
-
+    if (abs((a(2) + a(3) + a(4)) - 9.0) > 1e-6) error stop
 end program legacy_array_sections_10
+
+subroutine sub(x)
+    implicit none
+    real, intent(inout) :: x(3)
+
+    x(1) = 2.0
+    x(2) = 3.0
+    x(3) = 4.0
+end subroutine sub
