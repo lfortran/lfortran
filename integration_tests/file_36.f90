1cb15d2302fcbb5dc3832ea42a0b07a00fd8e5b8
Yuvraj Rathod <rathodyuvraj12022007@gmail.com>
2025-12-29 13:19:16 +0530

    Refactor file operations for better formatting

--- integration_tests/file_36.f90
+++ integration_tests/file_36.f90
@@ -3,12 +3,12 @@ program file_36
     integer :: unit_num
     character(len=100) :: result
 
-    open(file="test.txt", newunit=unit_num, delim="quote")
-    write(unit_num,*) "hello world"
+    open(file="test.txt", newunit=unit_num)
+    write(unit_num,'(A)') "hello world"
 
     rewind(unit_num)
 
-    read(unit_num,*) result
+    read(unit_num,'(A)') result
     close(unit_num)
 
     print *, result
