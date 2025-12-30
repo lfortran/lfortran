dd256358f9e9ff9fbcfa2c7a473e4211165a478f
Christopher Albert <albert@tugraz.at>
2025-12-30 13:57:41 +0100

    test: add common block declaration to main program in separate_compilation_28
    The main program needs to declare the common block to ensure one compilation
    unit defines the struct_instance_srnamc global variable. Without this, the
    linker reports undefined reference errors.
    
    This fixes the CI failures on macOS and Linux where the test failed with:
      undefined reference to `struct_instance_srnamc'

--- integration_tests/separate_compilation_28.f90
+++ integration_tests/separate_compilation_28.f90
@@ -1,6 +1,8 @@
 ! Common block with CHARACTER - main program
 program separate_compilation_28
     implicit none
+    character(32) :: srnamt
+    common /srnamc/ srnamt
     call set_name()
     call print_name()
 end program
