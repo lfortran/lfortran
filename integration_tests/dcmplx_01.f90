9cd020e98e5a6e108eab06307bd0c909842f480b
Christopher Albert <albert@tugraz.at>
2026-01-02 11:45:27 +0100

    test: use double precision values in dcmplx_01
    Use values with 15+ significant digits that require double precision
    to be accurately represented. Test for 1e-15 tolerance to verify that
    dcmplx returns complex(8) as expected.

--- integration_tests/dcmplx_01.f90
+++ integration_tests/dcmplx_01.f90
@@ -5,24 +5,25 @@ program dcmplx_01
     real(dp) :: x, y
     complex(dp) :: z
 
-    x = 1.5d0
-    y = 2.5d0
+    ! Use values that require double precision for accurate representation
+    ! These values would lose precision if converted to single precision
+    x = 1.123456789012345d0
+    y = 2.987654321098765d0
 
     ! Test dcmplx with single real argument
     z = dcmplx(x)
-    if (abs(real(z, dp) - 1.5d0) > 1d-15) error stop "dcmplx(x) real part failed"
-    if (abs(aimag(z)) > 1d-15) error stop "dcmplx(x) imag part failed"
+    if (abs(real(z, dp) - 1.123456789012345d0) > 1d-15) error stop
+    if (abs(aimag(z)) > 1d-15) error stop
 
     ! Test dcmplx with two real arguments
     z = dcmplx(x, y)
-    if (abs(real(z, dp) - 1.5d0) > 1d-15) error stop "dcmplx(x,y) real part failed"
-    if (abs(aimag(z) - 2.5d0) > 1d-15) error stop "dcmplx(x,y) imag part failed"
+    if (abs(real(z, dp) - 1.123456789012345d0) > 1d-15) error stop
+    if (abs(aimag(z) - 2.987654321098765d0) > 1d-15) error stop
 
     ! Test dconjg
-    z = (1.5d0, 2.5d0)
+    z = (1.123456789012345d0, 2.987654321098765d0)
     z = dconjg(z)
-    if (abs(real(z, dp) - 1.5d0) > 1d-15) error stop "dconjg real part failed"
-    if (abs(aimag(z) + 2.5d0) > 1d-15) error stop "dconjg imag part failed"
+    if (abs(real(z, dp) - 1.123456789012345d0) > 1d-15) error stop
+    if (abs(aimag(z) + 2.987654321098765d0) > 1d-15) error stop
 
-    print *, "All tests passed"
 end program dcmplx_01
