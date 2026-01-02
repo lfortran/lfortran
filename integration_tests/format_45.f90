53c8a9c6501ecc603d4b40b9ce2e5ff14730eb15
Harshita Kalani <harshitakalani02@gmail.com>
2026-01-02 13:19:10 +0530

    test: add and register test

---
+++ integration_tests/format_45.f90
@@ -0,0 +1,12 @@
+program format_45
+    implicit none
+    integer :: i1, i2
+    character(8) :: cdata
+
+    cdata = '1   1   '
+    read (cdata, 100) i1, i2
+    if (i1 /= 1) error stop
+    if (i2 /= 1000) error stop
+
+100 format (bn, i4, bz, i4)
+end program format_45
