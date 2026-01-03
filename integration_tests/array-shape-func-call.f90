00a209df850a3e9e0c10aa39828acd70bebc0256
Jainam-not-a-robot <b24cs1071@iitj.ac.in>
2026-01-03 19:06:23 +0530

    changed integration test to prevent regression

--- integration_tests/array-shape-func-call.f90
+++ integration_tests/array-shape-func-call.f90
@@ -1,17 +1,28 @@
 program pp
+  integer :: call_count = 0
+
   call ss(ff(f(), 20))
-  contains 
+
+  if (call_count /= 1) then
+    error stop "f() called more than once"
+  end if
+
+contains
+
   function ff(x, y) result(str)
     integer :: x
     integer :: y
     integer :: str(x)
   end function
+
   function f() result(ret)
     integer :: ret
+    call_count = call_count + 1
     ret = 10
-    print *, "called"
   end function
+
   subroutine ss(x)
     integer :: x(*)
-  end subroutine 
-end program 
+  end subroutine
+
+end program
