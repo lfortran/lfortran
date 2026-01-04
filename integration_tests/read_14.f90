8435a84b2c4dd7a5665e0937cc1e512f9bb27514
Baitato <prasham.pillu@gmail.com>
2026-01-04 00:28:12 +0000

    Added test

---
+++ integration_tests/read_14.f90
@@ -0,0 +1,12 @@
+program test_read
+  implicit none
+  integer :: i1, i2
+
+  read ('   1   2', '(i4, i4)') i1, i2
+
+  if (i1 == 1 .and. i2 == 2) then
+    print *, "OK"
+  else
+    print *, "FAIL"
+  end if
+end program
