dc7a1a1de592b7cb2d3a6a86682937dc54346f95
Baitato <prasham.pillu@gmail.com>
2026-01-05 03:47:11 +0000

    Moved tests to continue_compilation_1.f90

--- tests/errors/undefined_end_label.f90
+++
@@ -1,7 +0,0 @@
-program read_end_label_undefined
-    implicit none
-    integer :: val
-    ! Label 999 is not defined
-    read(*, *, end=999) val  
-    print *, val
-end program
--- tests/errors/undefined_err_label.f90
+++
@@ -1,7 +0,0 @@
-program read_end_label_undefined
-    implicit none
-    integer :: val
-    ! Label 500 is not defined
-    read(*, *, err=500) val  
-    print *, val
-end program
