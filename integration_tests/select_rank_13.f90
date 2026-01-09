75a849a808a9ef5ccfbcbb7689e1551b1135ac02
HarshilShah1804 <hkshah1978@gmail.com>
2026-01-09 15:14:20 +0530

    test: add and register test

--- integration_tests/select_rank_13.f90
+++ integration_tests/select_rank_13.f90
@@ -5,13 +5,20 @@ contains
       class(*), dimension(..), intent(in) :: arg
 
       select rank (assoc => arg)
+       rank (0)
+         select type (assoc)
+          type is (integer)
+            print *, "Received a scalar integer: ", assoc
+          class default
+            error stop
+         end select
        rank (1)
          select type (assoc)
           type is (integer)
             print *, "Received an array of integers (rank: 1)"
             ! If needed, access as 1D: print *, assoc
           class default
-            print *, "Received an array of some other type"
+            error stop
          end select
        rank (2)
          select type (assoc)
@@ -19,10 +26,10 @@ contains
             print *, "Received an array of integers (rank: 2)"
             ! If needed, access as 2D: print *, assoc
           class default
-            print *, "Received an array of some other type"
+            error stop
          end select
        rank default
-         print *, "Unsupported rank", rank(arg)
+         print *, "Unsupported array rank: ", rank(arg)
          error stop
       end select
    end subroutine check_arg
@@ -31,11 +38,11 @@ end module example_mod
 program test
    use example_mod
    implicit none
-!   integer :: scalar_int = 42
+   integer :: scalar_int = 42
    integer :: int_array1(3) = [1, 2, 3]
    integer :: int_array2(2,2) = reshape([1,2,3,4], [2,2])
 
-!   call check_arg(scalar_int)    ! Outputs: Received a scalar integer: 42
+   call check_arg(scalar_int)    ! Outputs: Received a scalar integer: 42
    call check_arg(int_array1)    ! Outputs: Received an array of integers (rank: 1)
    call check_arg(int_array2)    ! Outputs: Received an array of integers (rank: 2)
 end program test
