352e00ad33243e7241c36fd5d8272813f51a0d4f
Christopher Albert <albert@tugraz.at>
2026-01-09 18:08:02 +0100

    Simplify runtime array section test

--- tests/errors/array_shape_05.f90
+++ tests/errors/array_shape_05.f90
@@ -3,10 +3,8 @@ program array_shape_05
     integer :: a(2)
     integer :: temp(5)
     integer :: i
-    integer :: j
 
     call system_clock(count=i)
     i = 1 + mod(i, size(temp))
-    j = i
-    a = temp(i:j)
+    a = temp(i:i)
 end program
