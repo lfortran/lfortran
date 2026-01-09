2f40c8dd7ecf47b395262b4f47d1e03da24b816d
Christopher Albert <albert@tugraz.at>
2026-01-09 17:58:03 +0100

    Revert "Add compile-time bounds check to continue compilation test"
    This reverts commit feb7633e9b3524764ab125043e24db33d4b71e01.

--- tests/errors/continue_compilation_1.f90
+++ tests/errors/continue_compilation_1.f90
@@ -209,14 +209,14 @@ program continue_compilation_1
 
 
 
+
+
     ! Use the space above to insert new declarations, and remove the line, so
     ! that the lines below do not shift, to keep the diff minimal.
     !
     ! Only put statements below. If you need to call a function, put it into a
     ! module above.
 
-    a = 1
-    print *, a(10)
     a5 = 8
     b5 = 12_8
     c5 = 2
