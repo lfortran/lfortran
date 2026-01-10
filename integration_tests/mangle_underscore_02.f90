f49c6acf5262059b9c260ae3a5fb00afd0492a6b
Christopher Albert <albert@tugraz.at>
2026-01-10 11:25:39 +0100

    test: add mangle_underscore_02 for externals

---
+++ integration_tests/mangle_underscore_02.f90
@@ -0,0 +1,7 @@
+program mangle_underscore_02
+    implicit none
+
+    call foo()
+    print *, 1
+end program mangle_underscore_02
+
--- integration_tests/mangle_underscore_extern_01.f90
+++
@@ -1,7 +0,0 @@
-program mangle_underscore_extern_01
-    implicit none
-
-    call foo()
-    print *, 1
-end program mangle_underscore_extern_01
-
