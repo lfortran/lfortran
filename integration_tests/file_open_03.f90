c6ee91b6200cbb5b639b5b2411e890222caa958a
Yuvraj Singh <yuvrajsinghgmx@gmail.com>
2026-01-13 11:30:24 +0530

    tests: add and register

---
+++ integration_tests/file_open_03.f90
@@ -0,0 +1,21 @@
+program open_direct_form
+    use iso_fortran_env, only: int32
+    implicit none
+
+    integer(int32) :: x, y
+
+    x = 654321
+
+    open(10, file="data.bin", access="direct", recl=4)
+    write(10, rec=1) x
+    close(10)
+
+    open(10, file="data.bin", access="direct", recl=4)
+    read(10, rec=1) y
+    close(10)
+
+    if (y /= x) then
+        print *, "FAIL: expected", x, "got", y
+        error stop 1
+    end if
+end program
