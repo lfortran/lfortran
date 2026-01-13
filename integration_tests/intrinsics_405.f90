9ecaacdbc0dcd9e74616cb2cef94908b296a8d7b
Yash <yashnagdasub17@gmail.com>
2026-01-13 21:13:57 +0530

    apply suggestions & update tests

--- integration_tests/intrinsics_405.f90
+++ integration_tests/intrinsics_405.f90
@@ -1,6 +1,5 @@
 program intrinsics_405
     implicit none
-
     call print_any("  Hello World  ")
     contains
     subroutine print_any(generic)
@@ -12,31 +11,36 @@ program intrinsics_405
 
             a = trim(generic)
             print *, trim(generic)
+            if (trim(generic) /= "  Hello World  ") error stop
 
             b = adjustl(generic)
             print *, adjustl(generic)
+            if (adjustl(generic) /= "Hello World  ") error stop
 
             c = adjustr(generic)
             print *, adjustr(generic)
+            if (adjustr(generic) /= "    Hello World") error stop
 
             d = len_trim(generic)
             print *, len_trim(generic)
+            if (len_trim(generic) /= 13) error stop
             
             e = len(generic)            
             print *, len(generic)
+            if (len(generic) /= 15) error stop
 
         end select
 
         print *, a
-        if (a /= trim("  Hello World  ")) error stop
+        if (a /= "  Hello World  ") error stop
         print *, b
-        if (b /= adjustl("  Hello World  ")) error stop 
+        if (b /= "Hello World  ") error stop 
         print *, c 
-        if (c /= adjustr("  Hello World  ")) error stop
+        if (c /= "    Hello World") error stop
         print *, d
-        if (d /= len_trim("  Hello World  ")) error stop
-        print * ,e
-        if (e /= len("  Hello World  ")) error stop
+        if (d /= 13) error stop
+        print *, e
+        if (e /= 15) error stop
         
     end subroutine print_any
 
