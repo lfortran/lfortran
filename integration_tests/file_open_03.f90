027eceaae8469d1f58ff186b98aecb258ff0ed00
Yuvraj Singh <yuvrajsinghgmx@gmail.com>
2026-01-13 13:14:24 +0530

    remove tests for direct acess

--- integration_tests/file_open_03.f90
+++ integration_tests/file_open_03.f90
@@ -1,17 +1,17 @@
-program open_direct_form
+program open_stream_form
     use iso_fortran_env, only: int32
     implicit none
 
     integer(int32) :: x, y
 
-    x = 654321
+    x = 123456
 
-    open(10, file="file_open_03_data.bin", access="direct", recl=4)
-    write(10, rec=1) x
+    open(10, file="file_open_03_data.bin", access="stream")
+    write(10) x
     close(10)
 
-    open(10, file="file_open_03_data.bin", access="direct", recl=4)
-    read(10, rec=1) y
+    open(10, file="file_open_03_data.bin", access="stream")
+    read(10) y
     close(10)
 
     if (y /= x) then
