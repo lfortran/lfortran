07fd62cffdc9e63a1454d5764ec6d5c7e4965e5f
Anas <anas23khan083@gmail.com>
2026-01-18 22:50:50 +0530

    fix: added check for result

--- integration_tests/issue_7357.f90
+++ integration_tests/issue_7357.f90
@@ -2,11 +2,11 @@ module stdlib_hashmaps
     implicit none
 
     type open_map_entry_ptr
-        integer(4) :: inmap
+        integer(4) :: inmap 
     end type open_map_entry_ptr
 
     type :: open_hashmap_type
-        type(open_map_entry_ptr), allocatable :: inverse(:)
+        type(open_map_entry_ptr), allocatable  :: inverse(:)
     end type open_hashmap_type
 end module stdlib_hashmaps
 
@@ -21,4 +21,8 @@ program main
     call move_alloc( dummy_inverse, map % inverse )
 
     print *, size(map % inverse)
+
+    if (allocated(dummy_inverse)) error stop
+    if (.not. allocated(map%inverse)) error stop
+    if (size(map%inverse) /= 1) error stop
 end program
