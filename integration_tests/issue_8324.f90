2fcd222d4bad7bf634ea359f240ef1bdcc72198d
Anas <anas23khan083@gmail.com>
2026-01-18 22:46:06 +0530

    fix: added check for result

--- integration_tests/issue_8324.f90
+++ integration_tests/issue_8324.f90
@@ -3,4 +3,6 @@ program arraysection
   integer :: ilist(6,6)
   ilist(1,1:3) = [1,2,4]
   print "(3(1X,I0))", ilist(1,1:3)
+  
+  if (any(ilist(1,1:3) /= [1, 2, 4])) error stop
 end program arraysection
