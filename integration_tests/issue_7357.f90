506dfbf030d17b86b13bd650cc41c33919ef65e7
Anas <anas23khan083@gmail.com>
2026-01-18 18:12:42 +0530

    chore: Add regression test for issue 7357

---
+++ integration_tests/issue_7357.f90
@@ -0,0 +1,24 @@
+module stdlib_hashmaps
+    implicit none
+
+    type open_map_entry_ptr
+        integer(4) :: inmap 
+    end type open_map_entry_ptr
+
+    type :: open_hashmap_type
+        type(open_map_entry_ptr), allocatable  :: inverse(:)
+    end type open_hashmap_type
+end module stdlib_hashmaps
+
+program main
+    use stdlib_hashmaps
+    implicit none
+
+    type(open_hashmap_type) :: map
+    type(open_map_entry_ptr), allocatable :: dummy_inverse(:)
+
+    allocate(dummy_inverse(1))
+    call move_alloc( dummy_inverse, map % inverse )
+
+    print *, size(map % inverse)
+end program
