88678a5dc17cd4070e1fda16bd3a8f70fbf24e28
Christopher Albert <albert@tugraz.at>
2026-01-30 00:07:47 +0100

    fix: convert file unit numbers to i32 for ILP64 compatibility
    With -fdefault-integer-8, unit numbers are i64 but the runtime
    functions expect i32. Add convert_kind calls for flush, rewind,
    and backspace operations to match other file I/O functions.
    
    Fixes #9772

--- src/libasr/codegen/asr_to_llvm.cpp
+++ src/libasr/codegen/asr_to_llvm.cpp
@@ -14321,9 +14321,8 @@ public:
             fn = llvm::Function::Create(function_type,
                     llvm::Function::ExternalLinkage, runtime_func_name, module.get());
         }
-        llvm::Value *unit_val = nullptr;
         this->visit_expr_wrapper(x.m_unit, true);
-        unit_val = tmp;
+        llvm::Value *unit_val = llvm_utils->convert_kind(tmp, llvm::Type::getInt32Ty(context));
         builder->CreateCall(fn, {unit_val});
     }
 
@@ -14339,7 +14338,8 @@ public:
                     llvm::Function::ExternalLinkage, runtime_func_name, module.get());
         }
         this->visit_expr_wrapper(x.m_unit, true);
-        builder->CreateCall(fn, {tmp});
+        llvm::Value *unit_val = llvm_utils->convert_kind(tmp, llvm::Type::getInt32Ty(context));
+        builder->CreateCall(fn, {unit_val});
     }
 
     void visit_FileBackspace(const ASR::FileBackspace_t &x) {
@@ -14354,7 +14354,8 @@ public:
                     llvm::Function::ExternalLinkage, runtime_func_name, module.get());
         }
         this->visit_expr_wrapper(x.m_unit, true);
-        builder->CreateCall(fn, {tmp});
+        llvm::Value *unit_val = llvm_utils->convert_kind(tmp, llvm::Type::getInt32Ty(context));
+        builder->CreateCall(fn, {unit_val});
     }
 
     void visit_FileClose(const ASR::FileClose_t &x) {
