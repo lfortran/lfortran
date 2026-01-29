52092e27bc3618e654267be81c6888cd43c33650
Harshita Kalani <harshitakalani02@gmail.com>
2026-01-29 20:21:41 +0530

    check the value of xrenamed and x

--- integration_tests/modules_64.f90
+++ integration_tests/modules_64.f90
@@ -10,6 +10,8 @@ PROGRAM modules_64
   x = 666                       
   
   if (kind(xrenamed) == kind(x)) error stop
+  if (xrenamed /= -666) error stop
+  if (x /= 666) error stop
   if (kind(xrenamed) /= 8) error stop
   if (kind(x) /= 4) error stop
 END PROGRAM modules_64
