24942a54ca3d292b9096f02658983bde49efecee
HarshilShah1804 <hkshah1978@gmail.com>
2026-01-30 21:20:15 +0530

    fix: fix the unformatted read/write for logical variables

--- src/libasr/codegen/asr_to_llvm.cpp
+++ src/libasr/codegen/asr_to_llvm.cpp
@@ -14594,6 +14594,20 @@ public:
                     llvm::Value* str_data = llvm_utils->get_string_data(ASRUtils::get_string_type(m_values[i]), tmp);
                     args.push_back(str_data);
                     continue;
+                } else if (ASR::is_a<ASR::Logical_t>(*value_type_base)) {
+                    args.push_back(kind_val);
+                    this->visit_expr_wrapper(m_values[i], true);
+                    llvm::Value* logical_val = tmp;
+                    if (logical_val->getType()->isPointerTy()) {
+                        logical_val = llvm_utils->CreateLoad2(llvm::Type::getInt1Ty(context), logical_val);
+                    }
+                    llvm::Value* logical_i32 = builder->CreateZExt(
+                        logical_val, llvm::Type::getInt32Ty(context));
+                    llvm::Value* logical_ptr = llvm_utils->CreateAlloca(
+                        *builder, llvm::Type::getInt32Ty(context));
+                    builder->CreateStore(logical_i32, logical_ptr);
+                    args.push_back(logical_ptr);
+                    continue;
                 } else {
                     args.push_back(kind_val);
                     this->visit_expr_wrapper(m_values[i], true);
