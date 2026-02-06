58e9807ee0e90dd2da4f98c3306b95c35d64a1f5
Christopher Albert <albert@tugraz.at>
2026-02-06 11:13:33 +0100

    chore: remove duplicate tests, add class_105 for #2925
    Remove class_101.f90 (duplicate of merged class_98.f90 for #7952)
    and intrinsics_413.f90 (duplicate of merged string_99.f90 for #3938).
    Add class_105.f90 for #2925 (typed allocation of polymorphic arrays)
    which was lost when class_98.f90 was repurposed.

--- integration_tests/intrinsics_413.f90
+++
@@ -1,9 +0,0 @@
-! Test for https://github.com/lfortran/lfortran/issues/3938
-! bit_size used in character length specification
-program intrinsics_413
-  implicit none
-  character(bit_size(666)):: foo
-  if (bit_size(666) /= 32) error stop
-  if (len(foo) /= 32) error stop
-  print *, bit_size(666), len(foo)
-end program intrinsics_413
