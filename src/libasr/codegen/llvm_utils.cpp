6aaaeacf1503ea90bc772c6073abd756c157029f
Yuvraj Singh <qomfortzone@gmail.com>
2026-02-10 00:20:41 +0530

    Update src/libasr/codegen/llvm_utils.cpp
    Co-authored-by: Ondřej Čertík <ondrej@certik.us>

--- src/libasr/codegen/llvm_utils.cpp
+++ src/libasr/codegen/llvm_utils.cpp
@@ -8795,7 +8795,7 @@ llvm::Value* LLVMUtils::handle_global_nonallocatable_stringArray(Allocator& al, 
             llvm_utils->i8_ptr
         };
         std::vector<llvm::Constant*> type_info_member_values;
-        type_info_member_values.reserve(2); // A type-info object has minimum 2 member.
+        type_info_member_values.reserve(2); // A type-info object has minimum 2 members.
 
         // Intrinsic type ttype number + kind (used as a unique tag)
         type_info_member_values.push_back(llvm::ConstantExpr::getIntToPtr(
