95490d0a3a11bebe6e8e90f3e388b1322641ea83
Jatinagarwal24 <23110147@iitgn.ac.in>
2026-02-17 02:15:05 +0530

    test: add and register test

---
+++ integration_tests/intrinsics_414.f90
@@ -0,0 +1,7 @@
+program main
+    logical(8) :: ar1(3) = logical([.true., .false., .true.], 8)
+    print *, ar1
+    if (ar1(1) .neqv. .true.) error stop
+    if (ar1(2) .neqv. .false.) error stop
+    if (ar1(3) .neqv. .true.) error stop
+end program
