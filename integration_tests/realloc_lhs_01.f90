5e1abfdf2323f78ec187987b9dbbe54322dac108
Christopher Albert <albert@tugraz.at>
2026-02-20 09:03:11 +0100

    Move realloc_lhs test to integration tests
    Move infer_realloc_defaults_01 from reference tests to integration
    tests as realloc_lhs_01, hooked into CMake with --realloc-lhs-arrays.

--- integration_tests/realloc_lhs_01.f90
+++ integration_tests/realloc_lhs_01.f90
@@ -1,6 +1,6 @@
-program infer_realloc_defaults_01
+program realloc_lhs_01
     integer, allocatable :: x(:)
     x = [1, 2, 3]
     x = [4, 5]
     if (size(x) /= 2) error stop
-end program infer_realloc_defaults_01
+end program realloc_lhs_01
