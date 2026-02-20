523109463ab5db42a2b0c297a2cdf805d20b8bd9
Christopher Albert <albert@tugraz.at>
2026-02-20 17:06:57 +0100

    fix: run walrus error test with --infer, remove non-error case
    - Add options = "--infer" to continue_compilation_walrus test to
      suppress extension warnings, leaving only actual errors in output
    - Remove `a := obj` line (struct inference works, not an error)

--- tests/errors/continue_compilation_walrus.f90
+++ tests/errors/continue_compilation_walrus.f90
@@ -1,12 +1,7 @@
 program continue_compilation_walrus
     implicit none
     integer :: x
-    type :: t
-        integer :: n
-    end type
-    type(t) :: obj
     x := 5
-    a := obj
     y := 5
     y := 10
 end program
