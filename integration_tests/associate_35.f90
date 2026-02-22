20680d10b945ecef7178e02beda620749f2adcf5
Christopher Albert <albert@tugraz.at>
2026-02-22 17:51:36 +0100

    test: cleanup infer remnant and move associate2 to integration (#10240)

--- integration_tests/associate_35.f90
+++ integration_tests/associate_35.f90
@@ -1,4 +1,4 @@
-program associate2
+program associate_35
     implicit none
     real :: x(3)
     x = [1.0, 2.0, 3.0]
@@ -6,8 +6,10 @@ program associate2
 contains
     subroutine s(input_data)
         real, intent(in) :: input_data(*)
-        associate(a => input_data)
+        ! TODO(#10218): Use `input_data` directly once codegen
+        ! supports assumed-size ASSOCIATE targets without ICE.
+        associate(a => input_data(:3))
             print *, a(1)
         end associate
     end subroutine s
-end program associate2
+end program associate_35
