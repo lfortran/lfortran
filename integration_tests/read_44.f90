77bd424b431b81b662adbbb148b933bad9a7b3d7
Jainam-not-a-robot <b24cs1071@iitj.ac.in>
2026-02-23 20:53:25 +0000

    changed test

--- integration_tests/read_44.f90
+++ integration_tests/read_44.f90
@@ -2,28 +2,20 @@ program read_44
 
     type :: inner_t
         integer :: a
-        character(20) :: b
+        integer :: b
     end type inner_t
-
     type :: outer_t
         integer :: id
         type(inner_t) :: inner
     end type outer_t
-
     type(outer_t) :: x
-    character(len=20) :: s1, s2, s3
+    character(len=12) :: s
+    s = "001002003"
 
-    ! Simulated CI input (3 separate internal reads)
-    s1 = "1"
-    s2 = "10"
-    s3 = "hello"
-
-    read(s1, *) x%id
-    read(s2, *) x%inner%a
-    read(s3, *) x%inner%b
+    read(s, "(I3,I3,I3)") x
 
     if (x%id /= 1) error stop 1
-    if (x%inner%a /= 10) error stop 2
-    if (trim(x%inner%b) /= "hello") error stop 3
+    if (x%inner%a /= 2) error stop 2
+    if (x%inner%b /= 3) error stop 3
 
 end program read_44
