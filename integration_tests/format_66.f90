b5e25070b42416c277159584abab61f135cde487
Harshita Kalani <harshitakalani02@gmail.com>
2026-02-25 00:22:26 +0530

    test: update test references

--- integration_tests/format_66.f90
+++ integration_tests/format_66.f90
@@ -2,7 +2,6 @@ program format_66
     implicit none
     integer :: io, n
     character(len=80) :: line
-
     open(newunit=io, file="format_66_output.txt", status="replace", action="write")
 
     write(io, '("line1"/)')
@@ -10,45 +9,55 @@ program format_66
     write(io, '("line3"///)')
     write(io, '(i1,/,i1)') 4, 5
     write(io, '("end")')
-
     close(io)
-
     open(newunit=io, file="format_66_output.txt", status="old", action="read")
 
     read(io, '(A)') line
+    print *, line
     if (trim(line) /= "line1") error stop
 
     read(io, '(A)') line
+    print *, line
     if (trim(line) /= "") error stop
 
     read(io, '(A)') line
+    print *, line
     if (trim(line) /= "line2") error stop
 
     read(io, '(A)') line
+    print *, line
     if (trim(line) /= "") error stop
 
     read(io, '(A)') line
+    print *, line
     if (trim(line) /= "") error stop
 
     read(io, '(A)') line
+    print *, line
     if (trim(line) /= "line3") error stop
 
     read(io, '(A)') line
+    print *, line
     if (trim(line) /= "") error stop
 
     read(io, '(A)') line
+    print *, line
     if (trim(line) /= "") error stop
 
     read(io, '(A)') line
+    print *, line
     if (trim(line) /= "") error stop
 
     read(io, '(A)') line
+    print *, line
     if (trim(line) /= "4") error stop
 
     read(io, '(A)') line
+    print *, line
     if (trim(line) /= "5") error stop
 
     read(io, '(A)') line
+    print *, line
     if (trim(line) /= "end") error stop
 
     close(io, status="delete")
