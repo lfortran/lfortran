be6b32815aafd9f36807976859b721e7d29446bf
Harshita Kalani <harshitakalani02@gmail.com>
2026-03-01 16:49:29 +0530

    test: add and register test

---
+++ integration_tests/character_24.f90
@@ -0,0 +1,23 @@
+module halvestring_mod
+contains
+    pure function half(s1) result(s2)
+        character(*), intent(in) :: s1
+        character(len(s1)/2) :: s2
+        s2 = s1
+    end function half
+end module halvestring_mod
+
+program character_24
+use halvestring_mod, only: half
+call f()
+
+contains
+
+    subroutine f()
+    character(*), parameter :: spec_str = 'abcdeedcba'
+    character(len(half(spec_str))) :: spec_stringb
+    if (len(spec_stringb) /= 5) error stop
+    print *, len(spec_stringb)
+    end subroutine
+
+end program
