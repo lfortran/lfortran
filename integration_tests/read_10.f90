753edc25b81c30480277d17e31f3207af2b75a4b
Christopher Albert <albert@tugraz.at>
2026-03-02 10:22:58 +0100

    integration_tests: use newunit for scratch I/O parallel safety

--- integration_tests/read_10.f90
+++ integration_tests/read_10.f90
@@ -1,14 +1,14 @@
 program read_10
     implicit none
     integer :: vals(3)
-    integer :: i
+    integer :: i, unit_no
 
-    open(10, status='scratch')
-    write(10, *) 10, 20, 30
-    rewind(10)
+    open(newunit=unit_no, status='scratch')
+    write(unit_no, *) 10, 20, 30
+    rewind(unit_no)
 
-    read(10, *) (vals(i), i=1, 3)
-    close(10)
+    read(unit_no, *) (vals(i), i=1, 3)
+    close(unit_no)
 
     if (vals(1) /= 10) error stop "vals(1) should be 10"
     if (vals(2) /= 20) error stop "vals(2) should be 20"
