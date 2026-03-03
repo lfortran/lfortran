d4334c8711a79fac6b296f4665894d37d48ca78e
Jainam-not-a-robot <b24cs1071@iitj.ac.in>
2026-03-03 16:50:52 +0000

    added runtime support for read of array of strings

---
+++ integration_tests/string_104.f90
@@ -0,0 +1,23 @@
+program t
+  implicit none
+
+  character(len=10) :: s
+  character(5), allocatable :: a(:)
+  integer :: ios
+
+  allocate(a(1))
+
+  s = "hello"
+
+  read(s, *, iostat=ios) a
+  if (ios /= 0) then
+     error stop "READ failed"
+  end if
+
+  if (a(1) /= "hello") then
+     error stop "Wrong value in a(1)"
+  end if
+
+  print *, "PASS"
+
+end program
