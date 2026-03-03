91542e8fffdf9d3f189ebdf6cca002e752d93677
Ondřej Čertík <ondrej@certik.us>
2026-03-03 12:09:09 -0700

    118 -> 119

--- integration_tests/derived_types_119.f90
+++ integration_tests/derived_types_119.f90
@@ -1,6 +1,6 @@
 ! Test that a generic interface (custom constructor) re-exported through
 ! an intermediate module with default private access remains accessible.
-module derived_types_118_origin
+module derived_types_119_origin
    implicit none
    private
 
@@ -29,17 +29,17 @@ contains
       end if
    end function new_merge_config
 
-end module derived_types_118_origin
+end module derived_types_119_origin
 
-module derived_types_118_reexport
-   use derived_types_118_origin
+module derived_types_119_reexport
+   use derived_types_119_origin
    implicit none
    private
    public :: merge_config
-end module derived_types_118_reexport
+end module derived_types_119_reexport
 
-module derived_types_118_consumer
-   use derived_types_118_reexport
+module derived_types_119_consumer
+   use derived_types_119_reexport
    implicit none
    private
    public :: do_stuff
@@ -50,13 +50,13 @@ contains
       cfg = merge_config(keyval="overwrite")
       val = cfg%keyval
    end subroutine do_stuff
-end module derived_types_118_consumer
+end module derived_types_119_consumer
 
-program derived_types_118
-   use derived_types_118_consumer, only : do_stuff
+program derived_types_119
+   use derived_types_119_consumer, only : do_stuff
    implicit none
    integer :: val
    call do_stuff(val)
    if (val /= 2) error stop
    print *, val
-end program derived_types_118
+end program derived_types_119
