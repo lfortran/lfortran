68162f3c5b06350f84393d0f1767335dbc346b2d
Jainam-not-a-robot <b24cs1071@iitj.ac.in>
2026-03-10 04:58:47 +0000

    changed test program and module name

--- integration_tests/data_implied_do_07.f90
+++ integration_tests/data_implied_do_07.f90
@@ -1,14 +1,14 @@
-module mod
+module data_implied_do_07_mod
     implicit none
     integer :: i, iarx(3)
     data(iarx(i), i=1,3) / 3*-2 /
-end module mod
+end module data_implied_do_07_mod
 
-program main
+program data_implied_do_07
     use mod
     implicit none
     print *, iarx
     if (iarx(1) /= -2) error stop
     if (iarx(2) /= -2) error stop
     if (iarx(3) /= -2) error stop
-end program main
+end program data_implied_do_07
