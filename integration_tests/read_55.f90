1e8bd424c406b4e2fbc8420fd22a7c61f78be931
Jainam-not-a-robot <b24cs1071@iitj.ac.in>
2026-03-12 20:53:53 +0530

    add regression test

--- integration_tests/read_55.f90
+++ integration_tests/read_55.f90
@@ -9,13 +9,17 @@ program write_and_read_temp
 
     write(10) "HELLOWORLD"
     close(10)
+
     open(unit=10, file=filename, status='old', access="stream", form="unformatted")
-    major = 2   ! Try changing this to 1
+
+    major = 1   ! Try changing this to 1
 
     read(10, iostat=stat) buf(1:merge(4, 2, major > 1))
 
-    print *, "Status code:", stat
-    print *, "From file:", trim(buf)
+    if (stat /= 0) then
+        print *, "Read error, iostat =", stat
+        stop 1
+    end if
 
     close(10, status="delete")
 end program write_and_read_temp
