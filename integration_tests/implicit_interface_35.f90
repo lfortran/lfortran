542005d0911ee16f908992bf27f6bb5efc4f2144
Jainam-not-a-robot <b24cs1071@iitj.ac.in>
2026-03-16 15:11:55 +0530

    fixed ice and semantic error for unbounded array and fixed size array

---
+++ integration_tests/implicit_interface_35.f90
@@ -0,0 +1,50 @@
+program implicit_interface_35
+  implicit none
+
+! Compiling with --implicit-interface previously caused ICE / LLVM failure
+
+  external :: x
+  real :: x
+
+  integer :: i
+  real :: a(10)
+
+  a = [(i + 0.5, i = 1, 10)]
+
+  call sub_assumed(x, a)
+  call sub_fixed(x, a)
+
+end program implicit_deallocate_35
+
+subroutine sub_assumed(F, Y)
+
+  real F, FB
+  real Y(*)   ! Previously caused ICE
+
+  FB = F(Y)
+
+  if (abs(FB - 60.0) > 0.0001) then
+    error stop "assumed-size array argument failed"
+  end if
+
+end subroutine
+
+subroutine sub_fixed(F, Y)
+
+  real F, FB
+  real Y(10)  ! Previously caused LLVM code generation error
+ 
+  FB = F(Y)
+
+  if (abs(FB - 60.0) > 0.0001) then
+    error stop "fixed-size array argument failed"
+  end if
+
+end subroutine
+
+real function x(a)
+  real :: a(10)
+
+  x = sum(a)
+
+end function
