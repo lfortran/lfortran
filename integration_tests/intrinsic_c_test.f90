b03791c2a7d1bc6cc138e88e64fc0901084a66ed
venkata597 <rvsraghava5@gmail.com>
2026-03-28 20:21:26 +0530

    add intrinsic attribute to existing tests and enable fortran label in CMake build file

--- integration_tests/intrinsic_c_test.f90
+++
@@ -1,6 +0,0 @@
-program test_c_binding
-  use, intrinsic :: iso_c_binding, only: c_int
-  implicit none
-  integer(c_int) :: i = 42
-  print *, i
-end program
--- integration_tests/intrinsic_fortran_env_test.f90
+++
@@ -1,6 +0,0 @@
-program test_fortran_env
-  use, intrinsic :: iso_fortran_env, only: real64
-  implicit none
-  real(real64) :: x = 3.14
-  print *, x
-end program
