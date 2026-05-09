cba35eaa6186a3899cfb6571baf80a4570c247ce
Jatinagarwal24 <23110147@iitgn.ac.in>
2026-05-09 09:47:38 +0530

    test: added test cases with a step value and loop counting backwards.

--- integration_tests/implied_do_loops38.f90
+++ integration_tests/implied_do_loops38.f90
@@ -19,4 +19,32 @@ program implied_do_loops38
     i = -42
     inquire(iolength=iol) (data(i), i = 1, size(data))
     if (i /= size(data) + 1) error stop
+
+    open(10, status="scratch", form="formatted")
+    write(10, "(*(i5))") (data(i), i = 1, size(data), 2)
+
+    rewind(10)
+    i = -42
+    read(10, "(*(i5))") (data(i), i = 1, size(data), 2)
+    if (i /= size(data) + 2) error stop
+
+    close(10)
+
+    i = -42
+    inquire(iolength=iol) (data(i), i = 1, size(data), 2)
+    if (i /= size(data) + 2) error stop
+
+    open(10, status="scratch", form="formatted")
+    write(10, "(*(i5))") (data(i), i = size(data), 1, -2)
+
+    rewind(10)
+    i = -42
+    read(10, "(*(i5))") (data(i), i = size(data), 1, -2)
+    if (i /= -1) error stop
+
+    close(10)
+
+    i = -42
+    inquire(iolength=iol) (data(i), i = size(data), 1, -2)
+    if (i /= -1) error stop
 end program implied_do_loops38
