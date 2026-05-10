0b2d418325b094c15a1fb6733c060d8df90b8360
Jatinagarwal24 <23110147@iitgn.ac.in>
2026-05-10 10:06:35 +0530

    added checks to ensure the bounds are actually correct

--- integration_tests/nested_27.f90
+++ integration_tests/nested_27.f90
@@ -3,12 +3,16 @@ contains
    subroutine outer(x)
       real(8), allocatable, intent(in) :: x(:,:)
       print *, 'outer: lbound(x,1) is ', lbound(x,1)
+      if (lbound(x,1) /= -4) error stop
       print *, 'outer: ubound(x,1) is ', ubound(x,1)
+      if (ubound(x,1) /= 44) error stop
       call inner()
    contains
       subroutine inner()
          print *, 'inner: lbound(x,1) is ', lbound(x,1)
+         if (lbound(x,1) /= -4) error stop
          print *, 'inner: ubound(x,1) is ', ubound(x,1)
+         if (ubound(x,1) /= 44) error stop
       end subroutine inner      
    end subroutine outer
 end module nested
