25b0eb6291fb33a05380e358709ad668279bac76
Jatinagarwal24 <23110147@iitgn.ac.in>
2026-05-13 19:40:46 +0530

    test: updated test file

--- integration_tests/nested_vars_06.f90
+++ integration_tests/nested_vars_06.f90
@@ -1,26 +1,25 @@
 program nested_vars_06
-    implicit none
-    real :: xt
+    real xt
+
     xt = 5.0
     call b1(xt)
+
     if (abs(xt - 10.0) > 1e-5) error stop 1
+end program
 
-contains
+subroutine b1(xt)
+    real xt
+    real bvalu
+    real x, h
+    real g8
 
-    subroutine b1(xt)
-        real :: xt
-        real :: x, h
-        real :: g8
+    g8(x, h) = bvalu(xt)
 
-        ! Statement function with function call
-        g8(x, h) = bvalu(xt)
+    xt = g8(1.2, 3.4)
+end subroutine
 
-        xt = g8(1.2, 3.4)
-    end subroutine b1
+real function bvalu(x)
+    real x
 
-    real function bvalu(xt)
-        real :: xt
-        bvalu = xt * 2.0
-    end function bvalu
-
-end program nested_vars_06
+    bvalu = x * 2.0
+end function
