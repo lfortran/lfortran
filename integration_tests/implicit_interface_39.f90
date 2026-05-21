aa99d5f708183ad831692ae95882f327ca22d90b
jinangshah21 <jinang2110@gmail.com>
2026-05-21 12:05:10 +0530

    fix: handle internal llvm function linkage

---
+++ integration_tests/implicit_interface_39.f90
@@ -0,0 +1,5 @@
+program implicit_interface_39
+    integer implicit_interface_39b, implicit_interface_39c
+    if (implicit_interface_39b(11) .ne. 1) error stop
+    if (implicit_interface_39c(10) .ne. 0) error stop
+end program
---
+++ integration_tests/implicit_interface_39c.f90
@@ -0,0 +1,5 @@
+      integer function implicit_interface_39c(i)
+      integer i
+      implicit_interface_39c = mod(i, 2)
+      return
+      end function
