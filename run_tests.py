e8db14a70665eefaa16bb71f5e499eda4f044cc0
Jainam-not-a-robot <b24cs1071@iitj.ac.in>
2026-05-21 11:29:39 +0530

    ignore segfault while running tests.toml

--- run_tests.py
+++ run_tests.py
@@ -746,9 +746,9 @@ def single_test(test: Dict, verbose: bool, no_llvm: bool, skip_run_with_dbg: boo
         if pass_ is not None:
             pass_name = pass_.split(",")[0].strip()
             cmd = (
-                "lfortran --dump-all-passes-fortran --no-color {infile} "
+                "(lfortran --dump-all-passes-fortran --no-color {infile} "
                 + extra_args_fortran +
-                " 2>/dev/null; "
+                " >/dev/null 2>/dev/null || true); "
                 + "cat pass_fortran_*_" + pass_name + ".f90; "
                 + "rm -f pass_fortran_*_*.f90"
             )
