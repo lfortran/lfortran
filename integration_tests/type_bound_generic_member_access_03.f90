f5429aeeaeca8fce32d8ea4d0d61f93c199a4276
Jatinagarwal24 <23110147@iitgn.ac.in>
2026-05-25 21:13:26 +0530

    added a test case and moved my_sub inside the program to use that

--- integration_tests/type_bound_generic_member_access_03.f90
+++ integration_tests/type_bound_generic_member_access_03.f90
@@ -4,7 +4,8 @@ module type_bound_generic_member_access_03_m
   type :: runner
   contains
     procedure :: run_a1
-    generic :: run => run_a1
+    procedure :: run_a2
+    generic :: run => run_a1, run_a2
   end type
 contains
   subroutine run_a1(this, a1, f)
@@ -13,21 +14,35 @@ contains
     procedure() :: f
     call f()
   end subroutine
+
+  subroutine run_a2(this, a1, f2, arg)
+    class(runner), intent(inout) :: this
+    class(*), intent(in) :: a1
+    procedure() :: f2
+    integer, intent(in) :: arg
+    call f2(arg)
+  end subroutine
 end module
 
 program type_bound_generic_member_access_03
   use type_bound_generic_member_access_03_m
   implicit none
   type(runner) :: br
-  interface
-    subroutine my_sub()
-    end subroutine
-  end interface
+  logical :: called_with_arg = .false.
+
   call br%run(1, my_sub)
   if (.not. called) error stop 1
-end program
 
-subroutine my_sub()
-  use type_bound_generic_member_access_03_m, only: called
-  called = .true.
-end subroutine
+  call br%run(1, my_sub_with_arg, 42)
+  if (.not. called_with_arg) error stop 2
+
+contains
+  subroutine my_sub_with_arg(x)
+    integer, intent(in) :: x
+    if (x == 42) called_with_arg = .true.
+  end subroutine
+  subroutine my_sub()
+    use type_bound_generic_member_access_03_m, only: called
+    called = .true.
+  end subroutine
+end program
