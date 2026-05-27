1efa88dac25b59896de3405fee58c617425b6cfe
Jainam-not-a-robot <b24cs1071@iitj.ac.in>
2026-05-27 21:14:38 +0530

    remove workaround for optional args

--- src/libasr/codegen/asr_to_llvm.cpp
+++ src/libasr/codegen/asr_to_llvm.cpp
@@ -25152,10 +25152,8 @@ public:
             if (ASR::is_a<ASR::Function_t>(*symbol_get_past_external(x.m_name))) {
                 ASR::Function_t* subrout_called = ASR::down_cast<ASR::Function_t>(symbol_get_past_external(x.m_name));
                 for (size_t i = 0; i < subrout_called->n_args; i++) {
-                    if (i >= x.n_args) continue;
                     ASR::expr_t* expected_arg = subrout_called->m_args[i];
                     ASR::expr_t* passed_arg = x.m_args[i].m_value;
-                    if (!passed_arg) continue;
                     ASR::ttype_t* expected_arg_type = ASRUtils::expr_type(expected_arg);
                     ASR::ttype_t* passed_arg_type = ASRUtils::expr_type(passed_arg);
                     if (ASR::is_a<ASR::ArrayItem_t>(*passed_arg)) {
