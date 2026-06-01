d6ec26f80d64f56edaf0c98c6ce5b44e9a342f89
Jatinagarwal24 <23110147@iitgn.ac.in>
2026-06-01 18:33:53 +0530

    fix : ICE for internal read from equivalenced character array

--- src/libasr/pass/pass_utils.cpp
+++ src/libasr/pass/pass_utils.cpp
@@ -1643,23 +1643,28 @@ namespace LCompilers {
             bool perform_cast, ASR::cast_kindType cast_kind, ASR::ttype_t* casted_type) {
             const Location& loc = arr_var->base.loc;
             ASRUtils::ASRBuilder b(al, loc);
-            ASR::dimension_t* m_dims;
-            int n_dims = ASRUtils::extract_dimensions_from_ttype(ASRUtils::expr_type(arr_var), m_dims);
+            int arr_n_dims = ASRUtils::extract_n_dims_from_ttype(ASRUtils::expr_type(arr_var));
+            ASR::dimension_t* x_dims;
+            int x_n_dims = ASRUtils::extract_dimensions_from_ttype(x->m_type, x_dims);
+            LCOMPILERS_ASSERT(arr_n_dims == x_n_dims);
+            int n_dims = x_n_dims;
             Vec<int> strides;
             strides.resize(al, n_dims);
             // compute stride vars for each dimension in column major order
             strides.p[0] = 1;
             for( int i = 1; i < n_dims; i++ ) {
-                int64_t dim_size = -1;
-                ASRUtils::extract_value(ASRUtils::expr_value(m_dims[i - 1].m_length), dim_size);
+                int64_t dim_size = 1;
+                LCOMPILERS_ASSERT(ASRUtils::extract_value(x_dims[i - 1].m_length, dim_size));
                 strides.p[i] = strides[i - 1] * dim_size;
             }
 
             Vec<int64_t> lower_bounds_values;
             lower_bounds_values.resize(al, n_dims);
             for( int i = 0; i < n_dims; i++ ) {
-                int64_t dim_size = -1;
-                ASRUtils::extract_value(ASRUtils::expr_value(m_dims[i].m_start), dim_size);
+                int64_t dim_size = 1;
+                if( x_dims[i].m_start != nullptr ) {
+                    ASRUtils::extract_value(x_dims[i].m_start, dim_size);
+                }
                 lower_bounds_values.p[i] = dim_size;
             }
 
