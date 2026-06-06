357c6b8db04da1a9703b016650bb4e10a1bf52d7
srinjoy933 <srinjoymukhopadhyay@gmail.com>
2026-06-07 01:55:06 +0530

    test-add

---
+++ integration_tests/string_117.f90
@@ -0,0 +1,13 @@
+﻿program string_117
+    implicit none
+    character(20) :: i(2)
+    character(20) :: str
+
+    str = "LFortran"
+    i(1)  = str
+    print *, i(1)
+
+    str = "whatever"
+    print *, str
+    print *, i(1)
+end program string_117
