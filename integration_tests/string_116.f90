e66a66f651f7ebd3d1bb169a2127ebc5b6d8162e
Jatinagarwal24 <23110147@iitgn.ac.in>
2026-06-06 11:01:57 +0530

    test: added some more test cases

--- integration_tests/string_116.f90
+++ integration_tests/string_116.f90
@@ -11,6 +11,7 @@ module string_116_mod
     contains
         procedure :: fill_buffer
         procedure :: get_greeting
+        procedure :: test_modify_mid_sub
     end type buffer_type
 
 contains
@@ -29,11 +30,41 @@ contains
         res = "greetings!"
     end function get_greeting
 
+    ! Test: modifying buf_size mid-subroutine should NOT change len(buf)
+    ! The character length is frozen at subroutine entry.
+    subroutine test_modify_mid_sub(me)
+        class(buffer_type), intent(inout) :: me
+        character(len=me%buf_size) :: buf
+        integer :: original_size
+        original_size = me%buf_size
+        buf = "hello"
+        if (len(buf) /= original_size) error stop
+        me%buf_size = 3
+        ! len(buf) must still reflect the original value, not 3
+        if (len(buf) /= original_size) error stop
+        me%buf_size = original_size  ! restore
+    end subroutine test_modify_mid_sub
+
 end module string_116_mod
+
+! Test: character(len=n) where n is a normal variable (not a struct member)
+subroutine test_normal_variable(n)
+    implicit none
+    integer, intent(in) :: n
+    character(len=n) :: buf
+    buf = "hello world"
+    if (len(buf) /= n) error stop
+end subroutine test_normal_variable
 
 program string_116
     use string_116_mod
     implicit none
+
+    interface
+        subroutine test_normal_variable(n)
+            integer, intent(in) :: n
+        end subroutine test_normal_variable
+    end interface
 
     type(buffer_type) :: obj
     type(buffer_type) :: obj2
@@ -49,6 +80,18 @@ program string_116
     ! Test 3: function returning character(len=me%buf_size)
     result = obj%get_greeting()
     if (result /= "greetings!") error stop
+
+    ! Test 4: modifying buf_size mid-subroutine (length should be frozen)
+    obj%buf_size = 10
+    call obj%test_modify_mid_sub()
+
+    ! Test 5: after modification, next call should use new value
+    obj2%buf_size = 7
+    call obj2%fill_buffer()
+
+    ! Test 6: character(len=n) with a normal variable
+    call test_normal_variable(10)
+    call test_normal_variable(5)
 
     print *, "All tests passed."
 end program string_116
