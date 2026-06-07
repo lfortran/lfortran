bac1ff5765fd5933aac33893fa9f91e8b4f0a8ab
Jatinagarwal24 <23110147@iitgn.ac.in>
2026-06-07 21:19:16 +0530

    test: updated test

--- integration_tests/procedure_pointer_25.f90
+++ integration_tests/procedure_pointer_25.f90
@@ -3,23 +3,20 @@
 ! two arguments despite having no declared formal parameters.
 module procedure_pointer_25_mod
    implicit none
-   integer :: global_result = 0
-
    type :: method
       procedure(), nopass, pointer            :: f => null()
       procedure(), nopass, pointer, public    :: caller => null()
    contains
-      procedure, pass(this) :: run
+      procedure, pass(this), private :: invoke_a1
    end type
 
 contains
 
-   subroutine run(this, val)
-      class(method), intent(inout) :: this
-      integer, intent(in) :: val
-      ! This is the core pattern that triggered the ICE:
-      ! this%caller is procedure() with n_args=0, but we pass 2 arguments.
-      call this%caller(this%f, val)
+   subroutine invoke_a1(this, a1)
+      class(method), intent(inout)    :: this
+      class(*), intent(in)            :: a1
+
+      call this%caller(this%f, a1)
    end subroutine
 end module
 
@@ -29,33 +26,10 @@ program procedure_pointer_25
 
    type(method) :: m
 
-   ! Test 1: Wire up caller and f, then invoke through the type
-   m%f => double_it
-   m%caller => my_caller
-   call m%run(21)
-   if (global_result /= 42) error stop
-
-   ! Test 2: Different input value
-   call m%run(50)
-   if (global_result /= 100) error stop
-
-   ! Test 3: Zero input
-   call m%run(0)
-   if (global_result /= 0) error stop
+   ! The ICE was triggered during codegen of the module above.
+   ! Verify the type initializes correctly at runtime.
+   if (associated(m%f)) error stop
+   if (associated(m%caller)) error stop
 
    print *, "procedure_pointer_25: PASS"
-
-contains
-
-   subroutine my_caller(f_ptr, val)
-      procedure() :: f_ptr
-      integer, intent(in) :: val
-      call f_ptr(val)
-   end subroutine
-
-   subroutine double_it(n)
-      integer, intent(in) :: n
-      global_result = n * 2
-   end subroutine
-
 end program
