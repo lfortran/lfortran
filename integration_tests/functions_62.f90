eb93b851124bf9694a1bfd0c842a75f77920d5ca
Jatinagarwal24 <23110147@iitgn.ac.in>
2026-06-16 20:43:07 +0530

    test: corrected test added

--- integration_tests/functions_62.f90
+++ integration_tests/functions_62.f90
@@ -1,8 +1,17 @@
-﻿program main
+program main
+    implicit none
+    integer, allocatable :: val
+
+    val = value()
+    if (val /= 42) error stop
+
 contains
+
   integer function value()
     allocatable :: value
+
     allocate(value)
     value = 42
   end function value
+
 end program main
