be6e6bb17d8491dfcee2a295774f8d0f7383af68
srinjoy933 <srinjoymukhopadhyay@gmail.com>
2026-06-20 03:55:48 +0530

    Test updated

--- integration_tests/assumed_rank_11.f90
+++ integration_tests/assumed_rank_11.f90
@@ -7,6 +7,12 @@
 contains
   subroutine take_pointer_rank(x)
     integer, pointer, intent(in) :: x(..)
-    
+    call check_rank(x)
   end subroutine take_pointer_rank
+
+  subroutine check_rank(y)
+    integer, intent(in) :: y(..)
+
+    if (rank(y) /= 1) error stop
+  end subroutine check_rank
 end program main
