a9345f7bae5e02766900ecd7637c949350965a2e
srinjoy933 <srinjoymukhopadhyay@gmail.com>
2026-06-20 03:02:23 +0530

    Suggested changes

---
+++ integration_tests/assumed_rank_10.f90
@@ -0,0 +1,15 @@
+﻿program main
+  implicit none
+  integer, target :: a(2)
+
+  call take_pointer_rank(a)
+  print *, "ok"
+contains
+  subroutine take_pointer_rank(x)
+    integer, pointer, intent(in) :: x(..)
+
+    ! Accessing rank forces the backend to read the descriptor.
+    ! This triggers the ICE on main, but passes with the PR.
+    if (rank(x) /= 1) error stop
+  end subroutine take_pointer_rank
+end program main
