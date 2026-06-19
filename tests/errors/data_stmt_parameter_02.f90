fbbe833296e91abb1958fca28ff6d8368cb8149d
ambikeesshh <ambikesh2206@gmail.com>
2026-06-19 20:06:57 +0530

    test: move data-stmt-parameter cases into continue_compilation_3

--- tests/errors/data_stmt_parameter_02.f90
+++
@@ -1,6 +0,0 @@
-program data_stmt_parameter_02
-    implicit none
-    integer, parameter :: k(3) = [1, 2, 3]
-    integer :: i
-    data (k(i), i=1,3) / 4, 5, 6 /
-end program data_stmt_parameter_02
