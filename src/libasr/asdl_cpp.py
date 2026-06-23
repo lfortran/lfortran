529fddd7d90f53b9cfd2e8f28dc5701d01a7a4a8
srinjoy933 <srinjoymukhopadhyay@gmail.com>
2026-06-24 00:38:34 +0530

    Fix

--- src/libasr/asdl_cpp.py
+++ src/libasr/asdl_cpp.py
@@ -3049,6 +3049,10 @@ template <class T, class U>
 static inline T* down_cast(const U *f)
 {
     LCOMPILERS_ASSERT(f != nullptr);
+    if (!is_a<T>(*f)) {
+        printf("\n\n>>> ICE FAILED NODE: %%d IN: %%s <<<\n\n", (int)f->type, __PRETTY_FUNCTION__);
+        fflush(stdout);
+    }
     LCOMPILERS_ASSERT(is_a<T>(*f));
     return (T*)f;
 }
