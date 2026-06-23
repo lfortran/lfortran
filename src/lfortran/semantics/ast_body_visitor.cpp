10d61169c558e317ad6259d74cd3703fa1e518bb
Jainam Shah <b24cs1071@iitj.ac.in>
2026-06-24 05:28:29 +0530

    Support memory access across different images using prif_put calls (#11769)
    * support assignment for coarrayref in llvm and asr
    
    * add transform_stmts to handle prif_put
    
    * add test
    
    * refactor transform stmts
    
    * add prif_initial_team_index for image index
    
    * add assert
    
    * add sync all in prif_put test
    
    * update ref
    
    * push non coarray assignments

--- src/lfortran/semantics/ast_body_visitor.cpp
+++ src/lfortran/semantics/ast_body_visitor.cpp
@@ -6403,7 +6403,8 @@ public:
             target->type == ASR::exprType::StructInstanceMember ||
             target->type == ASR::exprType::UnionInstanceMember ||
             target->type == ASR::exprType::ComplexRe ||
-            target->type == ASR::exprType::ComplexIm
+            target->type == ASR::exprType::ComplexIm ||
+            target->type == ASR::exprType::CoarrayRef
         );
 
         is_valid_lhs = is_valid_lhs || (target->type == ASR::exprType::Cast &&
@@ -6448,7 +6449,8 @@ public:
                 target->type == ASR::exprType::StructInstanceMember ||
                 target->type == ASR::exprType::UnionInstanceMember ||
                 (target->type == ASR::exprType::Cast &&
-                 ASR::down_cast<ASR::Cast_t>(target)->m_kind == ASR::cast_kindType::ClassToIntrinsic)
+                 ASR::down_cast<ASR::Cast_t>(target)->m_kind == ASR::cast_kindType::ClassToIntrinsic) ||
+                target->type == ASR::exprType::CoarrayRef
             );
             if (lhs_supports_implicit_cast &&
                 !ASRUtils::check_equal_type(target_type, value_type, target, value)) {
