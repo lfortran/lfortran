358413b5128fff4440f4f18107b08070dc5b17ba
Madhav <mlnmadhavbhardwaj@gmail.com>
2026-06-24 07:22:24 +0000

    Make character co_max/co_min tests robust for 2 or more images

--- tests/coarrays_11.f90
+++ tests/coarrays_11.f90
@@ -1,10 +1,9 @@
 program coarrays_11
-    character(3) :: s
+    character(3) :: s = "abc"
 
-    if (this_image() == 1) s = "abc"
     if (this_image() == 2) s = "xyz"
 
     call co_max(s)
 
-    if (s /= "xyz") error stop ! run with 2 images
+    if (s /= "xyz") error stop ! run with 2 or more images
 end program coarrays_11
--- tests/coarrays_13.f90
+++ tests/coarrays_13.f90
@@ -1,10 +1,9 @@
 program coarrays_13
-    character(3) :: s
+    character(3) :: s = "xyz"
 
-    if (this_image() == 1) s = "xyz"
     if (this_image() == 2) s = "abc"
 
     call co_min(s)
 
-    if (s /= "abc") error stop ! run with 2 images
+    if (s /= "abc") error stop ! run with 2 or more images
 end program coarrays_13
