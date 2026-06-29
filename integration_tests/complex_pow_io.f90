471c12de7e9d657c77af104d2a35047d67948331
srinjoy933 <srinjoymukhopadhyay@gmail.com>
2026-06-30 01:50:38 +0530

    Change test

--- integration_tests/complex_pow_io.f90
+++ integration_tests/complex_pow_io.f90
@@ -3,11 +3,12 @@ program complex_pow_io
     complex :: z32, base32, res32
     complex(8) :: z64, base64, res64
 
+
     z32 = (3.0, 4.0)
-    if (z32**2 /= (-7.0, 24.0)) error stop 1
+    if (abs(z32**2 - (-7.0, 24.0)) > 1e-4) error stop 1
 
     z64 = (3.0d0, 4.0d0)
-    if (z64**2 /= (-7.0d0, 24.0d0)) error stop 2
+    if (abs(z64**2 - (-7.0d0, 24.0d0)) > 1e-10) error stop 2
 
     base32 = (0.0, 0.0)
     res32 = base32 ** 0.0
@@ -16,6 +17,7 @@ program complex_pow_io
     base64 = (0.0d0, 0.0d0)
     res64 = base64 ** 0.0d0
     res64 = base64 ** (-2.0d0)
+
 
     if (real(res32) == 999.0) print *, "dummy"
 
