570e8581cc73bacb85d238a754ef54dc86c377ae
srinjoy933 <srinjoymukhopadhyay@gmail.com>
2026-07-01 17:19:36 +0530

    suggested changes

--- integration_tests/array_128.f90
+++ integration_tests/array_128.f90
@@ -1,15 +1,16 @@
-﻿program matmul_param_bounds
-  implicit none
+﻿program array_128
+    implicit none
+    integer, parameter :: a_lo = -1
+    real :: ap(a_lo:a_lo, 1:1)
+    real :: bp(1, 1)
+    real :: cp(1, 1)
 
-  integer, parameter :: a_lo = -1
+    if (a_lo < 1) then
+        error stop "Error: Array bounds are invalid for matmul."
+    end if
 
-  real :: ap(a_lo:a_lo, 1:1)
-  real :: bp(1, 1)
-  real :: cp(1, 1)
-
-  ap = 1.0
-  bp = 2.0
-  cp = matmul(ap, bp)
-
-  print *, cp(1, 1)
-end program matmul_param_bounds
+    ap = 1.0
+    bp = 2.0
+    cp = matmul(ap, bp)
+    print *, cp(1, 1)
+end program array_128
