c1bbe0ce09ac0f62609cb56ce3bc5c78358e872c
srinjoy933 <srinjoymukhopadhyay@gmail.com>
2026-07-01 18:32:19 +0530

    add changed test with error stop

--- integration_tests/array_128.f90
+++ integration_tests/array_128.f90
@@ -1,16 +1,19 @@
 ﻿program array_128
     implicit none
+
     integer, parameter :: a_lo = -1
+
     real :: ap(a_lo:a_lo, 1:1)
     real :: bp(1, 1)
     real :: cp(1, 1)
 
-    if (a_lo < 1) then
-        error stop "Error: Array bounds are invalid for matmul."
-    end if
-
     ap = 1.0
     bp = 2.0
+  
     cp = matmul(ap, bp)
-    print *, cp(1, 1)
+
+    if (abs(cp(1, 1) - 2.0) > 1e-5) then
+        error stop "Error: matmul result is incorrect."
+    end if
+
 end program array_128
