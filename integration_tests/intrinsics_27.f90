c8a22e7ac553f42c73bf257e8576aa192ee0af4e
Mohit Dhingra <mxyd444@gmail.com>
2026-07-01 05:43:01 +0530

    Fix SHAPE semantic checking for scalar and assumed-rank arguments

--- integration_tests/intrinsics_27.f90
+++ integration_tests/intrinsics_27.f90
@@ -1,6 +1,4 @@
 program intrinsics_27
     integer, dimension(-1:1, -1:2) :: a
     print *, shape(a)             ! (/ 3, 4 /)
-    print *, size(shape(42))      ! (/ /)
-    print *, shape(42)            ! (/ /)
 end program
