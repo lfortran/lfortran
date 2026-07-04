355aea1fa463aa25778c53c07fb34caa8ab7df04
Jainam-not-a-robot <b24cs1071@iitj.ac.in>
2026-07-04 12:13:59 +0530

    update test and ref

--- tests/coarrays_17.f90
+++ tests/coarrays_17.f90
@@ -7,7 +7,7 @@ program coarrays_17
         x = 42
         sync images(2)
     else if (this_image() == 2) then
-        sync images(1)
+        sync images([1, 2])
         if (x[1] /= 42) error stop
     end if
 end program coarrays_17
