f6d46359c20dabf556d34288d6678226894a4a2c
Jainam-not-a-robot <b24cs1071@iitj.ac.in>
2026-07-06 19:26:54 +0530

    add tests and update ref

--- tests/coarrays_19.f90
+++ tests/coarrays_19.f90
@@ -1,9 +1,7 @@
 ! run with 2 images
 program coarrays_19
     integer :: a(3)[*] = 2
-
-    sync all
-    if (this_image() == 2) then
+        if (this_image() == 2) then
         if (a(1)[2] /= 2) error stop
         if (a(2)[2] /= 2) error stop
         if (a(3)[2] /= 2) error stop
---
+++ tests/coarrays_21.f90
@@ -0,0 +1,24 @@
+program coarrays_21
+    implicit none
+
+    integer :: a(4)[*], b(4)[*]
+    integer :: dest
+    integer :: i = 1
+    integer :: expected = mod(this_image() + num_images() - 2, num_images()) + 1
+
+    a = this_image()
+    b = 0
+
+    dest = mod(this_image(), num_images()) + 1
+
+    b(2)[dest] = a(2)
+
+    sync all
+    do i = 1, 4
+        if (i == 2) then
+            if (b(i) /= expected) error stop
+        else
+            if (b(i) /= 0) error stop
+        end if
+    end do
+end program coarrays_21
