de4fff52a49f26e34241858cd492000c1dbfc126
Ondřej Čertík <ondrej@certik.us>
2026-07-11 14:07:54 -0600

    test: consolidate defined-assignment coverage into two files
    Merge type-bound cases (plain array, section/source, 2-D, parent/child,
    scalar) into defined_assignment_01. Keep free interface assignment in
    defined_assignment_free_interface_01 so it does not share a module with
    type-bound assignment(=).

--- integration_tests/defined_assignment_free_interface_01.f90
+++ integration_tests/defined_assignment_free_interface_01.f90
@@ -1,11 +1,13 @@
 ! Free (non-type-bound) non-elemental interface assignment(=) with scalar
-! dummies does not match array LHS. Array assignment is intrinsic.
+! dummies does not match array LHS (F2023 10.2.1.4). Array assignment is
+! intrinsic; scalar assignment still uses the free interface.
 
-module derived_types_153_mod
+module defined_assignment_free_interface_01_mod
    implicit none
    type :: ti
       integer, pointer :: leaf => null()
       logical :: is_temporary = .false.
+      integer :: v = 0
    end type ti
 
    interface assignment(=)
@@ -19,12 +21,13 @@ contains
       type(ti), intent(in)  :: rhs
       lhs%leaf => rhs%leaf
       lhs%is_temporary = .true.
+      lhs%v = rhs%v
    end subroutine assign_ti
 
-end module derived_types_153_mod
+end module defined_assignment_free_interface_01_mod
 
-program derived_types_153
-   use derived_types_153_mod
+program defined_assignment_free_interface_01
+   use defined_assignment_free_interface_01_mod
    implicit none
    type(ti), dimension(3) :: a, b
    type(ti) :: scalar
@@ -33,6 +36,7 @@ program derived_types_153
    do i = 1, 3
       allocate(a(i)%leaf)
       a(i)%leaf = i * 11
+      a(i)%v = i * 11
       a(i)%is_temporary = .false.
       b(i)%is_temporary = .true.
    end do
@@ -41,7 +45,8 @@ program derived_types_153
    do i = 1, 3
       if (.not. associated(b(i)%leaf, a(i)%leaf)) error stop 301
       if (b(i)%leaf /= i * 11) error stop 302
-      if (b(i)%is_temporary) error stop 303
+      if (b(i)%v /= i * 11) error stop 303
+      if (b(i)%is_temporary) error stop 304
    end do
 
    do i = 1, 3
@@ -50,28 +55,30 @@ program derived_types_153
    end do
    b(1:3) = a(1:3)
    do i = 1, 3
-      if (.not. associated(b(i)%leaf, a(i)%leaf)) error stop 304
-      if (b(i)%is_temporary) error stop 305
+      if (.not. associated(b(i)%leaf, a(i)%leaf)) error stop 305
+      if (b(i)%is_temporary) error stop 306
    end do
 
    allocate(scalar%leaf)
    scalar%leaf = 55
+   scalar%v = 55
    scalar%is_temporary = .false.
    b = scalar
    do i = 1, 3
-      if (.not. associated(b(i)%leaf, scalar%leaf)) error stop 306
-      if (b(i)%leaf /= 55) error stop 307
-      if (b(i)%is_temporary) error stop 308
+      if (.not. associated(b(i)%leaf, scalar%leaf)) error stop 307
+      if (b(i)%v /= 55) error stop 308
+      if (b(i)%is_temporary) error stop 309
    end do
 
    ! Scalar free-interface defined assignment still applies
    b(1)%is_temporary = .false.
    b(1) = a(2)
-   if (.not. associated(b(1)%leaf, a(2)%leaf)) error stop 309
-   if (.not. b(1)%is_temporary) error stop 310
+   if (.not. associated(b(1)%leaf, a(2)%leaf)) error stop 310
+   if (b(1)%v /= 22) error stop 311
+   if (.not. b(1)%is_temporary) error stop 312
 
    do i = 1, 3
       deallocate(a(i)%leaf)
    end do
    deallocate(scalar%leaf)
-end program derived_types_153
+end program defined_assignment_free_interface_01
