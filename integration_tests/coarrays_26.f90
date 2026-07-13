568ef585a3826f17b7668d57fc46d2784d05458c
Jainam-not-a-robot <b24cs1071@iitj.ac.in>
2026-07-13 15:33:58 +0530

    update test

--- integration_tests/coarrays_26.f90
+++ integration_tests/coarrays_26.f90
@@ -3,6 +3,11 @@ module m
     integer :: x[*]
 end module
 
+module m2
+    implicit none
+    integer :: x[*]
+end module
+
 subroutine foo()
     implicit none
     integer, save :: x[*]
@@ -17,11 +22,13 @@ end subroutine
 
 program coarrays_26
     use m, only: module_x => x
+    use m2, only: module_x2 => x
     implicit none
 
     integer :: x[*]
 
     module_x = this_image()
+    module_x2 = this_image() + 1
     x = this_image() * 10
 
     call foo()
@@ -33,6 +40,10 @@ program coarrays_26
     end if
 
     if (module_x /= this_image()) then
-        error stop "Incorrect module coarray value"
+        error stop "Incorrect module coarray value in module m"
+    end if
+
+    if (module_x2 /= this_image() + 1) then
+        error stop "Incorrect module coarray value in module m2"
     end if
 end program coarrays_26
