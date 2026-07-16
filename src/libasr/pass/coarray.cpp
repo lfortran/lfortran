ea2668404492b86df26754e3b96d626228ac4460
Jainam-not-a-robot <b24cs1071@iitj.ac.in>
2026-07-16 17:23:36 +0530

    fix team type mismatch and keep is_struct true for derived type

--- src/libasr/pass/coarray.cpp
+++ src/libasr/pass/coarray.cpp
@@ -78,7 +78,7 @@ class PRIFInterface {
                 false, false, false, nullptr, 0, nullptr, nullptr, nullptr, 0);
             ASR::symbol_t *struct_sym = ASR::down_cast<ASR::symbol_t>(struct_asr);
             ASR::Struct_t *struct_t = ASR::down_cast<ASR::Struct_t>(struct_sym);
-            ASR::ttype_t *struct_type = ASRUtils::make_StructType_t_util(al, loc, struct_sym, false);
+            ASR::ttype_t *struct_type = ASRUtils::make_StructType_t_util(al, loc, struct_sym, true);
             struct_t->m_struct_signature = struct_type;
             global_scope->add_symbol(symbol_name, struct_sym);
             return struct_sym;
@@ -805,7 +805,7 @@ class PRIFInterface {
             global_scope->add_symbol(symbol_name, struct_sym);
             std::string type_info_symbol_name = get_mangled_name("prif", "prif_dummy_team_descriptor");
             ASR::symbol_t* type_info_sym = get_or_create_dummy_struct(loc, type_info_symbol_name);
-            ASR::ttype_t* type_info_type = ASRUtils::make_StructType_t_util(al, loc, type_info_sym, false);
+            ASR::ttype_t* type_info_type = ASRUtils::make_StructType_t_util(al, loc, type_info_sym, true);
             ASR::ttype_t *info_ptr_type = ASRUtils::TYPE(ASR::make_Pointer_t(al, loc, type_info_type ));
             declare_variable(struct_symtab, loc, "info", info_ptr_type, ASR::intentType::Local, type_info_sym, ASR::abiType::Source, ASR::accessType::Private, ASR::presenceType::Required, false);
 
@@ -815,7 +815,7 @@ class PRIFInterface {
             struct_t->m_members = members.p;
             struct_t->n_members = members.n;
 
-            ASR::ttype_t *struct_type = ASRUtils::make_StructType_t_util(al, loc, struct_sym, false);
+            ASR::ttype_t *struct_type = ASRUtils::make_StructType_t_util(al, loc, struct_sym, true);
             struct_t->m_struct_signature = struct_type;
 
             return struct_sym;
@@ -891,7 +891,7 @@ class PRIFInterface {
             ASR::ttype_t *int32_type = int32;
 
             ASR::symbol_t *team_type_sym = get_or_create_prif_team_type_struct(loc);
-            ASR::ttype_t *team_type = ASRUtils::make_StructType_t_util(al, loc, team_type_sym, false);
+            ASR::ttype_t *team_type = ASRUtils::make_StructType_t_util(al, loc, team_type_sym, true);
 
             ASR::symbol_t *team_sym = declare_variable(
                 fn_symtab, loc, "team", team_type, ASR::intentType::In, team_type_sym,
@@ -977,7 +977,7 @@ class PRIFInterface {
             ASR::ttype_t *int64_type = int64;
             
             ASR::symbol_t *team_type_sym = get_or_create_prif_team_type_struct(loc);
-            ASR::ttype_t *team_type = ASRUtils::make_StructType_t_util(al, loc, team_type_sym, false);
+            ASR::ttype_t *team_type = ASRUtils::make_StructType_t_util(al, loc, team_type_sym, true);
 
             ASR::symbol_t *team_number_sym = declare_variable(
                 fn_symtab, loc, "team_number", int64_type, ASR::intentType::In, nullptr,
@@ -1024,6 +1024,32 @@ class PRIFInterface {
                                              ASR::expr_t *errmsg_alloc = nullptr) {
             ASR::symbol_t *sub = get_or_create_prif_form_team_sub(loc);
             select_errmsg_arg(errmsg, errmsg_alloc);
+            
+            // Convert team_type to __module_prif_prif_team_type
+            if (team && ASR::is_a<ASR::Var_t>(*team)) {
+                ASR::symbol_t *team_sym = ASR::down_cast<ASR::Var_t>(team)->m_v;
+                if (ASR::is_a<ASR::Variable_t>(*ASRUtils::symbol_get_past_external(team_sym))) {
+                    ASR::Variable_t *team_var = ASR::down_cast<ASR::Variable_t>(ASRUtils::symbol_get_past_external(team_sym));
+                    ASR::ttype_t *team_type = team_var->m_type;
+                    if (ASR::is_a<ASR::StructType_t>(*team_type)) {
+                        ASR::symbol_t *prif_decl = get_or_create_prif_team_type_struct(loc);
+                        if (team_var->m_type_declaration != prif_decl) {
+                            ASR::symbol_t *orig_decl = team_var->m_type_declaration;
+                            team_var->m_type_declaration = prif_decl;
+                            team_var->m_type = ASRUtils::make_StructType_t_util(al, loc, prif_decl, true);
+                            if (orig_decl) {
+                                SymbolTable *parent_symtab = ASRUtils::symbol_parent_symtab(orig_decl);
+                                if (parent_symtab) {
+                                    std::string sym_name = std::string(ASRUtils::symbol_name(orig_decl));
+                                    if (parent_symtab->get_symbol(sym_name)) {
+                                        parent_symtab->erase_symbol(sym_name);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
             
             // Note: team_number might be passed as int32, we should cast to int64
             ASRUtils::ASRBuilder b(al, loc);
