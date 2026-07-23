9398e62e3c994acae4e8fd158226c891843209f1
Yousssf Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-07-23 21:06:41 +0300

    test: split module_real128_01 into separate module/program files

--- integration_tests/module_real128_01.f90
+++ integration_tests/module_real128_01.f90
@@ -1,12 +1,3 @@
-module module_real128_01_mod
-    implicit none
-contains
-    subroutine set_value(x)
-        real(16) :: x
-        x = 1.0_16
-    end subroutine set_value
-end module module_real128_01_mod
-
 program module_real128_01
     use module_real128_01_mod, only: set_value
     implicit none
---
+++ integration_tests/module_real128_01_mod.f90
@@ -0,0 +1,8 @@
+module module_real128_01_mod
+    implicit none
+contains
+    subroutine set_value(x)
+        real(16) :: x
+        x = 1.0_16
+    end subroutine set_value
+end module module_real128_01_mod
