fa7b10452aed2ae85639da26415776f51d3c7b05
Jainam-not-a-robot <b24cs1071@iitj.ac.in>
2026-07-27 16:40:57 +0530

    remove if condition for asserts

--- src/libasr/pass/intrinsic_functions.h
+++ src/libasr/pass/intrinsic_functions.h
@@ -1826,7 +1826,7 @@ namespace LCoBound {
         if (kind) {
             ASR::expr_t* kind_value = ASRUtils::expr_value(kind);
             if( kind_value && ASRUtils::is_value_constant(kind_value) ) {
-                if( !ASRUtils::extract_value(kind_value, kind_const) ) LCOMPILERS_ASSERT(false);
+                LCOMPILERS_ASSERT(ASRUtils::extract_value(kind_value, kind_const));
             }
         }
         ASR::ttype_t *return_type = ASRUtils::TYPE(ASR::make_Integer_t(al, loc, kind_const));
@@ -1877,7 +1877,7 @@ namespace UCoBound {
         if (kind) {
             ASR::expr_t* kind_value = ASRUtils::expr_value(kind);
             if( kind_value && ASRUtils::is_value_constant(kind_value) ) {
-                if( !ASRUtils::extract_value(kind_value, kind_const) ) LCOMPILERS_ASSERT(false);
+                LCOMPILERS_ASSERT(ASRUtils::extract_value(kind_value, kind_const));
             }
         }
         ASR::ttype_t *return_type = ASRUtils::TYPE(ASR::make_Integer_t(al, loc, kind_const));
