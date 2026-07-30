007dc779fdab206ea18fcc5f4a8696a6f2e538fe
Christopher Albert <albert@tugraz.at>
2026-07-30 07:23:43 +0200

    test: stabilize implicit interface diagnostic

--- integration_tests/implicit_interface_44.f90
+++ integration_tests/implicit_interface_44.f90
@@ -1,12 +1,13 @@
 program implicit_interface_44
-double precision diff, x, y, z
-x = 3.0d0
-y = 1.0d0
-z = diff(x, y)
-if (z .ne. 2.0d0) error stop
-end
+    double precision diff, x, y, z
+    external diff
+    x = 3.0d0
+    y = 1.0d0
+    z = diff(x, y)
+    if (z .ne. 2.0d0) error stop
+    end
 
-double precision function diff(x, y)
-double precision x, y
-diff = x - y
-end
+    double precision function diff(x, y)
+        double precision x, y
+        diff = x - y
+        end
