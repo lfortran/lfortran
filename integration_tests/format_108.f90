cab18ee66e546c189d7dcedb86e01c64bb34faad
opixdown <atharvawaghmare34@gmail.com>
2026-08-03 22:54:22 +0530

    tests: cover int8, int16 and integer arrays with A editing

--- integration_tests/format_108.f90
+++ integration_tests/format_108.f90
@@ -1,15 +1,21 @@
 program format_108
 ! "A" editing with an integer I/O list item must transfer the full
 ! kind-width bytes of the internal representation, not just one byte.
-use, intrinsic :: iso_fortran_env, only : int64
+use, intrinsic :: iso_fortran_env, only : int8, int16, int64
 implicit none
 
 integer :: word, i
 integer(int64) :: word8
+integer(int8) :: b1
+integer(int8) :: alphabet(26)
+integer(int16) :: h2
+integer :: warr(2)
 character(len=4) :: out4
 character(len=8) :: out8
 character(len=6) :: out6
 character(len=2) :: out2
+character(len=3) :: out3
+character(len=26) :: out26
 
 word = transfer("abcd", 0)
 write(out4, "(a)") word
@@ -34,6 +40,35 @@ if (iachar(out4(1:1)) /= 65) error stop 5
 do i = 2, 4
     if (iachar(out4(i:i)) /= 0) error stop 6
 end do
+
+! integer(int8): one byte per value, right-justified when wider
+b1 = int(iachar("Z"), int8)
+write(out2, "(a)") b1
+if (out2 /= "Z ") error stop 7
+write(out2, "(a2)") b1
+if (out2 /= " Z") error stop 8
+
+! integer(int16): two bytes
+h2 = transfer("xy", 0_int16)
+write(out2, "(a)") h2
+if (out2 /= "xy") error stop 9
+write(out3, "(a3)") h2
+if (out3 /= " xy") error stop 10
+
+! integer array: each element transfers its kind width
+warr(1) = transfer("abcd", 0)
+warr(2) = transfer("efgh", 0)
+write(out8, "(2a)") warr
+if (out8 /= "abcdefgh") error stop 11
+write(out8, "(*(a))") warr
+if (out8 /= "abcdefgh") error stop 12
+
+! integer(int8) array spelling the alphabet
+do i = 1, 26
+    alphabet(i) = int(64 + i, int8)
+end do
+write(out26, "(*(a))") alphabet
+if (out26 /= "ABCDEFGHIJKLMNOPQRSTUVWXYZ") error stop 13
 
 print *, "ok"
 end program format_108
