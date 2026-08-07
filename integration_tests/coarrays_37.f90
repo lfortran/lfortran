2aa103085d089d7d614b5feae20fc026f99c1b6f
Jainam Shah <b24cs1071@iitj.ac.in>
2026-08-07 14:26:55 +0530

    Add static bounds check
    Co-authored-by: Dan Bonachea <dobonachea@lbl.gov>

--- integration_tests/coarrays_37.f90
+++ integration_tests/coarrays_37.f90
@@ -2,12 +2,16 @@ program coarrays_37
     implicit none
 
     real, allocatable :: src_arr(:,:)[:]
-
+    integer :: static_arr(30:39, 40:49)[*]
+    
+    if (any(lbound(static_arr) /= [30, 40])) error stop "Wrong static_arr lbounds"
+    if (any(ubound(static_arr) /= [39, 49])) error stop "Wrong static_arr ubounds"    
+    
     allocate(src_arr(10:12, 20:23)[*])
-    if (this_image() == 1) then
-        if (any(lbound(src_arr) /= [10, 20])) error stop "Wrong lbounds"
-        if (any(ubound(src_arr) /= [12, 23])) error stop "Wrong ubounds"
-    end if
+
+    if (any(lbound(src_arr) /= [10, 20])) error stop "Wrong src_arr lbounds"
+    if (any(ubound(src_arr) /= [12, 23])) error stop "Wrong src_arr ubounds"
+
 
     deallocate(src_arr)
 end program coarrays_37
