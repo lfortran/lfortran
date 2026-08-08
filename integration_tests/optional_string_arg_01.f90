1a70b2aa981055489e0c14dad727f30be90ce8ed
HarshilShah1804 <hkshah1978@gmail.com>
2026-08-08 16:21:34 +0530

    test: add and register test

---
+++ integration_tests/optional_string_arg_01.f90
@@ -0,0 +1,10 @@
+program optional_string_arg_01
+   implicit none
+   call check(1)
+contains
+   subroutine check(n, s)
+      integer, intent(in) :: n
+      character(len=n), optional, intent(in) :: s
+      if (len(s) /= 1) error stop
+   end subroutine
+end program optional_string_arg_01
