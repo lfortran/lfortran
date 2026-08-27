a865320f195d17d183f3a5204d593abaa150ea47
Jainam-not-a-robot <b24cs1071@iitj.ac.in>
2026-08-27 19:01:12 +0530

    remove extra test file

--- integration_tests/1a.f90
+++
@@ -1,8 +0,0 @@
-subroutine getwid(se)
-   type settings
-      integer w
-   end type settings
-   type(settings), intent(in)  :: se
-   character(se % w)           :: stmin(10)
-
-end subroutine getwid
