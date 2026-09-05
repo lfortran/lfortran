b4b80d9b843e4ce5fad89ba1fb162e28cbd762cd
srinjoy 933 <srinjoymukhopadhyay@gmail.com>
2026-09-05 17:53:38 +0530

    fix: resolve ICE in same_type_as with polymorphic pointers

---
+++ integration_tests/class_151.f90
@@ -0,0 +1,15 @@
+﻿program class_151
+    implicit none
+    
+    type :: token
+    end type
+
+    class(token), pointer :: alias
+    type(token), target :: value
+    logical :: matches
+
+    alias => value
+    matches = same_type_as(alias, value)
+    
+    if (.not. matches) error stop
+end program class_151
