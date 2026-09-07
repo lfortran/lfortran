bf3665f77191bbcd227a9f1d8ca0e4627db26d0c
usefahmed07 <youssef.ahmed.hamaza@gmail.com>
2026-09-07 13:50:43 +0300

    Simplify integration test and register as COMPILE
    Per review feedback from @assem2002: simplified the test to focus
    on the core issue (character length referencing an enclosing
    function's return variable from a nested contains function),
    dropping the optional-argument branching and standalone program.
    Registered as COMPILE instead of RUN since the test only needs to
    verify successful ASR generation, not runtime output.

--- integration_tests/character_len_of_function_12614.f90
+++ integration_tests/character_len_of_function_12614.f90
@@ -1,25 +1,11 @@
 module character_len_of_function_12614_m
    implicit none
 contains
-
-   character(len=28) function myfunc(iarg)
-      integer,intent(in),optional :: iarg
-      if(present(iarg))then
-         myfunc=repeat('@',iarg)
-      else
-         myfunc='fini '//printdat()
-      end if
+   character(len=28) function myfunc()
    contains
-
       function printdat()
          character(len=len(myfunc)) :: printdat
-         printdat='printed'
+         printdat = 'printed'
       end function printdat
-
    end function myfunc
-
 end module character_len_of_function_12614_m
-program character_len_of_function_12614
-use character_len_of_function_12614_m, only : myfunc
-   write(*,*)myfunc(),myfunc(3)
-end program character_len_of_function_12614
