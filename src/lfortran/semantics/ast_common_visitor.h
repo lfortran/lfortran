9e351c12c1663553978f1a75ff716eece5a13887
srinjoy 933 <srinjoymukhopadhyay@gmail.com>
2026-09-09 20:43:35 +0530

    Fix

--- src/lfortran/semantics/ast_common_visitor.h
+++ src/lfortran/semantics/ast_common_visitor.h
@@ -17811,7 +17811,7 @@ public:
         ASR::ttype_t* base_w = ASRUtils::type_get_past_array(type_w);
 
         if (!ASRUtils::is_integer(*base_n) || !ASRUtils::is_integer(*base_w)) {
-            throw LCompilers::LFortran::SemanticError("Arguments to the 'shifta' intrinsic must be of type INTEGER", loc);
+            throw SemanticAbort("Arguments to the 'shifta' intrinsic must be of type INTEGER", loc);
         }
 
         ASR::ttype_t* cast_target_for_w = base_n;
