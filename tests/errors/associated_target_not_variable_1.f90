4de7641e3a1a87847cf0fb94103e5269adf60b39
usefahmed07 <youssef.ahmed.hamaza@gmail.com>
2026-09-20 05:24:09 +0300

    test: move associated target-not-variable case into continue_compilation_1
    Consistent with the sibling PR (#13264), folded the standalone
    associated_target_not_variable_1 test into continue_compilation_1.f90
    instead of keeping it as a separate file.

--- tests/errors/associated_target_not_variable_1.f90
+++
@@ -1,6 +0,0 @@
-program associated_target_not_variable_1
-    implicit none
-    integer, pointer :: a(:)
-    a => null()
-    if (associated(a, 11)) print *, "bad"  ! {Error} 'target' argument of 'associated' intrinsic must be a pointer or target variable or function
-end program
