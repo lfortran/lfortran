afe767ed8128985c12f32c6af73580b83d30ad95
Ondřej Čertík <ondrej@certik.us>
2026-09-26 04:33:11 +0000

    test: cover access of a derived type instantiated in a module
    Instantiate a renamed derived type in template_access_01 and register the
    file as an ASR reference test. LFortran does not yet enforce PRIVATE derived
    types on USE, so only the ASR shows that the instantiated type gets the
    instantiating module's (public) access instead of the template module's
    private default.

--- integration_tests/template_access_01.f90
+++ integration_tests/template_access_01.f90
@@ -1,5 +1,6 @@
-! A procedure instantiated in a module gets the accessibility of that module,
-! not the default accessibility of the module that defines the template.
+! A procedure or derived type instantiated in a module gets the accessibility
+! of that module, not the default accessibility of the module that defines the
+! template.
 module template_access_01_tmpl_m
     implicit none
     private
@@ -7,11 +8,20 @@ module template_access_01_tmpl_m
 
     template t_tmpl {T}
         deferred type :: T
+        type :: box
+            type(T) :: v
+        end type
     contains
         function f(x) result(r)
             type(T), intent(in) :: x
             type(T) :: r
             r = x
+        end function
+
+        function mk(x) result(r)
+            type(T), intent(in) :: x
+            type(box) :: r
+            r%v = x
         end function
     end template
 end module
@@ -19,7 +29,7 @@ end module
 module template_access_01_inst_m
     use template_access_01_tmpl_m, only: t_tmpl
     implicit none
-    instantiate t_tmpl {real}, only: g => f
+    instantiate t_tmpl {real}, only: g => f, rbox => box, rmk => mk
 end module
 
 module template_access_01_priv_m
@@ -34,7 +44,10 @@ program template_access_01
     use template_access_01_inst_m
     use template_access_01_priv_m
     implicit none
+    type(rbox) :: b
+    b = rmk(2.5)
+    if (abs(b%v - 2.5) > 1e-6) error stop
     if (abs(g(3.0) - 3.0) > 1e-6) error stop
     if (h(7) /= 7) error stop
-    print *, g(3.0), h(7)
+    print *, g(3.0), h(7), b%v
 end program
